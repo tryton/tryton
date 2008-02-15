a4585815ec349951f9e8bd253c568b9fd04edb8a
Bertrand Chenal <bch@b2ck.com>
2008-02-15 18:13:00 +0100

    Removed tabs

--- __tryton__.py
+++ __tryton__.py
@@ -1,11 +1,11 @@
 {
-	"name" : "Partners",
-	"version" : "1.0",
-	"author" : "B2CK",
-	"category" : "Generic Modules",
-	"description": "Define partners, addresses and co.",
-	"depends" : ["ir","res"],
-	"xml" : ["partner.xml"],
-	"installable": True,
+    "name" : "Partners",
+    "version" : "1.0",
+    "author" : "B2CK",
+    "category" : "Generic Modules",
+    "description": "Define partners, addresses and co.",
+    "depends" : ["ir","res"],
+    "xml" : ["partner.xml"],
+    "installable": True,
 }
 
