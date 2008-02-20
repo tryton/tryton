86a3f29435be0f728bfbad4e32dbccd683ef2d2f
Cédric Krier <ced@b2ck.com>
2008-02-20 13:08:04 +0100

    Fix setup.py for sdist

--- __tryton__.py
+++ __tryton__.py
@@ -2,6 +2,8 @@
     'name': 'Company',
     'version': '0.1',
     'author': 'B2CK',
+    'email': 'info@b2ck.com',
+    'website': 'http://www.tryton.org/',
     'category': 'Generic Modules',
     'description': 'Define company',
     'depends': ['ir', 'res', 'partner'],
