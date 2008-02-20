303066923011934cda8cc8059659216d04a4d5e0
Cédric Krier <ced@b2ck.com>
2008-02-20 13:07:40 +0100

    Fix setup.py for sdist

--- __tryton__.py
+++ __tryton__.py
@@ -1,7 +1,9 @@
 {
-    "name" : "partner",
+    "name" : "Partner",
     "version" : "0.1",
     "author" : "B2CK",
+    'email': 'info@b2ck.com',
+    'website': 'http://www.tryton.org/',
     "category" : "Generic Modules",
     "description": "Define partners, addresses and co.",
     "depends" : ["ir","res"],
