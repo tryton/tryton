0f94f2acf2f3865632a891c03c13b7beaf3779f0
Cédric Krier <ced@b2ck.com>
2008-03-04 11:56:55 +0100

    New __init__ signature

--- category.py
+++ category.py
@@ -1,5 +1,6 @@
 from trytond.osv import fields, OSV
 
+
 class Category(OSV):
     "Product Category"
     _name = "product.category"
@@ -10,15 +11,18 @@ class Category(OSV):
     complete_name = fields.Function('complete_name', type="char", string='Name')
     parent = fields.Many2One('product.category','Parent Category', select=True)
     childs = fields.One2Many('product.category', 'parent',
-                             string='Child Categories')
+            string='Child Categories')
 
-    def __init__(self, pool):
+    def __init__(self):
         super(Category, self).__init__(pool)
-        if pool:
-            self._rpc_allowed.extend([
-                    'complete_name',
-                    'name_get',
-                    ])
+        self._rpc_allowed.extend([
+                'complete_name',
+                'name_get',
+                ])
+        self._constraints += [
+            ('check_recursion',
+             'Error! You can not create recursive categories.', ['parent'])
+        ]
 
     def complete_name(self, cursor, user, obj_id, name, value, arg,
             context=None):
@@ -38,10 +42,5 @@ class Category(OSV):
                 name = category.name
             res.append((category.id, name))
         return res
-
-    _constraints = [
-        ('check_recursion',
-         'Error! You can not create recursive categories.', ['parent'])
-    ]
 
 Category()
