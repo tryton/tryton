7cf965ab31e4d09ff8a7823c00f6c02a8eb20076
Cédric Krier <ced@b2ck.com>
2008-03-05 11:14:16 +0100

    Fix xml coding guide lines

--- __tryton__.py
+++ __tryton__.py
@@ -1,12 +1,11 @@
 {
-    "name" : "Products",
+    "name" : "Product",
     "version" : "0.1",
     "author" : "B2CK",
     "category" : "Generic Modules",
-    "description": "Define products, categories of product, units"
-    "of measure, categories of units of measure.",
-    "depends" : ["ir","res"],
-    "xml" : ["product.xml","category.xml","template.xml","uom.xml"],
-    "installable": True,
+    "description": "Define products, categories of product, units" \
+        "of measure, categories of units of measure.",
+    "depends" : ["ir", "res"],
+    "xml" : ["product.xml", "category.xml", "uom.xml"],
 }
 
