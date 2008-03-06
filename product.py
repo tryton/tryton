e2695e2a4f38377b4da0262a8c020aa7389d702e
Bertrand Chenal <bch@b2ck.com>
2008-03-07 00:34:22 +0100

    Added code field (and namesearch) and description field + xml guidelines

--- product.py
+++ product.py
@@ -8,5 +8,21 @@ class Product(OSV):
 
     product_template = fields.Many2One('product.template', 'Product Template',
                                         required=True)
+    code = fields.Char("Code", size=1,) # TODO will be change to None
+                                        # when implemeted
+    description = fields.Text("Description",)
+
+
+    def name_search(self, cursor, user, name,  args=None, operator='ilike',
+                    context=None, limit=80):
+        if not args:
+            args=[]
+        ids = self.search(cursor, user, [('code','=',name)]+ args, limit=limit,
+                          context=context)
+        if not ids:
+            ids = self.search(cursor, user, [('name',operator,name)]+ args,
+                              limit=limit, context=context)
+        result = self.name_get(cursor, user, ids, context)
+        return result
 
 Product()
