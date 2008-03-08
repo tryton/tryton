43dc6728af69873a3ac1f4ead5ad58af20a0322f
Bertrand Chenal <bch@b2ck.com>
2008-03-08 12:00:49 +0100

    Use get_field for function names

--- category.py
+++ category.py
@@ -8,12 +8,12 @@ class Category(OSV):
     _order = "parent,name"
 
     name = fields.Char('Name', size=64, required=True)
-    complete_name = fields.Function('_complete_name', type="char", string='Complete Name')
+    complete_name = fields.Function('get_complete_name', type="char", string='Complete Name')
     parent = fields.Many2One('product.category','Parent Category', select=True)
     childs = fields.One2Many('product.category', 'parent',
             string='Child Categories')
 
-    def _complete_name(self, cursor, user, ids, name, arg, context):
+    def get_complete_name(self, cursor, user, ids, name, arg, context):
         res = self.name_get(cursor, user, ids, context)
         return dict(res)
 
