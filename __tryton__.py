6a19f057ecfb431daeffdbe19ef69ffc351104a5
Cédric Krier <ced@b2ck.com>
2008-03-12 18:10:46 +0100

    Initial commit with currency

---
+++ __tryton__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Account',
+    'version': '0.1',
+    'author': 'B2CK',
+    'email': 'info@b2ck.com',
+    'website': 'http://www.tryton.org/',
+    'category': 'Generic Modules',
+    'description': 'Financial and Accounting Module',
+    'depends': [
+        'ir',
+        'res',
+        'company',
+    ],
+    'xml': [
+        'account.xml',
+        'currency.xml',
+    ],
+}
