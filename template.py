b89b097f4c2bbe8802c6eae0a0b32cd93430063c
Cédric Krier <ced@b2ck.com>
2008-04-28 11:44:22 +0200

    Add cost_price_uom and fix some char field size

--- template.py
+++ template.py
@@ -10,8 +10,8 @@ class Template(OSV):
     _name = "product.template"
     _description = __doc__
 
-    name = fields.Char('Name', size=64, required=True, translate=True,
-            select=True, states=STATES)
+    name = fields.Char('Name', size=None, required=True, translate=True,
+            select=1, states=STATES)
     type = fields.Selection([
         ('stockable', 'Stockable'),
         ('consumable', 'Consumable'),
@@ -21,10 +21,12 @@ class Template(OSV):
             states=STATES)
     list_price = fields.Property(type='numeric', string='List Price',
             states=STATES, digits=(16, 4))
-    list_price_uom = fields.Function('get_list_price_uom', string='List Price',
+    list_price_uom = fields.Function('get_price_uom', string='List Price',
             type="numeric", digits=(16, 4))
     cost_price = fields.Property(type='numeric', string='Cost Price',
             states=STATES, digits=(16, 4))
+    cost_price_uom = fields.Function('get_price_uom', string='Cost Price',
+            type="numeric", digits=(16, 4))
     cost_price_method = fields.Selection([
         ("fixed", "Fixed"),
         ("average", "Average")
@@ -42,21 +44,22 @@ class Template(OSV):
     def default_cost_price_method(self, cursor, user, context=None):
         return 'fixed'
 
-    def get_list_price_uom(self, cursor, user, ids, name, arg, context=None):
+    def get_price_uom(self, cursor, user, ids, name, arg, context=None):
         product_uom_obj = self.pool.get('product.uom')
         res = {}
         if context is None:
             context = {}
+        field = name[:-4]
         if context.get('uom'):
             to_uom = self.pool.get('product.uom').browse(
                 cursor, user, context['uom'], context=context)
             for product in self.browse(cursor, user, ids, context=context):
                 res[product.id] = product_uom_obj.compute_price(
-                        cursor, user, product.default_uom, product.list_price,
+                        cursor, user, product.default_uom, product[field],
                         to_uom)
         else:
             for product in self.browse(cursor, user, ids, context=context):
-                res[product.id] = product.list_price
+                res[product.id] = product[field]
         return res
 
 Template()
