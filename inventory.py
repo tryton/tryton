3c3633ad776ca23d4c26c23fb2e484bc72ab5e0d
Bertrand Chenal <bch@b2ck.com>
2008-05-07 14:56:54 +0200

    Removed inventory day object

--- inventory.py
+++ inventory.py
@@ -7,85 +7,6 @@ STATES = {
     'readonly': "state == 'done'",
 }
 
-class InventoryDay(OSV):
-    'Stock Inventory Day'
-    _name = 'stock.inventory.day'
-    _description = __doc__
-    _rec_name = 'date'
-    _order = 'date'
-
-    date = fields.Date('Date', readonly=True)
-    lost_found = fields.Many2One(
-        'stock.location', 'Lost and Found', required=True,
-        domain="[('type', '=', 'inventory')]", states=STATES,)
-    inventories = fields.One2Many(
-        'stock.inventory', 'inventory_day', 'Inventories', states=STATES,)
-    state = fields.Selection(
-        [('open','Open'),('done','Done')], 'State', readonly=True,)
-
-    def __init__(self):
-        super(InventoryDay, self).__init__()
-        self._rpc_allowed += [
-            'set_state_done',
-            'set_state_open',
-            ]
-    def default_state(self, cursor, user, context=None):
-        return 'open'
-
-    def default_date(self, cursor, user, context=None):
-        return datetime.date.today()
-
-    def set_state_open(self, cursor, user, ids, context=None):
-        #TODO: check that this was the last inventory
-        self.write(cursor, user, ids, {'state':'open'})
-
-    def set_state_done(self, cursor, user, ids, context=None):
-        product_obj = self.pool.get('product.product')
-        move_obj = self.pool.get('stock.move')
-        inventory_obj = self.pool.get('stock.inventory')
-
-        for inventory_day in self.browse(cursor, user, ids, context=context):
-            location_ids = []
-            inv_by_loc = {}
-
-            for inventory in inventory_day.inventories:
-                location_ids.append(inventory.location.id)
-                inv_by_loc[inventory.location.id] = inventory.id
-            raw_data = product_obj.raw_products_by_location(
-                cursor, user, location_ids, context=context)
-
-            indexed_data = dict([(line[:3], line[3]) for line in raw_data])
-
-            for inventory in inventory_day.inventories:
-                for line in inventory.lines:
-                    key = (inventory.location.id, line.product.id, line.uom.id)
-                    expected_qty = indexed_data.get(key, 0.0)
-                    delta_qty = line.quantity - expected_qty
-                    indexed_data[key] = 0
-
-                    from_location = inventory.inventory_day.lost_found.id
-                    to_location = inventory.location.id
-                    if delta_qty < 0:
-                        (from_location, to_location, delta_qty) = \
-                            (to_location, from_location, -delta_qty)
-
-                    move_id = move_obj.create(
-                        cursor, user,
-                        {'from_location': from_location,
-                         'to_location': to_location,
-                         'quantity': delta_qty,
-                         'product': line.product.id,
-                         'uom': line.uom.id,
-                         'state': 'done'
-                         },
-                        context=context,
-                        )
-                inventory_obj.write(
-                    cursor, user, inventory.id, {'state': 'done'})
-        self.write(cursor, user, ids, {'state':'done'})
-
-InventoryDay()
-
 class Inventory(OSV):
     'Stock Inventory'
     _name = 'stock.inventory'
@@ -95,9 +16,10 @@ class Inventory(OSV):
     location = fields.Many2One(
         'stock.location', 'Location', required=True,
         domain="[('type', '=', 'storage')]", states=STATES,)
-    inventory_day = fields.Many2One(
-        'stock.inventory.day', "Inventory Day", required=True,
-        domain="[('state', '=', 'open')]", states=STATES,)
+    date = fields.Date('Date', readonly=True)
+    lost_found = fields.Many2One(
+        'stock.location', 'Lost and Found', required=True,
+        domain="[('type', '=', 'inventory')]", states=STATES,)
     lines = fields.One2Many(
         'stock.inventory.line', 'inventory', 'Inventory Lines', states=STATES,)
     state = fields.Selection(
@@ -105,14 +27,64 @@ class Inventory(OSV):
 
     def __init__(self):
         super(Inventory, self).__init__()
-        self._sql_constraints += [
-            ('location_day_uniq', 'UNIQUE(location,inventory_day)',
-             'Only one inventory for a given location is allowed '\
-                 'for an inventory day.'),
-        ]
+        self._rpc_allowed += [
+            'set_state_done',
+            'set_state_open',
+            ]
+
+    # TODO only one open inventory by location
 
     def default_state(self, cursor, user, context=None):
         return 'open'
+
+    def default_date(self, cursor, user, context=None):
+        return datetime.date.today()
+
+    def set_state_open(self, cursor, user, ids, context=None):
+        #TODO: check that this was the last inventory, and removes
+        #generetated moves.
+        self.write(cursor, user, ids, {'state':'open'})
+
+    def set_state_done(self, cursor, user, ids, context=None):
+        product_obj = self.pool.get('product.product')
+        move_obj = self.pool.get('stock.move')
+        inventories = self.browse(cursor, user, ids, context=context)
+        location_ids = []
+        inv_by_loc = {}
+
+        for inventory in inventories:
+            location_ids.append(inventory.location.id)
+            inv_by_loc[inventory.location.id] = inventory.id
+        raw_data = product_obj.raw_products_by_location(
+            cursor, user, location_ids, context=context)
+
+        indexed_data = dict([(line[:3], line[3]) for line in raw_data])
+
+        for inventory in inventories: #!! BUG quand il y a pls uom en stock et que lon encode que une seule !!
+            for line in inventory.lines:
+                key = (inventory.location.id, line.product.id, line.uom.id)
+                expected_qty = indexed_data.get(key, 0.0)
+                delta_qty = line.quantity - expected_qty
+                indexed_data[key] = 0
+
+                from_location = inventory.lost_found.id
+                to_location = inventory.location.id
+                if delta_qty < 0:
+                    (from_location, to_location, delta_qty) = \
+                        (to_location, from_location, -delta_qty)
+
+                move_id = move_obj.create(
+                    cursor, user,
+                    {'from_location': from_location,
+                     'to_location': to_location,
+                     'quantity': delta_qty,
+                     'product': line.product.id,
+                     'uom': line.uom.id,
+                     'state': 'done'
+                     },
+                    context=context,
+                    )
+        self.write(cursor, user, ids, {'state':'done'})
 
 Inventory()
 
