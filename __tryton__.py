ec4e0c4b3dbd63deac945984b42d7f0ef6df2a74
ced@localhost <ced@localhost>
2008-05-14 18:04:31 +0200

    Initial commit with class 0

---
+++ __tryton__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Account Belgium',
+    'version': '0.1',
+    'author': 'B2CK',
+    'email': 'info@b2ck.com',
+    'website': 'http://www.tryton.org/',
+    'category': 'Generic Modules',
+    'description': 'Financial and Accounting Module',
+    'depends': [
+        'account',
+    ],
+    'xml': [
+        'account_be.xml',
+    ],
+}
