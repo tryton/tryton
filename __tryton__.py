2aa41fa80368103d9ac780f70ffb67309a8cb3e2
Cédric Krier <ced@b2ck.com>
2008-05-23 16:00:52 +0200

    Set category to Accounting

--- __tryton__.py
+++ __tryton__.py
@@ -4,7 +4,7 @@
     'author': 'B2CK',
     'email': 'info@b2ck.com',
     'website': 'http://www.tryton.org/',
-    'category': 'Generic Modules',
+    'category': 'Accounting',
     'description': 'Financial and Accounting Module',
     'depends': [
         'ir',
