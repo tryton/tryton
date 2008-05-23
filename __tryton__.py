2f195c67ae46fb3132f07fba4548a6b0944ab684
Cédric Krier <ced@b2ck.com>
2008-05-23 16:01:08 +0200

    Set category to Generic

--- __tryton__.py
+++ __tryton__.py
@@ -4,7 +4,7 @@
     'author': 'B2CK',
     'email': 'info@b2ck.com',
     'website': 'http://www.tryton.org/',
-    'category': 'Generic Modules',
+    'category': 'Generic',
     'description': 'Define company',
     'depends': [
         'ir',
