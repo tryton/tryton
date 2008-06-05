737994800a4db348e63a27113520c352c6fe15fb
Cédric Krier <ced@b2ck.com>
2008-06-05 10:21:33 +0200

    Initial commit for google_maps module

---
+++ __tryton__.py
@@ -0,0 +1,16 @@
+{
+    "name" : "Google Maps",
+    "version" : "0.1",
+    "author" : "B2CK",
+    'email': 'info@b2ck.com',
+    'website': 'http://www.tryton.org/',
+    "category" : "Generic",
+    "description": "Add link from addresses to Google Maps",
+    "depends" : [
+        "ir",
+        "relationship"
+    ],
+    "xml" : [
+        "address.xml",
+    ],
+}
---
+++ address.py
@@ -0,0 +1,33 @@
+from trytond.osv import fields, OSV
+import urllib
+
+class Address(OSV):
+    _name = 'relationship.address'
+
+    google_maps_url = fields.Function('get_google_maps_url', type="char",
+            string="Google Maps")
+
+    def get_google_maps_url(self, cursor, user, ids, name, arg, context=None):
+        if context is None:
+            context = {}
+        res = {}
+        lang = context.get('language', 'en_US')[:2]
+        for address in self.browse(cursor, user, ids, context=context):
+            query = ''
+            if address.street:
+                query += ' ' + address.street
+            if address.streetbis:
+                query += ' ' + address.streetbis
+            if address.zip:
+                query += ' ' + address.zip
+            if address.city:
+                query += ' ' + address.city
+            if address.country:
+                query += ' ' + address.country.name
+            if address.state:
+                query += ' ' + address.state.name
+            res[address.id] = 'http://maps.google.com/maps?hl=%s&q=%s' % \
+                    (lang, urllib.quote(query.strip()))
+        return res
+
+Address()
