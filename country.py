ced3cef18571e97544d7536d904fb216a391cdde
Cédric Krier <ced@b2ck.com>
2008-06-08 13:11:27 +0200

    Remove some size constraint and add select=1 on names and code

--- country.py
+++ country.py
@@ -8,9 +8,9 @@ class Country(OSV):
     'Country'
     _name = 'relationship.country'
     _description = __doc__
-    name = fields.Char('Country Name', size=64,
-           help='The full name of the country.', required=True, translate=True)
-    code = fields.Char('Country Code', size=2,
+    name = fields.Char('Country Name', required=True, translate=True,
+           help='The full name of the country.', select=1)
+    code = fields.Char('Country Code', size=2, select=1,
            help='The ISO country code in two chars.\n'
            'You can use this field for quick search.', required=True)
     state = fields.One2Many('relationship.country.state', 'country', 'State')
@@ -60,8 +60,8 @@ class State(OSV):
     _description = __doc__
     country = fields.Many2One('relationship.country', 'Country',
             required=True)
-    name = fields.Char('State Name', size=64, required=True)
-    code = fields.Char('State Code', size=3, required=True)
+    name = fields.Char('State Name', required=True, select=1)
+    code = fields.Char('State Code', size=3, required=True, select=1)
 
     def __init__(self):
         super(State, self).__init__()
