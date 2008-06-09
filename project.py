78e4a05655c34fde33c2990fb9b19fb40b45fafd
Cédric Krier <ced@b2ck.com>
2008-06-09 16:54:49 +0200

    Rename work_type into work

--- project.py
+++ project.py
@@ -7,9 +7,9 @@ class Project(OSV):
     'Project'
     _name = 'project.project'
     _description = __doc__
-    _inherits = {'timesheet.work_type': 'work_type'}
+    _inherits = {'timesheet.work': 'work'}
 
-    work_type = fields.Many2One('timesheet.work_type', 'Work Type',
+    work = fields.Many2One('timesheet.work', 'Work Type',
             required=True)
     party = fields.Many2One('relationship.party', 'Party')
     party_address = fields.Many2One('relationship.address', 'Contact Address',
