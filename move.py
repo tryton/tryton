5249cc7143a3d75b1bae70b5740b0a2822f6b856
Cédric Krier <ced@b2ck.com>
2008-06-23 21:01:22 +0200

    Fix COPYRIGHT

--- move.py
+++ move.py
@@ -1,3 +1,4 @@
+#This file is part of Tryton.  The COPYRIGHT file at the top level of this repository contains the full copyright notices and license terms.
 "Move"
 from trytond.osv import fields, OSV, ExceptORM
 from trytond.wizard import Wizard, WizardOSV, ExceptWizard
