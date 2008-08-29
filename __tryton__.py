d76f4c90c160e20c9ad2f49ca93e1a0a00c2bbc2
Cédric Krier <ced@b2ck.com>
2008-08-29 11:13:59 +0200

    Fix typo for issue292

--- __tryton__.py
+++ __tryton__.py
@@ -5,7 +5,7 @@
     "author" : "B2CK",
     'website': 'http://www.tryton.org/',
     "category" : "Generic",
-    "description": "Define products, categories of product, units" \
+    "description": "Define products, categories of product, units " \
         "of measure, categories of units of measure.",
     "depends" : [
         "ir",
