c7d0c546124a528211230e060b81e25460f987b2
Cédric Krier <ced@b2ck.com>
2008-09-24 15:52:43 +0200

    Fix setup classifiers and license

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
-#This file is part of Tryton.  The COPYRIGHT file at the top level of this repository contains the full copyright notices and license terms.
+#This file is part of Tryton.  The COPYRIGHT file at the top level of
+#this repository contains the full copyright notices and license terms.
 
 from distutils.core import setup
 from distutils.command import sdist
@@ -32,19 +33,21 @@ setup(name='trytond-' + info['name'].lower(),
         'trytond.modules.' + info['name'].lower(),
     ],
     package_data={
-        'trytond.modules.' + info['name'].lower(): info.get('xml', []),
+        'trytond.modules.' + info['name'].lower(): info.get('xml', []) \
+                + info.get('translation', []),
     },
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Environment :: Plugins',
         'Intended Audience :: Developers',
         'Intended Audience :: Legal Industry',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
         'Natural Language :: English',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Topic :: Office/Business',
     ],
-    license=info.get('license', 'GPL-2'),
+    license='GPL-2',
     #requires=requires,
     cmdclass={
         'sdist': mysdist,
