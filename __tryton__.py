7c56fafecb9a08c026e86b39f7323aadcbe4cfaf
Cédric Krier <ced@b2ck.com>
2008-10-02 12:13:43 +0200

    Add missing email

--- __tryton__.py
+++ __tryton__.py
@@ -1,8 +1,10 @@
-#This file is part of Tryton.  The COPYRIGHT file at the top level of this repository contains the full copyright notices and license terms.
+#This file is part of Tryton.  The COPYRIGHT file at the top level of
+#this repository contains the full copyright notices and license terms.
 {
     "name" : "Product",
     "version" : "0.1",
     "author" : "B2CK",
+    'email': 'info@b2ck.com',
     'website': 'http://www.tryton.org/',
     "category" : "Generic",
     "description": "Define products, categories of product, units " \
