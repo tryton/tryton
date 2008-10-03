1789a50431f6a428378b0bcff33c512aa5715903
Cédric Krier <ced@b2ck.com>
2008-10-03 15:33:02 +0200

    Use setuptools

--- setup.py
+++ setup.py
@@ -2,27 +2,22 @@
 #This file is part of Tryton.  The COPYRIGHT file at the top level of
 #this repository contains the full copyright notices and license terms.
 
-from distutils.core import setup
-from distutils.command import sdist
-
-
-class mysdist(sdist.sdist):
-
-    def add_defaults(self):
-        sdist.sdist.add_defaults(self)
-        if self.distribution.has_pure_modules():
-            build_py = self.get_finalized_command('build_py')
-            data = []
-            for package in build_py.packages:
-                src_dir = build_py.get_package_dir(package)
-                data.extend(build_py.find_data_files(package, src_dir))
-            self.filelist.extend(data)
+from setuptools import setup, find_packages
+import re
 
 info = eval(file('__tryton__.py').read())
 
-requires = ['trytond'] + ['trytond-' + x for x in info.get('depends', [])]
+requires = []
+for dep in info.get('depends', []):
+    match = re.compile(
+            '(ir|res|workflow|webdav)((\s|$|<|>|<=|>=|==|!=).*?$)').match(dep)
+    if match:
+        dep = 'trytond' + match.group(2)
+    else:
+        dep = 'trytond_' + dep
+    requires.append(dep)
 
-setup(name='trytond-' + info['name'].lower(),
+setup(name='trytond_' + info['name'].lower(),
     version=info.get('version', '0'),
     description=info.get('description', ''),
     author=info.get('author', ''),
@@ -42,15 +37,15 @@ setup(name='trytond-' + info['name'].lower(),
         'Intended Audience :: Developers',
         'Intended Audience :: Financial and Insurance Industry',
         'Intended Audience :: Legal Industry',
+        'Intended Audience :: Manufacturing',
         'License :: OSI Approved :: GNU General Public License (GPL)',
         'Natural Language :: English',
+        'Natural Language :: French',
+        'Natural Language :: German',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Topic :: Office/Business',
     ],
     license='GPL-3',
-    #requires=requires,
-    cmdclass={
-        'sdist': mysdist,
-    },
+    install_requires=requires,
 )
