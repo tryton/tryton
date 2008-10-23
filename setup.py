7f1da2a5e06d9ebb494cbb30a72a7520488c4b56
Cédric Krier <ced@b2ck.com>
2008-10-23 16:39:42 +0200

    Fix version number Fix setup

--- setup.py
+++ setup.py
@@ -2,28 +2,27 @@
 #This file is part of Tryton.  The COPYRIGHT file at the top level of
 #this repository contains the full copyright notices and license terms.
 
-from distutils.core import setup
-from distutils.command import sdist
-
-
-class mysdist(sdist.sdist):
-
-    def add_defaults(self):
-        sdist.sdist.add_defaults(self)
-        if self.distribution.has_pure_modules():
-            build_py = self.get_finalized_command('build_py')
-            data = []
-            for package in build_py.packages:
-                src_dir = build_py.get_package_dir(package)
-                data.extend(build_py.find_data_files(package, src_dir))
-            self.filelist.extend(data)
+from setuptools import setup, find_packages
+import re
 
 info = eval(file('__tryton__.py').read())
 
-requires = ['trytond'] + ['trytond-' + x for x in info.get('depends', [])]
+requires = []
+for dep in info.get('depends', []):
+    match = re.compile(
+            '(ir|res|workflow|webdav)((\s|$|<|>|<=|>=|==|!=).*?$)').match(dep)
+    if match:
+        continue
+    else:
+        dep = 'trytond_' + dep
+    requires.append(dep)
 
-setup(name='trytond-' + info['name'].lower(),
-    version=info.get('version', '0'),
+major_version, minor_version, _ = info.get('version', '0.0.1').split('.', 2)
+requires.append('trytond >= %s.%s' % (major_version, minor_version))
+requires.append('trytond < %s.%s' % (major_version, str(int(minor_version) + 1)))
+
+setup(name='trytond_' + info['name'].lower(),
+    version=info.get('version', '0.0.1'),
     description=info.get('description', ''),
     author=info.get('author', ''),
     author_email=info.get('email', ''),
@@ -34,8 +33,7 @@ setup(name='trytond-' + info['name'].lower(),
     ],
     package_data={
         'trytond.modules.' + info['name'].lower(): info.get('xml', []) \
-                + info.get('translation', []) \
-                + ['packing_out.odt'],
+                + info.get('translation', []),
     },
     classifiers=[
         'Development Status :: 5 - Production/Stable',
@@ -43,17 +41,15 @@ setup(name='trytond-' + info['name'].lower(),
         'Intended Audience :: Developers',
         'Intended Audience :: Financial and Insurance Industry',
         'Intended Audience :: Legal Industry',
+        'Intended Audience :: Manufacturing',
         'License :: OSI Approved :: GNU General Public License (GPL)',
         'Natural Language :: English',
+        'Natural Language :: French',
         'Natural Language :: German',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Topic :: Office/Business',
-        'Topic :: Office/Business :: Financial',
     ],
     license='GPL-3',
-    #requires=requires,
-    cmdclass={
-        'sdist': mysdist,
-    },
+    install_requires=requires,
 )
