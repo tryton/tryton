49bcff6642296bee346a91fa20e0438624a0669d
Cédric Krier <ced@b2ck.com>
2008-10-23 16:21:18 +0200

    Fix version number Add trytond requires

--- __tryton__.py
+++ __tryton__.py
@@ -1,7 +1,8 @@
-#This file is part of Tryton.  The COPYRIGHT file at the top level of this repository contains the full copyright notices and license terms.
+#This file is part of Tryton.  The COPYRIGHT file at the top level of
+#this repository contains the full copyright notices and license terms.
 {
     'name': 'Currency',
-    'version': '0.1',
+    'version': '0.0.1',
     'author': 'B2CK',
     'email': 'info@b2ck.com',
     'website': 'http://www.tryton.org/',
