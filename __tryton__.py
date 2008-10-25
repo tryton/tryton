11a69ce825da321de48131b230b3d92baa709fcb
Cédric Krier <ced@b2ck.com>
2008-10-25 16:48:23 +0200

    Initial commit for account_product module

---
+++ __tryton__.py
@@ -0,0 +1,22 @@
+#This file is part of Tryton.  The COPYRIGHT file at the top level of
+#this repository contains the full copyright notices and license terms.
+{
+    'name': 'Account Product',
+    'version': '0.0.1',
+    'author': 'B2CK',
+    'email': 'info@b2ck.com',
+    'website': 'http://www.tryton.org/',
+    'category': 'Accounting',
+    'description': 'Add accounting on product.',
+    'depends': [
+        'ir',
+        'account',
+        'company',
+        'product',
+    ],
+    'xml': [
+        'product.xml',
+    ],
+    'translation': [
+    ]
+}
