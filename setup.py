d807eeba853b5f6f0090b39fc55fa8b71269ce8b
Cédric Krier <ced@b2ck.com>
2008-10-27 10:08:32 +0100

    Fix setup

--- setup.py
+++ setup.py
@@ -27,12 +27,12 @@ setup(name='trytond_analytic_invoice',
     author=info.get('author', ''),
     author_email=info.get('email', ''),
     url=info.get('website', ''),
-    package_dir={'trytond.modules.' + info['name'].lower(): '.'},
+    package_dir={'trytond.modules.analytic_invoice': '.'},
     packages=[
-        'trytond.modules.' + info['name'].lower(),
+        'trytond.modules.analytic_invoice',
     ],
     package_data={
-        'trytond.modules.' + info['name'].lower(): info.get('xml', []) \
+        'trytond.modules.analytic_invoice': info.get('xml', []) \
                 + info.get('translation', []),
     },
     classifiers=[
