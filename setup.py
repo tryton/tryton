555a06a691c99ea5d01cb7c266e001232dafac2b
Cédric Krier <ced@b2ck.com>
2008-10-27 10:15:32 +0100

    Fix setup

--- setup.py
+++ setup.py
@@ -27,12 +27,12 @@ setup(name='trytond_project_revenue',
     author=info.get('author', ''),
     author_email=info.get('email', ''),
     url=info.get('website', ''),
-    package_dir={'trytond.modules.' + info['name'].lower(): '.'},
+    package_dir={'trytond.modules.project_revenue': '.'},
     packages=[
-        'trytond.modules.' + info['name'].lower(),
+        'trytond.modules.project_revenue',
     ],
     package_data={
-        'trytond.modules.' + info['name'].lower(): info.get('xml', []) \
+        'trytond.modules.project_revenue': info.get('xml', []) \
                 + info.get('translation', []),
     },
     classifiers=[
