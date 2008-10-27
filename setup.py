bcc68d432cc94bcc55ffa43accdc352587696b18
Cédric Krier <ced@b2ck.com>
2008-10-27 10:12:00 +0100

    Fix setup

--- setup.py
+++ setup.py
@@ -27,12 +27,12 @@ setup(name='trytond_google_maps',
     author=info.get('author', ''),
     author_email=info.get('email', ''),
     url=info.get('website', ''),
-    package_dir={'trytond.modules.' + info['name'].lower(): '.'},
+    package_dir={'trytond.modules.google_maps': '.'},
     packages=[
-        'trytond.modules.' + info['name'].lower(),
+        'trytond.modules.google_maps',
     ],
     package_data={
-        'trytond.modules.' + info['name'].lower(): info.get('xml', []) \
+        'trytond.modules.google_maps': info.get('xml', []) \
                 + info.get('translation', []),
     },
     classifiers=[
