19707fbd6031b291d0c667ab8f0f19814a949765
Cédric Krier <ced@b2ck.com>
2008-10-27 10:10:26 +0100

    Fix setup

--- setup.py
+++ setup.py
@@ -27,12 +27,12 @@ setup(name='trytond_company',
     author=info.get('author', ''),
     author_email=info.get('email', ''),
     url=info.get('website', ''),
-    package_dir={'trytond.modules.' + info['name'].lower(): '.'},
+    package_dir={'trytond.modules.company': '.'},
     packages=[
-        'trytond.modules.' + info['name'].lower(),
+        'trytond.modules.company',
     ],
     package_data={
-        'trytond.modules.' + info['name'].lower(): info.get('xml', []) \
+        'trytond.modules.company': info.get('xml', []) \
                 + info.get('translation', []) \
                 + ['header_A4.odt', 'letter.odt'],
     },
