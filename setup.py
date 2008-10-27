bacee0fcc485a126f3e817f6060ab56c48c5bdd0
Cédric Krier <ced@b2ck.com>
2008-10-27 10:15:01 +0100

    Fix setup

--- setup.py
+++ setup.py
@@ -27,12 +27,12 @@ setup(name='trytond_project',
     author=info.get('author', ''),
     author_email=info.get('email', ''),
     url=info.get('website', ''),
-    package_dir={'trytond.modules.' + info['name'].lower(): '.'},
+    package_dir={'trytond.modules.project': '.'},
     packages=[
-        'trytond.modules.' + info['name'].lower(),
+        'trytond.modules.project',
     ],
     package_data={
-        'trytond.modules.' + info['name'].lower(): info.get('xml', []) \
+        'trytond.modules.project': info.get('xml', []) \
                 + info.get('translation', []),
     },
     classifiers=[
