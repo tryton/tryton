3d3b1fa05de7bd5a72de9936cc57695ed0d1f046
Cédric Krier <ced@b2ck.com>
2008-10-27 10:08:03 +0100

    Fix setup

--- setup.py
+++ setup.py
@@ -27,12 +27,12 @@ setup(name='trytond_analytic_account',
     author=info.get('author', ''),
     author_email=info.get('email', ''),
     url=info.get('website', ''),
-    package_dir={'trytond.modules.' + info['name'].lower(): '.'},
+    package_dir={'trytond.modules.analytic_account': '.'},
     packages=[
-        'trytond.modules.' + info['name'].lower(),
+        'trytond.modules.analytic_account',
     ],
     package_data={
-        'trytond.modules.' + info['name'].lower(): info.get('xml', []) \
+        'trytond.modules.analytic_account': info.get('xml', []) \
                 + info.get('translation', []),
     },
     classifiers=[
