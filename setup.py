21900a41bebc503fc9ae072def01e7fad5feb3c5
Cédric Krier <ced@b2ck.com>
2008-10-27 10:12:33 +0100

    Fix setup

--- setup.py
+++ setup.py
@@ -27,12 +27,12 @@ setup(name='trytond_party',
     author=info.get('author', ''),
     author_email=info.get('email', ''),
     url=info.get('website', ''),
-    package_dir={'trytond.modules.' + info['name'].lower(): '.'},
+    package_dir={'trytond.modules.party': '.'},
     packages=[
-        'trytond.modules.' + info['name'].lower(),
+        'trytond.modules.party',
     ],
     package_data={
-        'trytond.modules.' + info['name'].lower(): info.get('xml', []) \
+        'trytond.modules.party': info.get('xml', []) \
                 + info.get('translation', []) \
                 + ['label.odt'],
     },
