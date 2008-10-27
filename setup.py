1bce2a0e6a564aa805d7b62cdac3abfb2652ebf6
Cédric Krier <ced@b2ck.com>
2008-10-27 10:09:29 +0100

    Fix setup

--- setup.py
+++ setup.py
@@ -27,12 +27,12 @@ setup(name='trytond_analytic_sale',
     author=info.get('author', ''),
     author_email=info.get('email', ''),
     url=info.get('website', ''),
-    package_dir={'trytond.modules.' + info['name'].lower(): '.'},
+    package_dir={'trytond.modules.analytic_sale': '.'},
     packages=[
-        'trytond.modules.' + info['name'].lower(),
+        'trytond.modules.analytic_sale',
     ],
     package_data={
-        'trytond.modules.' + info['name'].lower(): info.get('xml', []) \
+        'trytond.modules.analytic_sale': info.get('xml', []) \
                 + info.get('translation', []),
     },
     classifiers=[
