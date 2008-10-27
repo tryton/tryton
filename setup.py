9dd8bb0c4cad9a9042ff3a74f29d8ead4e068a86
Cédric Krier <ced@b2ck.com>
2008-10-27 10:21:03 +0100

    Fix setup

--- setup.py
+++ setup.py
@@ -27,12 +27,12 @@ setup(name='trytond_timesheet',
     author=info.get('author', ''),
     author_email=info.get('email', ''),
     url=info.get('website', ''),
-    package_dir={'trytond.modules.' + info['name'].lower(): '.'},
+    package_dir={'trytond.modules.timesheet': '.'},
     packages=[
-        'trytond.modules.' + info['name'].lower(),
+        'trytond.modules.timesheet',
     ],
     package_data={
-        'trytond.modules.' + info['name'].lower(): info.get('xml', []) \
+        'trytond.modules.timesheet': info.get('xml', []) \
                 + info.get('translation', []),
     },
     classifiers=[
