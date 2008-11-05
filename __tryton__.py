f04edaea34735834248c9a5369210c9ab9ce42d3
Mathias Behrle <mathiasb@behrle.dyndns.org>
2008-11-06 00:56:22 +0100

    added translation for de_DE, changed double quotes to single quotes

--- __tryton__.py
+++ __tryton__.py
@@ -2,6 +2,7 @@
 #this repository contains the full copyright notices and license terms.
 {
     'name': 'Stock Supply Management',
+    'name_de_DE': 'Lagerverwaltung Beschaffung',
     'version': '0.0.1',
     'author': 'B2CK',
     'email': 'info@b2ck.com',
@@ -14,6 +15,14 @@
 With scheduler:
     - to generate purchase request based on order points
     - to generate internal packing based on order points
+''',
+    'description_de_DE': '''Beschaffungsmodul mit:
+    - Bestellpunkten
+    - Auftragserstellung
+
+Mit automatischer Auftragserstellung per Zeitplaner:
+    - um Bestellungen auf der Basis von Bestellpunkten zu erstellen
+    - um internen Versand auf der Basis von Bestellpunkten zu erstellen
 ''',
     'depends': [
         'ir',
