75d031f003bec1a3e2f83e501b528a08d071d205
Mathias Behrle <mathiasb@behrle.dyndns.org>
2008-11-06 00:56:21 +0100

    added translation for de_DE, changed double quotes to single quotes

--- __tryton__.py
+++ __tryton__.py
@@ -1,16 +1,18 @@
 #This file is part of Tryton.  The COPYRIGHT file at the top level of
 #this repository contains the full copyright notices and license terms.
 {
-    "name" : "Country",
-    "version" : "0.0.1",
-    "author" : "B2CK",
+    'name' : 'Country',
+    'name_de_DE': 'Länder',
+    'version' : '0.0.1',
+    'author' : 'B2CK',
     'email': 'info@b2ck.com',
     'website': 'http://www.tryton.org/',
-    "category" : "Generic",
-    "description": "Define all countries and subdivisions.",
-    "depends" : ["ir", "res"],
-    "xml" : [
-        "country.xml",
+    'category' : 'Generic',
+    'description': 'Define all countries and subdivisions.',
+    'description_de_DE': 'Stellt sämtliche Länder und ihre subnationalen Einheiten zur Verfügung.',
+    'depends' : ['ir', 'res'],
+    'xml' : [
+        'country.xml',
     ],
     'translation': [
         'fr_FR.csv',
