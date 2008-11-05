569f06b60a0cbfa5dcb8449ac525f9fc7785518d
Mathias Behrle <mathiasb@behrle.dyndns.org>
2008-11-06 00:56:21 +0100

    added translation for de_DE, changed double quotes to single quotes

--- __tryton__.py
+++ __tryton__.py
@@ -2,25 +2,27 @@
 #This file is part of Tryton.  The COPYRIGHT file at the top level of
 #this repository contains the full copyright notices and license terms.
 {
-    "name" : "Party",
-    "name_fr_FR": "Tiers",
-    "version" : "0.0.1",
-    "author" : "B2CK",
+    'name' : 'Party',
+    'name_fr_FR': 'Tiers',
+    'name_de_DE': 'Kontakte',
+    'version' : '0.0.1',
+    'author' : 'B2CK',
     'email': 'info@b2ck.com',
     'website': 'http://www.tryton.org/',
-    "category" : "Generic",
-    "description": "Define parties, addresses and co.",
-    "description_fr_FR": "Définit des tiers, des adresses, etc.",
-    "depends" : [
-        "ir",
-        "res",
-        "country",
+    'category' : 'Generic',
+    'description': 'Define parties, addresses and co.',
+    'description_fr_FR': 'Définit des tiers, des adresses, etc.',
+    'description_de_DE': 'Ermöglicht die Erstellung von Kontakten, Adressen, etc.',
+    'depends' : [
+        'ir',
+        'res',
+        'country',
     ],
-    "xml" : [
-        "party.xml",
-        "category.xml",
-        "address.xml",
-        "contact_mechanism.xml",
+    'xml' : [
+        'party.xml',
+        'category.xml',
+        'address.xml',
+        'contact_mechanism.xml',
     ],
     'translation': [
         'fr_FR.csv',
