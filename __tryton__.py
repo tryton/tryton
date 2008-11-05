d3a03eb875b005a5de01a8501deb773d7faa6393
Mathias Behrle <mathiasb@behrle.dyndns.org>
2008-11-06 00:56:21 +0100

    added translation for de_DE, changed double quotes to single quotes

--- __tryton__.py
+++ __tryton__.py
@@ -1,19 +1,21 @@
 #This file is part of Tryton.  The COPYRIGHT file at the top level of
 #this repository contains the full copyright notices and license terms.
 {
-    "name" : "Google Maps",
-    "version" : "0.0.1",
-    "author" : "B2CK",
+    'name' : 'Google Maps',
+    'name_de_DE': 'Google Maps',
+    'version' : '0.0.1',
+    'author' : 'B2CK',
     'email': 'info@b2ck.com',
     'website': 'http://www.tryton.org/',
-    "category" : "Generic",
-    "description": "Add link from addresses to Google Maps",
-    "depends" : [
-        "ir",
-        "party"
+    'category' : 'Generic',
+    'description': 'Add link from addresses to Google Maps',
+    'description_de_DE': 'Fügt einen Link für den automatischen Aufruf von Google Maps zu Adressen hinzu.',
+    'depends' : [
+        'ir',
+        'party'
     ],
-    "xml" : [
-        "address.xml",
+    'xml' : [
+        'address.xml',
     ],
     'translation': [
         'de_DE.csv',
