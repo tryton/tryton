de2341629a4fb2b3c815b2fc17418a1a6b5e4a45
Mathias Behrle <mathiasb@behrle.dyndns.org>
2008-11-06 00:56:20 +0100

    added translation for de_DE, changed double quotes to single quotes

--- __tryton__.py
+++ __tryton__.py
@@ -2,12 +2,15 @@
 #this repository contains the full copyright notices and license terms.
 {
     'name': 'Analytic Purchase',
+    'name_de_DE': 'Kostenstellen Einkauf',
     'version': '0.0.1',
     'author': 'B2CK',
     'email': 'info@b2ck.com',
     'website': 'http://www.tryton.org/',
     'category': 'Purchase',
     'description': '''Add analytic account on purchase lines.
+''',
+    'description_de_DE': '''Fügt kostenstellen zu Einkaufspositionen hinzu
 ''',
     'depends': [
         'purchase',
