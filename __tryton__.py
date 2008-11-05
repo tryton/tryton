20bddd60f026054d3671d22cf36d7473407785c4
Cédric Krier <ced@b2ck.com>
2008-11-05 12:50:55 +0100

    Add fr_FR descriptions

--- __tryton__.py
+++ __tryton__.py
@@ -1,13 +1,16 @@
+# -*- coding: utf-8 -*-
 #This file is part of Tryton.  The COPYRIGHT file at the top level of
 #this repository contains the full copyright notices and license terms.
 {
     "name" : "Party",
+    "name_fr_FR": "Tiers",
     "version" : "0.0.1",
     "author" : "B2CK",
     'email': 'info@b2ck.com',
     'website': 'http://www.tryton.org/',
     "category" : "Generic",
     "description": "Define parties, addresses and co.",
+    "description_fr_FR": "Définit des tiers, des adresses, etc.",
     "depends" : [
         "ir",
         "res",
