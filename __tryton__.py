c5117b5e4795afaa366f878513cd6d22349e8ae6
Mathias Behrle <mathiasb@behrle.dyndns.org>
2008-11-06 00:56:20 +0100

    added translation for de_DE, changed double quotes to single quotes

--- __tryton__.py
+++ __tryton__.py
@@ -2,6 +2,7 @@
 #this repository contains the full copyright notices and license terms.
 {
     'name': 'Account Invoice',
+    'name_de_DE': 'Buchhaltung Rechnungsstellung',
     'version': '0.0.1',
     'author': 'B2CK',
     'email': 'info@b2ck.com',
@@ -16,6 +17,16 @@ With the possibilities:
     - to follow the payment of the invoices.
     - to define invoice sequences on fiscal year or period.
     - to credit any invoice.
+''',
+    'description_de_DE': '''Modul für Buchhaltung und Rechnungsstellung mit:
+    - Definition von Zahlungsbedingungen
+    - Erstellung von Rechnungen und Gutschriften für Kunden
+    - Erstellung von Rechnungen und Gutschriften für Lieferanten
+
+Ermöglicht:
+    - die Verfolgung der Bezahlung von Rechnungen
+    - die Definition von Rechnungssequenzen für das Geschäftsjahr bzw. die Buchungszeiträume
+    - die Erstellung von Gutschriften zu jeglicher Rechnung
 ''',
     'depends': [
         'ir',
