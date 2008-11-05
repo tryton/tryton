292a548ac287876e68b4944b876ee1908d8979aa
Mathias Behrle <mathiasb@behrle.dyndns.org>
2008-11-06 00:56:21 +0100

    added translation for de_DE, changed double quotes to single quotes

--- __tryton__.py
+++ __tryton__.py
@@ -2,6 +2,7 @@
 #this repository contains the full copyright notices and license terms.
 {
     'name': 'Project Revenue',
+    'name_de_DE': 'Projekte Ertrag',
     'version': '0.0.1',
     'author': 'B2CK',
     'email': 'info@b2ck.com',
@@ -9,6 +10,9 @@
     'category': 'Project',
     'description': '''Add product on timesheet lines.
 Define allowed service for each employees.
+''',
+    'description_de_DE': ''' - Ermöglicht das Hinzufügen von Artikeln zu Zeitpositionen.
+ - Ermöglicht die Definition von Dienstleistungen, die für einen Mitarbeiter verwendet werden können.
 ''',
     'depends': [
         'ir',
