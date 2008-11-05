97a2ab0a9ac6ea790fe76a6ccb31c02f13de5385
Mathias Behrle <mathiasb@behrle.dyndns.org>
2008-11-06 00:56:21 +0100

    added translation for de_DE, changed double quotes to single quotes

--- __tryton__.py
+++ __tryton__.py
@@ -1,22 +1,24 @@
 #This file is part of Tryton.  The COPYRIGHT file at the top level of
 #this repository contains the full copyright notices and license terms.
 {
-    "name" : "Product",
-    "version" : "0.0.1",
-    "author" : "B2CK",
+    'name' : 'Product',
+    'name_de_DE': 'Artikel',
+    'version' : '0.0.1',
+    'author' : 'B2CK',
     'email': 'info@b2ck.com',
     'website': 'http://www.tryton.org/',
-    "category" : "Generic",
-    "description": "Define products, categories of product, units " \
-        "of measure, categories of units of measure.",
-    "depends" : [
-        "ir",
-        "res",
+    'category' : 'Generic',
+    'description': 'Define products, categories of product, units ' \
+        'of measure, categories of units of measure.',
+    'description_de_DE': 'Dient der Erstellung von Artikeln, Artikelkategorien, Maßeinheiten und Kategorien für Maßeinheiten.',
+    'depends' : [
+        'ir',
+        'res',
     ],
-    "xml" : [
-        "product.xml",
-        "category.xml",
-        "uom.xml",
+    'xml' : [
+        'product.xml',
+        'category.xml',
+        'uom.xml',
     ],
     'translation': [
         'fr_FR.csv',
