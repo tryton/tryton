528398833353780354c789f60a5db5a13f0dabe8
Mathias Behrle <mathiasb@behrle.dyndns.org>
2008-11-06 00:56:20 +0100

    added translation for de_DE, changed double quotes to single quotes

--- __tryton__.py
+++ __tryton__.py
@@ -2,6 +2,7 @@
 #this repository contains the full copyright notices and license terms.
 {
     'name': 'Company',
+    'name_de_DE': 'Unternehmen',
     'version': '0.0.1',
     'author': 'B2CK',
     'email': 'info@b2ck.com',
@@ -13,6 +14,13 @@ Add company on properties.
 Define new report parser for report with company header.
 Add letter template on party.
 Make the cron run on each companies.
+''',
+    'description_de_DE': ''' - Ermöglicht die Eingabe von Umternehmen und Mitarbeitern.
+ - Fügt Haupt- und aktuelles Unternehmen zu den Benutzereinstellungen hinzu.
+ - Fügt das Unternehmen den Eigenschaften hinzu.
+ - Ermöglicht die Definition eines neuen Berichtsanalysierers mit Unternehmen im Berichtskopf.
+ - Fügt den Kontakten die Berichtsvorlage für Briefe hinzu.
+ - Initialisiert die Aufgaben des Zeitplaners für jedes Unternehmen.
 ''',
     'depends': [
         'ir',
