c031be9e0eeb4ee8f72ed8a13c47b91820a5b21f
Cédric Krier <ced@b2ck.com>
2008-11-05 16:32:39 +0100

    Improve description of module

--- __tryton__.py
+++ __tryton__.py
@@ -1,4 +1,5 @@
-#This file is part of Tryton.  The COPYRIGHT file at the top level of this repository contains the full copyright notices and license terms.
+#This file is part of Tryton.  The COPYRIGHT file at the top level of
+#this repository contains the full copyright notices and license terms.
 {
     'name': 'Account',
     'version': '0.0.1',
@@ -6,7 +7,22 @@
     'email': 'info@b2ck.com',
     'website': 'http://www.tryton.org/',
     'category': 'Accounting',
-    'description': 'Financial and Accounting Module',
+    'description': '''Financial and Accounting Module with:
+    - General accounting
+    - Fiscal year management
+    - Taxes management
+    - Journal entries
+    - Reconciliation
+
+And with reports:
+    - General ledger
+    - Trial balance
+    - Balance sheet
+    - Income statement
+    - Third party balance
+    - Aged balance
+    - General journal
+''',
     'depends': [
         'ir',
         'res',
