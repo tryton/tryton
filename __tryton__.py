e34366c1f7bbd90ea4b9fbccfdb993d79095f852
Cédric Krier <ced@b2ck.com>
2008-11-06 20:59:05 +0100

    Add README and INSTALL, remove category

--- __tryton__.py
+++ __tryton__.py
@@ -7,7 +7,6 @@
     'author': 'B2CK',
     'email': 'info@b2ck.com',
     'website': 'http://www.tryton.org/',
-    'category': 'Project',
     'description': '''Project Module with:
     - Project management
 ''',
