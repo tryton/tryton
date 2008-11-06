7873e9816cb067e76bcd141126ba4d72bee34ee1
Bertrand Chenal <bch@b2ck.com>
2008-11-06 23:21:25 +0100

    Added french translation

--- __tryton__.py
+++ __tryton__.py
@@ -13,6 +13,10 @@
     'description_de_DE': ''' - Fügt Kostenstellen zu Rechnungszeilen hinzu
  - Erstellt Positionen für Kostenstellen bei Buchung von Rechnungen
 ''',
+    'description_fr_FR': '''Ajoute la comptabilité analytique sur les ligne '''
+    '''de facture et génère les lignes analytiques sur les mouvements de la '''
+    '''facture.''',
+
     'depends': [
         'account_invoice',
         'analytic_account',
@@ -23,5 +27,6 @@
     'translation': [
         'de_DE.csv',
         'es_ES.csv',
+        'fr_FR.csv',
     ],
 }
