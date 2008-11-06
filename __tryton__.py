c1be3d3c4821abe73116bc36ef32ad803aacae22
Cédric Krier <ced@b2ck.com>
2008-11-06 22:56:18 +0100

    Add README and INSTALL, remove category

--- __tryton__.py
+++ __tryton__.py
@@ -6,7 +6,6 @@
     "author" : "B2CK",
     'email': 'info@b2ck.com',
     'website': 'http://www.tryton.org/',
-    "category" : "Inventory",
     "description": "Define the delivery day of the week by suppliers.",
     "depends" : [
         "ir",
