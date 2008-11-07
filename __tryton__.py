3a0e0cbda1c57c10e09984fa24eaebd6ad3b6960
Bertrand Chenal <bch@b2ck.com>
2008-11-07 01:27:16 +0100

    Added french description

--- __tryton__.py
+++ __tryton__.py
@@ -3,6 +3,7 @@
 {
     'name': 'Company',
     'name_de_DE': 'Unternehmen',
+    'name_fr_FR': 'Compagnie',
     'version': '0.0.1',
     'author': 'B2CK',
     'email': 'info@b2ck.com',
@@ -12,7 +13,7 @@ Add main and current company on users preferences.
 Add company on properties.
 Define new report parser for report with company header.
 Add letter template on party.
-Make the cron run on each companies.
+Make the scheduler run on each companies.
 ''',
     'description_de_DE': ''' - Ermöglicht die Eingabe von Umternehmen und Mitarbeitern.
  - Fügt Haupt- und aktuelles Unternehmen zu den Benutzereinstellungen hinzu.
@@ -21,6 +22,14 @@ Make the cron run on each companies.
  - Fügt den Kontakten die Berichtsvorlage für Briefe hinzu.
  - Initialisiert die Aufgaben des Zeitplaners für jedes Unternehmen.
 ''',
+    'description_fr_FR': '''Defini compagnie et employé.
+Ajoute les compagnies principale et courante dans les préférences de l'utilisateur.
+Ajoute la compagnie sur les propriétés.
+Défini un nouveau moteur de rapport gérant une entête par compagnie.
+Ajoute un canva de lettre par compagnie.
+Lance les planificateurs sur chaque compagnie.
+''',
+
     'depends': [
         'ir',
         'res',
