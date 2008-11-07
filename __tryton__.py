9ea437ef5e9f251bbc8652f728599b44fb00fb24
Bertrand Chenal <bch@b2ck.com>
2008-11-07 01:27:33 +0100

    Added french description

--- __tryton__.py
+++ __tryton__.py
@@ -3,12 +3,14 @@
 {
     'name' : 'Google Maps',
     'name_de_DE': 'Google Maps',
+    'name': 'Cartes Google',
     'version' : '0.0.1',
     'author' : 'B2CK',
     'email': 'info@b2ck.com',
     'website': 'http://www.tryton.org/',
     'description': 'Add link from addresses to Google Maps',
     'description_de_DE': 'Fügt einen Link für den automatischen Aufruf von Google Maps zu Adressen hinzu.',
+    'description_fr_FR': 'Ajoute un lien sur les adresses vers les cartes de Google',
     'depends' : [
         'ir',
         'party'
