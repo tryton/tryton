925bf4820ca40c0eb642adfc9e37b00f9dd29e29
Bertrand Chenal <bch@b2ck.com>
2008-11-07 01:27:37 +0100

    Added french description

--- __tryton__.py
+++ __tryton__.py
@@ -3,6 +3,7 @@
 {
     'name' : 'Product',
     'name_de_DE': 'Artikel',
+    'name_fr_FR': 'Produit',
     'version' : '0.0.1',
     'author' : 'B2CK',
     'email': 'info@b2ck.com',
@@ -10,6 +11,8 @@
     'description': 'Define products, categories of product, units ' \
         'of measure, categories of units of measure.',
     'description_de_DE': 'Dient der Erstellung von Artikeln, Artikelkategorien, Maßeinheiten und Kategorien für Maßeinheiten.',
+    'description_fr_FR': 'Défini produits, catégories de produit, '
+    'unités de mesure et catégories d\'unité de mesure',
     'depends' : [
         'ir',
         'res',
