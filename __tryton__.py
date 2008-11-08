32296a1aa063a288e7735f422172650763b7be61
Igor Támara <igor@tamarapatino.org>
2008-11-07 23:42:39 -0500

    Translated module description

--- __tryton__.py
+++ __tryton__.py
@@ -4,6 +4,7 @@
     'name': 'Project Revenue',
     'name_de_DE': 'Projekte Ertrag',
     'name_fr_FR': 'Revenu des projets',
+    'name_es_ES': 'Ingresos por proyectos',
     'version': '0.0.1',
     'author': 'B2CK',
     'email': 'info@b2ck.com',
@@ -14,9 +15,13 @@ Define allowed services for each employees.
     'description_de_DE': ''' - Ermöglicht das Hinzufügen von Artikeln zu Zeitpositionen.
  - Ermöglicht die Definition von Dienstleistungen, die für einen Mitarbeiter verwendet werden können.
 ''',
-    'description': '''Ajoute le produit sur la ligne de présence.
+    'description_fr_FR':'''Ajoute le produit sur la ligne de présence.
 Défini par employé quels services sont autorisés.
-''',    'depends': [
+''',
+    'description_es_ES':''' - Se añade el producto a las tarjetas de registro de tiempos.
+ - Se define por empleado los servicios que tiene autorizados.
+''',
+    'depends': [
         'ir',
         'project',
         'company',
