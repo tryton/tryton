1d03f78ab76a64a850d5a13aba9c224a015482ae
Igor Támara <igor@tamarapatino.org>
2008-11-07 23:30:57 -0500

    Translated module description

--- __tryton__.py
+++ __tryton__.py
@@ -4,6 +4,7 @@
     'name': 'Timesheet',
     'name_de_DE': 'Zeiterfassung',
     'name_fr_FR': 'Feuille de présence',
+    'name_es_ES': 'Hoja de Asistencia',
     'version': '0.0.1',
     'author': 'B2CK',
     'email': 'info@b2ck.com',
@@ -26,7 +27,7 @@ Zugehörige Berichte:
     - Stunden pro Mitarbeiter pro Woche
     - Stunden pro Mitarbeiter pro Monat
 ''',
-    'description': '''Module feuille de présence, avec:
+    'description_fr_FR': '''Module feuille de présence, avec:
     - Tâche
     - Ligne de feuille de présence
 
@@ -34,6 +35,15 @@ Et les rapports:
     - Heures par tâche
     - Heures par employé par semaine
     - Heures par employé par mois
+''',
+    'description_es_ES': '''Módulo de Hoja de Asistencia con:
+    - Trabajo
+    - Líneas de tiempo laborado
+
+Y con reportes:
+    - Horas por semana
+    - Horas por empleado por semana
+    - Horas por empleado por mes
 ''',
 
     'depends': [
