7fb4d7dc6b094ead330fcecdffef0cb48e115df6
Cédric Krier <ced@b2ck.com>
2008-11-18 18:22:56 +0100

    Allow egg installation

--- setup.py
+++ setup.py
@@ -27,6 +27,8 @@ setup(name='trytond_company',
     author=info.get('author', ''),
     author_email=info.get('email', ''),
     url=info.get('website', ''),
+    download_url="http://downloads.tryton.org/" + \
+            info.get('version', '0.0.1').rsplit('.', 1)[0] + '/',
     package_dir={'trytond.modules.company': '.'},
     packages=[
         'trytond.modules.company',
@@ -54,4 +56,9 @@ setup(name='trytond_company',
     ],
     license='GPL-3',
     install_requires=requires,
+    zip_safe=False,
+    entry_points="""
+    [trytond.modules]
+    company = trytond.modules.company
+    """,
 )
