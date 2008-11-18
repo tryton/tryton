8cc052f3449213289c9a850d863ad138e10f36bc
Cédric Krier <ced@b2ck.com>
2008-11-18 18:51:36 +0100

    Allow egg installation

--- setup.py
+++ setup.py
@@ -27,6 +27,8 @@ setup(name='trytond_account_statement',
     author=info.get('author', ''),
     author_email=info.get('email', ''),
     url=info.get('website', ''),
+    download_url="http://downloads.tryton.org/" + \
+            info.get('version', '0.0.1').rsplit('.', 1)[0] + '/',
     package_dir={'trytond.modules.account_statement': '.'},
     packages=[
         'trytond.modules.account_statement',
@@ -51,4 +53,9 @@ setup(name='trytond_account_statement',
     ],
     license='GPL-3',
     install_requires=requires,
+    zip_safe=False,
+    entry_points="""
+    [trytond.modules]
+    account_statement = trytond.modules.account_statement
+    """,
 )
