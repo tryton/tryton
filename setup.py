dd6f8f6ac5db56bc2b569416cf5140bac11d6d18
Cédric Krier <ced@b2ck.com>
2008-11-18 18:16:26 +0100

    Allow egg installation

--- setup.py
+++ setup.py
@@ -27,6 +27,8 @@ setup(name='trytond_analytic_invoice',
     author=info.get('author', ''),
     author_email=info.get('email', ''),
     url=info.get('website', ''),
+    download_url="http://downloads.tryton.org/" + \
+            info.get('version', '0.0.1').rsplit('.', 1)[0] + '/',
     package_dir={'trytond.modules.analytic_invoice': '.'},
     packages=[
         'trytond.modules.analytic_invoice',
@@ -53,4 +55,9 @@ setup(name='trytond_analytic_invoice',
     ],
     license='GPL-3',
     install_requires=requires,
+    zip_safe=False,
+    entry_points="""
+    [trytond.modules]
+    analytic_invoice = trytond.modules.analytic_invoice
+    """,
 )
