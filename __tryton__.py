3e5a572e2ea3ec5caf80e43f4eb940406df2a7b9
Cédric Krier <ced@b2ck.com>
2008-11-18 17:51:44 +0100

    Increase version number

--- __tryton__.py
+++ __tryton__.py
@@ -5,7 +5,7 @@
     'name_de_DE': 'Währung',
     'name_fr_FR': 'Devise',
     'name_es_ES': 'Moneda',
-    'version': '1.0.0',
+    'version': '1.0.1',
     'author': 'B2CK',
     'email': 'info@b2ck.com',
     'website': 'http://www.tryton.org/',
