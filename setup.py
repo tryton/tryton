bc7ad7ae886a55f0f55e129e46c7625e137305a3
Cédric Krier <ced@b2ck.com>
2008-11-18 18:00:26 +0100

    Add missing download_url and changelog

--- setup.py
+++ setup.py
@@ -27,6 +27,8 @@ setup(name='trytond_country',
     author=info.get('author', ''),
     author_email=info.get('email', ''),
     url=info.get('website', ''),
+    download_url="http://downloads.tryton.org/" + \
+            info.get('version', '0.0.1').rsplit('.', 1)[0] + '/',
     package_dir={'trytond.modules.country': '.'},
     packages=[
         'trytond.modules.country',
