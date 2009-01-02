959f7f9288e32c30e2c2edc2622cb2770a894277
Cédric Krier <ced@b2ck.com>
2009-01-02 17:54:34 +0100

    Fix guidelines

---
+++ party.py
@@ -0,0 +1,23 @@
+#This file is part of Tryton.  The COPYRIGHT file at the top level of
+#this repository contains the full copyright notices and license terms.
+
+from trytond.osv import fields, OSV
+
+
+class Address(OSV):
+    _name = 'party.address'
+    invoice = fields.Boolean('Invoice')
+
+Address()
+
+
+class Party(OSV):
+    _name = 'party.party'
+    payment_term = fields.Property(type='many2one',
+            relation='account.invoice.payment_term',
+            string='Invoice Payment Term')
+    supplier_payment_term = fields.Property(type='many2one',
+            relation='account.invoice.payment_term',
+            string='Supplier Payment Term')
+
+Party()
