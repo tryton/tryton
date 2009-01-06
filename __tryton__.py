fe476dbf3f61d834b7c0ea4addcc5d0786830639
Cédric Krier <ced@b2ck.com>
2009-01-06 13:46:22 +0100

    Increase version number

--- __tryton__.py
+++ __tryton__.py
@@ -6,7 +6,7 @@
     'name_fr_FR': 'Tiers',
     'name_de_DE': 'Kontakte',
     'name_es_ES': 'Terceros',
-    'version' : '1.0.2',
+    'version' : '1.0.3',
     'author' : 'B2CK',
     'email': 'info@b2ck.com',
     'website': 'http://www.tryton.org/',
