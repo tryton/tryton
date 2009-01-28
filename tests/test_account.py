048eb7bf66e8e3ec89fbe5c698ae8cea11253b3c
Cédric Krier <ced@b2ck.com>
2009-01-28 12:36:22 +0100

    Fix unittest for new tests module

--- tests/test_account.py
+++ tests/test_account.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
-#This file is part of Tryton.  The COPYRIGHT file at the top level of this repository contains the full copyright notices and license terms.
+#This file is part of Tryton.  The COPYRIGHT file at the top level of
+#this repository contains the full copyright notices and license terms.
 
 import sys, os
 DIR = os.path.abspath(os.path.normpath(os.path.join(__file__,
@@ -8,9 +9,9 @@ if os.path.isdir(DIR):
     sys.path.insert(0, os.path.dirname(DIR))
 
 import unittest
-import trytond.tests
+import trytond.tests.test_tryton
 import trytond.modules.company.tests
-from trytond.tests import RPCProxy, CONTEXT, SOCK
+from trytond.tests.test_tryton import RPCProxy, CONTEXT, SOCK
 
 
 class AccountTestCase(unittest.TestCase):
@@ -19,13 +20,13 @@ class AccountTestCase(unittest.TestCase):
     '''
 
     def setUp(self):
-        trytond.tests.install_module('account')
+        trytond.tests.test_tryton.install_module('account')
 
 def suite():
     return unittest.TestLoader().loadTestsFromTestCase(AccountTestCase)
 
 if __name__ == '__main__':
-    suiteTrytond = trytond.tests.suite()
+    suiteTrytond = trytond.tests.test_tryton.suite()
     suiteCompany = trytond.modules.company.tests.suite()
     suiteAccount = suite()
     alltests = unittest.TestSuite([suiteTrytond, suiteCompany, suiteAccount])
