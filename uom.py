9cef7c30fce988e8bfffd6c9dc381ad79c4092f6
Cédric Krier <ced@b2ck.com>
2009-02-02 13:11:13 +0100

    Fix for new modelstorage

--- uom.py
+++ uom.py
@@ -1,5 +1,7 @@
-#This file is part of Tryton.  The COPYRIGHT file at the top level of this repository contains the full copyright notices and license terms.
-from trytond.osv import fields, OSV, OPERATORS
+#This file is part of Tryton.  The COPYRIGHT file at the top level of
+#this repository contains the full copyright notices and license terms.
+from trytond.osv import fields, OSV
+from trytond.model.modelstorage import OPERATORS
 from decimal import Decimal
 
 STATES = {
