c04094a90bd421e486cfb242c3172d82597f590f
Cédric Krier <ced@b2ck.com>
2009-02-28 17:05:08 +0100

    Remove OSV and WizardOSV

--- inventory.py
+++ inventory.py
@@ -1,11 +1,10 @@
 #This file is part of Tryton.  The COPYRIGHT file at the top level of
 #this repository contains the full copyright notices and license terms.
+from trytond.model import ModelView, fields
+from trytond.wizard import Wizard
 
-from trytond.wizard import Wizard, WizardOSV
-from trytond.osv import fields
 
-
-class CreateInventoriesInit(WizardOSV):
+class CreateInventoriesInit(ModelView):
     'Create Inventories Init'
     _name = 'stock.inventory.create.init'
     _description = __doc__
