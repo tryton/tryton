8e80db1fcf8726ae0a349cda477979469bf0e7f2
Cédric Krier <ced@b2ck.com>
2009-02-28 16:58:39 +0100

    Remove OSV and WizardOSV

--- invoice.py
+++ invoice.py
@@ -1,10 +1,9 @@
 #This file is part of Tryton.  The COPYRIGHT file at the top level of this repository contains the full copyright notices and license terms.
 "Invoice"
+from trytond.model import ModelView, ModelSQL, fields
 
-from trytond.osv import fields, OSV
 
-
-class InvoiceLine(OSV):
+class InvoiceLine(ModelSQL, ModelView):
     _name = 'account.invoice.line'
 
     analytic_accounts = fields.Many2One('analytic_account.account.selection',
@@ -184,7 +183,7 @@ class InvoiceLine(OSV):
 InvoiceLine()
 
 
-class Account(OSV):
+class Account(ModelSQL, ModelView):
     _name = 'analytic_account.account'
 
     def delete(self, cursor, user, ids, context=None):
