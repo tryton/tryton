b71f918d3a73f67028c4e54d2bdceb9be9dee55d
Cédric Krier <ced@b2ck.com>
2009-02-28 17:00:01 +0100

    Remove OSV and WizardOSV

--- address.py
+++ address.py
@@ -1,9 +1,9 @@
 #This file is part of Tryton.  The COPYRIGHT file at the top level of
 #this repository contains the full copyright notices and license terms.
-from trytond.osv import fields, OSV
+from trytond.model import ModelView, ModelSQL, fields
 import urllib
 
-class Address(OSV):
+class Address(ModelSQL, ModelView):
     _name = 'party.address'
 
     google_maps_url = fields.Function('get_google_maps_url', type="char",
