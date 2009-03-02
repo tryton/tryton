ad46683535c0428e47d5bcbb1290f41ad8f3d8c6
Cédric Krier <ced@b2ck.com>
2009-03-02 10:22:55 +0100

    Increase release number

--- __tryton__.py
+++ __tryton__.py
@@ -5,7 +5,7 @@
     'name_de_DE': 'Währung',
     'name_fr_FR': 'Devise',
     'name_es_ES': 'Moneda',
-    'version': '1.0.2',
+    'version': '1.0.3',
     'author': 'B2CK',
     'email': 'info@b2ck.com',
     'website': 'http://www.tryton.org/',
