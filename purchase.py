e81ed822e5cbc275308beba4b07f9b7b463126e6
Cédric Krier <ced@b2ck.com>
2009-03-09 17:38:12 +0100

    Allow to define more than one weekday on product_supplier

--- purchase.py
+++ purchase.py
@@ -7,8 +7,61 @@ import datetime
 class ProductSupplier(ModelSQL, ModelView):
     _name = 'purchase.product_supplier'
 
+    weekdays = fields.One2Many('purchase.product_supplier.day', 'product_supplier',
+            'Week Days')
+
+    def compute_supply_date(self, cursor, user, product_supplier, date=None,
+            context=None):
+        res = super(ProductSupplier, self).compute_supply_date(cursor, user,
+                product_supplier, date=date, context=context)
+        earlier_date = None
+        date = res[0]
+        next_date = res[1]
+        for day in product_supplier.weekdays:
+            weekday = int(day.weekday)
+            diff = weekday - date.weekday()
+            if diff < 0:
+                diff += 7
+            new_date = date + datetime.timedelta(diff)
+
+            if earlier_date and earlier_date <= new_date:
+                continue
+            earlier_date = new_date
+
+            diff = weekday - next_date.weekday()
+            if diff < 0:
+                diff += 7
+            new_next_date = next_date + datetime.timedelta(diff)
+            res = (new_date, new_next_date)
+        return res
+
+    def compute_purchase_date(self, cursor, user, product_supplier, date,
+            context=None):
+        earlier_date = None
+        for day in product_supplier.weekdays:
+            weekday = int(day.weekday)
+            diff = date.weekday() - weekday
+            if diff > 0:
+                diff -= 7
+            new_date = date + datetime.timedelta(diff)
+            if earlier_date and earlier_date <= new_date:
+                continue
+            earlier_date = new_date
+        if earlier_date:
+            date = earlier_date
+        return super(ProductSupplier, self).compute_purchase_date(cursor, user,
+                product_supplier, date, context=context)
+
+ProductSupplier()
+
+
+class ProductSupplierDay(ModelSQL, ModelView):
+    _name = 'purchase.product_supplier.day'
+    _rec_name = 'weekday'
+
+    product_supplier = fields.Many2One('purchase.product_supplier', 'Supplier',
+            required=True)
     weekday = fields.Selection([
-        ('-1', ''),
         ('0', 'Monday'),
         ('1', 'Tuesday'),
         ('2', 'Wednesday'),
@@ -18,38 +71,11 @@ class ProductSupplier(ModelSQL, ModelView):
         ('6', 'Sunday'),
         ], 'Week Day', required=True, sort=False)
 
+    def __init__(self):
+        super(ProductSupplierDay, self).__init__()
+        self._order.insert(0, ('weekday', 'ASC'))
+
     def default_weekday(self, cursor, user, context=None):
         return -1
 
-    def compute_supply_date(self, cursor, user, product_supplier, date=None,
-            context=None):
-        res = super(ProductSupplier, self).compute_supply_date(cursor, user,
-                product_supplier, date=date, context=context)
-        weekday = int(product_supplier.weekday)
-        if  weekday >= 0:
-            date = res[0]
-            next_date = res[1]
-            diff = weekday - date.weekday()
-            if diff < 0:
-                diff += 7
-            date = date + datetime.timedelta(diff)
-
-            diff = weekday - next_date.weekday()
-            if diff < 0:
-                diff += 7
-            next_date = next_date + datetime.timedelta(diff)
-            res = (date, next_date)
-        return res
-
-    def compute_purchase_date(self, cursor, user, product_supplier, date,
-            context=None):
-        weekday = int(product_supplier.weekday)
-        if weekday >= 0:
-            diff = date.weekday() - weekday
-            if diff > 0:
-                diff -= 7
-            date = date + datetime.timedelta(diff)
-        return super(ProductSupplier, self).compute_purchase_date(cursor, user,
-                product_supplier, date, context=context)
-
-ProductSupplier()
+ProductSupplierDay()
