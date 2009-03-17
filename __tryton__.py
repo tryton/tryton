368360d754cfbc0861b8f129193f966c4160b40f
Bertrand Chenal <bch@b2ck.com>
2009-03-17 12:55:25 +0100

    Added french description of module

--- __tryton__.py
+++ __tryton__.py
@@ -2,11 +2,21 @@
 #this repository contains the full copyright notices and license terms.
 {
     'name': 'Product Cost FIFO',
+    'name_fr_FR': 'Prix de revient produit FIFO',
     'version': '0.0.1',
     'author': 'B2CK',
     'email': 'info@b2ck.com',
     'website': 'http://www.tryton.org/',
-    'description': 'Add FIFO cost method on product.',
+    'description': '''Add FIFO cost method on the product form.
+Once installed, the price of the stock moves from/to a
+supplier/customer will update automatically the cost price of the
+related product (if the cost price method is FIFO)
+''',
+    'description_fr_FR':'''Ajoute 'FIFO' parmi les méthodes de coût du produit.
+Une fois le module installé, le prix de chaque mouvement de stock
+depuis un fournisseur ou vers un client modifiera automatiquement le
+prix de revient du produit concerné (si la méthode de coût est FIFO).
+''',
     'depends': [
         'ir',
         'product',
