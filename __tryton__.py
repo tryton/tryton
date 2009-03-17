d98dac7e7194504d5aee886fe02cc8c9549a1695
Bertrand Chenal <bch@b2ck.com>
2009-03-17 14:59:37 +0100

    Added french and english description of module

--- __tryton__.py
+++ __tryton__.py
@@ -1,12 +1,18 @@
 #This file is part of Tryton.  The COPYRIGHT file at the top level of
 #this repository contains the full copyright notices and license terms.
 {
-    "name" : "Stock Supply with Week Day",
+    "name" : "Stock Supply with Week Days",
+    "name_fr_FR" : "Approvisionnement par jours de semaine",
     "version" : "0.0.1",
     "author" : "B2CK",
     'email': 'info@b2ck.com',
     'website': 'http://www.tryton.org/',
-    "description": "Define the delivery day of the week by suppliers.",
+    "description": '''Define the delivery days of the week by suppliers.
+Impact supply dates computations.
+''',
+    "description_fr_FR": '''Défini les jours de livraisons par fournisseurs.
+Impacte les calculs de dates de livraisons.
+ ''',
     "depends" : [
         "ir",
         "purchase",
