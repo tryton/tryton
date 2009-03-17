33b2bf9221cd087182ed69fced826d25fe756fa5
Bertrand Chenal <bch@b2ck.com>
2009-03-17 14:59:03 +0100

    Added french and english description of module

--- __tryton__.py
+++ __tryton__.py
@@ -2,11 +2,22 @@
 #this repository contains the full copyright notices and license terms.
 {
     "name" : "Stock Product Location",
+    "name_fr_FR" : "Emplacement de produit",
     "version" : "0.0.1",
     "author" : "B2CK",
     'email': 'info@b2ck.com',
     'website': 'http://www.tryton.org/',
-    "description": "Define default storage location by warehouse on product.",
+
+    "description": '''Define default storage location by warehouse on product.
+Theses locations will be used by the supplier packing for generating
+inventory moves.
+''',
+
+    "description_fr_FR": '''Defini un emplacement Magasin par défaut par produit.
+Ces emplacements seront utilisé par les colisages fournisseurs pour
+générer les mouvements internes.
+''',
+
     "depends" : [
         "ir",
         "product",
