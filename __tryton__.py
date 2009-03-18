61e0bf2d39208c25a805a982a55baf162bcea5a8
Bertrand Chenal <bch@b2ck.com>
2009-03-18 18:03:37 +0100

    Added french description

--- __tryton__.py
+++ __tryton__.py
@@ -2,11 +2,19 @@
 #this repository contains the full copyright notices and license terms.
 {
     "name" : "Account Invoice Line Standalone",
+    "name_fr_FR" : "Ligne de facture autonome",
     "version" : "1.1.0",
     "author" : "B2CK",
     'email': 'info@b2ck.com',
     'website': 'http://www.tryton.org/',
-    "description": "Allow to create standalone invoice line and add it later to an invoice.",
+    "description": '''Allow to create standalone invoice lines and add them later to a draft
+invoice. The invoice will only accept invoice lines with the same
+type, company, currency and party.
+''',
+    "description_fr_FR": '''Permet de créer des lignes de facture autonomes et de les ajouter
+ensuite à des factures dans l'état brouillon. La facture n'acceptera
+que des lignes qui ont les même type, compagnie, devis et tiers.
+''',
     "depends" : [
         "ir",
         "account_invoice",
