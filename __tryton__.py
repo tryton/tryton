fbdf70fd969c0b2acaab01ae9fed3d3f46138f5c
Bertrand Chenal <bch@b2ck.com>
2009-03-18 18:06:04 +0100

    Added french description

--- __tryton__.py
+++ __tryton__.py
@@ -2,12 +2,19 @@
 #this repository contains the full copyright notices and license terms.
 {
     'name': 'Stock Inventory for many locations',
+    'name_fr_FR': 'Inventaire de stock par liste de locations',
     'version': '0.0.1',
     'author': 'B2CK',
     'email': 'info@b2ck.com',
     'website': 'http://www.tryton.org/',
-    'description': '''Stock Inventory for many locations
+    'description': '''Add a wizard that allows to create automatically inventories for a
+given list of locations. It also allows to filter by product and
+categories of product.
     ''',
+    'description_fr_FR': '''Ajoute un wizard qui permet de créer automatiquement des inventaires
+pour une liste donnée d'emplacements. Il permet aussi de filtrer par
+produit et par catégorie de produit.
+''',
     'depends': [
         'ir',
         'stock',
