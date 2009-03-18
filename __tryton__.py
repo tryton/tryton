496f59bfd6b23c7885dc9169e4709fb2ab464a46
Bertrand Chenal <bch@b2ck.com>
2009-03-18 19:37:25 +0100

    Added french description

--- __tryton__.py
+++ __tryton__.py
@@ -2,11 +2,20 @@
 #this repository contains the full copyright notices and license terms.
 {
     'name': 'Product Cost History',
+    'name_fr_FR': 'Historique prix de revient produit',
     'version': '1.1.0',
     'author': 'B2CK',
     'email': 'info@b2ck.com',
     'website': 'http://www.tryton.org/',
-    'description': 'Historize the product cost',
+    'description': '''Historize the product cost.
+This module add a button on the product form which open the list of
+all the past value of the cost price of the current product.
+''',
+    'description_fr_FR': '''Historique du prix de revient du produit.
+Ce module ajoute un bouton sur la fiche produit qui ouvre la liste de
+toutes les valeurs passées du prix de revient du produit.
+
+''',
     'depends': [
         'ir',
         'product',
