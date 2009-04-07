d3728af7d9e7375da98ec24907ef1084c53fb10f
Cédric Krier <ced@b2ck.com>
2009-04-07 19:33:56 +0200

    Fix typo for issue946

--- purchase.py
+++ purchase.py
@@ -573,7 +573,7 @@ class Purchase(ModelWorkflow, ModelSQL, ModelView):
     def search_rec_name(self, cursor, user, name, args, context=None):
         args2 = []
         i = 0
-        while i < lend(args):
+        while i < len(args):
             names = args[i][2].split(' - ', 1)
             ids = self.search(cursor, user, ['OR',
                 ('reference', args[i][1], names[0]),
