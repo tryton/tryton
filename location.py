4b6caba2a196e422cb704010a4e5ba772d921d1e
Cédric Krier <ced@b2ck.com>
2009-04-08 13:07:29 +0200

    Remove unused code

--- location.py
+++ location.py
@@ -285,7 +285,6 @@ class OpenProduct(Wizard):
     def _action_open_product(self, cursor, user, data, context=None):
         model_data_obj = self.pool.get('ir.model.data')
         act_window_obj = self.pool.get('ir.action.act_window')
-        product_obj = self.pool.get('product.product')
 
         model_data_ids = model_data_obj.search(cursor, user, [
             ('fs_id', '=', 'act_product_by_location'),
