896837b655da69cb112dfaf8ec22e4062f5dd596
Carlos Perelló Marín <carlos@pemas.es>
2009-04-15 09:34:41 +0200

    Split the Spanish translation between es_CO and es_ES

--- __tryton__.py
+++ __tryton__.py
@@ -3,17 +3,19 @@
 #this repository contains the full copyright notices and license terms.
 {
     'name' : 'Party',
-    'name_fr_FR': 'Tiers',
     'name_de_DE': 'Parteien',
+    'name_es_CO': 'Terceros',
     'name_es_ES': 'Terceros',
+    'name_fr_FR': 'Tiers',
     'version' : '1.1.0',
     'author' : 'B2CK',
     'email': 'info@b2ck.com',
     'website': 'http://www.tryton.org/',
     'description': 'Define parties, addresses and co.',
-    'description_fr_FR': 'Définit des tiers, des adresses, etc.',
     'description_de_DE': 'Ermöglicht die Erstellung von Parteien, Adressen, etc.',
+    'description_es_CO': 'Definición de terceros, direcciones, etc.',
     'description_es_ES': 'Definición de terceros, direcciones, etc.',
+    'description_fr_FR': 'Définit des tiers, des adresses, etc.',
     'depends' : [
         'ir',
         'res',
@@ -26,8 +28,9 @@
         'contact_mechanism.xml',
     ],
     'translation': [
-        'fr_FR.csv',
         'de_DE.csv',
+        'es_CO.csv',
         'es_ES.csv',
+        'fr_FR.csv',
     ],
 }
