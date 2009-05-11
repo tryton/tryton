80cfc0eaf1e192a11b22a306d81833fd4ca49baa
Cédric Krier <ced@b2ck.com>
2009-05-11 19:23:07 +0200

    Increase version number

--- __tryton__.py
+++ __tryton__.py
@@ -5,7 +5,7 @@
     'name_de_DE': 'Lagerverwaltung',
     'name_fr_FR': 'Gestion des stocks',
     'name_es_ES': 'Inventarios',
-    'version': '1.0.4',
+    'version': '1.0.5',
     'author': 'B2CK',
     'email': 'info@b2ck.com',
     'website': 'http://www.tryton.org/',
