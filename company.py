246d41c00a2f8e997e3faf4cd065e73d2422542b
Cédric Krier <ced@b2ck.com>
2009-05-21 18:55:34 +0200

    Add default value

--- company.py
+++ company.py
@@ -13,4 +13,16 @@ class Company(ModelSQL, ModelView):
     hours_per_work_month = fields.Float("Hours per Work Month", required=True)
     hours_per_work_year = fields.Float("Hours per Work Year", required=True)
 
+    def default_hours_per_work_day(self, cursor, user, context=None):
+        return 8
+
+    def default_hours_per_work_week(self, cursor, user, context=None):
+        return 40
+
+    def default_hours_per_work_month(self, cursor, user, context=None):
+        return 160
+
+    def default_hours_per_work_year(self, cursor, user, context=None):
+        return 4800
+
 Company()
