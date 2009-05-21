9e8f4df55f52d0c1c1e234990ec8bcf778a31ade
Bertrand Chenal <bch@b2ck.com>
2009-05-21 15:18:17 +0200

    Initial commit

---
+++ __tryton__.py
@@ -0,0 +1,20 @@
+#This file is part of Tryton.  The COPYRIGHT file at the top level of
+#this repository contains the full copyright notices and license terms.
+{
+    'name': 'Company Work Time',
+    'version': '0.0.1',
+    'author': 'B2CK',
+    'email': 'info@b2ck.com',
+    'website': 'http://www.tryton.org/',
+    'description': '''
+''',
+    'depends': [
+        'ir',
+        'company',
+    ],
+    'xml': [
+        'company.xml',
+    ],
+    'translation': [
+    ],
+}
---
+++ company.py
@@ -0,0 +1,16 @@
+#This file is part of Tryton.  The COPYRIGHT file at the top level of
+#this repository contains the full copyright notices and license terms.
+"Company"
+import copy
+from trytond.model import ModelView, ModelSQL, fields
+
+
+class Company(ModelSQL, ModelView):
+    _name = 'company.company'
+
+    hours_per_work_day = fields.Float("Hours per Work Day", required=True)
+    hours_per_work_week = fields.Float("Hours per Work Week", required=True)
+    hours_per_work_month = fields.Float("Hours per Work Month", required=True)
+    hours_per_work_year = fields.Float("Hours per Work Year", required=True)
+
+Company()
