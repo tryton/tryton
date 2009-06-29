a7fa982e286d50a3c5e48ebdb39861cd056ffc69
Cédric Krier <ced@b2ck.com>
2009-06-29 09:26:05 +0200

    Add test views

---
+++ tests/test_analytic_sale.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+#This file is part of Tryton.  The COPYRIGHT file at the top level of
+#this repository contains the full copyright notices and license terms.
+
+import sys, os
+DIR = os.path.abspath(os.path.normpath(os.path.join(__file__,
+    '..', '..', '..', '..', '..', 'trytond')))
+if os.path.isdir(DIR):
+    sys.path.insert(0, os.path.dirname(DIR))
+
+import unittest
+import trytond.tests.test_tryton
+from trytond.tests.test_tryton import RPCProxy, CONTEXT, SOCK, test_view
+
+
+class AnalyticSaleTestCase(unittest.TestCase):
+    '''
+    Test AnalyticSale module.
+    '''
+
+    def setUp(self):
+        trytond.tests.test_tryton.install_module('analytic_sale')
+
+    def test0005views(self):
+        '''
+        Test views.
+        '''
+        self.assertRaises(Exception, test_view('analytic_sale'))
+
+def suite():
+    return unittest.TestLoader().loadTestsFromTestCase(AnalyticSaleTestCase)
+
+if __name__ == '__main__':
+    suiteTrytond = trytond.tests.test_tryton.suite()
+    suiteAnalyticSale = suite()
+    alltests = unittest.TestSuite([suiteTrytond, suiteAnalyticSale])
+    unittest.TextTestRunner(verbosity=2).run(alltests)
+    SOCK.disconnect()
