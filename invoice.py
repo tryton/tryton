fd7c3e30ced1e15d528071bc5b38123da58281f2
Cédric Krier <ced@b2ck.com>
2009-07-14 16:47:53 +0200

    Use absolute value of amount second currency to compute the amounts to pay for issue1107

--- invoice.py
+++ invoice.py
@@ -485,9 +485,9 @@ class Invoice(ModelWorkflow, ModelSQL, ModelView):
                     continue
                 if line.second_currency.id == invoice.currency.id:
                     if line.debit - line.credit > Decimal('0.0'):
-                        amount_currency += line.amount_second_currency
+                        amount_currency += abs(line.amount_second_currency)
                     else:
-                        amount_currency -= line.amount_second_currency
+                        amount_currency -= abs(line.amount_second_currency)
                 else:
                     amount += line.debit - line.credit
             for line in invoice.payment_lines:
@@ -495,9 +495,9 @@ class Invoice(ModelWorkflow, ModelSQL, ModelView):
                     continue
                 if line.second_currency.id == invoice.currency.id:
                     if line.debit - line.credit > Decimal('0.0'):
-                        amount_currency += line.amount_second_currency
+                        amount_currency += abs(line.amount_second_currency)
                     else:
-                        amount_currency -= line.amount_second_currency
+                        amount_currency -= abs(line.amount_second_currency)
                 else:
                     amount += line.debit - line.credit
             if invoice.type in ('in_invoice', 'out_credit_note'):
