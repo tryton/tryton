743ada0b4c9e0d74ddab43c0c75128fdfe8fce90
Mathias Behrle <mathiasb@behrle.dyndns.org>
2009-07-18 13:35:31 +0200

    Guidelines
    Remove empty lines

--- __tryton__.py
+++ __tryton__.py
@@ -7,7 +7,6 @@
     'author': 'B2CK',
     'email': 'info@b2ck.com',
     'website': 'http://www.tryton.org/',
-
     'description': '''Provide the "Forecast" model in Inventory Management.
 The Forecast form allow to define the expected stock movement towards
 customers in any period of time in the future. A wizard allow to
@@ -16,7 +15,6 @@ past. Once the form confirmed, the corresponding moves are created and
 spread homogeneously across the period. Those moves will allow other
 process to take forecasts into account.
 ''',
-
     'description_fr_FR':'''Fournit le modèle "Prévision" dans la gestion des stocks.
 Le formulaire de prévision permet de définir les mouvements attendus
 vers les clients pour n'importe quelle période dans le futur. Un
@@ -26,7 +24,6 @@ correspondants sont créés et répartis sur la période donnée. Ces
 mouvement permettent aux autres processus de prendre en compte les
 prévisions.
 ''',
-
     'depends': [
         'ir',
         'res',
