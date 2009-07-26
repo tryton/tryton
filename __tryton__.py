0f39fec997d9c612926ceb5c42f243203277ab7f
Igor Támara <igor@tamarapatino.org>
2009-07-26 02:31:52 -0500

    Updated es_CO translation

--- __tryton__.py
+++ __tryton__.py
@@ -4,6 +4,7 @@
     'name': 'Stock Location Sequence',
     'name_de_DE': 'Lagerverwaltung Lagerortsequenz',
     'name_fr_FR': 'Séquence emplacement',
+    'name_es_CO': 'Secuencia de Sitio del Stock',
     'version': '1.3.0',
     'author': 'B2CK',
     'email': 'info@b2ck.com',
@@ -13,6 +14,8 @@
     'description_de_DE': '''Fügt dem Objekt Lagerort eine Sequenz hinzu
 ''',
     'description_fr_FR': '''Ajoute une séquence sur le modèle emplacement
+''',
+    'description_es_CO': '''Añade una secuencia e sitio a un objeto
 ''',
     'depends': [
         'ir',
@@ -24,5 +27,6 @@
     'translation': [
         'de_DE.csv',
         'fr_FR.csv',
+        'es_CO.csv',
     ],
 }
