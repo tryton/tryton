fb50d29b4734ee8851117903636207e3db0ecb6c
Igor Támara <igor@tamarapatino.org>
2009-07-26 02:31:51 -0500

    Updated es_CO translation

--- __tryton__.py
+++ __tryton__.py
@@ -4,6 +4,7 @@
     'name' : 'Google Maps',
     'name_de_DE': 'Google Maps',
     'name_fr_FR': 'Cartes Google',
+    'name_es_CO': 'Mapas de Google',
     'version' : '1.3.0',
     'author' : 'B2CK',
     'email': 'info@b2ck.com',
