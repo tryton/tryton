3484291bc3f7bd08ea5b34b92e1a16f3648b6eaf
Cédric Krier <ced@b2ck.com>
2009-07-30 16:40:18 +0200

    Increase version number

--- __tryton__.py
+++ __tryton__.py
@@ -6,7 +6,7 @@
     'name_de_DE': 'Artikel Kostenpreis Historisierung',
     'name_es_ES': 'Histórico del precio de coste',
     'name_es_CO': 'Histórico del costo para productos',
-    'version': '1.2.0',
+    'version': '1.3.0',
     'author': 'B2CK',
     'email': 'info@b2ck.com',
     'website': 'http://www.tryton.org/',
