ebe5e8eaf62516b311d7b30d866dda662c202765
Mathias Behrle <mathiasb@behrle.dyndns.org>
2009-08-25 20:01:56 +0200

    Add translation for de_DE

--- __tryton__.py
+++ __tryton__.py
@@ -2,6 +2,7 @@
 #this repository contains the full copyright notices and license terms.
 {
     "name" : "Account Invoice Line Standalone",
+    "name_de_DE" : "Rechnungsstellung ungebundene Rechnungsposition",
     "name_fr_FR" : "Ligne de facture autonome",
     "version" : "1.1.0",
     "author" : "B2CK",
@@ -10,6 +11,12 @@
     "description": '''Allow to create standalone invoice lines and add them later to a draft
 invoice. The invoice will only accept invoice lines with the same
 type, company, currency and party.
+''',
+    "description_de_DE": '''Rechnungsstellung aus ungebundenen Rechnungspositionen
+    - Ermöglicht die Erstellung ungebundener Rechnungspositionen, die später zu
+      Rechnungen mit Status Entwurf hinzugefügt werden können.
+    - Innerhalb einer Rechnung können nur Rechnungspositionen mit
+      übereinstimmendem Typ, Unternehmen, Währung und Partei verwendet werden.
 ''',
     "description_fr_FR": '''Permet de créer des lignes de facture autonomes et de les ajouter
 ensuite à des factures dans l'état brouillon. La facture n'acceptera
@@ -23,6 +30,7 @@ que des lignes qui ont les même type, compagnie, devis et tiers.
         "invoice.xml",
     ],
     'translation': [
+        'de_DE.csv',
         'fr_FR.csv',
     ],
 }
