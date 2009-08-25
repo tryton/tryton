0bdcb5b2f207d8d858670fe7846dd86cbc29bc7c
Mathias Behrle <mathiasb@behrle.dyndns.org>
2009-08-25 19:28:26 +0200

    Add translation for de_DE

--- __tryton__.py
+++ __tryton__.py
@@ -2,6 +2,7 @@
 #this repository contains the full copyright notices and license terms.
 {
     "name" : "Purchase Invoice Line Standalone",
+    "name_de_DE" : "Einkauf ungebundene Rechnungsposition",
     "name_fr_FR" : "Ligne de facture autonome - Achat",
     "version" : "0.0.1",
     "author" : "B2CK",
@@ -11,6 +12,12 @@
 Change the purchase order behaviour to create standalone invoice lines instead of a
 complete invoice. This allow to compose invoices with lines originating
 from differents purchases.
+''',
+    "description_de_DE": '''Einkäufe mit ungebundenen Rechnungspositionen
+    - Ändert das Verhalten im Einkaufsvorgang, indem unabhängige Rechnungspositionen
+      an Stelle einer kompletten Rechnung erstellt werden.
+    - Ermöglicht die Zusammenstellung von Rechnungen aus Einkaufspositionen
+      verschiedener Rechnungen.
 ''',
     "description_fr_FR": '''Modifie le comportement de l'ordre d'achat pour créer des lignes de
 facture autonomes au lieu de factures complètes. Cela permet de
@@ -25,6 +32,7 @@ composer des factures avec des lignes provenant de différents achats.
         "purchase.xml",
     ],
     'translation': [
+        'de_DE.csv',
         'fr_FR.csv',
     ],
 }
