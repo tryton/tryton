c1af92b7127160145c2611f25621e4724792f51e
Cédric Krier <ced@b2ck.com>
2009-09-08 18:39:51 +0200

    Prepare release 1.2.0

--- __tryton__.py
+++ __tryton__.py
@@ -6,7 +6,7 @@
     "name_fr_FR" : "Ligne de facture autonome",
     "name_es_CO" : "Línea de factura autónoma",
     "name_es_ES" : "Línea de factura independiente",
-    "version" : "1.1.0",
+    "version" : "1.2.0",
     "author" : "B2CK",
     'email': 'info@b2ck.com',
     'website': 'http://www.tryton.org/',
