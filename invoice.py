585437fcf0b7ad2e735d27e489f6d96fe9c994b0
Korbinian Preisler <timitos@virtual-things.biz>
2009-09-08 21:52:19 +0200

    use given date for computation of amount with differing currency instead of currenct date

--- invoice.py
+++ invoice.py
@@ -2314,7 +2314,7 @@ class PayInvoice(Wizard):
         ctx['date'] = data['form']['date']
         amount = currency_obj.compute(cursor, user, data['form']['currency'],
                 data['form']['amount'], invoice.company.currency,
-                context=context)
+                context=ctx)
         res = invoice_obj.get_reconcile_lines_for_amount(cursor, user, invoice,
                 amount)
         if res[1] == Decimal('0.0') and amount <= invoice.amount_to_pay:
@@ -2386,7 +2386,7 @@ class PayInvoice(Wizard):
         ctx['date'] = data['form']['date']
         amount = currency_obj.compute(cursor, user, data['form']['currency'],
                 data['form']['amount'], invoice.company.currency,
-                context=context)
+                context=ctx)
 
         reconcile_lines = invoice_obj.get_reconcile_lines_for_amount(cursor,
                 user, invoice, amount)
