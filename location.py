99c8d3053773a945aad5b71b98f5fa21576f21c3
Bertrand Chenal <bch@b2ck.com>
2009-09-30 08:11:27 +0200

    Rename packing into shipment

--- location.py
+++ location.py
@@ -27,17 +27,17 @@ class ProductLocation(ModelSQL, ModelView):
 ProductLocation()
 
 
-class PackingIn(ModelSQL, ModelView):
-    _name = 'stock.packing.in'
+class ShipmentIn(ModelSQL, ModelView):
+    _name = 'stock.shipment.in'
 
     def _get_inventory_moves(self, cursor, user, incoming_move, context=None):
-        res = super(PackingIn, self)._get_inventory_moves(cursor, user,
+        res = super(ShipmentIn, self)._get_inventory_moves(cursor, user,
                 incoming_move, context=context)
         for product_location in incoming_move.product.locations:
             if product_location.warehouse.id != \
-                    incoming_move.packing_in.warehouse.id:
+                    incoming_move.shipment_in.warehouse.id:
                 continue
             res['to_location'] = product_location.location.id
         return res
 
-PackingIn()
+ShipmentIn()
