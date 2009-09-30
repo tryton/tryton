b382b83860f0f9ee2614530498baa362417ae2b3
Mathias Behrle <mathiasb@behrle.dyndns.org>
2009-09-30 14:05:46 +0200

    Add translation for de_DE

--- __tryton__.py
+++ __tryton__.py
@@ -2,11 +2,15 @@
 #this repository contains the full copyright notices and license terms.
 {
     'name': 'Sale',
+    'name_de_DE': 'Verkauf Preislisten',
     'version': '0.0.1',
     'author': 'B2CK',
     'email': 'info@b2ck.com',
     'website': 'http://www.tryton.org/',
     'description': '''Define price list on party and sale order.
+''',
+    'description_de_DE': '''Preislisten für Verkauf
+    - Ermöglicht die Definition von Preislisten per Partei und Verkauf.
 ''',
     'depends': [
         'ir',
@@ -20,5 +24,6 @@
         'sale.xml',
     ],
     'translation': [
+        'de_DE.csv',
     ],
 }
