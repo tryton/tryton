ab8612c289c2c368780de900268e8af2adf19176
Cédric Krier <ced@b2ck.com>
2009-10-19 09:25:02 +0200

    Fix guidelines

--- __tryton__.py
+++ __tryton__.py
@@ -2,10 +2,10 @@
 #this repository contains the full copyright notices and license terms.
 {
     'name': 'Product Cost FIFO',
-    'name_fr_FR': 'Prix de revient produit FIFO',
     'name_de_DE': 'Artikel Kostenpreisermittlung FIFO',
-    'name_es_ES': 'Coste FIFO de producto',
     'name_es_CO': 'Costo FIFO de producto',
+    'name_es_ES': 'Coste FIFO de producto',
+    'name_fr_FR': 'Prix de revient produit FIFO',
     'version': '1.3.0',
     'author': 'B2CK',
     'email': 'info@b2ck.com',
@@ -15,11 +15,6 @@ Once installed, the price of the stock moves from a supplier or to a
 customer will update automatically the cost price of the related
 product (if the cost price method is FIFO)
 ''',
-    'description_fr_FR':'''Ajoute 'FIFO' parmi les méthodes de coût du produit.
-Une fois le module installé, le prix de chaque mouvement de stock
-depuis un fournisseur ou vers un client modifiera automatiquement le
-prix de revient du produit concerné (si la méthode de coût est FIFO).
-''',
     'description_de_DE':'''Kostenpreisermittlung für Artikel nach Methode 'FIFO'
     - Fügt 'FIFO' (First In - First Out) zu den Methoden für die
       Kostenpreisermittlung für Artikel hinzu.
@@ -27,15 +22,20 @@ prix de revient du produit concerné (si la méthode de coût est FIFO).
       den die Methode FIFO gewählt wurde) automatisch bei jeder Lagerbewegung
       von einem Lieferanten oder zu einem Kunden angepasst.
 ''',
+    'description_es_CO': '''Añade el método de costo FIFO al formulario de producto.
+Cuando está instalado, el precio de los movimientos de stock desde un proveedor
+o a un cliente actualizarán automáticamente el precio de costo del producto
+relacionado (Si método del costo de producto es FIFO)
+''',
     'description_es_ES': '''Añade el método de coste FIFO en el formulario de producto.
 Una vez instalado, el precio de los movimientos de stock desde un proveedor o
 a un cliente actualizará automáticamente el precio de coste del producto
 relacionado (si el método de cálculo del precio de coste es FIFO)
 ''',
-    'description_es_CO': '''Añade el método de costo FIFO al formulario de producto.
-Cuando está instalado, el precio de los movimientos de stock desde un proveedor
-o a un cliente actualizarán automáticamente el precio de costo del producto
-relacionado (Si método del costo de producto es FIFO)
+    'description_fr_FR':'''Ajoute 'FIFO' parmi les méthodes de coût du produit.
+Une fois le module installé, le prix de chaque mouvement de stock
+depuis un fournisseur ou vers un client modifiera automatiquement le
+prix de revient du produit concerné (si la méthode de coût est FIFO).
 ''',
     'depends': [
         'ir',
@@ -45,9 +45,9 @@ relacionado (Si método del costo de producto es FIFO)
     'xml': [
     ],
     'translation': [
-        'fr_FR.csv',
         'de_DE.csv',
-        'es_ES.csv',
         'es_CO.csv',
+        'es_ES.csv',
+        'fr_FR.csv',
     ],
 }
