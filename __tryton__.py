c16cb0ad79a7491a2d45aeee037687fb87506828
Cédric Krier <ced@b2ck.com>
2009-10-19 09:26:30 +0200

    Fix guidelines

--- __tryton__.py
+++ __tryton__.py
@@ -2,10 +2,10 @@
 #this repository contains the full copyright notices and license terms.
 {
     "name" : "Stock Product Location",
-    "name_fr_FR" : "Emplacement de produit",
     "name_de_DE" : "Lagerverwaltung Artikel Lagerort",
-    "name_es_ES" : "Ubicación de existencias de producto",
     "name_es_CO" : "Ubicación de existencias de producto",
+    "name_es_ES" : "Ubicación de existencias de producto",
+    "name_fr_FR" : "Emplacement de produit",
     "version" : "1.3.0",
     "author" : "B2CK",
     'email': 'info@b2ck.com',
@@ -14,22 +14,22 @@
 Theses locations will be used by the supplier shipment for generating
 inventory moves.
 ''',
-    "description_fr_FR": '''Defini un emplacement de rangement par défaut par produit.
-Ces emplacements seront utilisés par les colisages fournisseurs pour
-générer les mouvements internes.
-''',
     "description_de_DE": '''Standardlagerorte für Artikel
     - Ermöglicht die Definition von Standardlagerorten für Artikel in einem Warenlager
     - Diese Lagerorte werden von Lieferposten von Lieferanten für die
       Lagerbewegungen benutzt
 ''',
-    "description_es_ES": '''Define la ubicación del almacén predeterminado por depósito y producto.
-Estas ubicaciones se utilizarán en el envio de proveedor para generar movimientos de inventario.
-''',
     "description_es_CO": '''Define la ubicación de almacenamiento predeterminada
 por depósito y producto.
 Esta ubicación la utilizará el envío del proveedor para generar movimientos
 de inventaio.
+''',
+    "description_es_ES": '''Define la ubicación del almacén predeterminado por depósito y producto.
+Estas ubicaciones se utilizarán en el envio de proveedor para generar movimientos de inventario.
+''',
+    "description_fr_FR": '''Defini un emplacement de rangement par défaut par produit.
+Ces emplacements seront utilisés par les colisages fournisseurs pour
+générer les mouvements internes.
 ''',
     "depends" : [
         "ir",
@@ -41,9 +41,9 @@ de inventaio.
         "product.xml",
     ],
     'translation': [
-        'fr_FR.csv',
         'de_DE.csv',
-        'es_ES.csv',
         'es_CO.csv',
+        'es_ES.csv',
+        'fr_FR.csv',
     ],
 }
