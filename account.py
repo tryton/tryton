f13bceb1ce5667cf77674216e9a871c157fa819a
Cédric Krier <ced@b2ck.com>
2009-12-21 12:01:56 +0100

    Use root user for create/delete default properties in account chart creation (transplanted from 6a5adb9683c2c26293ad3f946b0a42d3f08780c2)

--- account.py
+++ account.py
@@ -1897,8 +1897,8 @@ class CreateChartAccount(Wizard):
             ('res', '=', False),
             ('company', '=', datas['form']['company']),
             ], context=context)
-        property_obj.delete(cursor, user, property_ids, context=context)
-        property_obj.create(cursor, user, {
+        property_obj.delete(cursor, 0, property_ids, context=context)
+        property_obj.create(cursor, 0, {
             'name': 'account_receivable',
             'field': account_receivable_field_id,
             'value': 'account.account,' + \
@@ -1915,8 +1915,8 @@ class CreateChartAccount(Wizard):
             ('res', '=', False),
             ('company', '=', datas['form']['company']),
             ], context=context)
-        property_obj.delete(cursor, user, property_ids, context=context)
-        property_obj.create(cursor, user, {
+        property_obj.delete(cursor, 0, property_ids, context=context)
+        property_obj.create(cursor, 0, {
             'name': 'account_payable',
             'field': account_payable_field_id,
             'value': 'account.account,' + \
