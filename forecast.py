3333f1db1de32d8db360809f1de689d46b264241
Cédric Krier <ced@b2ck.com>
2009-12-29 18:02:01 +0100

    Remove unused company_obj

--- forecast.py
+++ forecast.py
@@ -71,7 +71,6 @@ class Forecast(ModelWorkflow, ModelSQL, ModelView):
         return False
 
     def default_company(self, cursor, user, context=None):
-        company_obj = self.pool.get('company.company')
         if context is None:
             context = {}
         if context.get('company'):
