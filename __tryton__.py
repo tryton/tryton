748558a646309b97c892400f76ecb9f8f9baec8a
Mathias Behrle <mathiasb@mbsolutions.selfip.biz>
2009-12-31 18:43:17 +0100

    Add translation for de_DE.

--- __tryton__.py
+++ __tryton__.py
@@ -2,11 +2,15 @@
 #this repository contains the full copyright notices and license terms.
 {
     'name': 'Dashboard',
+    'name_de_DE': 'Dashboard',
     'version': '0.0.1',
     'author': 'B2CK',
     'email': 'info@b2ck.com',
     'website': 'http://www.tryton.org/',
     'description': '''Dashboard''',
+    'description_de_DE': '''Dashboard:
+    - Ermöglicht die Einrichtung einer personalisierten Startseite.
+''',
     'depends': [
         'ir',
         'res',
@@ -17,5 +21,6 @@
         'ir.xml',
     ],
     'translation': [
+        'de_DE.csv',
     ],
 }
