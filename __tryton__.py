d01dc2b866b357685d063a69e409a2bf0e0b0938
Mathias Behrle <mathiasb@mbsolutions.selfip.biz>
2010-01-24 19:14:48 +0100

    Add translation for de_DE

--- __tryton__.py
+++ __tryton__.py
@@ -3,12 +3,20 @@
 #this repository contains the full copyright notices and license terms.
 {
     'name' : 'Party - Siret',
+    'name_de_DE' : 'Parteien - SIRET',
     'name_fr_FR': 'Tiers - Siret',
     'version' : '0.0.1',
     'author' : 'B2CK',
     'email': 'info@b2ck.com',
     'website': 'http://www.tryton.org/',
     'description': 'Add SIRET/SIREN number on party',
+    'description_de_DE' : '''
+Fügt ein Feld für die SIRET/SIREN-Nummer bei Parteien hinzu.
+    Diese Nummern dienen in Frankreich der Identifikation und geographischen
+    Zuordnung von Unternehmen:
+    - SIREN (Système d’Identification du Répertoire des ENtreprises)
+    - SIRET (Système d’Identification du Répertoire des ETablissements)
+''',
     'description_fr_FR': 'Ajoute le numéro SIRET/SIREN sur les tiers',
     'depends' : [
         'ir',
@@ -19,6 +27,7 @@
         'address.xml',
     ],
     'translation': [
+        'de_DE.csv',
         'fr_FR.csv',
     ],
 }
