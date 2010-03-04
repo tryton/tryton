e537cf711d6b3ff035b2e236ef136504275eff9c
Cédric Krier <ced@b2ck.com>
2010-03-04 14:12:55 +0100

    New unittest design

--- tests/test_sale_price_list.py
+++ tests/test_sale_price_list.py
@@ -2,6 +2,9 @@
 #This file is part of Tryton.  The COPYRIGHT file at the top level of
 #this repository contains the full copyright notices and license terms.
 
+import logging
+logging.basicConfig(level=logging.FATAL)
+
 import sys, os
 DIR = os.path.abspath(os.path.normpath(os.path.join(__file__,
     '..', '..', '..', '..', '..', 'trytond')))
@@ -10,7 +13,7 @@ if os.path.isdir(DIR):
 
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import RPCProxy, CONTEXT, SOCK, test_view
+from trytond.tests.test_tryton import test_view
 
 
 class SalePriceListTestCase(unittest.TestCase):
@@ -28,11 +31,10 @@ class SalePriceListTestCase(unittest.TestCase):
         self.assertRaises(Exception, test_view('sale_price_list'))
 
 def suite():
-    return unittest.TestLoader().loadTestsFromTestCase(SalePriceListTestCase)
+    suite = trytond.tests.test_tryton.suite()
+    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
+        SalePriceListTestCase))
+    return suite
 
 if __name__ == '__main__':
-    suiteTrytond = trytond.tests.test_tryton.suite()
-    suiteSalePriceList = suite()
-    alltests = unittest.TestSuite([suiteTrytond, suiteSalePriceList])
-    unittest.TextTestRunner(verbosity=2).run(alltests)
-    SOCK.disconnect()
+    unittest.TextTestRunner(verbosity=2).run(suite())
