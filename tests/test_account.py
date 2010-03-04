3f0993e793b5b7abf71a325d2dbd65a3765ac7eb
Cédric Krier <ced@b2ck.com>
2010-03-04 14:03:54 +0100

    New unittest design

--- tests/test_account.py
+++ tests/test_account.py
@@ -2,6 +2,9 @@
 #This file is part of Tryton.  The COPYRIGHT file at the top level of
 #this repository contains the full copyright notices and license terms.
 
+import logging
+logging.basicConfig(level=logging.FATAL)
+
 import sys, os
 DIR = os.path.abspath(os.path.normpath(os.path.join(__file__,
     '..', '..', '..', '..', '..', 'trytond')))
@@ -10,8 +13,7 @@ if os.path.isdir(DIR):
 
 import unittest
 import trytond.tests.test_tryton
-import trytond.modules.company.tests
-from trytond.tests.test_tryton import RPCProxy, CONTEXT, SOCK, test_view
+from trytond.tests.test_tryton import test_view
 
 
 class AccountTestCase(unittest.TestCase):
@@ -29,12 +31,10 @@ class AccountTestCase(unittest.TestCase):
         self.assertRaises(Exception, test_view('account'))
 
 def suite():
-    return unittest.TestLoader().loadTestsFromTestCase(AccountTestCase)
+    suite = trytond.tests.test_tryton.suite()
+    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
+        AccountTestCase))
+    return suite
 
 if __name__ == '__main__':
-    suiteTrytond = trytond.tests.test_tryton.suite()
-    suiteCompany = trytond.modules.company.tests.suite()
-    suiteAccount = suite()
-    alltests = unittest.TestSuite([suiteTrytond, suiteCompany, suiteAccount])
-    unittest.TextTestRunner(verbosity=2).run(alltests)
-    SOCK.disconnect()
+    unittest.TextTestRunner(verbosity=2).run(suite())
