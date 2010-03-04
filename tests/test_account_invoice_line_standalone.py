ba5a5810049593331583982c18c0e346fec9c905
Cédric Krier <ced@b2ck.com>
2010-03-04 14:04:29 +0100

    New unittest design

--- tests/test_account_invoice_line_standalone.py
+++ tests/test_account_invoice_line_standalone.py
@@ -2,6 +2,9 @@
 #This file is part of Tryton.  The COPYRIGHT file at the top level of
 #this repository contains the full copyright notices and license terms.
 
+import logging
+logging.basicConfig(level=logging.FATAL)
+
 import sys, os
 DIR = os.path.abspath(os.path.normpath(os.path.join(__file__,
     '..', '..', '..', '..', '..', 'trytond')))
@@ -10,7 +13,7 @@ if os.path.isdir(DIR):
 
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import RPCProxy, CONTEXT, SOCK, test_view
+from trytond.tests.test_tryton import test_view
 
 
 class AccountInvoiceLineStandaloneTestCase(unittest.TestCase):
@@ -30,13 +33,10 @@ class AccountInvoiceLineStandaloneTestCase(unittest.TestCase):
             'account_invoice_line_standalone'))
 
 def suite():
-    return unittest.TestLoader().loadTestsFromTestCase(
-            AccountInvoiceLineStandaloneTestCase)
+    suite = trytond.tests.test_tryton.suite()
+    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
+            AccountInvoiceLineStandaloneTestCase))
+    return suite
 
 if __name__ == '__main__':
-    suiteTrytond = trytond.tests.test_tryton.suite()
-    suiteAccountInvoiceLineStandalone = suite()
-    alltests = unittest.TestSuite([suiteTrytond,
-        suiteAccountInvoiceLineStandalone])
-    unittest.TextTestRunner(verbosity=2).run(alltests)
-    SOCK.disconnect()
+    unittest.TextTestRunner(verbosity=2).run(suite())
