bbf3595c9a7f882f98f4ebbb1844cb59390a5944
Cédric Krier <ced@b2ck.com>
2010-03-06 13:35:01 +0100

    Add fr_FR translation

--- __tryton__.py
+++ __tryton__.py
@@ -4,16 +4,22 @@
     'name': 'Dashboard',
     'name_de_DE': 'Dashboard',
     'name_es_ES': 'Escritorio',
+    'name_fr_FR': 'Tableau de bord',
     'version': '0.0.1',
     'author': 'B2CK',
     'email': 'info@b2ck.com',
     'website': 'http://www.tryton.org/',
-    'description': '''Dashboard''',
+    'description': '''Dashboard:
+    - Allow to create a personalized dashboard.
+''',
     'description_de_DE': '''Dashboard:
     - Ermöglicht die Einrichtung einer personalisierten Startseite.
 ''',
     'description_es_ES': '''Escritorio:
     - Permite la creación de una página de inicio personalizada.
+''',
+    'description_fr_FR': '''Tableau de bord
+    - Permet de créer un tableau de bord personalisé.
 ''',
     'depends': [
         'ir',
@@ -27,5 +33,6 @@
     'translation': [
         'de_DE.csv',
         'es_ES.csv',
+        'fr_FR.csv',
     ],
 }
