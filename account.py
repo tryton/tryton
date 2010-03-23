244a8afe48c487926e46864ea1b3a6ce89b99aeb
Cédric Krier <ced@b2ck.com>
2010-03-23 11:25:42 +0100

    searcher on Function fields take only one domain clause as argument

--- account.py
+++ account.py
@@ -250,18 +250,13 @@ class Account(ModelSQL, ModelView):
                 res[account.id] = unicode(account.name)
         return res
 
-    def search_rec_name(self, cursor, user, name, args, context=None):
-        args2 = []
-        i = 0
-        while i < len(args):
-            ids = self.search(cursor, user, [('code', args[i][1], args[i][2])],
-                    limit=1, context=context)
-            if ids:
-                args2.append(('code', args[i][1], args[i][2]))
-            else:
-                args2.append((self._rec_name, args[i][1], args[i][2]))
-            i += 1
-        return args2
+    def search_rec_name(self, cursor, user, name, clause, context=None):
+        ids = self.search(cursor, user, [('code',) + clause[1:]], limit=1,
+                context=context)
+        if ids:
+            return [('code',) + clause[1:]]
+        else:
+            return [(self._rec_name,) + clause[1:]]
 
     def convert_view(self, cursor, user, tree, context=None):
         res = tree.xpath('//field[@name=\'analytic_accounts\']')
