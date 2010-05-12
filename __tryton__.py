529854616a520c798a365a870aa08542cd6daf43
Cédric Krier <ced@b2ck.com>
2010-05-12 22:51:35 +0200

    Prepare release 1.6.0

--- __tryton__.py
+++ __tryton__.py
@@ -3,7 +3,7 @@
 {
     'name': 'German Chart of Accounts SKR03',
     'name_de_DE': 'Deutscher Kontenrahmen SKR03',
-    'version': '1.5.0',
+    'version': '1.6.0',
     'author': 'virtual things',
     'email': 'info@virtual-things.biz',
     'website': 'http://www.virtual-things.biz/',
