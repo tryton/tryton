7069b7872c95461c40704c841d3691fe293b65fb
Cédric Krier <ced@b2ck.com>
2010-08-20 07:56:54 +0200

    Add missing with_statement import

--- fiscalyear.py
+++ fiscalyear.py
@@ -1,5 +1,6 @@
 #This file is part of Tryton.  The COPYRIGHT file at the top level of
 #this repository contains the full copyright notices and license terms.
+from __future__ import with_statement
 import datetime
 import time
 from dateutil.relativedelta import relativedelta
