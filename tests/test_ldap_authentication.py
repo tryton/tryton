48e1b3a3b8075484686d97bc8de2f7244f1a0e85
Cédric Krier <ced@b2ck.com>
2010-08-27 12:18:12 +0200

    Fix test_view for issue1651

--- tests/test_ldap_authentication.py
+++ tests/test_ldap_authentication.py
@@ -25,7 +25,7 @@ class LDAPAuthenticationTestCase(unittest.TestCase):
         '''
         Test views.
         '''
-        self.assertRaises(Exception, test_view('ldap_authentication'))
+        test_view('ldap_authentication')
 
 def suite():
     suite = trytond.tests.test_tryton.suite()
