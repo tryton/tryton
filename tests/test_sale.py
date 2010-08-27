8e7777ccf264c7acfd052d667183aaa4a723dd08
Cédric Krier <ced@b2ck.com>
2010-08-27 12:18:25 +0200

    Fix test_view for issue1651

--- tests/test_sale.py
+++ tests/test_sale.py
@@ -25,7 +25,7 @@ class SaleTestCase(unittest.TestCase):
         '''
         Test views.
         '''
-        self.assertRaises(Exception, test_view('sale'))
+        test_view('sale')
 
 def suite():
     suite = trytond.tests.test_tryton.suite()
