fee8c495a4cb00961a882c48586b4c1ebea599c0
Cédric Krier <ced@b2ck.com>
2010-08-27 12:18:07 +0200

    Fix test_view for issue1651

--- tests/test_company_work_time.py
+++ tests/test_company_work_time.py
@@ -25,7 +25,7 @@ class CompanyWorkTimeTestCase(unittest.TestCase):
         '''
         Test views.
         '''
-        self.assertRaises(Exception, test_view('company_work_time'))
+        test_view('company_work_time')
 
 def suite():
     suite = trytond.tests.test_tryton.suite()
