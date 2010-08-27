4292c9523e18d9b68b4b232699833971b265336a
Cédric Krier <ced@b2ck.com>
2010-08-27 12:18:00 +0200

    Fix test_view for issue1651

--- tests/test_analytic_invoice.py
+++ tests/test_analytic_invoice.py
@@ -25,7 +25,7 @@ class AnalyticInvoiceTestCase(unittest.TestCase):
         '''
         Test views.
         '''
-        self.assertRaises(Exception, test_view('analytic_invoice'))
+        test_view('analytic_invoice')
 
 def suite():
     suite = trytond.tests.test_tryton.suite()
