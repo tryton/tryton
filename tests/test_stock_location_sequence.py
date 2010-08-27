0fe7f74268d9c30b82b5060190779934f52c0e1f
Cédric Krier <ced@b2ck.com>
2010-08-27 12:18:29 +0200

    Fix test_view for issue1651

--- tests/test_stock_location_sequence.py
+++ tests/test_stock_location_sequence.py
@@ -25,7 +25,7 @@ class StockLocationSequenceTestCase(unittest.TestCase):
         '''
         Test views.
         '''
-        self.assertRaises(Exception, test_view('stock_location_sequence'))
+        test_view('stock_location_sequence')
 
 def suite():
     suite = trytond.tests.test_tryton.suite()
