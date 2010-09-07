a26a7f01b415edb96a27a8943410844d39925354
Cédric Krier <ced@b2ck.com>
2010-09-07 19:55:51 +0200

    Fix test_view for issue1651

--- tests/test_sale_opportunity.py
+++ tests/test_sale_opportunity.py
@@ -25,7 +25,7 @@ class SaleOpportunityTestCase(unittest.TestCase):
         '''
         Test views.
         '''
-        self.assertRaises(Exception, test_view('sale_opportunity'))
+        test_view('sale_opportunity')
 
 def suite():
     suite = trytond.tests.test_tryton.suite()
