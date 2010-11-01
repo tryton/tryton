a41bb919d0c426b2d6d9d6ab472c3c2a0e2e5f82
Cédric Krier <ced@b2ck.com>
2010-11-01 20:57:11 +0100

    Prepare release 1.8.0

--- __tryton__.py
+++ __tryton__.py
@@ -6,7 +6,7 @@
     'name_es_CO': 'Compras',
     'name_es_ES': 'Compras',
     'name_fr_FR': 'Achat',
-    'version': '1.7.0',
+    'version': '1.8.0',
     'author': 'B2CK',
     'email': 'info@b2ck.com',
     'website': 'http://www.tryton.org/',
