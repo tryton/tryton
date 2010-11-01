ffbd0f069daf2827e8c04e0d545358597a3d235b
Cédric Krier <ced@b2ck.com>
2010-11-01 20:25:45 +0100

    Prepare release 1.8.0

--- __tryton__.py
+++ __tryton__.py
@@ -7,7 +7,7 @@
     'name_es_CO' : 'Terceros - SIRET',
     'name_es_ES': 'Terceros - SIRET',
     'name_fr_FR': 'Tiers - Siret',
-    'version' : '1.7.0',
+    'version' : '1.8.0',
     'author' : 'B2CK',
     'email': 'info@b2ck.com',
     'website': 'http://www.tryton.org/',
