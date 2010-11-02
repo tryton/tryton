bf120c3e69bfbcab63157d3e7b765522a90eaaa8
Cédric Krier <ced@b2ck.com>
2010-11-02 13:42:44 +0100

    Increase version number

--- __tryton__.py
+++ __tryton__.py
@@ -6,7 +6,7 @@
     'name_es_CO': 'Contabilidad de Inventarios',
     'name_es_ES': 'Contabilidad de productos',
     'name_fr_FR': 'Compte produit',
-    'version': '1.8.0',
+    'version': '1.8.1',
     'author': 'B2CK',
     'email': 'info@b2ck.com',
     'website': 'http://www.tryton.org/',
