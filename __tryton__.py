189f6fbeab90a327889af31d2a60c045ab0c910b
Mathias Behrle <mbehrle@m9s.biz>
2010-11-02 21:11:57 +0100

    Increase version number

--- __tryton__.py
+++ __tryton__.py
@@ -6,7 +6,7 @@
     'name_es_CO': 'Ventas',
     'name_es_ES': 'Ventas',
     'name_fr_FR': 'Vente',
-    'version': '1.2.4',
+    'version': '1.2.5',
     'author': 'B2CK',
     'email': 'info@b2ck.com',
     'website': 'http://www.tryton.org/',
