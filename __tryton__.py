7b5a8655c2b7460efe49df612576d485b1465bbb
Mathias Behrle <mbehrle@m9s.biz>
2010-11-02 21:11:40 +0100

    Increase version number

--- __tryton__.py
+++ __tryton__.py
@@ -3,7 +3,7 @@
 {
     'name': 'German Chart of Accounts SKR03',
     'name_de_DE': 'Deutscher Kontenrahmen SKR03',
-    'version': '1.2.1',
+    'version': '1.2.2',
     'author': 'virtual things',
     'email': 'info@virtual-things.biz',
     'website': 'http://www.virtual-things.biz/',
