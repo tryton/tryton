3928b7ed6c5fa0944cba62c36f7f13b2de21ee45
Mathias Behrle <mbehrle@m9s.biz>
2010-11-03 15:43:45 +0100

    Increase version number

--- __tryton__.py
+++ __tryton__.py
@@ -6,7 +6,7 @@
     "name_es_CO" : "Ubicación de existencias de producto",
     "name_es_ES" : "Ubicación de existencias de producto",
     "name_fr_FR" : "Emplacement de produit",
-    "version" : "1.4.1",
+    "version": "1.4.2",
     "author" : "B2CK",
     'email': 'info@b2ck.com',
     'website': 'http://www.tryton.org/',
