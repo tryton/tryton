cdc64918b0d64797e4bcf4fe22599afe3b30acec
Cédric Krier <ced@b2ck.com>
2010-12-12 16:51:12 +0100

    Fix default fifo_quantity naming (transplanted from 59fe15af5e1c4da1b482626b3305cfa962f20cac)

--- move.py
+++ move.py
@@ -22,7 +22,7 @@ class Move(ModelSQL, ModelView):
                     'for FIFO cost price!',
             })
 
-    def default_fifo_quantity_out(self):
+    def default_fifo_quantity(self):
         return 0.0
 
     def _update_fifo_out_product_cost_price(self, product, quantity, uom):
