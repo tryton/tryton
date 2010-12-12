497ee15b0a7d0f7f347947da193e59a249df590d
Nickolay Kolev <nickyk@gmx.net>
2010-12-12 20:38:24 +0200

    Add Bulgarian translation

--- __tryton__.py
+++ __tryton__.py
@@ -2,6 +2,7 @@
 #this repository contains the full copyright notices and license terms.
 {
     'name': 'Company Work Time',
+    'name_bg_BG': 'Работно време на фирма',
     'name_de_DE': 'Unternehmen Arbeitszeit',
     'name_es_CO': 'Tiempo de Trabajo en la Compañía',
     'name_es_ES': 'Tiempo de trabajo en la empresa',
@@ -20,6 +21,7 @@
         'company.xml',
     ],
     'translation': [
+        'bg_BG.csv',
         'de_DE.csv',
         'es_CO.csv',
         'es_ES.csv',
