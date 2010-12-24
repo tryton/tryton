3d2974987a09fd9f000f2e0f3b9ac4753bc86c9a
Cédric Krier <ced@b2ck.com>
2010-12-25 00:14:16 +0100

    Prepare release 1.8.0

--- proteus/__init__.py
+++ proteus/__init__.py
@@ -3,7 +3,7 @@
 '''
 A library to access Tryton's models like a client.
 '''
-__version__ = "0.0.1"
+__version__ = "1.8.0"
 __all__ = ['Model', 'Wizard']
 from types import NoneType
 import threading
