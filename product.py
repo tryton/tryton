d6dd058c810f7061c309de79cc5ab5f46ca3a4ad
Cédric Krier <ced@b2ck.com>
2011-02-07 23:53:34 +0100

    Fix required PySON statement for issue1850

--- product.py
+++ product.py
@@ -1,7 +1,7 @@
 #This file is part of Tryton.  The COPYRIGHT file at the top level of
 #this repository contains the full copyright notices and license terms.
 from trytond.model import ModelView, ModelSQL, fields
-from trytond.pyson import Not, Eval, Bool, Or, In
+from trytond.pyson import Not, Eval, Bool, Or, In, And
 
 
 class Category(ModelSQL, ModelView):
@@ -30,7 +30,9 @@ class Template(ModelSQL, ModelView):
         states={
             'invisible': Or(Not(Bool(Eval('company'))),
                 Bool(Eval('account_category'))),
-            'required': In(Eval('type'), ['stockable', 'consumable']),
+            'required': And(In(Eval('type'), ['stockable', 'consumable']),
+                Bool(Eval('company')),
+                Not(Bool(Eval('account_category')))),
         }, help='This account will be used instead of the one defined '
         'on the category.', depends=['account_category']))
     account_cogs_used = fields.Function(fields.Many2One('account.account',
