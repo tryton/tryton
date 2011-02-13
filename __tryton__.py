006b12d41fc1aa2cbdc6a5b9b9481509bca5c17d
Mathias Behrle <mbehrle@m9s.biz>
2011-02-13 17:30:24 +0100

    Increase version number

--- __tryton__.py
+++ __tryton__.py
@@ -6,7 +6,7 @@
     'name_es_CO': 'Contabilidad',
     'name_es_ES': 'Contabilidad',
     'name_fr_FR': 'Comptabilité',
-    'version': '1.6.2',
+    'version': '1.6.3',
     'author': 'B2CK',
     'email': 'info@b2ck.com',
     'website': 'http://www.tryton.org/',
