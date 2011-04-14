fe40d0e7e846c754aacdb1ecba7b1a96a015fb74
Mathias Behrle <mbehrle@m9s.biz>
2011-04-15 00:57:26 +0200

    Add translation for de_DE.

--- __tryton__.py
+++ __tryton__.py
@@ -2,11 +2,15 @@
 #this repository contains the full copyright notices and license terms.
 {
     'name': 'Account Stock Continental',
+    'name_de_DE': 'Buchhaltung Lagerbewertung Kontinentale Methode',
     'version': '1.9.0',
     'author': 'B2CK',
     'email': 'info@b2ck.com',
     'website': 'http://www.tryton.org/',
     'description': '''Add continental stock accounting for real-time stock valuation.
+''',
+    'description_de_DE': '''
+    - Fügt die kontinentale Methode der Lagerbewertung in Echt-Zeit hinzu.
 ''',
     'depends': [
         'ir',
@@ -20,5 +24,6 @@
         'account.xml',
     ],
     'translation': [
+        'de_DE.csv',
     ],
 }
