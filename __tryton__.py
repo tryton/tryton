0c991eec32e087c8d4a1e53e09827f14185ef31f
Cédric Krier <ced@b2ck.com>
2011-04-27 19:05:33 +0200

    Increase version number

--- __tryton__.py
+++ __tryton__.py
@@ -7,7 +7,7 @@
     'name_es_CO': 'Ventas Analíticas',
     'name_es_ES': 'Venta analítica',
     'name_fr_FR': 'Vente analytique',
-    'version': '2.0.0',
+    'version': '2.1.0',
     'author': 'B2CK',
     'email': 'info@b2ck.com',
     'website': 'http://www.tryton.org/',
