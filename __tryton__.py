66da309bc300458d28a69beafc1a5aaa400fd5c6
Cédric Krier <ced@b2ck.com>
2011-04-27 16:25:38 +0200

    Prepare release 2.0.0

--- __tryton__.py
+++ __tryton__.py
@@ -7,7 +7,7 @@
     'name_es_CO': 'Contabilidad Analítica',
     'name_es_ES': 'Contabilidad analítica',
     'name_fr_FR': 'Comptabilité analytique',
-    'version': '1.9.0',
+    'version': '2.0.0',
     'author': 'B2CK',
     'email': 'info@b2ck.com',
     'website': 'http://www.tryton.org/',
