5c7f4c81ad2bfa1aa8d74a6bc84a7f3c21be094b
Sharoon Thomas <sharoon.thomas@openlabs.co.in>
2011-05-01 13:14:32 -0400

    Fix missing import of transaction in issue2001

--- product.py
+++ product.py
@@ -4,6 +4,7 @@ from __future__ import with_statement
 import copy
 import datetime
 from trytond.model import ModelView, ModelSQL, fields
+from trytond.transaction import Transaction
 
 
 class Template(ModelSQL, ModelView):
