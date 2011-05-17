4673f93e0b4a7f840f4d93e7058b9ace8ee2cc31
Nicolas Évrard <nicoe@b2ck.com>
2011-05-17 15:37:34 +0200

    Preserver purchase line order in invoice (transplanted from b268b0a90674cf72302423cf550836a14b248a67)

--- purchase.py
+++ purchase.py
@@ -768,12 +768,14 @@ class Purchase(ModelWorkflow, ModelSQL, ModelView):
         vals = self._get_invoice_purchase(cursor, user, purchase, context=context)
         invoice_id = invoice_obj.create(cursor, 0, vals, context=ctx)
 
-        for line_id in invoice_lines:
-            for vals in invoice_lines[line_id]:
+        for line in purchase.lines:
+            if line.id not in invoice_lines:
+                continue
+            for vals in invoice_lines[line.id]:
                 vals['invoice'] = invoice_id
                 invoice_line_id = invoice_line_obj.create(cursor, 0, vals,
                         context=ctx)
-                purchase_line_obj.write(cursor, user, line_id, {
+                purchase_line_obj.write(cursor, user, line.id, {
                     'invoice_lines': [('add', invoice_line_id)],
                     }, context=context)
 
