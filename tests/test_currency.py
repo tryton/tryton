dcfdb5345a8a189e4b5e543e786f036e9b943b83
Cédric Krier <ced@b2ck.com>
2011-07-12 00:17:46 +0200

    Remove support of Python2.5 and fix py3 warnings

--- tests/test_currency.py
+++ tests/test_currency.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 #This file is part of Tryton.  The COPYRIGHT file at the top level of
 #this repository contains the full copyright notices and license terms.
-from __future__ import with_statement
 import sys, os
 DIR = os.path.abspath(os.path.normpath(os.path.join(__file__,
     '..', '..', '..', '..', '..', 'trytond')))
