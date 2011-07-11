8cd735115cb1fcc71c63728fd57b11dbb9bc65ca
Cédric Krier <ced@b2ck.com>
2011-07-12 00:18:04 +0200

    Remove support of Python2.5 and fix py3 warnings

--- opportunity.py
+++ opportunity.py
@@ -1,7 +1,6 @@
 #This file is part of Tryton.  The COPYRIGHT file at the top level of
 #this repository contains the full copyright notices and license terms.
 "Sales extension for managing leads and opportunities"
-from __future__ import with_statement
 import datetime
 import time
 from trytond.model import ModelView, ModelSQL, ModelWorkflow, fields
