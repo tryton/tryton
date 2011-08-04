e059d0c5c93d154ba3cf30ee5ff7405ffeb91c74
Cédric Krier <ced@b2ck.com>
2011-08-04 16:57:30 +0200

    Add test for depends

--- tests/test_ldap_authentication.py
+++ tests/test_ldap_authentication.py
@@ -10,7 +10,7 @@ if os.path.isdir(DIR):
 
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view
+from trytond.tests.test_tryton import test_view, test_depends
 
 
 class LDAPAuthenticationTestCase(unittest.TestCase):
@@ -26,6 +26,12 @@ class LDAPAuthenticationTestCase(unittest.TestCase):
         Test views.
         '''
         test_view('ldap_authentication')
+
+    def test0006depends(self):
+        '''
+        Test depends.
+        '''
+        test_depends()
 
 def suite():
     suite = trytond.tests.test_tryton.suite()
