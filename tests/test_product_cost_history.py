77c63fee44b6c3e3de4f24ea54408e4135ef7478
Cédric Krier <ced@b2ck.com>
2011-08-04 16:57:35 +0200

    Add test for depends

--- tests/test_product_cost_history.py
+++ tests/test_product_cost_history.py
@@ -10,7 +10,7 @@ if os.path.isdir(DIR):
 
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view
+from trytond.tests.test_tryton import test_view, test_depends
 
 
 class ProductCostHistoryTestCase(unittest.TestCase):
@@ -26,6 +26,12 @@ class ProductCostHistoryTestCase(unittest.TestCase):
         Test views.
         '''
         test_view('product_cost_history')
+
+    def test0006depends(self):
+        '''
+        Test depends.
+        '''
+        test_depends()
 
 def suite():
     suite = trytond.tests.test_tryton.suite()
