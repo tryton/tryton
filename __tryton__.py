d411ab463fd751c7219486aa6fd8d1f7ffcd70d2
Cédric Krier <ced@b2ck.com>
2011-08-07 11:22:51 +0200

    Initial commit of carrier module

---
+++ __tryton__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+#This file is part of Tryton.  The COPYRIGHT file at the top level of
+#this repository contains the full copyright notices and license terms.
+{
+    'name' : 'Carrier',
+    'version' : '0.0.1',
+    'author' : 'B2CK',
+    'email': 'info@b2ck.com',
+    'website': 'http://www.tryton.org/',
+    'description': 'Define carriers.',
+    'depends' : [
+        'ir',
+        'res',
+        'party',
+        'product',
+    ],
+    'xml' : [
+        'carrier.xml',
+    ],
+    'translation': [
+    ],
+}
