c3fdc4d23ef5d4520c9de595264a4a5922a8fc96
Cédric Krier <ced@b2ck.com>
2011-08-21 10:52:39 +0200

    Initial commit of stock_supply_forecast
    review78001

---
+++ __tryton__.py
@@ -0,0 +1,20 @@
+#This file is part of Tryton.  The COPYRIGHT file at the top level of
+#this repository contains the full copyright notices and license terms.
+{
+    'name': 'Stock Supply Forecast',
+    'version': '0.0.1',
+    'author': 'B2CK',
+    'email': 'info@b2ck.com',
+    'website': 'http://www.tryton.org/',
+    'description': '''Take care of forecast when generating purchase requests.
+''',
+    'depends': [
+        'ir',
+        'stock_supply',
+        'stock_forecast',
+    ],
+    'xml': [
+    ],
+    'translation': [
+    ],
+}
