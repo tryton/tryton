5d5acd6895c8f481e6cf84c45e0f565135a81042
Cédric Krier <ced@b2ck.com>
2011-09-14 12:46:22 +0200

    Report only first failure in scenario
    issue2147

--- tests/test_account_stock_continental.py
+++ tests/test_account_stock_continental.py
@@ -52,7 +52,8 @@ def suite():
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
         AccountStockContinentalTestCase))
     suite.addTests(doctest.DocFileSuite('scenario_account_stock_continental.rst',
-        setUp=doctest_dropdb, tearDown=doctest_dropdb, encoding='utf-8'))
+        setUp=doctest_dropdb, tearDown=doctest_dropdb, encoding='utf-8',
+            optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
     return suite
 
 if __name__ == '__main__':
