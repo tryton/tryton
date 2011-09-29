a0495727dd837d4f50524b024cf93553313f3982
Cédric Krier <ced@b2ck.com>
2011-09-29 23:24:04 +0200

    Set Tryton classifier
    issue2134

--- setup.py
+++ setup.py
@@ -40,6 +40,7 @@ setup(name='trytond_currency',
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Environment :: Plugins',
+        'Framework :: Tryton',
         'Intended Audience :: Developers',
         'Intended Audience :: Financial and Insurance Industry',
         'Intended Audience :: Legal Industry',
