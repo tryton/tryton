efa019a82f498a9046c89525cc74cc022a8997e0
Cédric Krier <ced@b2ck.com>
2011-09-29 23:24:20 +0200

    Set Tryton classifier
    issue2134

--- setup.py
+++ setup.py
@@ -39,6 +39,7 @@ setup(name='trytond_stock_location_sequence',
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Environment :: Plugins',
+        'Framework :: Tryton',
         'Intended Audience :: Developers',
         'Intended Audience :: Financial and Insurance Industry',
         'Intended Audience :: Legal Industry',
