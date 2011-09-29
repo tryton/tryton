fb14c112c77d372ab2929135c67cd12623ebaf34
Cédric Krier <ced@b2ck.com>
2011-09-29 23:24:06 +0200

    Set Tryton classifier
    issue2134

--- setup.py
+++ setup.py
@@ -39,6 +39,7 @@ setup(name='trytond_ldap_authentication',
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Environment :: Plugins',
+        'Framework :: Tryton',
         'Intended Audience :: Developers',
         'Intended Audience :: Financial and Insurance Industry',
         'Intended Audience :: Legal Industry',
