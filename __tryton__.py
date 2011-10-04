bcca5c7347129521003fd71679a603d46aba9ffd
Nicolas Évrard <nicoe@b2ck.com>
2011-10-04 13:13:39 +0200

    replace .csv by .po file for translations
    issue1163

--- __tryton__.py
+++ __tryton__.py
@@ -21,10 +21,10 @@
         'company.xml',
     ],
     'translation': [
-        'bg_BG.csv',
-        'de_DE.csv',
-        'es_CO.csv',
-        'es_ES.csv',
-        'fr_FR.csv',
+        'locale/bg_BG.po',
+        'locale/de_DE.po',
+        'locale/es_CO.po',
+        'locale/es_ES.po',
+        'locale/fr_FR.po',
     ],
 }
