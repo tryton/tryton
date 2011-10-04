e24e6126ecfd8a4814fec57464cdc713eee2480c
Nicolas Évrard <nicoe@b2ck.com>
2011-10-04 13:13:33 +0200

    replace .csv by .po file for translations
    issue1163

--- __tryton__.py
+++ __tryton__.py
@@ -75,10 +75,10 @@ sur base des règles d'approvisionnement
         'shipment.xml',
     ],
     'translation': [
-        'bg_BG.csv',
-        'de_DE.csv',
-        'es_CO.csv',
-        'es_ES.csv',
-        'fr_FR.csv',
+        'locale/bg_BG.po',
+        'locale/de_DE.po',
+        'locale/es_CO.po',
+        'locale/es_ES.po',
+        'locale/fr_FR.po',
     ]
 }
