372d5c47c6f59a0559dc6dbaeadfa13f408e6d29
Nicolas Évrard <nicoe@b2ck.com>
2011-10-04 13:13:41 +0200

    replace .csv by .po file for translations
    issue1163

--- __tryton__.py
+++ __tryton__.py
@@ -38,10 +38,10 @@ Impacte les calculs de dates de livraisons.
         "purchase.xml",
     ],
     'translation': [
-        'bg_BG.csv',
-        'de_DE.csv',
-        'es_CO.csv',
-        'es_ES.csv',
-        'fr_FR.csv',
+        'locale/bg_BG.po',
+        'locale/de_DE.po',
+        'locale/es_CO.po',
+        'locale/es_ES.po',
+        'locale/fr_FR.po',
     ],
 }
