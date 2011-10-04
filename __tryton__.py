2b88642dc81b25458ca065e11964800693f7bbf6
Nicolas Évrard <nicoe@b2ck.com>
2011-10-04 13:13:33 +0200

    replace .csv by .po file for translations
    issue1163

--- __tryton__.py
+++ __tryton__.py
@@ -141,11 +141,11 @@ Met bijbehorende rapporten:
         'party.xml',
     ],
     'translation': [
-        'bg_BG.csv',
-        'de_DE.csv',
-        'es_CO.csv',
-        'es_ES.csv',
-        'fr_FR.csv',
-        'nl_NL.csv',
+        'locale/bg_BG.po',
+        'locale/de_DE.po',
+        'locale/es_CO.po',
+        'locale/es_ES.po',
+        'locale/fr_FR.po',
+        'locale/nl_NL.po',
     ],
 }
