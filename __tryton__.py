9f25eac5d68747c9810d1eed422039c596d34efa
Cédric Krier <ced@b2ck.com>
2011-10-22 02:15:13 +0200

    Add fr_FR

--- __tryton__.py
+++ __tryton__.py
@@ -2,12 +2,16 @@
 #this repository contains the full copyright notices and license terms.
 {
     'name': 'Stock Supply Forecast',
+    'name_fr_FR': 'Prévisions et approvisionnemenets de stock',
     'version': '2.1.0',
     'author': 'B2CK',
     'email': 'info@b2ck.com',
     'website': 'http://www.tryton.org/',
     'description': '''Take care of forecast when generating purchase requests.
 ''',
+    'description_fr_FR': '''
+Prend en compte les prévisions lors de la génération des demandes d'achat.
+'''
     'depends': [
         'ir',
         'stock_supply',
