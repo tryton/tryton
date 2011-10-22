8f2fdb6367bd651ea3de74e63754209c19ddfeb2
Mathias Behrle <mbehrle@m9s.biz>
2011-10-22 13:45:55 +0200

    Add translation for de_DE

--- __tryton__.py
+++ __tryton__.py
@@ -2,12 +2,16 @@
 #this repository contains the full copyright notices and license terms.
 {
     'name': 'Stock Supply Forecast',
+    'name_de_DE': 'Lagerverwaltung Bestellwesen Bedarfsermittlung',
     'name_fr_FR': 'Prévisions et approvisionnemenets de stock',
     'version': '2.1.0',
     'author': 'B2CK',
     'email': 'info@b2ck.com',
     'website': 'http://www.tryton.org/',
     'description': '''Take care of forecast when generating purchase requests.
+''',
+    'description_de_DE': '''Bestellwesen mit Bedarfsermittlung
+    - Berücksichtigt die Vorhersagen der Bedarfsermittlung im Bestellwesen
 ''',
     'description_fr_FR': '''
 Prend en compte les prévisions lors de la génération des demandes d'achat.
