689c984504ff1ffa29ff583aa25818ec9aacd134
Cédric Krier <ced@b2ck.com>
2011-10-24 00:48:14 +0200

    Clean translations

--- __tryton__.py
+++ __tryton__.py
@@ -36,9 +36,12 @@
     ],
     'translation': [
         'locale/bg_BG.po',
+        'locale/cs_CZ.po',
         'locale/de_DE.po',
         'locale/es_CO.po',
         'locale/es_ES.po',
         'locale/fr_FR.po',
+        'locale/nl_NL.po',
+        'locale/ru_RU.po',
     ],
 }
