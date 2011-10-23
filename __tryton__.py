745d719b83ddb7ae069cecc9b191aa1bd2ab6e39
Cédric Krier <ced@b2ck.com>
2011-10-24 00:56:01 +0200

    Clean translations

--- __tryton__.py
+++ __tryton__.py
@@ -45,9 +45,12 @@ que des lignes qui ont les même type, société, devis et tiers.
     ],
     'translation': [
         'locale/bg_BG.po',
+        'locale/cs_CZ.po',
         'locale/de_DE.po',
         'locale/es_CO.po',
         'locale/es_ES.po',
         'locale/fr_FR.po',
+        'locale/nl_NL.po',
+        'locale/ru_RU.po',
     ],
 }
