b41af4c572ff75484f10f620d1bc9596b2f64573
Cédric Krier <ced@b2ck.com>
2011-10-24 00:59:26 +0200

    Clean translations

--- __tryton__.py
+++ __tryton__.py
@@ -142,10 +142,12 @@ Met bijbehorende rapporten:
     ],
     'translation': [
         'locale/bg_BG.po',
+        'locale/cs_CZ.po',
         'locale/de_DE.po',
         'locale/es_CO.po',
         'locale/es_ES.po',
         'locale/fr_FR.po',
         'locale/nl_NL.po',
+        'locale/ru_RU.po',
     ],
 }
