4fcffed6c68c543d50e370511a631c2b3df83c09
Cédric Krier <ced@b2ck.com>
2011-10-24 00:23:51 +0200

    Clean translations

--- __tryton__.py
+++ __tryton__.py
@@ -54,9 +54,12 @@ prix de revient du produit concerné (si la méthode de coût est FIFO).
     ],
     'translation': [
         'locale/bg_BG.po',
+        'locale/cs_CZ.po',
         'locale/de_DE.po',
         'locale/es_CO.po',
         'locale/es_ES.po',
         'locale/fr_FR.po',
+        'locale/nl_NL.po',
+        'locale/ru_RU.po',
     ],
 }
