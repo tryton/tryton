5fe4a01da6de400cc941c6175f418385b154e593
Cédric Krier <ced@b2ck.com>
2011-10-24 23:31:44 +0200

    Increase version number

--- __tryton__.py
+++ __tryton__.py
@@ -7,7 +7,7 @@
     'name_es_CO': 'Lista de precios de producto',
     'name_es_ES': 'Lista de precios de producto',
     'name_fr_FR' : 'Liste de prix produit',
-    'version': '2.2.0',
+    'version': '2.2.1',
     'author': 'B2CK',
     'email': 'info@b2ck.com',
     'website': 'http://www.tryton.org/',
