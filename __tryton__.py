272c70809c758355c6906c3fa4aa963c1b5d3e04
Cédric Krier <ced@b2ck.com>
2011-10-24 19:36:02 +0200

    Prepare release 2.2.0

--- __tryton__.py
+++ __tryton__.py
@@ -9,7 +9,7 @@
     'name_es_ES': 'Empresa',
     'name_fr_FR': 'Société',
     'name_nl_NL': 'Bedrijf',
-    'version': '2.1.0',
+    'version': '2.2.0',
     'author': 'B2CK',
     'email': 'info@b2ck.com',
     'website': 'http://www.tryton.org/',
