9c67bdcc0c51fdd0a6b7bd95be040c57c373f0c5
Cédric Krier <ced@b2ck.com>
2011-10-25 11:04:26 +0200

    Prepare release 2.2.0

--- __tryton__.py
+++ __tryton__.py
@@ -7,7 +7,7 @@
     'name_es_CO': 'Inventario de existencias para muchas ubicaciones',
     'name_es_ES': 'Inventario de existencias para muchas ubicaciones',
     'name_fr_FR': 'Inventaire de stock par liste de locations',
-    'version': '2.1.0',
+    'version': '2.2.0',
     'author': 'B2CK',
     'email': 'info@b2ck.com',
     'website': 'http://www.tryton.org/',
