29f75a70ce5aae64dd16753aa83f597dc4b0dcfc
Nicolas Évrard <nicoe@b2ck.com>
2011-10-28 11:05:25 +0200

    Use correct variable name

--- stock.py
+++ stock.py
@@ -91,7 +91,7 @@ class ShipmentOut(ModelWorkflow, ModelSQL, ModelView):
         currency_obj = pool.get('currency.currency')
 
         try:
-            result = super(ShipmentOut, self).on_change_lines(value)
+            result = super(ShipmentOut, self).on_change_lines(values)
         except AttributeError:
             result = {}
         carrier = carrier_obj.browse(values['carrier'])
