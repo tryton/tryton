1ad29eb1cf3757876bff9f16c62d845341965b41
Nicolas Évrard <nicoe@b2ck.com>
2011-12-14 17:04:27 +0100

    Use relative imports

--- __init__.py
+++ __init__.py
@@ -1,10 +1,10 @@
 #This file is part of Tryton.  The COPYRIGHT file at the top level of this
 #repository contains the full copyright notices and license terms.
 
-from location import *
-from shipment import *
-from period import *
-from move import *
-from product import *
-from inventory import *
-from configuration import *
+from .location import *
+from .shipment import *
+from .period import *
+from .move import *
+from .product import *
+from .inventory import *
+from .configuration import *
