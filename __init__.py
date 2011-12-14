79055b94b63a7b550674b063018d7a2aceba9d37
Nicolas Évrard <nicoe@b2ck.com>
2011-12-14 17:04:24 +0100

    Use relative imports

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 #This file is part of Tryton.  The COPYRIGHT file at the top level of
 #this repository contains the full copyright notices and license terms.
 
-from uom import *
-from category import *
-from product import *
+from .uom import *
+from .category import *
+from .product import *
