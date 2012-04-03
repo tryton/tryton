1c7bb9f860a40c4dbc384cc34cfcbf1f85f68d96
Cédric Krier <ced@b2ck.com>
2012-04-03 20:07:04 +0200

    Fix pep8 and pyflakes

--- __tryton__.py
+++ __tryton__.py
@@ -1,31 +1,31 @@
 #This file is part of Tryton.  The COPYRIGHT file at the top level of
 #this repository contains the full copyright notices and license terms.
 {
-    'name' : 'Google Maps',
+    'name': 'Google Maps',
     'name_bg_BG': 'Google карти',
     'name_ca_ES': 'Mapes de Google',
     'name_de_DE': 'Google Maps',
     'name_es_CO': 'Mapas de Google',
     'name_es_ES': 'Mapas de Google',
     'name_fr_FR': 'Cartes Google',
-    'version' : '2.3.0',
-    'author' : 'B2CK',
+    'version': '2.3.0',
+    'author': 'B2CK',
     'email': 'info@b2ck.com',
     'website': 'http://www.tryton.org/',
     'description': 'Add link from addresses to Google Maps',
     'description_bg_BG': 'Добавя връзка към адрес от Google карти.',
-    'description_ca_ES': '''Afegeix un enllaç en les adreces per obrir-les amb 
+    'description_ca_ES': '''Afegeix un enllaç en les adreces per obrir-les amb
 el Google Maps.
 ''',
     'description_de_DE': 'Fügt einen Link für den automatischen Aufruf von Google Maps zu Adressen hinzu.',
     'description_es_CO': 'Añade un enlace a la dirección en Google Maps',
     'description_es_ES': 'Añade un enlace de la dirección a Google Maps',
     'description_fr_FR': 'Ajoute un lien sur les adresses vers les cartes de Google',
-    'depends' : [
+    'depends': [
         'ir',
         'party'
     ],
-    'xml' : [
+    'xml': [
         'address.xml',
     ],
     'translation': [
