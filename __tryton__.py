5071f756baacca85fd2ee6b5cc357429890bfd7b
Cédric Krier <ced@b2ck.com>
2012-04-03 20:07:14 +0200

    Fix pep8 and pyflakes

--- __tryton__.py
+++ __tryton__.py
@@ -2,15 +2,15 @@
 #This file is part of Tryton.  The COPYRIGHT file at the top level of
 #this repository contains the full copyright notices and license terms.
 {
-    'name' : 'Party - Siret',
-    'name_bg_BG' : 'Партньор - SIRET',
+    'name': 'Party - Siret',
+    'name_bg_BG': 'Партньор - SIRET',
     'name_ca_ES': 'Tercers - SIRET',
-    'name_de_DE' : 'Parteien - SIRET',
-    'name_es_CO' : 'Terceros - SIRET',
+    'name_de_DE': 'Parteien - SIRET',
+    'name_es_CO': 'Terceros - SIRET',
     'name_es_ES': 'Terceros - SIRET',
     'name_fr_FR': 'Tiers - Siret',
-    'version' : '2.3.0',
-    'author' : 'B2CK',
+    'version': '2.3.0',
+    'author': 'B2CK',
     'email': 'info@b2ck.com',
     'website': 'http://www.tryton.org/',
     'description': '''
@@ -20,7 +20,7 @@ Add SIRET/SIREN number fields on party.
     - SIREN (Systeme d'Identification du Repertoire des ENtreprises)
     - SIRET (Systeme d'Identification du Repertoire des ETablissements)
 ''',
-    'description_bg_BG' : '''
+    'description_bg_BG': '''
 Добавя SIRET/SIREN полета към партньор.
     SIRET/SIREN са френски еквивалент на БУЛСТАТ и служат географско
     идентифициране на фирма:
@@ -36,21 +36,21 @@ així com la seva ubicació geogràfica (SIRET):
 - SIREN (Système d’Identification du Répertoire des ENtreprises)
 - SIRET (Système d’Identification du Répertoire des ETablissements)
 ''',
-    'description_de_DE' : '''
+    'description_de_DE': '''
 Fügt ein Feld für die SIRET/SIREN-Nummer bei Parteien hinzu.
     Diese Nummern dienen in Frankreich der Identifikation und geographischen
     Zuordnung von Unternehmen:
     - SIREN (Système d’Identification du Répertoire des ENtreprises)
     - SIRET (Système d’Identification du Répertoire des ETablissements)
 ''',
-    'description_es_CO' : '''
+    'description_es_CO': '''
 Añade campos para los números SIRET/SIREN en Terceros.
     Estos números son utilizados en Francia, para la identificación de
     empresas (SIREN) así como su ubicación geográfica (SIRET).
     - SIREN (Système d’Identification du Répertoire des ENtreprises)
     - SIRET (Système d’Identification du Répertoire des ETablissements)
 ''',
-    'description_es_ES' : '''
+    'description_es_ES': '''
 Añade campos para los números SIRET/SIREN en Terceros.
     Estos números son utilizados en Francia, para la identificación de
     empresas (SIREN) así como su ubicación geográfica (SIRET).
@@ -64,11 +64,11 @@ Ajoute les champs numéro SIRET/SIREN sur les tiers.
     - SIREN (Système d’Identification du Répertoire des ENtreprises)
     - SIRET (Système d’Identification du Répertoire des ETablissements)
 ''',
-    'depends' : [
+    'depends': [
         'ir',
         'party',
     ],
-    'xml' : [
+    'xml': [
         'party.xml',
         'address.xml',
     ],
--- luhn.py
+++ luhn.py
@@ -1,6 +1,7 @@
 #This file is part of Tryton.  The COPYRIGHT file at the top level of
 #this repository contains the full copyright notices and license terms.
 
+
 def validate(value):
     """
     Validate value with Luhn algorithm
