ebfe9ab1634dcd13678c1108a12c26c4c9193edd
Bruno M. Villasanti <bvillasanti@thymbra.com>
2012-04-21 20:50:02 -0300

    Add Spanish Argentina translation

--- __tryton__.py
+++ __tryton__.py
@@ -5,6 +5,7 @@
     'name_bg_BG': 'Работно време на фирма',
     'name_ca_ES': 'Hores de treball a la companyia',
     'name_de_DE': 'Unternehmen Arbeitszeit',
+    'name_es_AR': 'Tiempo de trabajo en la empresa',
     'name_es_CO': 'Tiempo de Trabajo en la Compañía',
     'name_es_ES': 'Tiempo de trabajo en la empresa',
     'name_fr_FR': 'Temps de travail dans la société',
@@ -16,6 +17,9 @@
 ''',
     'description_ca_ES': '''Afegeix les hores de treball al dia, setmana, mes i
 any per companyia.
+''',
+    'description_es_AR': '''Añade horas de trabajo por día, semana, mes y
+año por empresa.
 ''',
     'depends': [
         'ir',
@@ -29,6 +33,7 @@ any per companyia.
         'locale/ca_ES.po',
         'locale/cs_CZ.po',
         'locale/de_DE.po',
+        'locale/es_AR.po',
         'locale/es_CO.po',
         'locale/es_ES.po',
         'locale/fr_FR.po',
