a797fbdd89544a4b5923174fd7f13c6d7ebe1143
Bruno M. Villasanti <bvillasanti@thymbra.com>
2012-04-21 20:11:06 -0300

    Add Spanish Argentina translation

--- __tryton__.py
+++ __tryton__.py
@@ -5,6 +5,7 @@
     'name_bg_BG': 'Хронология на фактуриране',
     'name_ca_ES': 'Històric de facturació',
     'name_de_DE': 'Fakturierung Historisierung',
+    'name_es_AR': 'Histórico de Facturación',
     'name_es_CO': 'Histórico de Facturación',
     'name_es_ES': 'Histórico de facturación',
     'name_fr_FR': 'Historisation facture',
@@ -19,6 +20,8 @@
     'description_ca_ES': '''Afegeix un històric pels camps de la factura
 ''',
     'description_de_DE': '''Fügt Historisierung zu den Rechnungsfeldern hinzu
+''',
+    'description_es_AR': '''Añade un histórico para los campos de factura
 ''',
     'description_es_CO': '''Histórico de Facturación a nivel de campos
 ''',
@@ -36,6 +39,7 @@
         'locale/ca_ES.po',
         'locale/cs_CZ.po',
         'locale/de_DE.po',
+        'locale/es_AR.po',
         'locale/es_CO.po',
         'locale/es_ES.po',
         'locale/fr_FR.po',
