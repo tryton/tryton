131a67f52fc606282320a859197dcb217d304603
Nickolay Kolev <nickyk@gmx.net>
2012-04-22 13:39:20 +0300

    Bulgarian translation

--- __tryton__.py
+++ __tryton__.py
@@ -3,6 +3,7 @@
 #this repository contains the full copyright notices and license terms.
 {
     'name': 'Carrier',
+    'name_bg_BG': 'Управление на превозвачи',
     'name_es_AR': 'Transportistas',
     'name_es_ES': 'Transportista',
     'name_de_DE': 'Frachtführer',
@@ -12,6 +13,7 @@
     'email': 'info@b2ck.com',
     'website': 'http://www.tryton.org/',
     'description': 'Define carriers.',
+    'description_bg_BG': 'Задаване на превозвачи.',
     'description_es_ES': 'Define transportistas.',
     'description_de_DE': '''
     - Ermöglicht die Definition von Frachtführern.
