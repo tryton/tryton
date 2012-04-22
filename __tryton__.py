5825c0ac48e120ac90850efd1dcd396389add04b
Jordi Esteve <jesteve@zikzakmedia.com>
2012-04-22 23:06:09 +0200

    Update module name and description for es_ES

--- __tryton__.py
+++ __tryton__.py
@@ -3,11 +3,11 @@
 {
     'name': 'Product Price List',
     'name_bg_BG': 'Ценова листа на продукт',
-    'name_ca_ES': 'Llista de preus de producte',
+    'name_ca_ES': 'Tarifes de producte',
     'name_de_DE': 'Artikel Preisliste',
     'name_es_AR': 'Lista de precios de producto',
     'name_es_CO': 'Lista de precios de producto',
-    'name_es_ES': 'Lista de precios de producto',
+    'name_es_ES': 'Tarifas de producto',
     'name_fr_FR': 'Liste de prix produit',
     'version': '2.3.0',
     'author': 'B2CK',
@@ -15,13 +15,13 @@
     'website': 'http://www.tryton.org/',
     'description': '''Define price list rules by parties.''',
     'description_bg_BG': 'Задаване на правила на ценова листа по партньор.',
-    'description_ca_ES': '''Defineix regles de llista de preus per tercer.''',
+    'description_ca_ES': '''Defineix regles de tarifa per tercer.''',
     'description_de_DE': '''Preislisten für Artikel
     - Ermöglicht die Definition von Preislisten für Parteien.
 ''',
     'description_es_AR': 'Define reglas de lista de precios por tercero.',
     'description_es_CO': 'Define reglas de lista de precios por tercero.',
-    'description_es_ES': 'Define reglas de lista de precios por tercero.',
+    'description_es_ES': 'Define reglas de tarifa por tercero.',
     'description_fr_FR': '''Défini des listes de prix par tiers''',
     'depends': [
         'ir',
