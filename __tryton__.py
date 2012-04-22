df3867b7dd420d88330ced328947c68d6ecf895e
Bruno M. Villasanti <bvillasanti@thymbra.com>
2012-04-21 21:06:13 -0300

    Add Spanish Argentina translation

--- __tryton__.py
+++ __tryton__.py
@@ -4,6 +4,7 @@
 {
     'name': 'Product Measurements',
     'name_de_DE': 'Artikel Abmessungen',
+    'name_es_AR': 'Mediciones de Productos',
     'version': '2.3.0',
     'author': 'B2CK',
     'email': 'info@b2ck.com',
@@ -12,6 +13,7 @@
     'description_de_DE': '''
     - Fügt Abmessungen für Artikel hinzu.
     ''',
+    'description_es_AR': 'Añade mediciones al producto',
     'depends': [
         'ir',
         'res',
@@ -22,5 +24,6 @@
     ],
     'translation': [
         'locale/de_DE.po',
+        'locale/es_AR.po',
     ]
 }
