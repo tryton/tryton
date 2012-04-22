20791786bef39d479c84d145233f8dbea637611a
Jordi Esteve <jesteve@zikzakmedia.com>
2012-04-22 23:03:46 +0200

    Update module name and description for es_ES

--- __tryton__.py
+++ __tryton__.py
@@ -3,7 +3,7 @@
 {
     'name': 'Company Work Time',
     'name_bg_BG': 'Работно време на фирма',
-    'name_ca_ES': 'Hores de treball a la companyia',
+    'name_ca_ES': 'Hores de treball a l''empresa',
     'name_de_DE': 'Unternehmen Arbeitszeit',
     'name_es_AR': 'Tiempo de trabajo en la empresa',
     'name_es_CO': 'Tiempo de Trabajo en la Compañía',
@@ -16,9 +16,12 @@
     'description': '''
 ''',
     'description_ca_ES': '''Afegeix les hores de treball al dia, setmana, mes i
-any per companyia.
+any per empresa.
 ''',
     'description_es_AR': '''Añade horas de trabajo por día, semana, mes y
+año por empresa.
+''',
+    'description_es_ES': '''Añade las horas de trabajo por día, semana, mes y
 año por empresa.
 ''',
     'depends': [
