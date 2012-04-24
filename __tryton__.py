46d9953a522653207b69c226bd7b06361093e0e3
Cédric Krier <ced@b2ck.com>
2012-04-24 11:36:39 +0200

    Prepare release 2.4.0

--- __tryton__.py
+++ __tryton__.py
@@ -9,7 +9,7 @@
     'name_es_CO': 'Secuencia de Sitio del Stock',
     'name_es_ES': 'Secuencia para las ubicaciones de stock',
     'name_fr_FR': 'Séquence emplacement',
-    'version': '2.3.0',
+    'version': '2.4.0',
     'author': 'B2CK',
     'email': 'info@b2ck.com',
     'website': 'http://www.tryton.org/',
