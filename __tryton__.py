5c44218cae16eb9de4e397ee3b3d4bdbed58ca59
Mathias Behrle <mbehrle@m9s.biz>
2012-05-07 11:09:47 +0200

    Increase version number

--- __tryton__.py
+++ __tryton__.py
@@ -10,7 +10,7 @@
     'name_fr_FR': 'Tableau de bord',
     'name_nl_NL': 'Prikbord',
     'name_ru_RU': 'Информационная панель',
-    'version': '2.2.1',
+    'version': '2.2.2',
     'author': 'B2CK',
     'email': 'info@b2ck.com',
     'website': 'http://www.tryton.org/',
