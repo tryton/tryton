787e09a8de6a3e29d75b09a80a371bfb6791ffc6
Cédric Krier <ced@b2ck.com>
2012-05-29 18:12:23 +0200

    Add invisible states on locations
    issue2629

--- product.py
+++ product.py
@@ -1,12 +1,15 @@
 #This file is part of Tryton.  The COPYRIGHT file at the top level of
 #this repository contains the full copyright notices and license terms.
 from trytond.model import ModelView, ModelSQL, fields
+from trytond.pyson import Eval
 
 
 class Product(ModelSQL, ModelView):
     _name = 'product.product'
 
     locations = fields.One2Many('stock.product.location', 'product',
-            'Default Locations')
+        'Default Locations', states={
+            'invisible': ~Eval('type').in_(['goods', 'assets']),
+            })
 
 Product()
