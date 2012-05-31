66915c0e8b55b2c9e72f098a38580ebf4351197d
Mathias Behrle <mbehrle@m9s.biz>
2012-05-31 13:45:41 +0200

    Add translation for de_DE

--- __tryton__.py
+++ __tryton__.py
@@ -2,12 +2,14 @@
 #this repository contains the full copyright notices and license terms.
 {
     'name': 'Stock Lot Management',
+    'name_de_DE': 'Lager Chargenverwaltung',
     'name_es_ES': 'Gestión de lotes de stock',
     'version': '2.4.0',
     'author': 'B2CK',
     'email': 'info@b2ck.com',
     'website': 'http://www.tryton.org/',
     'description': '''Stock Lot Management''',
+    'description_de_DE': '''Verwaltung von Chargen im Lager''',
     'description_es_ES': '''Gestión de lotes de stock''',
     'depends': [
         'ir',
@@ -19,6 +21,7 @@
         'product.xml',
     ],
     'translation': [
+        'locale/de_DE.po',
         'locale/es_ES.po',
     ],
 }
