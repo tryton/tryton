a9a656526a46a2aebb34cce708b66bb5488f757d
Mathias Behrle <mbehrle@m9s.biz>
2012-05-31 13:45:23 +0200

    Add translation for de_DE

--- __tryton__.py
+++ __tryton__.py
@@ -2,12 +2,14 @@
 #this repository contains the full copyright notices and license terms.
 {
     'name': 'Stock Split',
+    'name_de_DE': 'Lager Aufteilung Lagerbewegung',
     'name_es_ES': 'Partición stock',
     'version': '2.4.0',
     'author': 'B2CK',
     'email': 'info@b2ck.com',
     'website': 'http://www.tryton.org/',
     'description': 'Allow to split move.',
+    'description_de_DE': '''Ermöglicht die Aufteilung einer Lagerbewegung.''',
     'description_es_ES': '''Permite partir un movimiento de stock.''',
     'depends': [
         'ir',
@@ -17,6 +19,7 @@
         'stock.xml',
     ],
     'translation': [
+        'locale/de_DE.po',
         'locale/es_ES.po',
     ],
 }
