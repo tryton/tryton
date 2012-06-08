efe964c059ed53afde7215c2469b1229c94381ae
Cédric Krier <ced@b2ck.com>
2012-06-08 19:34:27 +0200

    Simplify module information with python configuration
    review363001

--- setup.py
+++ setup.py
@@ -4,8 +4,19 @@
 
 from setuptools import setup
 import re
+import os
+import ConfigParser
 
-info = eval(open('__tryton__.py').read())
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+config = ConfigParser.ConfigParser()
+config.readfp(open('tryton.cfg'))
+info = dict(config.items('tryton'))
+for key in ('depends', 'extras_depend', 'xml'):
+    if key in info:
+        info[key] = info[key].strip().splitlines()
 major_version, minor_version, _ = info.get('version', '0.0.1').split('.', 2)
 major_version = int(major_version)
 minor_version = int(minor_version)
@@ -23,22 +34,21 @@ tests_require = ['proteus >= %s.%s, < %s.%s' %
 
 setup(name='trytond_stock',
     version=info.get('version', '0.0.1'),
-    description=info.get('description', ''),
-    author=info.get('author', ''),
-    author_email=info.get('email', ''),
-    url=info.get('website', ''),
+    description='Tryton module for stock and inventory',
+    long_description=read('README'),
+    author='Tryton',
+    url='http://www.tryton.org/',
     download_url="http://downloads.tryton.org/" + \
-            info.get('version', '0.0.1').rsplit('.', 1)[0] + '/',
+        info.get('version', '0.0.1').rsplit('.', 1)[0] + '/',
     package_dir={'trytond.modules.stock': '.'},
     packages=[
         'trytond.modules.stock',
         'trytond.modules.stock.tests',
-    ],
+        ],
     package_data={
         'trytond.modules.stock': info.get('xml', []) \
-                + info.get('translation', []) \
-                + ['*.odt', 'icons/*.svg'],
-    },
+            + ['tryton.cfg', 'locale/*.po', '*.odt', 'icons/*.svg'],
+        },
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Environment :: Plugins',
@@ -60,7 +70,7 @@ setup(name='trytond_stock',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Topic :: Office/Business',
-    ],
+        ],
     license='GPL-3',
     install_requires=requires,
     zip_safe=False,
@@ -71,4 +81,4 @@ setup(name='trytond_stock',
     test_suite='tests',
     test_loader='trytond.test_loader:Loader',
     tests_require=tests_require,
-)
+    )
