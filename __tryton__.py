1047f96c33fbbe66722ac4edef766e4420474b38
Jordi Esteve <jesteve@zikzakmedia.com>
2012-06-11 10:11:35 +0200

    Added es_ES translation

--- __tryton__.py
+++ __tryton__.py
@@ -2,11 +2,15 @@
 #this repository contains the full copyright notices and license terms.
 {
     'name': 'Stock Supply Production',
+    'name_ca_ES': 'Proveïment d''estoc en producció',
+    'name_es_ES': 'Abastecimiento de stock en producción',
     'version': '2.4.0',
     'author': 'B2CK',
     'email': 'info@b2ck.com',
     'website': 'http://www.tryton.org/',
     'description': '''Supply Stock with Production''',
+    'description_ca_ES': '''Proveïment d'estoc en producció.''',
+    'description_es_ES': '''Abastecimiento de stock en producción.''',
     'depends': [
         'ir',
         'product',
