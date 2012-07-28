ba8df71a22265c7501f5084b73d508539a48b831
Bertrand Chenal <bch@b2ck.com>
2012-07-28 21:52:37 +0200

    Add french translation

--- __tryton__.py
+++ __tryton__.py
@@ -4,6 +4,7 @@
     'name': 'Stock Lot Management',
     'name_de_DE': 'Lager Chargenverwaltung',
     'name_es_ES': 'Gestión de lotes de stock',
+    'name_fr_FR': 'Gestion des lots de stock',
     'version': '2.4.0',
     'author': 'B2CK',
     'email': 'info@b2ck.com',
@@ -11,6 +12,7 @@
     'description': '''Stock Lot Management''',
     'description_de_DE': '''Verwaltung von Chargen im Lager''',
     'description_es_ES': '''Gestión de lotes de stock''',
+    'description_fr_FR': '''Gestion des lots de stock.''',
     'depends': [
         'ir',
         'product',
@@ -23,5 +25,6 @@
     'translation': [
         'locale/de_DE.po',
         'locale/es_ES.po',
+        'locale/fr_FR.po',
     ],
 }
