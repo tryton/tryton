78324ebcb20bf2e7487395ca706de9c919158d84
Cédric Krier <ced@b2ck.com>
2012-09-19 22:20:38 +0200

    Fix domain concatenation between list and tuple (transplanted from f0d5fbea32c29fa7361348bb17a8f4124074a21f)

--- party.py
+++ party.py
@@ -181,9 +181,9 @@ class Party(ModelSQL, ModelView):
         return new_ids
 
     def search_rec_name(self, name, clause):
-        ids = self.search([('code',) + clause[1:]], order=[])
+        ids = self.search([('code',) + tuple(clause[1:])], order=[])
         if ids:
-            ids += self.search([('name',) + clause[1:]], order=[])
+            ids += self.search([('name',) + tuple(clause[1:])], order=[])
             return [('id', 'in', ids)]
         return [('name',) + clause[1:]]
 
