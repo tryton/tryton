273a87f9de1a909dd555e245a6d22f6ced28674c
Marco Huymajer <kantntreiber@gmail.com>
2012-10-11 16:33:24 +0200

    Add scenario files to the source and binary distribution
    issue2798
    review537003

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setup(name='trytond_carrier_weight',
         ],
     package_data={
         'trytond.modules.carrier_weight': info.get('xml', []) \
-            + ['tryton.cfg', 'locale/*.po'],
+            + ['tryton.cfg', 'locale/*.po', 'tests/*.rst'],
         },
     classifiers=[
         'Development Status :: 5 - Production/Stable',
