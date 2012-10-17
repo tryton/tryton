957d225f63467d63fc86130d146c8098506331c7
Cédric Krier <ced@b2ck.com>
2012-10-17 16:30:38 +0200

    Fix duration computation
    issue2827

--- work.py
+++ work.py
@@ -119,7 +119,7 @@ class Work:
             all_works = cls.search([
                     ('parent', 'child_of', ids),
                     ('active', '=', True)]) + works
-            all_works = list(set(all_works))
+            all_works = set(all_works)
 
             durations = {}
             id2work = {}
@@ -138,15 +138,20 @@ class Work:
                 durations[work.id] = work.effort / (total_allocation / 100.0)
 
             while leafs:
-                parents = set()
                 for work_id in leafs:
                     work = id2work[work_id]
+                    all_works.remove(work)
                     if not work.active:
                         continue
                     if work.parent and work.parent.id in durations:
                         durations[work.parent.id] += durations[work_id]
-                        parents.add(work.parent.id)
-                leafs = parents
+                next_leafs = set(w.id for w in all_works)
+                for work in all_works:
+                    if not work.parent:
+                        continue
+                    if work.parent.id in next_leafs and work.parent in works:
+                        next_leafs.remove(work.parent.id)
+                leafs = next_leafs
             res['duration'] = durations
 
         fun_fields = ('early_start_date', 'early_finish_date',
