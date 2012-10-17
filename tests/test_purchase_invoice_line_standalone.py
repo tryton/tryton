1f61c6b665cf71d18a2e223be65239a4b6dc6db9
Nicolas Évrard <nicoe@b2ck.com>
2012-10-17 18:55:03 +0200

    Add doctests

--- tests/test_purchase_invoice_line_standalone.py
+++ tests/test_purchase_invoice_line_standalone.py
@@ -10,8 +10,10 @@ if os.path.isdir(DIR):
     sys.path.insert(0, os.path.dirname(DIR))
 
 import unittest
+import doctest
 import trytond.tests.test_tryton
 from trytond.tests.test_tryton import test_view, test_depends
+from trytond.backend.sqlite.database import Database as SQLiteDatabase
 
 
 class PurchaseInvoiceLineStandaloneTestCase(unittest.TestCase):
@@ -36,10 +38,24 @@ class PurchaseInvoiceLineStandaloneTestCase(unittest.TestCase):
         test_depends()
 
 
+def doctest_dropdb(test):
+    database = SQLiteDatabase().connect()
+    cursor = database.cursor(autocommit=True)
+    try:
+        database.drop(cursor, ':memory:')
+        cursor.commit()
+    finally:
+        cursor.close()
+
+
 def suite():
     suite = trytond.tests.test_tryton.suite()
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
             PurchaseInvoiceLineStandaloneTestCase))
+    suite.addTests(doctest.DocFileSuite(
+            'scenario_purchase_invoice_line_standalone.rst',
+            setUp=doctest_dropdb, tearDown=doctest_dropdb, encoding='UTF-8',
+            optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
     return suite
 
 if __name__ == '__main__':
