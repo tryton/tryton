3a8fd019065ba697a52205f930a9fcf524ebba47
Cédric Krier <ced@b2ck.com>
2012-11-05 20:13:05 +0100

    Increase version number

--- __tryton__.py
+++ __tryton__.py
@@ -6,7 +6,7 @@
     'name_es_CO': 'Ingresos por proyectos',
     'name_es_ES': 'Beneficio de proyecto',
     'name_fr_FR': 'Revenu des projets',
-    'version': '1.8.3',
+    'version': '1.8.4',
     'author': 'B2CK',
     'email': 'info@b2ck.com',
     'website': 'http://www.tryton.org/',
