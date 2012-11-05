4b602d9e39fcb89cb55f0f435c9cd2046641e721
Cédric Krier <ced@b2ck.com>
2012-11-05 20:17:23 +0100

    Increase version number

--- __tryton__.py
+++ __tryton__.py
@@ -6,7 +6,7 @@
     'name_es_CO': 'Hoja de Asistencia',
     'name_es_ES': 'Partes de trabajo',
     'name_fr_FR': 'Feuille de présence',
-    'version': '1.8.2',
+    'version': '1.8.3',
     'author': 'B2CK',
     'email': 'info@b2ck.com',
     'website': 'http://www.tryton.org/',
