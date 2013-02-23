c2e09c2072a6fc5c659c17b58b5fc6c5fe4340b3
Cédric Krier <ced@b2ck.com>
2013-02-23 22:06:29 +0100

    Fix flake8

--- setup.py
+++ setup.py
@@ -25,10 +25,10 @@ requires = ['lxml']
 for dep in info.get('depends', []):
     if not re.match(r'(ir|res|webdav)(\W|$)', dep):
         requires.append('trytond_%s >= %s.%s, < %s.%s' %
-                (dep, major_version, minor_version, major_version,
-                    minor_version + 1))
+            (dep, major_version, minor_version, major_version,
+                minor_version + 1))
 requires.append('trytond >= %s.%s, < %s.%s' %
-        (major_version, minor_version, major_version, minor_version + 1))
+    (major_version, minor_version, major_version, minor_version + 1))
 
 setup(name='trytond_dashboard',
     version=info.get('version', '0.0.1'),
@@ -36,16 +36,16 @@ setup(name='trytond_dashboard',
     long_description=read('README'),
     author='Tryton',
     url='http://www.tryton.org/',
-    download_url="http://downloads.tryton.org/" + \
-        info.get('version', '0.0.1').rsplit('.', 1)[0] + '/',
+    download_url=("http://downloads.tryton.org/" +
+        info.get('version', '0.0.1').rsplit('.', 1)[0] + '/'),
     package_dir={'trytond.modules.dashboard': '.'},
     packages=[
         'trytond.modules.dashboard',
         'trytond.modules.dashboard.tests',
         ],
     package_data={
-        'trytond.modules.dashboard': info.get('xml', []) \
-                + ['tryton.cfg', 'locale/*.po'],
+        'trytond.modules.dashboard': (info.get('xml', [])
+            + ['tryton.cfg', 'locale/*.po']),
         },
     classifiers=[
         'Development Status :: 5 - Production/Stable',
