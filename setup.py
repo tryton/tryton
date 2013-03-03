774e0b17f54df2ec6c9baa8c0018484530151bf4
Cédric Krier <ced@b2ck.com>
2013-03-03 19:21:43 +0100

    Use separate XML files for views

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setup(name='trytond_analytic_sale',
         ],
     package_data={
         'trytond.modules.analytic_sale': (info.get('xml', [])
-            + ['tryton.cfg', 'locale/*.po']),
+            + ['tryton.cfg', 'view/*.xml', 'locale/*.po']),
         },
     classifiers=[
         'Development Status :: 5 - Production/Stable',
