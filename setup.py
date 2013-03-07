4960fc409057873419e4b703ea4d3985f725e00d
Cédric Krier <ced@b2ck.com>
2013-03-07 23:55:57 +0100

    Use separate XML files for views

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setup(name='trytond_ldap_authentication',
         ],
     package_data={
         'trytond.modules.ldap_authentication': (info.get('xml', [])
-            + ['tryton.cfg', 'locale/*.po']),
+            + ['tryton.cfg', 'view/*.xml', 'locale/*.po']),
         },
     classifiers=[
         'Development Status :: 5 - Production/Stable',
