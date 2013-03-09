75c2aa02389786337cf31483560894287c8f6b3c
Cédric Krier <ced@b2ck.com>
2013-03-09 15:24:45 +0100

    Use separate XML files for views

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setup(name='trytond_party_siret',
         ],
     package_data={
         'trytond.modules.party_siret': (info.get('xml', [])
-            + ['tryton.cfg', 'locale/*.po']),
+            + ['tryton.cfg', 'view/*.xml', 'locale/*.po']),
         },
     classifiers=[
         'Development Status :: 5 - Production/Stable',
