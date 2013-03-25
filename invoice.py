d96ad80376a057024fc5a88c577537ca0ff1d65c
Marco Huymajer <kantntreiber@gmail.com>
2013-03-25 09:07:52 +0100

    Use accounting date instead of invoice date
    For the analytic lines generated out of invoice lines use the accounting
    date instead of the invoice date.
    
    issue3075
    review728002
    (transplanted from 0103490821cb72dd23fe4c0821b756fa0ce22fe7)

--- invoice.py
+++ invoice.py
@@ -203,7 +203,8 @@ class InvoiceLine(ModelSQL, ModelView):
                 vals['credit'] = res['credit']
                 vals['account'] = account.id
                 vals['journal'] = line.invoice.journal.id
-                vals['date'] = line.invoice.invoice_date
+                vals['date'] = (line.invoice.accounting_date
+                    or line.invoice.invoice_date)
                 vals['reference'] = line.invoice.reference
                 vals['party'] = line.invoice.party.id
                 res['analytic_lines'].append(('create', vals))
