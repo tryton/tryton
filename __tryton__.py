a0aced628c6be6e7dfa016d3153cdb821b3bf269
Cédric Krier <ced@b2ck.com>
2013-05-02 09:51:51 +0200

    Increase version number

--- __tryton__.py
+++ __tryton__.py
@@ -6,7 +6,7 @@
     'name_es_CO': 'Previsión de existencias',
     'name_es_ES': 'Previsión de existencias',
     'name_fr_FR': 'Prévision de stock',
-    'version': '1.8.2',
+    'version': '1.8.3',
     'author': 'B2CK',
     'email': 'info@b2ck.com',
     'website': 'http://www.tryton.org/',
