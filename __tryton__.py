bc233c84ee5f07a02763bca0075fd3c45522a5fd
Cédric Krier <ced@b2ck.com>
2013-05-02 10:03:28 +0200

    Increase version number

--- __tryton__.py
+++ __tryton__.py
@@ -6,7 +6,7 @@
     'name_de_DE': 'Verkauf Lieferposten Kosten',
     'name_es_ES': 'Costo del método de envio',
     'name_fr_FR': "Coût d'expédition de vente",
-    'version': '2.2.3',
+    'version': '2.2.4',
     'author' : 'B2CK',
     'email': 'info@b2ck.com',
     'website': 'http://www.tryton.org/',
