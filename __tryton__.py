14d030eed5aadde699969e1b7fd613e5d23ee3a8
Cédric Krier <ced@b2ck.com>
2013-05-02 09:55:12 +0200

    Increase version number

--- __tryton__.py
+++ __tryton__.py
@@ -4,7 +4,7 @@
     'name': 'Account Stock Continental',
     'name_de_DE': 'Buchhaltung Lagerbewertung Kontinentale Methode',
     'name_fr_FR': 'Gestion de stock continentale',
-    'version': '2.0.4',
+    'version': '2.0.5',
     'author': 'B2CK',
     'email': 'info@b2ck.com',
     'website': 'http://www.tryton.org/',
