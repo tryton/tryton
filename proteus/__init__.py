fd165b62f8ac1ce7163a78f4eaeaec42505cb2a7
Cédric Krier <ced@b2ck.com>
2013-05-02 09:45:08 +0200

    Increase version number

--- proteus/__init__.py
+++ proteus/__init__.py
@@ -3,7 +3,7 @@
 '''
 A library to access Tryton's models like a client.
 '''
-__version__ = "2.0.4"
+__version__ = "2.0.5"
 __all__ = ['Model', 'Wizard']
 from types import NoneType
 import threading
