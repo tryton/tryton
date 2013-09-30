bc89a4932f0057bf4707ee3a7998ae46fdeaa8fc
Guillem Barba <guillem@nan-tic.com>
2013-09-30 17:41:37 +0200

    Fix domain concatenation between list and tuple
    issue3386
    review1099002
    (transplanted from dac8c752fd39ff10cc7885e3df8fd16be2f2b51a)

--- invoice.py
+++ invoice.py
@@ -934,12 +934,12 @@ class Invoice(ModelWorkflow, ModelSQL, ModelView):
 
     def search_rec_name(self, name, clause):
         ids = self.search(['OR',
-            ('number',) + clause[1:],
-            ('reference',) + clause[1:],
+            ('number',) + tuple(clause[1:]),
+            ('reference',) + tuple(clause[1:]),
             ], order=[])
         if ids:
             return [('id', 'in', ids)]
-        return [('party',) + clause[1:]]
+        return [('party',) + tuple(clause[1:])]
 
     def delete(self, ids):
         if not ids:
