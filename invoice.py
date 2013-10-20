b29a3f7ed2ce1652ec985ac51b5030dc7f9910eb
Cédric Krier <ced@b2ck.com>
2013-10-20 20:43:58 +0200

    Fix states of analytic account fields
    Set the same state of the real one2many and use a custom one for required.
    
    issue3012
    review1151003

--- invoice.py
+++ invoice.py
@@ -35,7 +35,9 @@ class InvoiceLine:
                 ['analytic_accounts'])['analytic_accounts']
 
         fields.update(AnalyticAccount.analytic_accounts_fields_get(
-                analytic_accounts_field, fields_names))
+                analytic_accounts_field, fields_names,
+                states=cls.analytic_accounts.states,
+                required_states=Eval('type') == 'line'))
         return fields
 
     @classmethod
