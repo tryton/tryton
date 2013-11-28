9493d0d6a99407fcec992662086bbe00ac6ded31
Cédric Krier <ced@b2ck.com>
2013-11-28 23:09:22 +0100

    Store report cache using buffer
    SQLite doesn't detect the binary even if stored in BLOB if the type is not
    buffer.
    
    issue2704
    review1941002
    (transplanted from 3b77f743131cb946fbecd50bc49cb418c67d584e)

--- invoice.py
+++ invoice.py
@@ -2137,7 +2137,7 @@ class InvoiceReport(Report):
         if invoice.state in ('open', 'paid'):
             Invoice.write([Invoice(invoice.id)], {
                 'invoice_report_format': res[0],
-                'invoice_report_cache': res[1],
+                'invoice_report_cache': buffer(res[1]),
                 })
         return res
 
