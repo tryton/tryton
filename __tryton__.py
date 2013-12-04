cf5cbefed3c916fc664240280e4fe645a55f7ad2
Cédric Krier <ced@b2ck.com>
2013-12-04 18:38:18 +0100

    Increase version number

--- __tryton__.py
+++ __tryton__.py
@@ -8,7 +8,7 @@
     'name_es_ES': 'Facturación',
     'name_fr_FR': 'Facturation',
     'name_nl_NL': 'Facturatie',
-    'version': '2.2.4',
+    'version': '2.2.5',
     'author': 'B2CK',
     'email': 'info@b2ck.com',
     'website': 'http://www.tryton.org/',
