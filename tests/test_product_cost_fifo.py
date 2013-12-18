05f15884b62e84493155ef36ce6c8776f2c53b27
Cédric Krier <ced@b2ck.com>
2013-12-18 18:30:07 +0100

    Use common doctest_dropdb
    review2151002

--- tests/test_product_cost_fifo.py
+++ tests/test_product_cost_fifo.py
@@ -12,8 +12,7 @@ if os.path.isdir(DIR):
 import unittest
 import doctest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_depends
-from trytond.backend.sqlite.database import Database as SQLiteDatabase
+from trytond.tests.test_tryton import test_depends, doctest_dropdb
 
 
 class ProductCostFIFOTestCase(unittest.TestCase):
@@ -26,19 +25,6 @@ class ProductCostFIFOTestCase(unittest.TestCase):
 
     def test0006depends(self):
         test_depends()
-
-
-def doctest_dropdb(test):
-    '''
-    Remove sqlite memory database
-    '''
-    database = SQLiteDatabase().connect()
-    cursor = database.cursor(autocommit=True)
-    try:
-        database.drop(cursor, ':memory:')
-        cursor.commit()
-    finally:
-        cursor.close()
 
 
 def suite():
