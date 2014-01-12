d0597aab7f9d9cf5a1978d92a8733546d1967640
Cédric Krier <ced@b2ck.com>
2014-01-12 20:00:12 +0100

    Cleanup tests
    review2491002

--- tests/test_stock.py
+++ tests/test_stock.py
@@ -1,14 +1,5 @@
-#!/usr/bin/env python
 #This file is part of Tryton.  The COPYRIGHT file at the top level of
 #this repository contains the full copyright notices and license terms.
-
-import sys
-import os
-DIR = os.path.abspath(os.path.normpath(os.path.join(__file__,
-    '..', '..', '..', '..', '..', 'trytond')))
-if os.path.isdir(DIR):
-    sys.path.insert(0, os.path.dirname(DIR))
-
 import unittest
 import doctest
 import datetime
@@ -22,9 +13,7 @@ from trytond.transaction import Transaction
 
 
 class StockTestCase(unittest.TestCase):
-    '''
-    Test Stock module.
-    '''
+    'Test Stock module'
 
     def setUp(self):
         trytond.tests.test_tryton.install_module('stock')
@@ -40,21 +29,15 @@ class StockTestCase(unittest.TestCase):
         self.cache = POOL.get('stock.period.cache')
 
     def test0005views(self):
-        '''
-        Test views.
-        '''
+        'Test views'
         test_view('stock')
 
     def test0006depends(self):
-        '''
-        Test depends.
-        '''
+        'Test depends'
         test_depends()
 
     def test0010move_internal_quantity(self):
-        '''
-        Test Move.internal_quantity.
-        '''
+        'Test Move.internal_quantity'
         with Transaction().start(DB_NAME, USER, context=CONTEXT):
             category, = self.category.create([{
                         'name': 'Test Move.internal_quantity',
@@ -111,9 +94,7 @@ class StockTestCase(unittest.TestCase):
                         internal_quantity)
 
     def test0020products_by_location(self):
-        '''
-        Test products_by_location.
-        '''
+        'Test products_by_location'
         with Transaction().start(DB_NAME, USER,
                 context=CONTEXT) as transaction:
             category, = self.category.create([{
@@ -446,9 +427,7 @@ class StockTestCase(unittest.TestCase):
                 1)
 
     def test0030period(self):
-        '''
-        Test period.
-        '''
+        'Test period'
         with Transaction().start(DB_NAME, USER,
                 context=CONTEXT) as transaction:
             category, = self.category.create([{
@@ -640,6 +619,3 @@ def suite():
             setUp=doctest_dropdb, tearDown=doctest_dropdb, encoding='utf-8',
             optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
     return suite
-
-if __name__ == '__main__':
-    unittest.TextTestRunner(verbosity=2).run(suite())
