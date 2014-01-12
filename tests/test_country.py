373e4b1fdd36a98656aa4fcd623e9a7dce35aad7
Cédric Krier <ced@b2ck.com>
2014-01-12 19:59:42 +0100

    Cleanup tests
    review2491002

--- tests/test_country.py
+++ tests/test_country.py
@@ -1,37 +1,22 @@
-#!/usr/bin/env python
 #This file is part of Tryton.  The COPYRIGHT file at the top level of
 #this repository contains the full copyright notices and license terms.
-
-import sys
-import os
-DIR = os.path.abspath(os.path.normpath(os.path.join(__file__,
-    '..', '..', '..', '..', '..', 'trytond')))
-if os.path.isdir(DIR):
-    sys.path.insert(0, os.path.dirname(DIR))
-
 import unittest
 import trytond.tests.test_tryton
 from trytond.tests.test_tryton import test_view, test_depends
 
 
 class CountryTestCase(unittest.TestCase):
-    '''
-    Test Country module.
-    '''
+    'Test Country module'
 
     def setUp(self):
         trytond.tests.test_tryton.install_module('country')
 
     def test0005views(self):
-        '''
-        Test views.
-        '''
+        'Test views'
         test_view('country')
 
     def test0006depends(self):
-        '''
-        Test depends.
-        '''
+        'Test depends'
         test_depends()
 
 
@@ -40,6 +25,3 @@ def suite():
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
         CountryTestCase))
     return suite
-
-if __name__ == '__main__':
-    unittest.TextTestRunner(verbosity=2).run(suite())
