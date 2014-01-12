83ea513b986ef1ae38d01ac5f6647fbd97918f79
Cédric Krier <ced@b2ck.com>
2014-01-12 19:59:20 +0100

    Cleanup tests
    review2491002

--- tests/test_account_dunning.py
+++ tests/test_account_dunning.py
@@ -1,14 +1,5 @@
-#!/usr/bin/env python
 #This file is part of Tryton.  The COPYRIGHT file at the top level of
 #this repository contains the full copyright notices and license terms.
-
-import sys
-import os
-DIR = os.path.abspath(os.path.normpath(os.path.join(__file__,
-    '..', '..', '..', '..', '..', 'trytond')))
-if os.path.isdir(DIR):
-    sys.path.insert(0, os.path.dirname(DIR))
-
 import unittest
 import doctest
 import trytond.tests.test_tryton
@@ -16,23 +7,17 @@ from trytond.tests.test_tryton import test_view, test_depends, doctest_dropdb
 
 
 class AccountDunningTestCase(unittest.TestCase):
-    '''
-    Test AccountDunning module.
-    '''
+    'Test AccountDunning module'
 
     def setUp(self):
         trytond.tests.test_tryton.install_module('account_dunning')
 
     def test0005views(self):
-        '''
-        Test views.
-        '''
+        'Test views'
         test_view('account_dunning')
 
     def test0006depends(self):
-        '''
-        Test depends.
-        '''
+        'Test depends'
         test_depends()
 
 
@@ -45,6 +30,3 @@ def suite():
             setUp=doctest_dropdb, tearDown=doctest_dropdb, encoding='utf-8',
             optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
     return suite
-
-if __name__ == '__main__':
-    unittest.TextTestRunner(verbosity=2).run(suite())
