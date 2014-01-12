60d5e0fecda3adf2e5ea62ec869d2eaf344bf55e
Cédric Krier <ced@b2ck.com>
2014-01-12 19:59:56 +0100

    Cleanup tests
    review2491002

--- tests/test_production.py
+++ tests/test_production.py
@@ -1,38 +1,23 @@
-#!/usr/bin/env python
 #This file is part of Tryton.  The COPYRIGHT file at the top level of
 #this repository contains the full copyright notices and license terms.
-import sys
-import os
 import unittest
 import doctest
-
-DIR = os.path.abspath(os.path.normpath(os.path.join(__file__,
-    '..', '..', '..', '..', '..', 'trytond')))
-if os.path.isdir(DIR):
-    sys.path.insert(0, os.path.dirname(DIR))
-
 import trytond.tests.test_tryton
 from trytond.tests.test_tryton import test_view, test_depends, doctest_dropdb
 
 
 class ProductionTestCase(unittest.TestCase):
-    '''
-    Test Production module.
-    '''
+    'Test Production module'
 
     def setUp(self):
         trytond.tests.test_tryton.install_module('production')
 
     def test0005views(self):
-        '''
-        Test views.
-        '''
+        'Test views'
         test_view('production')
 
     def test0006depends(self):
-        '''
-        Test depends.
-        '''
+        'Test depends'
         test_depends()
 
 
@@ -44,6 +29,3 @@ def suite():
         setUp=doctest_dropdb, tearDown=doctest_dropdb, encoding='utf-8',
             optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
     return suite
-
-if __name__ == '__main__':
-    unittest.TextTestRunner(verbosity=2).run(suite())
