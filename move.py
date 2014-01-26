c5022e0bf5aafa4a2fbaa0a557808feee33e9733
Cédric Krier <ced@b2ck.com>
2014-01-26 21:57:41 +0100

    Add with_on_change switch to default_get
    issue3602
    review2781002

--- move.py
+++ move.py
@@ -643,14 +643,14 @@ class Line(ModelSQL, ModelView):
         return Decimal(0)
 
     @classmethod
-    def default_get(cls, fields, with_rec_name=True):
+    def default_get(cls, fields, with_rec_name=True, with_on_change=True):
         pool = Pool()
         Move = pool.get('account.move')
         Tax = pool.get('account.tax')
         Account = pool.get('account.account')
         TaxCode = pool.get('account.tax.code')
         values = super(Line, cls).default_get(fields,
-                with_rec_name=with_rec_name)
+                with_rec_name=with_rec_name, with_on_change=with_on_change)
 
         if 'move' not in fields:
             #Not manual entry
