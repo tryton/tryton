caaf3a224db1c9c0c24ac4b22e0fc706e93a5d1f
Cédric Krier <ced@b2ck.com>
2014-01-26 21:59:43 +0100

    Add with_on_change switch to default_get
    issue3602
    review2781002

--- sale.py
+++ sale.py
@@ -42,10 +42,10 @@ class SaleLine:
         return res
 
     @classmethod
-    def default_get(cls, fields, with_rec_name=True):
+    def default_get(cls, fields, with_rec_name=True, with_on_change=True):
         fields = [x for x in fields if not x.startswith('analytic_account_')]
         return super(SaleLine, cls).default_get(fields,
-            with_rec_name=with_rec_name)
+            with_rec_name=with_rec_name, with_on_change=with_on_change)
 
     @classmethod
     def read(cls, ids, fields_names=None):
