dae0106a1bba569380134462892d5e095c331d86
Cédric Krier <ced@b2ck.com>
2014-01-26 22:15:20 +0100

    Use work effort instead of total to compute revenue
    total_effort contains the effort of the children but sum_tree needs only the
    value of the work without the children.
    
    review2931003

--- work.py
+++ work.py
@@ -94,7 +94,7 @@ class Work:
 
         def getter(work):
             if work.list_price:
-                return work.list_price * Decimal(str(work.total_effort))
+                return work.list_price * Decimal(str(work.effort))
             else:
                 return Decimal(0)
 
