5c1fb72e8f3b63ff69fe01acd1a10bfa507c1799
Cédric Krier <ced@b2ck.com>
2014-03-04 23:12:33 +0100

    Fix get_invoice_state
    When there are invoice lines, the state should be waiting.
    
    issue3723
    review3961002
    (grafted from 2120885915659d7654206232ac76e3cf5afecc80)
    
    --HG--
    branch : 2.6

--- purchase.py
+++ purchase.py
@@ -67,6 +67,8 @@ class Purchase:
                     and all(l.invoice for l in invoice_lines)
                     and all(l.invoice.state == 'paid' for l in invoice_lines)):
                 return 'paid'
+            else:
+                return 'waiting'
         return state
 
     @classmethod
