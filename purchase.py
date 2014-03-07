2e3c74b9bdf9e08291817e24be9bf8a9861e56a7
Cédric Krier <ced@b2ck.com>
2014-03-07 19:51:24 +0100

    Fix arguments of missing_account_expense{,_property} error message
    The error was introduced by the changeset 4edebebe71df
    Thanks to Laurent Defert
    
    issue3699
    review3701002
    (grafted from 632ee043ec76e68652826fc82cbbc7640009df98)
    
    --HG--
    branch : 3.0

--- purchase.py
+++ purchase.py
@@ -1205,7 +1205,7 @@ class PurchaseLine(ModelSQL, ModelView):
             if not invoice_line.account:
                 self.raise_user_error('missing_account_expense', {
                         'product': invoice_line.product.rec_name,
-                        'purchase': invoice_line.purchase.rec_name,
+                        'purchase': self.purchase.rec_name,
                         })
         else:
             for model in ('product.template', 'product.category'):
@@ -1214,7 +1214,7 @@ class PurchaseLine(ModelSQL, ModelView):
                     break
             if not invoice_line.account:
                 self.raise_user_error('missing_account_expense_property',
-                    (invoice_line.purchase.rec_name,))
+                    {'purchase': self.purchase.rec_name})
         return [invoice_line]
 
     @classmethod
