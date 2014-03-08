08a0f9df0301844d5061961333ad9a7e31fb8148
Cédric Krier <ced@b2ck.com>
2014-03-08 13:00:40 +0100

    Improve setup.py to allow dev publication
    issue3436
    issue1891
    review3951003

--- setup.py
+++ setup.py
@@ -5,22 +5,43 @@ from setuptools import setup, find_packages
 import os
 import proteus
 
-major_version, minor_version, _ = proteus.__version__.split('.', 2)
-major_version = int(major_version)
-minor_version = int(minor_version)
-
 
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
-setup(name='proteus',
-    version=proteus.__version__,
+
+def get_require_version(name):
+    if minor_version % 2:
+        require = '%s >= %s.%s.dev0, < %s.%s'
+    else:
+        require = '%s >= %s.%s, < %s.%s'
+    require %= (name, major_version, minor_version,
+        major_version, minor_version + 1)
+    return require
+
+name = 'proteus'
+version = proteus.__version__
+major_version, minor_version, _ = version.split('.', 2)
+major_version = int(major_version)
+minor_version = int(minor_version)
+
+download_url = 'http://downloads.tryton.org/%s.%s/' % (
+    major_version, minor_version)
+if minor_version % 2:
+    version = '%s.%s.dev0' % (major_version, minor_version)
+    download_url = 'hg+http://hg.tryton.org/%s#egg=%s-%s' % (
+        name, name, version)
+
+
+setup(name=name,
+    version=version,
     description='Library to access Tryton server as a client',
     long_description=read('README'),
     author='Tryton',
+    author_email='issue_tracker@tryton.org',
     url='http://www.tryton.org/',
-    download_url=("http://downloads.tryton.org/" +
-        proteus.__version__.rsplit('.', 1)[0] + '/'),
+    download_url=download_url,
+    keywords='tryton library cli',
     packages=find_packages(),
     classifiers=[
         'Development Status :: 5 - Production/Stable',
@@ -35,17 +56,17 @@ setup(name='proteus',
         'Programming Language :: Python :: 2.7',
         'Topic :: Office/Business',
         ],
+    platforms='any',
     license='LGPL-3',
     install_requires=[
         "python-dateutil",
         ],
     extras_require={
-        'trytond': ['trytond >= %s.%s, < %s.%s' %
-            (major_version, minor_version, major_version, minor_version + 1)],
+        'trytond': [get_require_version('trytond')],
         'simplejson': ['simplejson'],
         'cdecimal': ['cdecimal'],
         },
+    zip_safe=True,
     test_suite='proteus.tests',
-    tests_require=('trytond >= %s.%s, < %s.%s' %
-        (major_version, minor_version, major_version, minor_version + 1))
+    tests_require=[get_require_version('trytond')],
     )
