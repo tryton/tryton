633b7ad2f4f267765301bba97db3088cfbfdc1ac
Nicolas Évrard <nicoe@b2ck.com>
2014-03-24 17:35:11 +0100

    Use correct name of recreated moves
    issue3781
    review4571002
    (grafted from ec6ccbc600a5f42ba16d0a24c6b60d9d50ec7479)
    
    --HG--
    branch : 2.6

--- purchase.py
+++ purchase.py
@@ -1851,7 +1851,7 @@ class HandleShipmentException(Wizard):
                 if move.state == 'cancel' and move not in skip:
                     moves.append(move.id)
         return {
-            'to_recreate': moves,
+            'recreate_moves': moves,
             'domain_moves': moves,
             }
 
@@ -1921,7 +1921,7 @@ class HandleInvoiceException(Wizard):
             if invoice.state == 'cancel' and invoice not in skip:
                 invoices.append(invoice.id)
         return {
-            'to_recreate': invoices,
+            'recreate_invoices': invoices,
             'domain_invoices': invoices,
             }
 
