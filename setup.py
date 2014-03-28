d3ec97da32713b5cf7b0a6a562df26439dc5465b
Udo Spallek <udono@virtual-things.biz>
2014-03-28 16:29:13 +0000

    Fix wrong package name
    issue3801
    review4741003

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ version = info.get('version', '0.0.1')
 major_version, minor_version, _ = version.split('.', 2)
 major_version = int(major_version)
 minor_version = int(minor_version)
-name = 'carrier'
+name = 'trytond_carrier'
 
 download_url = 'http://downloads.tryton.org/%s.%s/' % (
     major_version, minor_version)
