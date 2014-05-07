93c8602fbba82e69fefb89f66e364bdd80de3dbf
Cédric Krier <ced@b2ck.com>
2014-05-07 22:02:29 +0200

    Increase version number
    --HG--
    branch : 2.4

--- __tryton__.py
+++ __tryton__.py
@@ -10,7 +10,7 @@
     'name_es_ES': 'Contabilidad',
     'name_fr_FR': 'Comptabilité',
     'name_nl_NL': 'Boekhouden',
-    'version': '2.4.8',
+    'version': '2.4.9',
     'author': 'B2CK',
     'email': 'info@b2ck.com',
     'website': 'http://www.tryton.org/',
