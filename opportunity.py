b8a107d2e003a948165c353b2393bd09a9b505a8
Sergi Almacellas Abellana <sergi@koolpi.com>
2014-05-29 10:28:32 +0200

    Fix states keyword
    Fix error introduced on 70d4e3e9a178
    review5371002

--- opportunity.py
+++ opportunity.py
@@ -63,7 +63,7 @@ class SaleOpportunity(Workflow, ModelSQL, ModelView):
     currency_digits = fields.Function(fields.Integer('Currency Digits'),
             'get_currency_digits')
     amount = fields.Numeric('Amount', digits=(16, Eval('currency_digits', 2)),
-        STATES=_STATES_STOP, depends=_DEPENDS_STOP + ['currency_digits'],
+        states=_STATES_STOP, depends=_DEPENDS_STOP + ['currency_digits'],
         help='Estimated revenue amount')
     payment_term = fields.Many2One('account.invoice.payment_term',
         'Payment Term', states={
