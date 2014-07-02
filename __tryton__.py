46faeba2127f01959b717a34812741cef74300ae
Cédric Krier <ced@b2ck.com>
2014-07-03 00:03:35 +0200

    Increase version number
    --HG--
    branch : 2.4

--- __tryton__.py
+++ __tryton__.py
@@ -10,7 +10,7 @@
     'name_es_ES': 'Stocks',
     'name_fr_FR': 'Gestion des stocks',
     'name_ru_RU': 'Управление складами',
-    'version': '2.4.9',
+    'version': '2.4.10',
     'author': 'B2CK',
     'email': 'info@b2ck.com',
     'website': 'http://www.tryton.org/',
