d24604402d7fc4259e6dbbb3f45b5809cd9b4534
Cédric Krier <ced@b2ck.com>
2014-07-20 12:31:50 +0200

    Add missing tests rst in package_data
    issue4058
    (grafted from 7aeacc140b7a3c508fee45388b96ee010a6f5884)
    
    --HG--
    branch : 2.8

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ setup(name='trytond_account_asset',
         ],
     package_data={
         'trytond.modules.account_asset': (info.get('xml', [])
-            + ['tryton.cfg', 'view/*.xml', 'locale/*.po']),
+            + ['tryton.cfg', 'view/*.xml', 'locale/*.po', 'tests/*.rst']),
         },
     classifiers=[
         'Development Status :: 5 - Production/Stable',
