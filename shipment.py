a117afd83a58ec90ea2bb5ea13a817259d9b076b
Sharoon Thomas <sharoon.thomas@openlabs.co.in>
2014-08-11 17:12:07 +0530

    Fix typo in transition state
    issue4119
    review11431002
    (grafted from 454302de05f93b007c211a5b35b81be4a1f1d781)
    
    --HG--
    branch : 3.0

--- shipment.py
+++ shipment.py
@@ -1408,7 +1408,7 @@ class ShipmentOutReturn(Workflow, ModelSQL, ModelView):
         cls._transitions |= set((
                 ('draft', 'received'),
                 ('received', 'done'),
-                ('received', 'draf'),
+                ('received', 'draft'),
                 ('draft', 'cancel'),
                 ('received', 'cancel'),
                 ('cancel', 'draft'),
