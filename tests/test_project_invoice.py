6885e13303c6c160688c8768a114cbb477200269
Cédric Krier <ced@b2ck.com>
2014-09-04 08:45:24 +0200

    Use new doctest setup/teardown
    issue3627
    review7401002

--- tests/test_project_invoice.py
+++ tests/test_project_invoice.py
@@ -3,7 +3,8 @@
 import unittest
 import doctest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends, doctest_dropdb
+from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import doctest_setup, doctest_teardown
 
 
 class ProjectInvoiceTestCase(unittest.TestCase):
@@ -27,10 +28,10 @@ def suite():
             ProjectInvoiceTestCase))
     suite.addTests(doctest.DocFileSuite(
             'scenario_project_invoice_timesheet.rst',
-            setUp=doctest_dropdb, tearDown=doctest_dropdb, encoding='utf-8',
+            setUp=doctest_setup, tearDown=doctest_teardown, encoding='utf-8',
             optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
     suite.addTests(doctest.DocFileSuite(
             'scenario_project_invoice_effort.rst',
-            setUp=doctest_dropdb, tearDown=doctest_dropdb, encoding='utf-8',
+            setUp=doctest_setup, tearDown=doctest_teardown, encoding='utf-8',
             optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
     return suite
