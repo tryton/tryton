5bfbd3c2e30ff4f2cb742483ebf48567b518d441
Cédric Krier <ced@b2ck.com>
2014-09-04 08:45:31 +0200

    Use new doctest setup/teardown
    issue3627
    review7401002

--- tests/test_sale.py
+++ tests/test_sale.py
@@ -3,7 +3,8 @@
 import unittest
 import doctest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends, doctest_dropdb
+from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import doctest_setup, doctest_teardown
 
 
 class SaleTestCase(unittest.TestCase):
@@ -25,6 +26,6 @@ def suite():
     suite = trytond.tests.test_tryton.suite()
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(SaleTestCase))
     suite.addTests(doctest.DocFileSuite('scenario_sale.rst',
-            setUp=doctest_dropdb, tearDown=doctest_dropdb, encoding='utf-8',
+            setUp=doctest_setup, tearDown=doctest_teardown, encoding='utf-8',
             optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
     return suite
