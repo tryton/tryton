8aeb1767e13a95a31b67fd98bb34abcfedba37ac
Cédric Krier <ced@b2ck.com>
2014-09-04 08:44:49 +0200

    Use new doctest setup/teardown
    issue3627
    review7401002

--- tests/test_account_statement.py
+++ tests/test_account_statement.py
@@ -3,7 +3,8 @@
 import unittest
 import doctest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends, doctest_dropdb
+from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import doctest_setup, doctest_teardown
 
 
 class AccountStatementTestCase(unittest.TestCase):
@@ -26,6 +27,6 @@ def suite():
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
         AccountStatementTestCase))
     suite.addTests(doctest.DocFileSuite('scenario_account_statement.rst',
-        setUp=doctest_dropdb, tearDown=doctest_dropdb, encoding='utf-8',
+        setUp=doctest_setup, tearDown=doctest_teardown, encoding='utf-8',
             optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
     return suite
