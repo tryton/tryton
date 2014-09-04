21f4856c0f9ed2286d57ed2b2876c32fc515b003
Cédric Krier <ced@b2ck.com>
2014-09-04 08:45:52 +0200

    Use new doctest setup/teardown
    issue3627
    review7401002

--- tests/test_stock_supply.py
+++ tests/test_stock_supply.py
@@ -6,7 +6,8 @@ import datetime
 from decimal import Decimal
 import trytond.tests.test_tryton
 from trytond.tests.test_tryton import POOL, DB_NAME, USER, CONTEXT, test_view,\
-    test_depends, doctest_dropdb
+    test_depends
+from trytond.tests.test_tryton import doctest_setup, doctest_teardown
 from trytond.transaction import Transaction
 
 DATES = [
@@ -132,6 +133,6 @@ def suite():
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
         StockSupplyTestCase))
     suite.addTests(doctest.DocFileSuite('scenario_stock_internal_supply.rst',
-            setUp=doctest_dropdb, tearDown=doctest_dropdb, encoding='utf-8',
+            setUp=doctest_setup, tearDown=doctest_teardown, encoding='utf-8',
             optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
     return suite
