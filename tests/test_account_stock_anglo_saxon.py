a8e4a5883462c417518291cb9327d24dffb70616
Cédric Krier <ced@b2ck.com>
2014-09-04 08:44:50 +0200

    Use new doctest setup/teardown
    issue3627
    review7401002

--- tests/test_account_stock_anglo_saxon.py
+++ tests/test_account_stock_anglo_saxon.py
@@ -3,7 +3,8 @@
 import unittest
 import doctest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends, doctest_dropdb
+from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import doctest_setup, doctest_teardown
 
 
 class AccountStockAngloSaxonTestCase(unittest.TestCase):
@@ -27,10 +28,10 @@ def suite():
         AccountStockAngloSaxonTestCase))
     suite.addTests(doctest.DocFileSuite(
             'scenario_account_stock_anglo_saxon.rst',
-            setUp=doctest_dropdb, tearDown=doctest_dropdb, encoding='utf-8',
+            setUp=doctest_setup, tearDown=doctest_teardown, encoding='utf-8',
             optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
     suite.addTests(doctest.DocFileSuite(
             'scenario_account_stock_anglo_saxon_with_drop_shipment.rst',
-            setUp=doctest_dropdb, tearDown=doctest_dropdb, encoding='utf-8',
+            setUp=doctest_setup, tearDown=doctest_teardown, encoding='utf-8',
             optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
     return suite
