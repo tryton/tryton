c5dec91e74a50158de9e9733f3c8a455cceb9ae2
Cédric Krier <ced@b2ck.com>
2014-09-04 08:45:29 +0200

    Use new doctest setup/teardown
    issue3627
    review7401002

--- tests/test_purchase_shipment_cost.py
+++ tests/test_purchase_shipment_cost.py
@@ -4,7 +4,8 @@
 import unittest
 import doctest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends, doctest_dropdb
+from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import doctest_setup, doctest_teardown
 
 
 class PurchaseShipmentCostTestCase(unittest.TestCase):
@@ -27,14 +28,14 @@ def suite():
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
         PurchaseShipmentCostTestCase))
     suite.addTests(doctest.DocFileSuite('scenario_purchase_shipment_cost.rst',
-            setUp=doctest_dropdb, tearDown=doctest_dropdb, encoding='utf-8',
+            setUp=doctest_setup, tearDown=doctest_teardown, encoding='utf-8',
             optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
     suite.addTests(doctest.DocFileSuite(
             'scenario_purchase_shipment_cost_with_account_stock.rst',
-            setUp=doctest_dropdb, tearDown=doctest_dropdb, encoding='utf-8',
+            setUp=doctest_setup, tearDown=doctest_teardown, encoding='utf-8',
             optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
     suite.addTests(doctest.DocFileSuite(
             'scenario_purchase_shipment_cost_with_account_stock_anglo_saxon.rst',
-            setUp=doctest_dropdb, tearDown=doctest_dropdb, encoding='utf-8',
+            setUp=doctest_setup, tearDown=doctest_teardown, encoding='utf-8',
             optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
     return suite
