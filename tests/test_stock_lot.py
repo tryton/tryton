32a96bafe1082fdabcceed67382f14014ff6ff03
Cédric Krier <ced@b2ck.com>
2014-09-04 08:45:46 +0200

    Use new doctest setup/teardown
    issue3627
    review7401002

--- tests/test_stock_lot.py
+++ tests/test_stock_lot.py
@@ -8,7 +8,8 @@ from decimal import Decimal
 
 import trytond.tests.test_tryton
 from trytond.tests.test_tryton import POOL, DB_NAME, USER, CONTEXT, test_view,\
-    test_depends, doctest_dropdb
+    test_depends
+from trytond.tests.test_tryton import doctest_setup, doctest_teardown
 from trytond.transaction import Transaction
 
 
@@ -243,6 +244,6 @@ def suite():
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
             StockLotTestCase))
     suite.addTests(doctest.DocFileSuite('scenario_stock_lot_shipment_out.rst',
-        setUp=doctest_dropdb, tearDown=doctest_dropdb, encoding='utf-8',
+        setUp=doctest_setup, tearDown=doctest_teardown, encoding='utf-8',
             optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
     return suite
