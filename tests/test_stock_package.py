6c8dc5f72afe72d22c18b9eba555f1dabdac6001
Cédric Krier <ced@b2ck.com>
2014-09-04 08:45:48 +0200

    Use new doctest setup/teardown
    issue3627
    review7401002

--- tests/test_stock_package.py
+++ tests/test_stock_package.py
@@ -3,8 +3,8 @@
 import unittest
 import doctest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends, \
-    doctest_dropdb
+from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import doctest_setup, doctest_teardown
 
 
 class StockPackageTestCase(unittest.TestCase):
@@ -33,6 +33,6 @@ def suite():
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
             StockPackageTestCase))
     suite.addTests(doctest.DocFileSuite('scenario_stock_package.rst',
-            setUp=doctest_dropdb, tearDown=doctest_dropdb, encoding='utf-8',
+            setUp=doctest_setup, tearDown=doctest_teardown, encoding='utf-8',
             optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
     return suite
