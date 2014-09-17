50fd8169863ddbd7b22f5468fdc383d54c91c084
Cédric Krier <ced@b2ck.com>
2014-09-17 21:45:10 +0200

    Use context to check model and field access
    issue4081
    review8481002

--- invoice.py
+++ invoice.py
@@ -128,8 +128,7 @@ class InvoiceLine:
                     accounts = []
                     if not line.analytic_accounts:
                         # Create missing selection
-                        with Transaction().set_user(0):
-                                selection, = Selection.create([{}])
+                        selection, = Selection.create([{}])
                         cls.write([line], {
                                 'analytic_accounts': selection.id,
                                 })
