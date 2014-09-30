df2f0ccc127855abe9e22ad676e54902c2aa370c
Cédric Krier <ced@b2ck.com>
2014-09-30 09:44:28 +0200

    Round quantity before using it on invoice line or stock move
    Precision can be lost also with only addition and substraction because of
    float, so quantity must be rounded before used on invoice line or stock move.
    
    issue4191
    review13551002
    (grafted from b1e1adc84de0956c8d27b26349e7c2ac7599925e)
    
    --HG--
    branch : 2.6

--- purchase.py
+++ purchase.py
@@ -1152,10 +1152,12 @@ class PurchaseLine(ModelSQL, ModelView):
             if old_invoice_line.id not in skip_ids:
                 quantity -= Uom.compute_qty(old_invoice_line.unit,
                         old_invoice_line.quantity, self.unit)
-        invoice_line.quantity = quantity
 
-        if invoice_line.quantity <= 0.0:
+        rounding = self.unit.rounding if self.unit else 0
+        invoice_line.quantity = Uom.round(quantity, rounding)
+        if invoice_line.quantity <= 0:
             return []
+
         invoice_line.unit = self.unit
         invoice_line.product = self.product
         invoice_line.unit_price = self.unit_price
@@ -1205,8 +1207,11 @@ class PurchaseLine(ModelSQL, ModelView):
             if move not in skip:
                 quantity -= Uom.compute_qty(move.uom, move.quantity,
                     self.unit)
-        if quantity <= 0.0:
+
+        quantity = Uom.round(quantity, self.unit.rounding)
+        if quantity <= 0:
             return
+
         if not self.purchase.party.supplier_location:
             self.raise_user_error('supplier_location_required')
         with Transaction().set_user(0, set_context=True):
