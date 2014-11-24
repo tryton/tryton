63c42a2e6feee93375482d9126e84d4e0c5c9ae0
Robin Baumgartner <robin@baumgartners.ch>
2014-11-24 14:02:22 +0100

    Use Null constant from python-sql for comparisons
    issue4143
    review13701003

--- work.py
+++ work.py
@@ -1,5 +1,7 @@
 #This file is part of Tryton.  The COPYRIGHT file at the top level of
 #this repository contains the full copyright notices and license terms.
+from sql import Null
+
 from trytond.model import ModelView, ModelSQL, fields
 from trytond.pyson import Eval
 from trytond import backend
@@ -58,7 +60,7 @@ class Work(ModelSQL, ModelView):
     @staticmethod
     def order_sequence(tables):
         table, _ = tables[None]
-        return [table.sequence == None, table.sequence]
+        return [table.sequence == Null, table.sequence]
 
     @staticmethod
     def default_type():
