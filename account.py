ebeca506637eddf4b411759082b5a29de890ec82
Cédric Krier <ced@b2ck.com>
2014-12-02 12:54:35 +0100

    Fix most flake8 errors
    review8771002

--- account.py
+++ account.py
@@ -1,8 +1,7 @@
-#This file is part of Tryton.  The COPYRIGHT file at the top level of
-#this repository contains the full copyright notices and license terms.
+# This file is part of Tryton.  The COPYRIGHT file at the top level of
+# this repository contains the full copyright notices and license terms.
 from trytond.pool import Pool, PoolMeta
 from trytond.model import fields
-from trytond.transaction import Transaction
 
 
 __all__ = ['Configuration', 'MoveLine']
