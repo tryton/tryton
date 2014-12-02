d359a162b2c15f7d4c14677810dcdc5843a0a150
Cédric Krier <ced@b2ck.com>
2014-12-02 12:55:17 +0100

    Fix most flake8 errors
    review8771002

--- scripts/currencies.py
+++ scripts/currencies.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-#This file is part of Tryton.  The COPYRIGHT file at the top level of
-#this repository contains the full copyright notices and license terms.
+# This file is part of Tryton.  The COPYRIGHT file at the top level of
+# this repository contains the full copyright notices and license terms.
 
 import sys
 import pycountry
