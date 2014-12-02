fee43a0043d3c7ccde08a3a1f1226dafe4b5ec1b
Cédric Krier <ced@b2ck.com>
2014-12-02 12:56:08 +0100

    Fix most flake8 errors
    review8771002

--- tests/test_stock_location_sequence.py
+++ tests/test_stock_location_sequence.py
@@ -1,5 +1,5 @@
-#This file is part of Tryton.  The COPYRIGHT file at the top level of
-#this repository contains the full copyright notices and license terms.
+# This file is part of Tryton.  The COPYRIGHT file at the top level of
+# this repository contains the full copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
 from trytond.tests.test_tryton import test_view, test_depends
