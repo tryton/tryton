b8f1b8558e38c1f39962a71ac4fd5d348b781df6
Cédric Krier <ced@b2ck.com>
2014-12-02 12:55:54 +0100

    Fix most flake8 errors
    review8771002

--- sale.py
+++ sale.py
@@ -1,8 +1,7 @@
-#This file is part of Tryton.  The COPYRIGHT file at the top level of this
-#repository contains the full copyright notices and license terms.
+# This file is part of Tryton.  The COPYRIGHT file at the top level of
+# this repository contains the full copyright notices and license terms.
 
 from trytond.pool import Pool, PoolMeta
-from trytond.transaction import Transaction
 
 __metaclass__ = PoolMeta
 __all__ = ['Sale']
