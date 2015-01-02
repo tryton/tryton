6422dda95b65d645af4248641a57aa04dcceaf88
Nicolas Évrard <nicoe@b2ck.com>
2015-01-02 16:08:10 +0100

    Add test for access rights of menu and actions
    issue4295
    review12661002

--- tests/test_stock_package.py
+++ tests/test_stock_package.py
@@ -3,7 +3,7 @@
 import unittest
 import doctest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import test_view, test_depends, test_menu_action
 from trytond.tests.test_tryton import doctest_setup, doctest_teardown
 
 
@@ -26,6 +26,10 @@ class StockPackageTestCase(unittest.TestCase):
         Test depends.
         '''
         test_depends()
+
+    def test0007menu_actions(self):
+        'Test menu actions'
+        test_menu_action('stock_package')
 
 
 def suite():
