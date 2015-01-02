011ad55703f393c8e4a9c920a392f0981a3d1782
Nicolas Évrard <nicoe@b2ck.com>
2015-01-02 16:07:53 +0100

    Add test for access rights of menu and actions
    issue4295
    review12661002

--- tests/test_account_invoice_line_standalone.py
+++ tests/test_account_invoice_line_standalone.py
@@ -2,7 +2,7 @@
 # this repository contains the full copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import test_view, test_depends, test_menu_action
 
 
 class AccountInvoiceLineStandaloneTestCase(unittest.TestCase):
@@ -19,6 +19,10 @@ class AccountInvoiceLineStandaloneTestCase(unittest.TestCase):
     def test0006depends(self):
         'Test depends'
         test_depends()
+
+    def test0007menu_actions(self):
+        'Test menu actions'
+        test_menu_action('account_invoice_line_standalone')
 
 
 def suite():
