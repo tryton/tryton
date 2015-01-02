696cd1f340b645ebafe412a12fd847a3fb00d65b
Nicolas Évrard <nicoe@b2ck.com>
2015-01-02 16:08:08 +0100

    Add test for access rights of menu and actions
    issue4295
    review12661002

--- tests/test_purchase_shipment_cost.py
+++ tests/test_purchase_shipment_cost.py
@@ -4,7 +4,7 @@
 import unittest
 import doctest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import test_view, test_depends, test_menu_action
 from trytond.tests.test_tryton import doctest_setup, doctest_teardown
 
 
@@ -21,6 +21,10 @@ class PurchaseShipmentCostTestCase(unittest.TestCase):
     def test0006depends(self):
         'Test depends'
         test_depends()
+
+    def test0007menu_actions(self):
+        'Test menu actions'
+        test_menu_action('purchase_shipment_cost')
 
 
 def suite():
