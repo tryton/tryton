b425b9821275b67d46c5f9efddc6e4e66156dbbe
Nicolas Évrard <nicoe@b2ck.com>
2015-01-02 16:07:52 +0100

    Add test for access rights of menu and actions
    issue4295
    review12661002

--- tests/test_account_dunning.py
+++ tests/test_account_dunning.py
@@ -3,7 +3,7 @@
 import unittest
 import doctest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import test_view, test_depends, test_menu_action
 from trytond.tests.test_tryton import doctest_setup, doctest_teardown
 
 
@@ -20,6 +20,10 @@ class AccountDunningTestCase(unittest.TestCase):
     def test0006depends(self):
         'Test depends'
         test_depends()
+
+    def test0007menu_actions(self):
+        'Test menu actions'
+        test_menu_action('account_dunning')
 
 
 def suite():
