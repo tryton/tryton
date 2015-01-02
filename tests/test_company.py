32ea9897a13930c8eb1b4812d5d76abe24dc510e
Nicolas Évrard <nicoe@b2ck.com>
2015-01-02 16:08:00 +0100

    Add test for access rights of menu and actions
    issue4295
    review12661002

--- tests/test_company.py
+++ tests/test_company.py
@@ -3,7 +3,7 @@
 import unittest
 import trytond.tests.test_tryton
 from trytond.tests.test_tryton import POOL, DB_NAME, USER, CONTEXT, test_view,\
-    test_depends
+    test_depends, test_menu_action
 from trytond.transaction import Transaction
 
 
@@ -25,6 +25,10 @@ class CompanyTestCase(unittest.TestCase):
     def test0006depends(self):
         'Test depends'
         test_depends()
+
+    def test0007menu_actions(self):
+        'Test menu actions'
+        test_menu_action('company')
 
     def test0010company(self):
         'Create company'
