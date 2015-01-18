187178d32870a99c9c11709b43973fed58c96faa
Cédric Krier <ced@b2ck.com>
2015-01-18 22:58:52 +0100

    Skip test_model_graph if pydot is not installed
    As pydot doesn't always install correctly, we don't want the test to fail
    because of it.
    (grafted from a96437a34fc22b2eb438208379301576a3150013)
    
    --HG--
    branch : 3.4

--- proteus/tests/test_report.py
+++ proteus/tests/test_report.py
@@ -1,12 +1,18 @@
 #This file is part of Tryton.  The COPYRIGHT file at the top level of
 #this repository contains the full copyright notices and license terms.
-from unittest import TestCase
-from proteus import config, Report, Model
+import unittest
+try:
+    import pydot
+except ImportError:
+    pydot = None
+
+from proteus import Report, Model
 from .common import ProteusTestCase
 
 
 class TestReport(ProteusTestCase):
 
+    @unittest.skipIf(not pydot, 'requires pydot')
     def test_model_graph(self):
         IrModel = Model.get('ir.model')
         models = IrModel.find([])
