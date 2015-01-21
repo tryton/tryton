88d36af3657876543f08f8bc521eb6fcbffcaa81
Cédric Krier <ced@b2ck.com>
2015-01-21 17:35:19 +0100

    Add missing account_statement in tests_require

--- setup.py
+++ setup.py
@@ -47,7 +47,8 @@ for dep in info.get('depends', []):
         requires.append(get_require_version('trytond_%s' % dep))
 requires.append(get_require_version('trytond'))
 
-tests_require = [get_require_version('proteus')]
+tests_require = [get_require_version('proteus'),
+    get_require_version('trytond_account_statement')]
 dependency_links = []
 if minor_version % 2:
     # Add development index for testing with proteus
