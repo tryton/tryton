641f58b3eaa073deeeca5e43ce4f4eadefd56a13
Cédric Krier <ced@b2ck.com>
2015-01-25 19:40:58 +0100

    Add tox and drone
    issue4431
    (grafted from 28a76e76dd245f7ba61f83f5101bb7dae74d6af5)
    
    --HG--
    branch : 2.8

--- setup.py
+++ setup.py
@@ -30,6 +30,9 @@ for dep in info.get('depends', []):
 requires.append('trytond >= %s.%s, < %s.%s' %
     (major_version, minor_version, major_version, minor_version + 1))
 
+tests_require = ['proteus >= %s.%s, < %s.%s' %
+    (major_version, minor_version, major_version, minor_version + 1)]
+
 setup(name='trytond_purchase_invoice_line_standalone',
     version=info.get('version', '0.0.1'),
     description='Tryton module for standalone invoice line from purchase',
@@ -79,4 +82,5 @@ setup(name='trytond_purchase_invoice_line_standalone',
     """,
     test_suite='tests',
     test_loader='trytond.test_loader:Loader',
+    tests_require=tests_require,
     )
