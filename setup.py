4308f3716b9e232e19d4805184bd561973b6057f
Cédric Krier <ced@b2ck.com>
2015-01-26 12:59:19 +0100

    Add tox and drone
    issue4431
    
    --HG--
    branch : 3.2

--- setup.py
+++ setup.py
@@ -47,6 +47,10 @@ for dep in info.get('depends', []):
 requires.append(get_require_version('trytond'))
 
 tests_require = [get_require_version('proteus')]
+dependency_links = []
+if minor_version % 2:
+    # Add development index for testing with proteus
+    dependency_links.append('https://trydevpi.tryton.org/')
 
 setup(name=name,
     version=version,
@@ -90,6 +94,7 @@ setup(name=name,
         ],
     license='GPL-3',
     install_requires=requires,
+    dependency_links=dependency_links,
     zip_safe=False,
     entry_points="""
     [trytond.modules]
