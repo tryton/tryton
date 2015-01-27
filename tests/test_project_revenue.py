2c54ec4dc3f91100e3221bb83b8dc002576dcf95
Cédric Krier <ced@b2ck.com>
2015-01-27 18:13:54 +0100

    Add check for default model access
    When group access exists, a default model access should be defined otherwise
    all access are given to non group users.
    
    issue4485
    review5941002

--- tests/test_project_revenue.py
+++ tests/test_project_revenue.py
@@ -2,26 +2,12 @@
 # this repository contains the full copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends, test_menu_action
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class ProjectRevenueTestCase(unittest.TestCase):
+class ProjectRevenueTestCase(ModuleTestCase):
     'Test ProjectRevenue module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('project_revenue')
-
-    def test0005views(self):
-        'Test views'
-        test_view('project_revenue')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
-
-    def test0007menu_actions(self):
-        'Test menu actions'
-        test_menu_action('project_revenue')
+    module = 'project_revenue'
 
 
 def suite():
