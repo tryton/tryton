d1fe233fe02d7c3f1efdc12109174a458ef5c510
Cédric Krier <ced@b2ck.com>
2015-01-27 18:13:34 +0100

    Add check for default model access
    When group access exists, a default model access should be defined otherwise
    all access are given to non group users.
    
    issue4485
    review5941002

--- tests/test_google_maps.py
+++ tests/test_google_maps.py
@@ -2,26 +2,12 @@
 # this repository contains the full copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends, test_menu_action
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class GoogleMapsTestCase(unittest.TestCase):
+class GoogleMapsTestCase(ModuleTestCase):
     'Test GoogleMaps module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('google_maps')
-
-    def test0005views(self):
-        'Test views'
-        test_view('google_maps')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
-
-    def test0007menu_actions(self):
-        'Test menu actions'
-        test_menu_action('google_maps')
+    module = 'google_maps'
 
 
 def suite():
