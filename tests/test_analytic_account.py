3e703ef35f49d465d071d70ff6cc75c39a259ce2
Sergi Almacellas Abellana <sergi@koolpi.com>
2015-02-15 12:07:35 +0100

    Add doctest scenario
    issue4528
    review11921002

--- tests/test_analytic_account.py
+++ tests/test_analytic_account.py
@@ -6,6 +6,7 @@ from decimal import Decimal
 import trytond.tests.test_tryton
 from trytond.tests.test_tryton import ModuleTestCase
 from trytond.tests.test_tryton import POOL, DB_NAME, USER, CONTEXT
+from trytond.tests.test_tryton import doctest_setup, doctest_teardown
 from trytond.transaction import Transaction
 
 
@@ -139,4 +140,7 @@ def suite():
             suite.addTest(test)
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
         AnalyticAccountTestCase))
+    suite.addTests(doctest.DocFileSuite('scenario_analytic_account.rst',
+            setUp=doctest_setup, tearDown=doctest_teardown, encoding='utf-8',
+            optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
     return suite
