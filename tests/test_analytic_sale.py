1639e0b81131edd5853c16250a4316348970c79a
Sergi Almacellas Abellana <sergi@koolpi.com>
2015-02-16 18:30:59 +0100

    Prefix scenario name with analytic
    issue4528

--- tests/test_analytic_sale.py
+++ tests/test_analytic_sale.py
@@ -16,7 +16,7 @@ def suite():
     suite = trytond.tests.test_tryton.suite()
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
         AnalyticSaleTestCase))
-    suite.addTests(doctest.DocFileSuite('scenario_sale.rst',
+    suite.addTests(doctest.DocFileSuite('scenario_analytic_sale.rst',
             setUp=doctest_setup, tearDown=doctest_teardown, encoding='utf-8',
             optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
     return suite
