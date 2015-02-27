284d7f51a4c63e2bd88a575d8d9f2ba06ba83857
Cédric Krier <ced@b2ck.com>
2015-02-27 11:23:20 +0100

    Add support for tax included price list
    issue4446
    review13851002

--- product.py
+++ product.py
@@ -16,18 +16,25 @@ class Product:
         PriceList = pool.get('product.price_list')
         Party = pool.get('party.party')
         Uom = pool.get('product.uom')
+        Tax = pool.get('account.tax')
+        context = Transaction().context
 
         prices = super(Product, cls).get_sale_price(products,
             quantity=quantity)
-        if (Transaction().context.get('price_list')
-                and Transaction().context.get('customer')):
+        if context.get('price_list') and context.get('customer'):
             price_list = PriceList(Transaction().context['price_list'])
             customer = Party(Transaction().context['customer'])
             context_uom = None
-            if Transaction().context.get('uom'):
+            if context.get('uom'):
                 context_uom = Uom(Transaction().context['uom'])
+            taxes = None
+            if context.get('taxes'):
+                taxes = Tax.browse(context.get('taxes'))
             for product in products:
                 uom = context_uom or product.default_uom
-                prices[product.id] = price_list.compute(customer, product,
-                    prices[product.id], quantity, uom)
+                price = price_list.compute(
+                     customer, product, prices[product.id], quantity, uom)
+                if price_list.tax_included and taxes:
+                    price = Tax.reverse_compute(price, taxes)
+                prices[product.id] = price
         return prices
