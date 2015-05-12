652207a79d6d0e2b87bc01087d864ac9687a0377
Cédric Krier <ced@b2ck.com>
2015-05-12 09:56:55 +0200

    Typo missing space
    issue4692
    review9251002

--- tax.py
+++ tax.py
@@ -634,7 +634,7 @@ class Tax(ModelSQL, ModelView):
             },
         depends=['parent'],
         help=('If checked then the unit price for further tax computation will'
-            'be modified by this tax'))
+            ' be modified by this tax'))
     parent = fields.Many2One('account.tax', 'Parent', ondelete='CASCADE')
     childs = fields.One2Many('account.tax', 'parent', 'Children')
     company = fields.Many2One('company.company', 'Company', required=True,
