b1a6a5869bfc805b06d79293e1c016fe74601600
Cédric Krier <ced@b2ck.com>
2015-08-10 10:40:47 +0200

    Initial commit
    issue4796
    review12171002

---
+++ __init__.py
@@ -0,0 +1,16 @@
+# This file is part of Tryton.  The COPYRIGHT file at the top level of
+# this repository contains the full copyright notices and license terms.
+
+from trytond.pool import Pool
+from customs import *
+from product import *
+
+
+def register():
+    Pool.register(
+        TariffCode,
+        DutyRate,
+        Category,
+        Template,
+        Product_TariffCode,
+        module='customs', type_='model')
