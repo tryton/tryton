e887ff449cdda00f8c9f6bcee17580df1b71a6eb
David Harper <google@unicode2013.org>
2015-08-28 10:50:30 +0200

    Fix calculation of displayed writeoff amount
    issue4917
    review16541002
    (grafted from 90d4db8f1491dc4916759ce6bd36bc51253863f1)
    
    --HG--
    branch : 3.6

--- invoice.py
+++ invoice.py
@@ -2408,7 +2408,7 @@ class PayInvoiceAsk(ModelView):
         return 'partial'
 
     @fields.depends('lines', 'amount', 'currency', 'currency_writeoff',
-        'invoice')
+        'invoice', 'payment_lines')
     def on_change_lines(self):
         Currency = Pool().get('currency.currency')
 
@@ -2419,6 +2419,8 @@ class PayInvoiceAsk(ModelView):
         self.amount_writeoff = Decimal('0.0')
         for line in self.lines:
             self.amount_writeoff += line.debit - line.credit
+        for line in self.payment_lines:
+            self.amount_writeoff += line.debit - line.credit            
         if self.invoice.type in ('in_invoice', 'out_credit_note'):
             self.amount_writeoff = - self.amount_writeoff - amount
         else:
