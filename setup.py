0e2be8ade5054d5a376649dee60a00f3e1b866fb
Cédric Krier <ced@b2ck.com>
2015-09-02 10:26:24 +0200

    Fix package name of stock_supply in tests_require

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ requires.append(get_require_version('trytond'))
 
 tests_require = [
     get_require_version('proteus'),
-    get_require_version('stock_supply'),
+    get_require_version('trytond_stock_supply'),
     ]
 dependency_links = []
 if minor_version % 2:
