947985218bb81a44345b1d2f7cc81148d29bf775
Cédric Krier <ced@b2ck.com>
2015-10-02 22:00:10 +0200

    Reload context before getting preferences

--- src/sao.js
+++ src/sao.js
@@ -191,33 +191,35 @@ var Sao = {};
 
     Sao.get_preferences = function() {
         var session = Sao.Session.current_session;
-        return Sao.rpc({
-            'method': 'model.res.user.get_preferences',
-            'params': [false, {}]
-        }, session).then(function(preferences) {
-            var deferreds = [];
-            deferreds.push(Sao.common.MODELACCESS.load_models());
-            deferreds.push(Sao.common.ICONFACTORY.load_icons());
-            deferreds.push(Sao.common.MODELHISTORY.load_history());
-            deferreds.push(Sao.common.VIEW_SEARCH.load_searches());
-            return jQuery.when.apply(jQuery, deferreds).then(function() {
-                (preferences.actions || []).forEach(function(action_id) {
-                    Sao.Action.execute(action_id, {}, null, {});
-                });
-                var title = 'Tryton';
-                if (!jQuery.isEmptyObject(preferences.status_bar)) {
-                    title += ' - ' + preferences.status_bar;
-                }
-                document.title = title;
-                var new_lang = preferences.language != Sao.i18n.getLocale();
-                var prm = jQuery.Deferred();
-                Sao.i18n.setlang(preferences.language).always(function() {
-                    if (new_lang) {
-                        Sao.user_menu(preferences);
+        return session.reload_context().then(function() {
+            return Sao.rpc({
+                'method': 'model.res.user.get_preferences',
+                'params': [false, {}]
+            }, session).then(function(preferences) {
+                var deferreds = [];
+                deferreds.push(Sao.common.MODELACCESS.load_models());
+                deferreds.push(Sao.common.ICONFACTORY.load_icons());
+                deferreds.push(Sao.common.MODELHISTORY.load_history());
+                deferreds.push(Sao.common.VIEW_SEARCH.load_searches());
+                return jQuery.when.apply(jQuery, deferreds).then(function() {
+                    (preferences.actions || []).forEach(function(action_id) {
+                        Sao.Action.execute(action_id, {}, null, {});
+                    });
+                    var title = 'Tryton';
+                    if (!jQuery.isEmptyObject(preferences.status_bar)) {
+                        title += ' - ' + preferences.status_bar;
                     }
-                    prm.resolve(preferences);
+                    document.title = title;
+                    var new_lang = preferences.language != Sao.i18n.getLocale();
+                    var prm = jQuery.Deferred();
+                    Sao.i18n.setlang(preferences.language).always(function() {
+                        if (new_lang) {
+                            Sao.user_menu(preferences);
+                        }
+                        prm.resolve(preferences);
+                    });
+                    return prm;
                 });
-                return prm;
             });
         });
     };
