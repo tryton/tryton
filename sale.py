f5a99a00cd4a2b16df068e20e5dc3b4204870c24
Cédric Krier <ced@b2ck.com>
2015-10-18 19:28:46 +0200

    Fix typo introduced in changeset a28587a2078f when resolving graft
    --HG--
    branch : 2.8

--- sale.py
+++ sale.py
@@ -687,7 +687,7 @@ class Sale(Workflow, ModelSQL, ModelView):
 
         invoice = self._get_invoice_sale(invoice_type)
         invoice.lines = list(chain.from_iterable(invoice_lines[l.id]
-                for l in self.lines if l.id in invoice_lines)))
+                for l in self.lines if l.id in invoice_lines))
         invoice.save()
 
         with Transaction().set_user(0, set_context=True):
