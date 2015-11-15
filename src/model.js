200d1d0bf353baf688e1b74bf802ddd89ac5511e
Cédric Krier <ced@b2ck.com>
2015-11-15 12:41:13 -0300

    Construct the Record.expr_eval context the same way as tryton
    issue5122
    review23641002
    (grafted from a2e1982c0416c9315182d7f4d9cc1e0de4aa788b)
    
    --HG--
    branch : 3.8

--- src/model.js
+++ src/model.js
@@ -1019,8 +1019,9 @@
         },
         expr_eval: function(expr) {
             if (typeof(expr) != 'string') return expr;
-            var ctx = jQuery.extend({}, this.get_context());
-            ctx.context = jQuery.extend({}, this.model.session.context, ctx);
+            var ctx = jQuery.extend({}, this.model.session.context);
+            ctx.context = jQuery.extend({}, ctx.context);
+            jQuery.extend(ctx.context, this.get_context());
             jQuery.extend(ctx, this.get_eval());
             ctx.active_model = this.model.name;
             ctx.active_id = this.id;
