0c4347f3dc995a68b6a0de90782fbb6f1ad1cdd0
Cédric Krier <ced@b2ck.com>
2016-02-06 19:12:24 +0100

    Make TestCase create and drop its database
    and use with_transaction decorator.
    
    issue5263
    review20711002

--- tests/test_bank.py
+++ tests/test_bank.py
@@ -2,63 +2,61 @@
 # this repository contains the full copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import ModuleTestCase
-from trytond.tests.test_tryton import POOL, DB_NAME, USER, CONTEXT
-from trytond.transaction import Transaction
+from trytond.tests.test_tryton import ModuleTestCase, with_transaction
+from trytond.pool import Pool
 
 
 class BankTestCase(ModuleTestCase):
     'Test Bank module'
     module = 'bank'
 
-    def setUp(self):
-        super(BankTestCase, self).setUp()
-        self.bank = POOL.get('bank')
-        self.party = POOL.get('party.party')
-        self.account = POOL.get('bank.account')
-        self.number = POOL.get('bank.account.number')
-
-    def test0010iban_format(self):
+    @with_transaction()
+    def test_iban_format(self):
         'Test IBAN format'
-        with Transaction().start(DB_NAME, USER, context=CONTEXT):
-            party = self.party(name='Test')
-            party.save()
-            bank = self.bank(party=party)
-            bank.save()
-            account, = self.account.create([{
-                        'bank': bank.id,
-                        'numbers': [('create', [{
-                                        'type': 'iban',
-                                        'number': 'BE82068896274468',
-                                        }, {
-                                        'type': 'other',
-                                        'number': 'not IBAN',
-                                        }])],
-                        }])
+        pool = Pool()
+        Party = pool.get('party.party')
+        Bank = pool.get('bank')
+        Account = pool.get('bank.account')
+        Number = pool.get('bank.account.number')
 
-            iban_number, other_number = account.numbers
-            self.assertEqual(iban_number.type, 'iban')
-            self.assertEqual(other_number.type, 'other')
+        party = Party(name='Test')
+        party.save()
+        bank = Bank(party=party)
+        bank.save()
+        account, = Account.create([{
+                    'bank': bank.id,
+                    'numbers': [('create', [{
+                                    'type': 'iban',
+                                    'number': 'BE82068896274468',
+                                    }, {
+                                    'type': 'other',
+                                    'number': 'not IBAN',
+                                    }])],
+                    }])
 
-            # Test format on create
-            self.assertEqual(iban_number.number, 'BE82 0688 9627 4468')
-            self.assertEqual(other_number.number, 'not IBAN')
+        iban_number, other_number = account.numbers
+        self.assertEqual(iban_number.type, 'iban')
+        self.assertEqual(other_number.type, 'other')
 
-            # Test format on write
-            iban_number.number = 'BE82068896274468'
-            iban_number.type = 'iban'
-            iban_number.save()
-            self.assertEqual(iban_number.number, 'BE82 0688 9627 4468')
+        # Test format on create
+        self.assertEqual(iban_number.number, 'BE82 0688 9627 4468')
+        self.assertEqual(other_number.number, 'not IBAN')
 
-            other_number.number = 'still not IBAN'
-            other_number.save()
-            self.assertEqual(other_number.number, 'still not IBAN')
+        # Test format on write
+        iban_number.number = 'BE82068896274468'
+        iban_number.type = 'iban'
+        iban_number.save()
+        self.assertEqual(iban_number.number, 'BE82 0688 9627 4468')
 
-            self.number.write([iban_number, other_number], {
-                    'number': 'BE82068896274468',
-                    })
-            self.assertEqual(iban_number.number, 'BE82 0688 9627 4468')
-            self.assertEqual(other_number.number, 'BE82068896274468')
+        other_number.number = 'still not IBAN'
+        other_number.save()
+        self.assertEqual(other_number.number, 'still not IBAN')
+
+        Number.write([iban_number, other_number], {
+                'number': 'BE82068896274468',
+                })
+        self.assertEqual(iban_number.number, 'BE82 0688 9627 4468')
+        self.assertEqual(other_number.number, 'BE82068896274468')
 
 
 def suite():
