3252952b0972df876da6342671058267c31ec7a1
Cédric Krier <ced@b2ck.com>
2016-02-06 19:13:18 +0100

    Make TestCase create and drop its database
    and use with_transaction decorator.
    
    issue5263
    review20711002

--- tests/test_stock_forecast.py
+++ tests/test_stock_forecast.py
@@ -1,81 +1,72 @@
 # This file is part of Tryton.  The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
 import unittest
-import doctest
 from decimal import Decimal
 import datetime
 from dateutil.relativedelta import relativedelta
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import ModuleTestCase
-from trytond.tests.test_tryton import POOL, DB_NAME, USER, CONTEXT
+from trytond.tests.test_tryton import ModuleTestCase, with_transaction
 from trytond.transaction import Transaction
+from trytond.pool import Pool
+
+from trytond.modules.company.tests import create_company, set_company
 
 
 class StockForecastTestCase(ModuleTestCase):
     'Test StockForecast module'
     module = 'stock_forecast'
 
-    def setUp(self):
-        super(StockForecastTestCase, self).setUp()
-        self.category = POOL.get('product.category')
-        self.uom = POOL.get('product.uom')
-        self.template = POOL.get('product.template')
-        self.product = POOL.get('product.product')
-        self.location = POOL.get('stock.location')
-        self.company = POOL.get('company.company')
-        self.user = POOL.get('res.user')
-        self.forecast = POOL.get('stock.forecast')
-        self.line = POOL.get('stock.forecast.line')
-        self.move = POOL.get('stock.move')
-        self.forecast_complete = POOL.get('stock.forecast.complete',
-            type='wizard')
-
-    def test0020distribute(self):
+    @with_transaction()
+    def test_distribute(self):
         'Test distribute'
-        with Transaction().start(DB_NAME, USER, context=CONTEXT):
-            line = self.line()
-            for values, result in (
-                    ((1, 5), {0: 5}),
-                    ((4, 8), {0: 2, 1: 2, 2: 2, 3: 2}),
-                    ((2, 5), {0: 2, 1: 3}),
-                    ((10, 4), {0: 0, 1: 1, 2: 0, 3: 1, 4: 0,
-                            5: 0, 6: 1, 7: 0, 8: 1, 9: 0}),
-                    ):
-                self.assertEqual(line.distribute(*values), result)
+        pool = Pool()
+        Line = pool.get('stock.forecast.line')
+        line = Line()
+        for values, result in (
+                ((1, 5), {0: 5}),
+                ((4, 8), {0: 2, 1: 2, 2: 2, 3: 2}),
+                ((2, 5), {0: 2, 1: 3}),
+                ((10, 4), {0: 0, 1: 1, 2: 0, 3: 1, 4: 0,
+                        5: 0, 6: 1, 7: 0, 8: 1, 9: 0}),
+                ):
+            self.assertEqual(line.distribute(*values), result)
 
-    def test0030create_moves(self):
+    @with_transaction()
+    def test_create_moves(self):
         'Test create_moves'
-        with Transaction().start(DB_NAME, USER, context=CONTEXT):
-            category, = self.category.create([{
-                        'name': 'Test create_moves',
-                        }])
-            unit, = self.uom.search([('name', '=', 'Unit')])
-            template, = self.template.create([{
-                        'name': 'Test create_moves',
-                        'type': 'goods',
-                        'category': category.id,
-                        'cost_price_method': 'fixed',
-                        'default_uom': unit.id,
-                        'list_price': Decimal('1'),
-                        'cost_price': Decimal(0),
-                        }])
-            product, = self.product.create([{
-                        'template': template.id,
-                        }])
-            customer, = self.location.search([('code', '=', 'CUS')])
-            warehouse, = self.location.search([('code', '=', 'WH')])
-            storage, = self.location.search([('code', '=', 'STO')])
-            company, = self.company.search([
-                    ('rec_name', '=', 'Dunder Mifflin'),
-                    ])
-            self.user.write([self.user(USER)], {
-                    'main_company': company.id,
-                    'company': company.id,
-                    })
+        pool = Pool()
+        Category = pool.get('product.category')
+        Uom = pool.get('product.uom')
+        Template = pool.get('product.template')
+        Product = pool.get('product.product')
+        Location = pool.get('stock.location')
+        Forecast = pool.get('stock.forecast')
+        Move = pool.get('stock.move')
 
+        category, = Category.create([{
+                    'name': 'Test create_moves',
+                    }])
+        unit, = Uom.search([('name', '=', 'Unit')])
+        template, = Template.create([{
+                    'name': 'Test create_moves',
+                    'type': 'goods',
+                    'category': category.id,
+                    'cost_price_method': 'fixed',
+                    'default_uom': unit.id,
+                    'list_price': Decimal('1'),
+                    'cost_price': Decimal(0),
+                    }])
+        product, = Product.create([{
+                    'template': template.id,
+                    }])
+        customer, = Location.search([('code', '=', 'CUS')])
+        warehouse, = Location.search([('code', '=', 'WH')])
+        storage, = Location.search([('code', '=', 'STO')])
+        company = create_company()
+        with set_company(company):
             today = datetime.date.today()
 
-            forecast, = self.forecast.create([{
+            forecast, = Forecast.create([{
                         'warehouse': warehouse.id,
                         'destination': customer.id,
                         'from_date': today + relativedelta(months=1, day=1),
@@ -91,19 +82,19 @@ class StockForecastTestCase(ModuleTestCase):
                                 ),
                             ],
                         }])
-            self.forecast.confirm([forecast])
+            Forecast.confirm([forecast])
 
-            self.forecast.create_moves([forecast])
+            Forecast.create_moves([forecast])
             line, = forecast.lines
             self.assertEqual(line.quantity_executed, 0)
             self.assertEqual(len(line.moves), 5)
             self.assertEqual(sum(move.quantity for move in line.moves), 10)
 
-            self.forecast.delete_moves([forecast])
+            Forecast.delete_moves([forecast])
             line, = forecast.lines
             self.assertEqual(len(line.moves), 0)
 
-            self.move.create([{
+            Move.create([{
                         'from_location': storage.id,
                         'to_location': customer.id,
                         'product': product.id,
@@ -117,48 +108,50 @@ class StockForecastTestCase(ModuleTestCase):
             line, = forecast.lines
             self.assertEqual(line.quantity_executed, 2)
 
-            self.forecast.create_moves([forecast])
+            Forecast.create_moves([forecast])
             line, = forecast.lines
             self.assertEqual(line.quantity_executed, 2)
             self.assertEqual(len(line.moves), 4)
             self.assertEqual(sum(move.quantity for move in line.moves), 8)
 
-    def test0040complete(self):
-        '''
-        Test complete.
-        '''
-        with Transaction().start(DB_NAME, USER, context=CONTEXT):
-            category, = self.category.create([{
-                        'name': 'Test complete',
-                        }])
-            unit, = self.uom.search([('name', '=', 'Unit')])
-            template, = self.template.create([{
-                        'name': 'Test complete',
-                        'type': 'goods',
-                        'category': category.id,
-                        'cost_price_method': 'fixed',
-                        'default_uom': unit.id,
-                        'list_price': Decimal('1'),
-                        'cost_price': Decimal(0),
-                        }])
-            product, = self.product.create([{
-                        'template': template.id,
-                        }])
-            customer, = self.location.search([('code', '=', 'CUS')])
-            supplier, = self.location.search([('code', '=', 'SUP')])
-            warehouse, = self.location.search([('code', '=', 'WH')])
-            storage, = self.location.search([('code', '=', 'STO')])
-            company, = self.company.search([
-                    ('rec_name', '=', 'Dunder Mifflin'),
-                    ])
-            self.user.write([self.user(USER)], {
-                    'main_company': company.id,
-                    'company': company.id,
-                    })
+    @with_transaction()
+    def test_complete(self):
+        'Test complete'
+        pool = Pool()
+        Category = pool.get('product.category')
+        Uom = pool.get('product.uom')
+        Template = pool.get('product.template')
+        Product = pool.get('product.product')
+        Location = pool.get('stock.location')
+        Forecast = pool.get('stock.forecast')
+        Move = pool.get('stock.move')
+        ForecastComplete = pool.get('stock.forecast.complete', type='wizard')
 
+        category, = Category.create([{
+                    'name': 'Test complete',
+                    }])
+        unit, = Uom.search([('name', '=', 'Unit')])
+        template, = Template.create([{
+                    'name': 'Test complete',
+                    'type': 'goods',
+                    'category': category.id,
+                    'cost_price_method': 'fixed',
+                    'default_uom': unit.id,
+                    'list_price': Decimal('1'),
+                    'cost_price': Decimal(0),
+                    }])
+        product, = Product.create([{
+                    'template': template.id,
+                    }])
+        customer, = Location.search([('code', '=', 'CUS')])
+        supplier, = Location.search([('code', '=', 'SUP')])
+        warehouse, = Location.search([('code', '=', 'WH')])
+        storage, = Location.search([('code', '=', 'STO')])
+        company = create_company()
+        with set_company(company):
             today = datetime.date.today()
 
-            moves = self.move.create([{
+            moves = Move.create([{
                         'from_location': supplier.id,
                         'to_location': storage.id,
                         'product': product.id,
@@ -181,9 +174,9 @@ class StockForecastTestCase(ModuleTestCase):
                         'currency': company.currency.id,
                         'unit_price': Decimal('1'),
                         }])
-            self.move.do(moves)
+            Move.do(moves)
 
-            forecast, = self.forecast.create([{
+            forecast, = Forecast.create([{
                         'warehouse': warehouse.id,
                         'destination': customer.id,
                         'from_date': today + relativedelta(months=1, day=1),
@@ -192,12 +185,12 @@ class StockForecastTestCase(ModuleTestCase):
                         }])
 
             with Transaction().set_context(active_id=forecast.id):
-                session_id, _, _ = self.forecast_complete.create()
-                forecast_complete = self.forecast_complete(session_id)
-                forecast_complete.ask.from_date = (today
-                        + relativedelta(months=-1, day=1))
-                forecast_complete.ask.to_date = (today
-                        + relativedelta(months=-1, day=20))
+                session_id, _, _ = ForecastComplete.create()
+                forecast_complete = ForecastComplete(session_id)
+                forecast_complete.ask.from_date = (
+                    today + relativedelta(months=-1, day=1))
+                forecast_complete.ask.to_date = (
+                    today + relativedelta(months=-1, day=20))
                 forecast_complete.transition_complete()
 
             self.assertEqual(len(forecast.lines), 1)
@@ -210,10 +203,6 @@ class StockForecastTestCase(ModuleTestCase):
 
 def suite():
     suite = trytond.tests.test_tryton.suite()
-    from trytond.modules.company.tests import test_company
-    for test in test_company.suite():
-        if test not in suite and not isinstance(test, doctest.DocTestCase):
-            suite.addTest(test)
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
             StockForecastTestCase))
     return suite
