1f4a678a6b03088a5aa12f3de59beda26dc2a696
Jonathan Levy <jonl@advocatetax.com>
2016-03-09 21:00:24 +0100

    Add Python3 support
    issue3211
    review19071002

--- tests/test_account_stock_landed_cost_weight.py
+++ tests/test_account_stock_landed_cost_weight.py
@@ -6,6 +6,7 @@ import doctest
 import trytond.tests.test_tryton
 from trytond.tests.test_tryton import ModuleTestCase
 from trytond.tests.test_tryton import doctest_setup, doctest_teardown
+from trytond.tests.test_tryton import doctest_checker
 
 
 class AccountStockLandedCostTestCase(ModuleTestCase):
@@ -20,5 +21,6 @@ def suite():
     suite.addTests(doctest.DocFileSuite(
             'scenario_account_stock_landed_cost_weight.rst',
             setUp=doctest_setup, tearDown=doctest_teardown, encoding='utf-8',
+            checker=doctest_checker,
             optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
     return suite
