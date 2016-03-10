4ef2ded832e7710d5ab834bc23f582502646755e
Jonathan Levy <jonl@advocatetax.com>
2016-03-10 22:25:22 +0100

    Add Python3 support
    issue 3211
    review22861002

--- tests/test_sale_stock_quantity.py
+++ tests/test_sale_stock_quantity.py
@@ -6,6 +6,7 @@ import doctest
 import trytond.tests.test_tryton
 from trytond.tests.test_tryton import ModuleTestCase
 from trytond.tests.test_tryton import doctest_setup, doctest_teardown
+from trytond.tests.test_tryton import doctest_checker
 
 
 class SaleStockQuantityTestCase(ModuleTestCase):
@@ -19,5 +20,6 @@ def suite():
         SaleStockQuantityTestCase))
     suite.addTests(doctest.DocFileSuite('scenario_sale_stock_quantity.rst',
             setUp=doctest_setup, tearDown=doctest_teardown, encoding='utf-8',
+            checker=doctest_checker,
             optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
     return suite
