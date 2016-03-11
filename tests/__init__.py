3fe984e49195a197ea05b217e173726b72e9764b
Cédric Krier <ced@b2ck.com>
2016-03-11 22:36:55 +0100

    Add missing extra import in tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -2,7 +2,8 @@
 # this repository contains the full copyright notices and license terms.
 
 try:
-    from trytond.modules.account_invoice.tests.test_account_invoice import suite
+    from trytond.modules.account_invoice.tests.test_account_invoice import (
+        suite, set_invoice_sequences)
 except ImportError:
     from .test_account_invoice import suite, set_invoice_sequences
 
