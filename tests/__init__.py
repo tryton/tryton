c0a2b3d223b861bdf14ba3d88a5a8f4853084a30
Cédric Krier <ced@b2ck.com>
2016-03-11 21:57:37 +0100

    Use converted tests converted by 2to3
    distutils import tests module from the source code, so we must force it to use
    the test suite from the absolute path because it will be the converted on in
    case of 2to3 otherwise we fall-back to the relative module.
    
    issue3211

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,6 +1,9 @@
 # This file is part of Tryton.  The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
 
-from .test_timesheet_cost import suite
+try:
+    from trytond.modules.timesheet_cost.tests.test_timesheet_cost import suite
+except ImportError:
+    from .test_timesheet_cost import suite
 
 __all__ = ['suite']
