d6fc7fc73876ba71f46be768305696182b7e8e0b
Cédric Krier <ced@b2ck.com>
2016-03-16 22:06:16 +0100

    Round tax amount for manual tax
    The on_change_with_amount must round the amount using the invoice currency when
    the tax is created manually.
    
    issue5288
    review22881002
    (grafted from 36c73c53b4e2e07f02fbdebbc1d91d43a73d8afd)
    
    --HG--
    branch : 3.2

--- invoice.py
+++ invoice.py
@@ -2223,7 +2223,8 @@ class InvoiceTax(ModelSQL, ModelView):
                 changes['account'] = tax.credit_note_account.id
         return changes
 
-    @fields.depends('tax', 'base', 'amount', 'manual')
+    @fields.depends('tax', 'base', 'amount', 'manual',
+        '_parent_invoice.currency')
     def on_change_with_amount(self):
         Tax = Pool().get('account.tax')
         if self.tax and self.manual:
@@ -2232,7 +2233,10 @@ class InvoiceTax(ModelSQL, ModelView):
             for values in Tax.compute([tax], base, 1):
                 if (values['tax'] == tax
                         and values['base'] == base):
-                    return values['amount']
+                    amount = values['amount']
+                    if self.invoice.currency:
+                        amount = self.invoice.currency.round(amount)
+                    return amount
         return self.amount
 
     @classmethod
