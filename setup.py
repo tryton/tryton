47cec3a38f9c230bf1b26923fdff36eb3e25cade
Sergi Almacellas Abellana <sergi@koolpi.com>
2016-03-17 15:08:55 +0100

    Update python-sql version to at least 0.7
    Since changeset 87a443dc57c7
    
    issue5410
    review17951002

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ if minor_version % 2:
         'hg+http://hg.tryton.org/modules/%s#egg=%s-%s' % (
             name[8:], name, version))
 
-requires = ['python-dateutil', 'python-sql >= 0.4', 'simpleeval']
+requires = ['python-dateutil', 'python-sql >= 0.7', 'simpleeval']
 for dep in info.get('depends', []):
     if not re.match(r'(ir|res)(\W|$)', dep):
         requires.append(get_require_version('trytond_%s' % dep))
