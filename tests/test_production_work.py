17584a92da525d151cef373cd7d7397cb14875bf
Cédric Krier <ced@b2ck.com>
2016-03-21 23:56:59 +0100

    Add missing doctest checker for Python3

--- tests/test_production_work.py
+++ tests/test_production_work.py
@@ -5,6 +5,7 @@ import doctest
 import trytond.tests.test_tryton
 from trytond.tests.test_tryton import ModuleTestCase
 from trytond.tests.test_tryton import doctest_setup, doctest_teardown
+from trytond.tests.test_tryton import doctest_checker
 
 
 class ProductionWorkTestCase(ModuleTestCase):
@@ -17,6 +18,7 @@ def suite():
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
             ProductionWorkTestCase))
     suite.addTests(doctest.DocFileSuite('scenario_production_work.rst',
-        setUp=doctest_setup, tearDown=doctest_teardown, encoding='utf-8',
+            setUp=doctest_setup, tearDown=doctest_teardown, encoding='utf-8',
+            checker=doctest_checker,
             optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
     return suite
