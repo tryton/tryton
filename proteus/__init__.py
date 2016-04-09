758adbe6bec5179588851b4165a24cdc673b4fde
Nicolas Évrard <nicoe@b2ck.com>
2016-04-09 14:20:23 +0200

    Run actions in wizard even if a view is specified
    issue5468
    review17221002

--- proteus/__init__.py
+++ proteus/__init__.py
@@ -1098,7 +1098,6 @@ class Wizard(object):
                 self.form._default_set(view['defaults'])
                 self.states = [b['state'] for b in view['buttons']]
                 self.form_state = view['state']
-                return
             else:
                 self.state = self.end_state
 
@@ -1108,6 +1107,9 @@ class Wizard(object):
                     context=self._context)
                 if proteus_action:
                     self.actions.append(proteus_action)
+
+            if 'view' in result:
+                return
 
         if self.state == self.end_state:
             self._proxy.delete(self.session_id, self._config.context)
