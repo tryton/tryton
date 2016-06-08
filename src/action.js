3430bc4d2d82dafd2a881cd64c80742689ca7158
Sergi Almacellas Abellana <sergi@koolpi.com>
2016-06-08 10:35:54 +0200

    Remove window_name on Action Window
    issue5611
    review25381002

--- src/action.js
+++ src/action.js
@@ -79,10 +79,7 @@
                     params.tab_domain.push(
                         [element[0], decoder.decode(element[1])]);
                 });
-                params.name = false;
-                if (action.window_name) {
-                    params.name = action.name;
-                }
+                params.name = action.name;
                 params.model = action.res_model || data.res_model;
                 params.res_id = action.res_id || data.res_id;
                 params.context_model = action.context_model;
--- src/board.js
+++ src/board.js
@@ -242,10 +242,8 @@
 
                 if (attributes.string) {
                     this.title.html(attributes.string);
-                } else if (this.action.window_name !== undefined) {
-                    this.title.html(this.action.name);
                 } else {
-                    this.title.html(this.screen.current_view.title);
+                    this.title.html(this.action.name);
                 }
                 this.screen.switch_view().done(function() {
                     this.body.append(this.screen.screen_container.el);
