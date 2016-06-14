3a4784459c05da4d29fe5c18e3e40f17cef4e390
Cédric Krier <ced@b2ck.com>
2016-06-14 16:21:03 +0200

    Don't disable open button on readonly Many2One
    The search behaviour is deactivated in the callback.
    
    issue5634
    review18231002
    (grafted from a7597ce17b1aecd681ee1f92aed54ec948716ce0)
    
    --HG--
    branch : 3.8

--- src/view/form.js
+++ src/view/form.js
@@ -1640,7 +1640,7 @@
         _set_button_sensitive: function() {
             this.entry.prop('readonly', this._readonly);
             this.but_open.prop('disabled',
-                    this._readonly || !this.read_access());
+                    !this.read_access());
         },
         get_access: function(type) {
             var model = this.get_model();
@@ -1697,7 +1697,7 @@
                 win = new Sao.Window.Form(screen, callback.bind(this), {
                     save_current: true
                 });
-            } else if (model) {
+            } else if (model && !this._readonly) {
                 var dom;
                 var domain = this.field().get_domain(record);
                 var context = this.field().get_context(record);
