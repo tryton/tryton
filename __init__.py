f58ba569aef347f150a2a8110be0a52ac758b500
Cédric Krier <ced@b2ck.com>
2016-06-27 19:18:17 +0200

    Initial commit
    issue5544
    review8451002

---
+++ __init__.py
@@ -0,0 +1,19 @@
+# This file is part of Tryton.  The COPYRIGHT file at the top level of
+# this repository contains the full copyright notices and license terms.
+
+from trytond.pool import Pool
+from .user import *
+
+
+def register():
+    Pool.register(
+        User,
+        UserAuthenticateAttempt,
+        UserSession,
+        module='web_user', type_='model')
+    Pool.register(
+        module='web_user', type_='wizard')
+    Pool.register(
+        EmailValidation,
+        EmailResetPassword,
+        module='web_user', type_='report')
