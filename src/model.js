ae63c087539d72fd7cff988e7e4d06436033a410
Sergi Almacellas Abellana <sergi@koolpi.com>
2016-06-29 09:38:32 +0200

    Fix usage of apply function
    issue5666
    review22121003
    (grafted from a9e7e4a2beeec020cd68a1dc0a304d716c2a3a3f)
    
    --HG--
    branch : 4.0

--- src/model.js
+++ src/model.js
@@ -794,7 +794,7 @@
                         }
                     }
                     this.set_default(values);
-                    jQuery.when.apply(promises).then(function() {
+                    jQuery.when.apply(jQuery, promises).then(function() {
                         dfd.resolve(values);
                     });
                 }.bind(this));
@@ -835,7 +835,7 @@
                 this._loaded[fname] = true;
                 fieldnames.push(fname);
             }
-            return jQuery.when.apply(promises).then(function() {
+            return jQuery.when.apply(jQuery, promises).then(function() {
                 return this.on_change(fieldnames).then(function() {
                     return this.on_change_with(fieldnames).then(function() {
                         var callback = function() {
@@ -1024,7 +1024,7 @@
                 }
                 promises.push(this.do_autocomplete(fname));
             }
-            return jQuery.when.apply(promises);
+            return jQuery.when.apply(jQuery, promises);
         },
         do_autocomplete: function(fieldname) {
             this.autocompletion[fieldname] = [];
@@ -1838,7 +1838,7 @@
                         }
                     });
                 }
-                return jQuery.when.apply(promises);
+                return jQuery.when.apply(jQuery, promises);
             };
             return prm.then(set_value);
         },
