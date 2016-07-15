e54476fe301c108de4a5360546538eb89f588e2d
Xavier Codinas <xavier19966@gmail.com>
2016-07-15 20:52:41 +0700

    Always return promise in field binary get data
    If record id negative, it must still return a promise.
    The default value for data must be an empty list.
    
    issue5676
    review28431002

--- src/model.js
+++ src/model.js
@@ -2296,8 +2296,8 @@
             return data;
         },
         get_data: function(record) {
-            var prm;
-            var data = record._values[this.name] || 0;
+            var data = record._values[this.name] || [];
+            var prm = jQuery.when(data);
             if (!(data instanceof Uint8Array)) {
                 if (record.id < 0) {
                     return prm;
@@ -2307,8 +2307,6 @@
                     context).then(function(data) {
                         return data[0][this.name];
                     }.bind(this));
-            } else {
-                prm = jQuery.when(data);
             }
             return prm;
         }
