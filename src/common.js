5cf950dda05bb1aafefd463991c0682470a03600
Xavier Codinas <xavier19966@gmail.com>
2016-07-18 23:57:39 +0700

    Return empty object in MODELACCESS.model.get
    It is to allow to retrieve access keys with default empty
    value without failing.
    
    issue5708
    review29381003
    (grafted from 8a1509f90e458fe83fdc920180dafb3eb2108385)
    
    --HG--
    branch : 3.8

--- src/common.js
+++ src/common.js
@@ -443,7 +443,7 @@
             return prm;
         },
         get: function(model) {
-            return this._access[model];
+            return this._access[model] || {};
         }
     });
     Sao.common.MODELACCESS = new Sao.common.ModelAccess();
