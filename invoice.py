462c419b77c39620ea9ef7f9143236133ca546e5
Sergi Almacellas Abellana <sergi@koolpi.com>
2016-07-18 14:03:30 +0200

    Replace readonly groups by ir.model.button
    issue5068
    review29351004

--- invoice.py
+++ invoice.py
@@ -16,7 +16,7 @@ from trytond.report import Report
 from trytond.wizard import Wizard, StateView, StateTransition, StateAction, \
     StateReport, Button
 from trytond import backend
-from trytond.pyson import If, Eval, Bool, Id
+from trytond.pyson import If, Eval, Bool
 from trytond.tools import reduce_ids, grouped_slice
 from trytond.transaction import Transaction
 from trytond.pool import Pool
@@ -257,8 +257,6 @@ class Invoice(Workflow, ModelSQL, ModelView, TaxableMixin):
                     },
                 'pay': {
                     'invisible': Eval('state') != 'posted',
-                    'readonly': ~Eval('groups', []).contains(
-                        Id('account', 'group_account')),
                     },
                 })
 
