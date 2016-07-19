c9ab805932b1fb6e5707f2b3f968a1eeeaa074a8
Cédric Krier <ced@b2ck.com>
2016-07-19 07:39:16 +0700

    Retrieve cost price methods from product template
    Retrieve dynamically the cost price methods to avoid other modules that extend
    the cost price method field to also extend the configuration field.
    
    issue5720
    review25521002

--- configuration.py
+++ configuration.py
@@ -2,8 +2,7 @@
 # this repository contains the full copyright notices and license terms.
 from trytond.model import ModelView, ModelSQL, ModelSingleton, fields
 from trytond.pool import Pool
-
-from .product import COST_PRICE_METHODS
+from trytond.rpc import RPC
 
 __all__ = ['Configuration']
 
@@ -13,13 +12,26 @@ class Configuration(ModelSingleton, ModelSQL, ModelView):
     __name__ = 'product.configuration'
 
     default_cost_price_method = fields.Function(fields.Selection(
-            [(None, '')] + COST_PRICE_METHODS, 'Default Cost Method'),
+            'get_cost_price_methods', 'Default Cost Method'),
         'get_default_cost_price_method',
         setter='set_default_cost_price_method')
 
     @classmethod
+    def __setup__(cls):
+        super(Configuration, cls).__setup__()
+        cls.__rpc__['get_cost_price_methods'] = RPC()
+
+    @classmethod
     def default_default_cost_price_method(cls):
         return cls().get_default_cost_price_method('default_cost_price_method')
+
+    @classmethod
+    def get_cost_price_methods(cls):
+        pool = Pool()
+        Template = pool.get('product.template')
+        return (Template.fields_get(
+                ['cost_price_method'])['cost_price_method']['selection']
+            + [(None, '')])
 
     @classmethod
     def _get_product_field(cls, name):
