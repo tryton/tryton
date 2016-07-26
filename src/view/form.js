59d3769b0d79542bcf50d1fd7b5b0a6d78a53403
Diego Abad <dabada83@gmail.com>
2016-07-26 17:18:21 +0200

    Fix widget Image to expect data instead object
    Since changeset 16076b99d5be get_data function on Binary
    field was fixed to return data instead object.
    
    Image widget was implemented on changesets 90eca9355575,
    3b2b20cb44b3 (form and tree view) relied on the bad behaviour
    of get_data and so it must be fixed.
    
    issue5745
    review29421002

--- src/view/form.js
+++ src/view/form.js
@@ -3102,7 +3102,7 @@
                 if (!data) {
                     url = null;
                 } else {
-                    blob = new Blob([data[0][this.field_name]]);
+                    blob = new Blob([data]);
                     url = window.URL.createObjectURL(blob);
                 }
                 this.image.attr('src', url);
