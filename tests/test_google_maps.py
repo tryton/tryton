881e0e991d66c12a0f88d554818442fe311e1511
Cédric Krier <ced@b2ck.com>
2016-08-19 21:25:16 +0200

    Do not decode/encode url for Python3
    urllib.parse.quote works with str but not bytes in Python3 which is the
    opposite of urllib.quote in Python2 who works with str but not unicode.
    
    issue5768
    review31401002

--- tests/test_google_maps.py
+++ tests/test_google_maps.py
@@ -2,12 +2,28 @@
 # this repository contains the full copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import ModuleTestCase
+from trytond.tests.test_tryton import ModuleTestCase, with_transaction
+from trytond.pool import Pool
 
 
 class GoogleMapsTestCase(ModuleTestCase):
     'Test GoogleMaps module'
     module = 'google_maps'
+
+    @with_transaction()
+    def test_google_maps_url(self):
+        "Test Google Maps URL"
+        pool = Pool()
+        Address = pool.get('party.address')
+        address = Address()
+        address.street = "300 Cliff Street"
+        address.zip = "18503"
+        address.city = "Scranton"
+
+        self.assertEqual(
+            address.on_change_with_google_maps_url(),
+            'http://maps.google.com/maps?hl=en&'
+            'q=300%20Cliff%20Street%2018503%20Scranton')
 
 
 def suite():
