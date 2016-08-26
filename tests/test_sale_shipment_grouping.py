7281ef65cd8d18f84c366e07674da00b35f02ebf
Cédric Krier <ced@b2ck.com>
2016-08-26 09:59:46 +0200

    Use install_modules tools in scenario
    issue5794

--- tests/test_sale_shipment_grouping.py
+++ tests/test_sale_shipment_grouping.py
@@ -4,7 +4,7 @@ import unittest
 import doctest
 import trytond.tests.test_tryton
 from trytond.tests.test_tryton import ModuleTestCase
-from trytond.tests.test_tryton import doctest_setup, doctest_teardown
+from trytond.tests.test_tryton import doctest_teardown
 from trytond.tests.test_tryton import doctest_checker
 
 
@@ -18,7 +18,7 @@ def suite():
     loader = unittest.TestLoader()
     suite.addTests(loader.loadTestsFromTestCase(SaleShipmentGroupingTestCase))
     suite.addTests(doctest.DocFileSuite('scenario_sale_shipment_grouping.rst',
-            setUp=doctest_setup, tearDown=doctest_teardown, encoding='utf-8',
+            tearDown=doctest_teardown, encoding='utf-8',
             checker=doctest_checker,
             optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
     return suite
