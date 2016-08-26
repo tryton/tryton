ea5c17b5d16d399290114c07c82141c31865ce1c
Cédric Krier <ced@b2ck.com>
2016-08-26 09:58:37 +0200

    Use install_modules tools in scenario
    issue5794

--- tests/test_account_stock_landed_cost_weight.py
+++ tests/test_account_stock_landed_cost_weight.py
@@ -5,7 +5,7 @@ import doctest
 
 import trytond.tests.test_tryton
 from trytond.tests.test_tryton import ModuleTestCase
-from trytond.tests.test_tryton import doctest_setup, doctest_teardown
+from trytond.tests.test_tryton import doctest_teardown
 from trytond.tests.test_tryton import doctest_checker
 
 
@@ -20,7 +20,7 @@ def suite():
         AccountStockLandedCostTestCase))
     suite.addTests(doctest.DocFileSuite(
             'scenario_account_stock_landed_cost_weight.rst',
-            setUp=doctest_setup, tearDown=doctest_teardown, encoding='utf-8',
+            tearDown=doctest_teardown, encoding='utf-8',
             checker=doctest_checker,
             optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
     return suite
