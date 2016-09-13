c2e082d8d85fa8e4ee8ca82106c786a67a48d317
Sergi Almacellas Abellana <sergi@koolpi.com>
2016-09-13 14:07:52 +0200

    Use sequence_ordered
    issue5105

--- routing.py
+++ routing.py
@@ -1,9 +1,6 @@
 # This file is part of Tryton.  The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
-from sql import Null
-from sql.conditionals import Case
-
-from trytond.model import ModelSQL, ModelView, fields
+from trytond.model import ModelSQL, ModelView, fields, sequence_ordered
 
 __all__ = ['Routing', 'Operation', 'RoutingStep', 'Routing_BOM']
 
@@ -33,25 +30,14 @@ class Operation(ModelSQL, ModelView):
         return True
 
 
-class RoutingStep(ModelSQL, ModelView):
+class RoutingStep(sequence_ordered(), ModelSQL, ModelView):
     'Route'
     __name__ = 'production.routing.step'
     _rec_name = 'operation'
-    sequence = fields.Integer('Sequence')
     operation = fields.Many2One('production.routing.operation', 'Operation',
         required=True)
     routing = fields.Many2One('production.routing', 'Routing', required=True,
         ondelete='CASCADE')
-
-    @classmethod
-    def __setup__(cls):
-        super(RoutingStep, cls).__setup__()
-        cls._order.insert(0, ('sequence', 'ASC'))
-
-    @classmethod
-    def order_sequence(cls, tables):
-        table, _ = tables[None]
-        return [Case((table.sequence == Null, 0), else_=1), table.sequence]
 
     def get_rec_name(self, name):
         return self.operation.rec_name
