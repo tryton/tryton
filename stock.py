c4ff523e3b086a45b8272e9ab0ae095ef910ff7f
Sergi Almacellas Abellana <sergi@koolpi.com>
2016-09-14 12:09:27 +0200

    Preserve previous reaonly states for sequence field
    The sequence_mixin may add some states on the sequence field, so we should not
    update the states values as this will remove the previous states.
    
    review30471003

--- stock.py
+++ stock.py
@@ -1,7 +1,7 @@
 # This file is part of Tryton.  The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
 from trytond.model import sequence_ordered
-from trytond.pyson import Not, Eval, Bool
+from trytond.pyson import Eval, Bool
 from trytond import backend
 from trytond.pool import PoolMeta
 
@@ -16,9 +16,8 @@ class Location(sequence_ordered()):
     @classmethod
     def __setup__(cls):
         super(Location, cls).__setup__()
-        cls.sequence.states.update({
-            'readonly': Not(Bool(Eval('active'))),
-            })
+        previous_readonly = cls.sequence.states.get('readonly', Bool(False))
+        cls.sequence.states['readonly'] = previous_readonly | ~Eval('active')
         cls.sequence.depends = ['active']
 
     @classmethod
