1401b4d07eeb7915bc9878318e30f5fbf169e2d5
Sergi Almacellas Abellana <sergi@koolpi.com>
2016-09-20 09:49:57 +0200

    Merge recursive results in DomainParser.parse_clause
    We should use jQuery.merge instead of Array.concat, as concat creates a new
    Array (which was not correctly returned) and jQuery.merge merges the contents
    of both arrays in the first array.
    
    issue5883
    review26651003
    (grafted from b47a9dde0a137ddb6e11ceee2a78c0b7c59f9ef8)
    
    --HG--
    branch : 4.0

--- src/common.js
+++ src/common.js
@@ -1556,7 +1556,7 @@
             var result = [];
             tokens.forEach(function(clause) {
                 if (this.is_generator(clause)) {
-                    result.concat(this.parse_clause(clause));
+                    jQuery.merge(result, this.parse_clause(clause));
                 } else if ((clause == 'OR') || (clause == 'AND')) {
                     result.push(clause);
                 } else if ((clause.length == 1) &&
