6430f8bffb989944a913452b4fbcafa73e6fb9d9
Sergi Almacellas Abellana <sergi@koolpi.com>
2016-11-24 16:42:48 +0100

    Set date on PayInvoice.ask transition
    Otherwise when using a different currency it raises an error because an
    exchange rate can not be found for an empty date.
    issue6053
    review32821002
    (grafted from cc09d96dec84f4bcdfc41609428a988b04372c14)
    
    --HG--
    branch : 3.6

--- invoice.py
+++ invoice.py
@@ -2506,6 +2506,7 @@ class PayInvoice(Wizard):
                 if not x.reconciliation]
 
         default['amount'] = self.start.amount
+        default['date'] = self.start.date
         default['currency'] = self.start.currency.id
         default['currency_digits'] = self.start.currency_digits
         default['company'] = invoice.company.id
