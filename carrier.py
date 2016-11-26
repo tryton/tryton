381c49ea1f7b0dee14e9707f422b24fff0fac00c
Cédric Krier <ced@b2ck.com>
2016-11-26 13:19:38 +0100

    Use absolute import to import locale
    Since locale folder has been created with the translations, Python3 tests
    failed because 2to3 converts the import of locale into a relative import
    because it detects the locale folder.

--- carrier.py
+++ carrier.py
@@ -1,5 +1,7 @@
 # This file is part of Tryton.  The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
+from __future__ import absolute_import
+
 import locale
 
 from zeep.exceptions import Fault
--- stock.py
+++ stock.py
@@ -1,5 +1,6 @@
 # This file is part of Tryton.  The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
+from __future__ import absolute_import
 
 import locale
 from io import BytesIO
