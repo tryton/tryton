133667f6f7bdc56605d0f7635946f92a39d0bbac
Cédric Krier <ced@b2ck.com>
2016-11-27 22:13:22 +0100

    Manage None as value of field in on_change method
    issue6069
    review27851002

--- move.py
+++ move.py
@@ -1974,7 +1974,8 @@ class ReconcileShow(ModelView):
 
     @fields.depends('account')
     def on_change_with_currency_digits(self, name=None):
-        return self.account.company.currency.digits
+        if self.account:
+            return self.account.company.currency.digits
 
 
 class CancelMoves(Wizard):
