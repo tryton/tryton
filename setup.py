63c07f185586331648a94ef09adb0d795006fff9
Cédric Krier <ced@b2ck.com>
2017-01-04 01:02:38 +0100

    Fix convert_2to3_doctests typo (grafted from 396df4254c693ebb0d82e554f819f9a1e9fe699a)
    --HG--
    branch : 4.0

--- setup.py
+++ setup.py
@@ -121,7 +121,7 @@ setup(name=name,
     test_loader='trytond.test_loader:Loader',
     tests_require=tests_require,
     use_2to3=True,
-    convert_2to3_doctest=[
+    convert_2to3_doctests=[
         'tests/scenario_sale_complaint.rst',
         ],
     )
