ae9c0a545d40558c8ff3dff0f8de1834126e6fb2
Sergi Almacellas Abellana <sergi@koolpi.com>
2017-03-08 09:42:36 +0100

    Add test for view read
    issue6311
    review33921002

--- tests/test_dashboard.py
+++ tests/test_dashboard.py
@@ -2,12 +2,22 @@
 # this repository contains the full copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import ModuleTestCase
+from trytond.tests.test_tryton import ModuleTestCase, with_transaction
+from trytond.pool import Pool
 
 
 class DashboardTestCase(ModuleTestCase):
     'Test Dashboard module'
     module = 'dashboard'
+
+    @with_transaction()
+    def test_view_read(self):
+        'Test dashboard view read'
+        pool = Pool()
+        View = pool.get('ir.ui.view')
+        dashboard_id = View.dashboard_id()
+        result, = View.read([dashboard_id])
+        self.assertIn('board', result['arch'])
 
 
 def suite():
