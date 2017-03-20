e77c9a394f94cc41511a51746ddefc0df79815d1
Cédric Krier <ced@b2ck.com>
2017-03-20 10:47:23 +0100

    Use shipment_party location when available
    The shipment will use the shipment_party so the location of moves should also
    otherwise the outgoing move domain is not respected.
    
    issue6368
    review33231002
    (grafted from 93468680f0425652e1ba09d041ed87e00e30566e)
    
    --HG--
    branch : 4.0

--- sale.py
+++ sale.py
@@ -1265,11 +1265,13 @@ class SaleLine(ModelSQL, ModelView):
             if self.warehouse:
                 return self.warehouse.output_location.id
         else:
-            return self.sale.party.customer_location.id
+            party = self.sale.shipment_party or self.sale.party
+            return party.customer_location.id
 
     def get_to_location(self, name):
         if (self.quantity or 0) >= 0:
-            return self.sale.party.customer_location.id
+            party = self.sale.shipment_party or self.sale.party
+            return party.customer_location.id
         else:
             if self.warehouse:
                 return self.warehouse.input_location.id
