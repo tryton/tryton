85d3577a405cea474755e7f83cd6579b1fb3f13e
Cédric Krier <ced@b2ck.com>
2017-04-26 19:37:00 +0200

    Skip validate only on constant readonly fields
    A field can be readonly by the domain inversion in the validate method. So it
    should be validated again in case its domain inversion change and reset it a
    non-readonly.
    So only field that are always readonly can be skipped for the validation.
    
    issue6448
    review38341002
    (grafted from e9613d7e20c4f3d793d949eccdaa82d66e80953c)
    
    --HG--
    branch : 3.8

--- src/model.js
+++ src/model.js
@@ -1072,7 +1072,7 @@
                     if (fields && !~fields.indexOf(fname)) {
                         continue;
                     }
-                    if (field.get_state_attrs(this).readonly) {
+                    if (field.description.readonly) {
                         continue;
                     }
                     if (~exclude_fields.indexOf(fname)) {
