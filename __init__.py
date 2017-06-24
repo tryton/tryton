7346835f42b0633bdfd699d240251f9e26063999
Cédric Krier <ced@b2ck.com>
2017-06-24 10:40:28 +0200

    Initial commit
    issue6436
    review38321003

---
+++ __init__.py
@@ -0,0 +1,21 @@
+# This file is part of Tryton.  The COPYRIGHT file at the top level of
+# this repository contains the full copyright notices and license terms.
+
+from trytond.pool import Pool
+
+from . import stock
+from . import account
+
+__all__ = ['register']
+
+
+def register():
+    Pool.register(
+        stock.Location,
+        stock.LocationLeadTime,
+        stock.Move,
+        stock.ShipmentInternal,
+        stock.Inventory,
+        stock.OrderPoint,
+        account.InvoiceLine,
+        module='stock_consignment', type_='model')
