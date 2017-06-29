68066533ce4a2af69a30af70295bfb403ec5894f
Cédric Krier <ced@b2ck.com>
2017-06-29 23:13:16 +0200

    Check location type against moves from all companies
    issue6592
    review38411002
    (grafted from 4b9c3e3ea6295b746936f3e7c842a95bd447d60e)
    
    --HG--
    branch : 4.2

--- location.py
+++ location.py
@@ -176,15 +176,19 @@ class Location(ModelSQL, ModelView):
         """ Check locations with moves have types compatible with moves. """
         invalid_move_types = ['warehouse', 'view']
         Move = Pool().get('stock.move')
-        if (self.type in invalid_move_types
-                and Move.search([
+        if self.type in invalid_move_types:
+            # Use root to compute for all companies
+            with Transaction().set_user(0):
+                moves = Move.search([
                         ['OR',
                             ('to_location', '=', self.id),
                             ('from_location', '=', self.id),
                             ],
                         ('state', 'not in', ['staging', 'draft']),
-                        ])):
-            self.raise_user_error('invalid_type_for_moves', (self.rec_name,))
+                        ])
+            if moves:
+                self.raise_user_error(
+                    'invalid_type_for_moves', (self.rec_name,))
 
     @staticmethod
     def default_active():
