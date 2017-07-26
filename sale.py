da54b3b27535cda00d26229707255e49a9290a6c
Cédric Krier <ced@b2ck.com>
2017-07-26 23:22:25 +0200

    Ensure empty sale goes to done state
    As there is not transition from confirmed to done, the sale must first be
    processed.
    
    issue6639
    review35121002
    (grafted from 9cbb6e0da7c6e58372d4ece84fc38dc708c9d99c)
    
    --HG--
    branch : 3.6

--- sale.py
+++ sale.py
@@ -456,7 +456,8 @@ class Sale(Workflow, ModelSQL, ModelView, TaxableMixin):
         return 'invoice'
 
     def get_tax_amount(self):
-        return sum(v['amount'] for v in self._get_taxes().itervalues())
+        return sum(
+            (v['amount'] for v in self._get_taxes().itervalues()), Decimal(0))
 
     @classmethod
     def get_amount(cls, sales, names):
@@ -925,6 +926,8 @@ class Sale(Workflow, ModelSQL, ModelView, TaxableMixin):
             sale.set_shipment_state()
             if sale.is_done():
                 if sale.state != 'done':
+                    if sale.state == 'confirmed':
+                        process.append(sale)
                     done.append(sale)
             elif sale.state != 'processing':
                 process.append(sale)
