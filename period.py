260efd193fe924e7973f65fefa5733cf21a6dede
Cédric Krier <ced@b2ck.com>
2017-08-10 12:33:01 +0200

    Remove wrong classmethod on get_rec_name of Period
    Introduced in changeset 706df76d7266
    (grafted from 8aae93025cd4bc56faaeea06df304c0dc7063dc1)
    
    --HG--
    branch : 4.2

--- period.py
+++ period.py
@@ -69,7 +69,6 @@ class Period(Workflow, ModelSQL, ModelView):
         if grouping == ('product',):
             return pool.get('stock.period.cache')
 
-    @classmethod
     def get_rec_name(self, name):
         return str(self.date)
 
