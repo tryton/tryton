f6404ca76f0c5948e438a312a8e129fe80d02c9a
Cédric Krier <ced@b2ck.com>
2017-08-31 22:52:57 +0200

    Use Flat OpenDocument
    issue6545
    review35031002

--- setup.py
+++ setup.py
@@ -75,7 +75,7 @@ setup(name=name,
         ],
     package_data={
         'trytond.modules.account_dunning_fee': (info.get('xml', [])
-            + ['tryton.cfg', 'view/*.xml', 'locale/*.po', '*.odt',
+            + ['tryton.cfg', 'view/*.xml', 'locale/*.po', '*.fodt',
                 'icons/*.svg', 'tests/*.rst']),
         },
     classifiers=[
