43e5033ae65a037c4cd16e0f25f1e2ac7b446e7d
Cédric Krier <ced@b2ck.com>
2017-08-31 22:53:03 +0200

    Use Flat OpenDocument
    issue6545
    review35031002

--- setup.py
+++ setup.py
@@ -75,7 +75,7 @@ setup(name=name,
         ],
     package_data={
         'trytond.modules.production_work_timesheet': (info.get('xml', [])
-            + ['tryton.cfg', 'view/*.xml', 'locale/*.po', '*.odt',
+            + ['tryton.cfg', 'view/*.xml', 'locale/*.po', '*.fodt',
                 'icons/*.svg', 'tests/*.rst']),
         },
     classifiers=[
