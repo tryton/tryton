e95ba210a0d3251e22ebb3c1f51a11f0ce6ed148
Cédric Krier <ced@b2ck.com>
2017-08-31 22:53:05 +0200

    Use Flat OpenDocument
    issue6545
    review35031002

--- setup.py
+++ setup.py
@@ -77,7 +77,7 @@ setup(name=name,
         ],
     package_data={
         'trytond.modules.sale_advance_payment': (info.get('xml', [])
-            + ['tryton.cfg', 'view/*.xml', 'locale/*.po', '*.odt',
+            + ['tryton.cfg', 'view/*.xml', 'locale/*.po', '*.fodt',
                 'icons/*.svg', 'tests/*.rst']),
         },
     classifiers=[
