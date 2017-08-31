c354a71772e8d2b4f464cd0503555626403ee8a0
Cédric Krier <ced@b2ck.com>
2017-08-31 22:53:04 +0200

    Use Flat OpenDocument
    issue6545
    review35031002

--- setup.py
+++ setup.py
@@ -76,7 +76,7 @@ setup(name=name,
         ],
     package_data={
         'trytond.modules.purchase_request': (info.get('xml', [])
-            + ['tryton.cfg', 'view/*.xml', 'locale/*.po', '*.odt',
+            + ['tryton.cfg', 'view/*.xml', 'locale/*.po', '*.fodt',
                 'icons/*.svg', 'tests/*.rst']),
         },
     classifiers=[
