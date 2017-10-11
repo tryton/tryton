67117fba0c0be54814e5a199f82475158ffa4ee1
Cédric Krier <ced@b2ck.com>
2017-10-11 22:46:50 +0200

    Unselect other rows if not ctrl or single selection
    issue6807
    review44361002
    (grafted from 313f98539c6f8283d57c83983762863cbb208818)
    
    --HG--
    branch : 4.2

--- src/view/tree.js
+++ src/view/tree.js
@@ -893,7 +893,7 @@
                 this.tree.select_changed(this.record);
                 this.switch_row();
             } else {
-                if (!event_.ctrlKey &&
+                if (!event_.ctrlKey ||
                         this.tree.selection_mode ==
                         Sao.common.SELECTION_SINGLE) {
                     this.tree.rows.forEach(function(row) {
