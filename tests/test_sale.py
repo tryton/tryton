3d8aa609b87dc655d07b757cd0d81f379b199bb6
Cédric Krier <ced@b2ck.com>
2017-11-11 16:09:07 +0100

    Invoice shipped product instead of sold product
    issue6841
    review43601002

--- tests/test_sale.py
+++ tests/test_sale.py
@@ -23,4 +23,9 @@ def suite():
             tearDown=doctest_teardown, encoding='utf-8',
             optionflags=doctest.REPORT_ONLY_FIRST_FAILURE,
             checker=doctest_checker))
+    suite.addTests(doctest.DocFileSuite(
+            'scenario_sale_ship_other_product.rst',
+            tearDown=doctest_teardown, encoding='utf-8',
+            optionflags=doctest.REPORT_ONLY_FIRST_FAILURE,
+            checker=doctest_checker))
     return suite
