0cfb0aa9c6c491613dcdd6107d071ed6feec0021
Cédric Krier <ced@b2ck.com>
2017-12-06 01:10:35 +0100

    Compare amount in the invoice currency against amount_to_pay
    issue6842
    review40841002
    (grafted from f8fe6f58c812260f3a5ba6bbccf50cddeb78072c)
    
    --HG--
    branch : 4.0

--- invoice.py
+++ invoice.py
@@ -2457,8 +2457,11 @@ class PayInvoice(Wizard):
         with Transaction().set_context(date=self.start.date):
             amount = Currency.compute(self.start.currency,
                 self.start.amount, invoice.company.currency)
+            amount_invoice = Currency.compute(
+                self.start.currency, self.start.amount, invoice.currency)
         _, remainder = self.get_reconcile_lines_for_amount(invoice, amount)
-        if remainder == Decimal('0.0') and amount <= invoice.amount_to_pay:
+        if (remainder == Decimal('0.0')
+                and amount_invoice <= invoice.amount_to_pay):
             return 'pay'
         return 'ask'
 
@@ -2481,6 +2484,8 @@ class PayInvoice(Wizard):
         with Transaction().set_context(date=self.start.date):
             amount = Currency.compute(self.start.currency,
                 self.start.amount, invoice.company.currency)
+            amount_invoice = Currency.compute(
+                self.start.currency, self.start.amount, invoice.currency)
 
         if invoice.company.currency.is_zero(amount):
             lines = invoice.lines_to_pay
@@ -2509,7 +2514,7 @@ class PayInvoice(Wizard):
         default['currency_digits_writeoff'] = invoice.company.currency.digits
         default['invoice'] = invoice.id
 
-        if (amount > invoice.amount_to_pay
+        if (amount_invoice > invoice.amount_to_pay
                 or invoice.company.currency.is_zero(amount)):
             default['type'] = 'writeoff'
         return default
