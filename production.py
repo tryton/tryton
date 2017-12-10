598a8029c36becde34ddb67ee1a241179e95be7c
Cédric Krier <ced@b2ck.com>
2017-12-10 17:35:59 +0100

    Use correct type for default value for Eval of inputs/outputs
    The __or__/__and__ operators use the default type to test if they must add or
    not a Bool statement.
    If the Bool statement is not added and empty list is evaluated as True by the
    javascript version of PYSON.
    
    issue6882
    (grafted from 5bd1f7b4db5a7fa9ed74eaadf2f3b8257dd858ae)
    
    --HG--
    branch : 4.2

--- production.py
+++ production.py
@@ -62,7 +62,7 @@ class Production(Workflow, ModelSQL, ModelView):
             ],
         states={
             'readonly': (~Eval('state').in_(['request', 'draft'])
-                | Eval('inputs', True) | Eval('outputs', True)),
+                | Eval('inputs', [-1]) | Eval('outputs', [-1])),
             },
         depends=['state'])
     location = fields.Many2One('stock.location', 'Location', required=True,
@@ -71,7 +71,7 @@ class Production(Workflow, ModelSQL, ModelView):
             ],
         states={
             'readonly': (~Eval('state').in_(['request', 'draft'])
-                | Eval('inputs', True) | Eval('outputs', True)),
+                | Eval('inputs', [-1]) | Eval('outputs', [-1])),
             },
         depends=['state'])
     product = fields.Many2One('product.product', 'Product',
