41e79d75612971518c96d2099206cce1def71c01
Cédric Krier <ced@b2ck.com>
2017-12-29 22:35:43 +0100

    Remove unused import of importlib

--- res.py
+++ res.py
@@ -1,7 +1,6 @@
 # This file is part of Tryton.  The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
 import datetime
-import importlib
 import logging
 import random
 
