1385f071658f24aa67a7b25e97c8886f0575dfa8
Cédric Krier <ced@b2ck.com>
2018-01-15 23:04:10 +0100

    Use is_instance_method tool compatible with Python3
    issue7053
    review37921002
    (grafted from 3a1f08485f6f238542f42cf2a2e744b5c0b9ef31)
    
    --HG--
    branch : 4.2

--- proteus/config.py
+++ proteus/config.py
@@ -154,8 +154,9 @@ class _TrytondMethod(object):
 
     def __call__(self, *args):
         from trytond.cache import Cache
-        from trytond.transaction import Transaction
         from trytond.rpc import RPC
+        from trytond.tools import is_instance_method
+        from trytond.transaction import Transaction
 
         if self._name in self._object.__rpc__:
             rpc = self._object.__rpc__[self._name]
@@ -170,7 +171,8 @@ class _TrytondMethod(object):
             args, kwargs, transaction.context, transaction.timestamp = \
                 rpc.convert(self._object, *args)
             meth = getattr(self._object, self._name)
-            if not hasattr(meth, 'im_self') or meth.im_self:
+            if (rpc.instantiate is None
+                    or not is_instance_method(self._object, meth)):
                 result = rpc.result(meth(*args, **kwargs))
             else:
                 assert rpc.instantiate == 0
