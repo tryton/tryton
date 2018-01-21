2b084baa217c64c1df9b7f15bee0af2cc658dfa8
Cédric Krier <ced@b2ck.com>
2018-01-21 19:57:56 +0100

    Improve dependency links
    issue7049

--- setup.py
+++ setup.py
@@ -57,7 +57,6 @@ tests_require = [get_require_version('proteus'),
     get_require_version('trytond_purchase')]
 dependency_links = []
 if minor_version % 2:
-    # Add development index for testing with proteus
     dependency_links.append('https://trydevpi.tryton.org/')
 
 
