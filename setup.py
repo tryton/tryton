b0620ebbc57e20774d1290f1566ec0553ee6d20b
Cédric Krier <ced@b2ck.com>
2018-01-21 19:57:58 +0100

    Improve dependency links
    issue7049

--- setup.py
+++ setup.py
@@ -58,7 +58,6 @@ tests_require = [get_require_version('proteus'),
     'python-dateutil']
 dependency_links = []
 if minor_version % 2:
-    # Add development index for testing with proteus
     dependency_links.append('https://trydevpi.tryton.org/')
 
 setup(name=name,
