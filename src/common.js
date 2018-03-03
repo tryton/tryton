fce8373173a986d291f5096fd89d69766a9cf17c
Cédric Krier <ced@b2ck.com>
2018-03-04 00:53:46 +0100

    Correctly evaluate domain between xxx2many and null
    The value of empty xxx2Many is an empty Array which must be comparable to
    null.
    
    issue7163
    review44681002
    (grafted from 0cd7e3300add1e4c9b6b43eafc092b0311e6be75)
    
    --HG--
    branch : 4.4

--- src/common.js
+++ src/common.js
@@ -2063,6 +2063,9 @@
                     context_field = Sao.Date.min;
                 }
             }
+            if ((context_field instanceof Array) & (value === null)) {
+                value = [];
+            }
             if ((typeof context_field == 'string') &&
                     (value instanceof Array) && value.length == 2) {
                 value = value.join(',');
