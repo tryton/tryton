ce57e5652cab1fd690aaba29a04690c0a7f2d100
Cédric Krier <ced@b2ck.com>
2018-03-15 00:24:43 +0100

    Bind the set_value callback in One2Many._set_value
    this.name is used in the callback.
    
    issue7206
    review41171002
    (grafted from 79c69576388542ca730268104092e17ac32b2f5d)
    
    --HG--
    branch : 4.0

--- src/model.js
+++ src/model.js
@@ -1856,7 +1856,7 @@
                 }
                 return jQuery.when.apply(jQuery, promises);
             };
-            return prm.then(set_value);
+            return prm.then(set_value.bind(this));
         },
         set: function(record, value, _default) {
             if (_default === undefined) {
