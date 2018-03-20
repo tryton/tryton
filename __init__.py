e42c93b3228efa5253f887e5f0f94600f0d0a527
Cédric Krier <ced@b2ck.com>
2018-03-20 17:29:09 +0100

    Add depends on Pool.register
    issue7178
    review37051002

--- __init__.py
+++ __init__.py
@@ -1,20 +1,23 @@
 # This file is part of Tryton.  The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
 from trytond.pool import Pool
-from .payment import *
-from .account import *
-from .statement import *
+from . import payment
+from . import account
+from . import statement
 
 
 def register():
     Pool.register(
-        Journal,
-        Payment,
-        SucceedStart,
-        Move,
-        Statement,
-        StatementLine,
+        payment.Journal,
+        payment.Payment,
+        payment.SucceedStart,
+        account.Move,
         module='account_payment_clearing', type_='model')
     Pool.register(
-        Succeed,
+        statement.Statement,
+        statement.StatementLine,
+        module='account_payment_clearing', type_='model',
+        depends=['account_statement'])
+    Pool.register(
+        payment.Succeed,
         module='account_payment_clearing', type_='wizard')
