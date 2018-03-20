cf701f584500820d3eeb5bc4ba08219a89ddc866
Cédric Krier <ced@b2ck.com>
2018-03-20 17:27:51 +0100

    Add depends on Pool.register
    issue7178
    review37051002

--- __init__.py
+++ __init__.py
@@ -1,39 +1,41 @@
 # This file is part of Tryton.  The COPYRIGHT file at the top level of this
 # repository contains the full copyright notices and license terms.
 from trytond.pool import Pool
-from .asset import *
-from .product import *
-from .invoice import *
-from .account import *
-from .purchase import *
+from . import asset
+from . import product
+from . import invoice
+from . import account
+from . import purchase
 
 
 def register():
     Pool.register(
-        Asset,
-        AssetLine,
-        AssetUpdateMove,
-        CreateMovesStart,
-        UpdateAssetStart,
-        UpdateAssetShowDepreciation,
-        PrintDepreciationTableStart,
-        Category,
-        CategoryAccount,
-        Template,
-        TemplateAccount,
-        Product,
-        InvoiceLine,
-        Configuration,
-        ConfigurationAssetSequence,
-        Move,
-        Period,
-        PurchaseLine,
+        asset.Asset,
+        asset.AssetLine,
+        asset.AssetUpdateMove,
+        asset.CreateMovesStart,
+        asset.UpdateAssetStart,
+        asset.UpdateAssetShowDepreciation,
+        asset.PrintDepreciationTableStart,
+        product.Category,
+        product.CategoryAccount,
+        product.Template,
+        product.TemplateAccount,
+        product.Product,
+        invoice.InvoiceLine,
+        account.Configuration,
+        account.ConfigurationAssetSequence,
+        account.Move,
+        account.Period,
         module='account_asset', type_='model')
     Pool.register(
-        CreateMoves,
-        UpdateAsset,
-        PrintDepreciationTable,
+        purchase.PurchaseLine,
+        module='account_asset', type_='model', depends=['purchase'])
+    Pool.register(
+        asset.CreateMoves,
+        asset.UpdateAsset,
+        asset.PrintDepreciationTable,
         module='account_asset', type_='wizard')
     Pool.register(
-        AssetDepreciationTable,
+        asset.AssetDepreciationTable,
         module='account_asset', type_='report')
