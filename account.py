408a8e22303a9d310a9bce95064704cd0b916e76
Cédric Krier <ced@b2ck.com>
2018-03-24 13:34:43 +0100

    Initial commit
    issue7197
    review38041002

---
+++ account.py
@@ -0,0 +1,112 @@
+# This file is part of Tryton.  The COPYRIGHT file at the top level of
+# this repository contains the full copyright notices and license terms.
+from io import BytesIO
+
+import ofxparse
+
+from trytond.pool import PoolMeta, Pool
+
+
+class StatementImportStart:
+    __metaclass__ = PoolMeta
+    __name__ = 'account.statement.import.start'
+
+    @classmethod
+    def __setup__(cls):
+        super(StatementImportStart, cls).__setup__()
+        cls.file_format.selection.append(('ofx', "OFX"))
+
+
+class StatementImport:
+    __metaclass__ = PoolMeta
+    __name__ = 'account.statement.import'
+
+    @classmethod
+    def __setup__(cls):
+        super(StatementImport, cls).__setup__()
+        cls._error_messages.update({
+                'ofx_no_journal': (
+                    'No journal found for the bank account "%(account)s".'),
+                'ofx_wrong_currency': (
+                    'The journal "%(journal)s" must have '
+                    'the currency "%(ofx_currency)s" instead of '
+                    '"%(currency)s".'),
+                'ofx_not_statement': (
+                    "The OFX file is not a statement."),
+                })
+
+    def parse_ofx(self):
+        file_ = BytesIO(self.start.file_)
+        ofx = ofxparse.OfxParser.parse(file_)
+
+        for account in ofx.accounts:
+            statement = self.ofx_statement(ofx, account)
+            origins = []
+            for transaction in account.statement.transactions:
+                origins.extend(self.ofx_origin(account, transaction))
+            statement.origins = origins
+            yield statement
+
+    def ofx_statement(self, ofx, ofx_account):
+        pool = Pool()
+        Statement = pool.get('account.statement')
+        Journal = pool.get('account.statement.journal')
+
+        statement = Statement()
+        statement.name = ofx.trnuid
+        statement.company = self.start.company
+        statement.journal = Journal.get_by_bank_account(
+            statement.company, ofx_account.number)
+        if not statement.journal:
+            self.raise_user_error('ofx_no_journal', {
+                    'account': ofx_account.number,
+                    })
+        if statement.journal.currency.code != ofx_account.curdef:
+            self.raise_user_error('ofx_wrong_currency', {
+                    'journal': statement.journal.rec_name,
+                    'currency': statement.journal.currency.rec_name,
+                    'ofx_currency': ofx_account.curdef,
+                    })
+        if not isinstance(ofx_account.statement, ofxparse.Statement):
+            self.raise_user_error('ofx_not_statement')
+        statement.date = ofx_account.statement.balance_date.date()
+        statement.total_amount = ofx_account.statement.balance
+        statement.number_of_lines = len(ofx_account.statement.transactions)
+        return statement
+
+    def ofx_origin(self, ofx_account, transaction):
+        pool = Pool()
+        Origin = pool.get('account.statement.origin')
+
+        origin = Origin()
+        origin.number = transaction.id
+        origin.date = transaction.date.date()
+        origin.amount = transaction.amount
+        origin.party = self.ofx_party(ofx_account, transaction)
+        origin.description = transaction.memo
+        origin.informations = self.ofx_informations(ofx_account, transaction)
+        return [origin]
+
+    def ofx_party(self, ofx_account, transaction):
+        pool = Pool()
+        Party = pool.get('party.party')
+
+        if not transaction.payee:
+            return
+        parties = Party.search([('rec_name', 'ilike', transaction.payee)])
+        if len(parties) == 1:
+            party, = parties
+            return party
+
+    def ofx_informations(self, ofx_account, transaction):
+        informations = {}
+        for name in [
+                'checknum',
+                'mcc',
+                'sic',
+                'type',
+                ]:
+            value = getattr(transaction, name)
+            if value:
+                informations['ofx_' + name] = value
+        return informations
---
+++ tests/scenario_account_statement_ofx.rst
@@ -0,0 +1,119 @@
+==============================
+Account Statement OFX Scenario
+==============================
+
+Imports::
+
+    >>> from proteus import Model, Wizard
+    >>> from trytond.tests.tools import activate_modules
+    >>> from trytond.tools import file_open
+    >>> from trytond.modules.currency.tests.tools import get_currency
+    >>> from trytond.modules.company.tests.tools import create_company, \
+    ...     get_company
+    >>> from trytond.modules.account.tests.tools import create_chart, \
+    ...     get_accounts
+
+Install account_statement_ofx::
+
+    >>> config = activate_modules('account_statement_ofx')
+
+Create company::
+
+    >>> _ = create_company(currency=get_currency('EUR'))
+    >>> company = get_company()
+
+Create chart of accounts::
+
+    >>> _ = create_chart(company)
+    >>> accounts = get_accounts(company)
+    >>> cash = accounts['cash']
+
+Create parties::
+
+    >>> Party = Model.get('party.party')
+    >>> michael_scott_paper = Party(name="Michael Scott Paper Company")
+    >>> michael_scott_paper.save()
+    >>> bank_party = Party(name="Bank")
+    >>> bank_party.save()
+
+Create Bank Account::
+
+    >>> Bank = Model.get('bank')
+    >>> BankAccount = Model.get('bank.account')
+
+    >>> bank = Bank()
+    >>> bank.party = bank_party
+    >>> bank.save()
+    >>> bank_account = BankAccount()
+    >>> bank_account.bank = bank
+    >>> bank_account.owners.append(Party(company.party.id))
+    >>> bank_account.currency = company.currency
+    >>> bank_account_number = bank_account.numbers.new()
+    >>> bank_account_number.type = 'other'
+    >>> bank_account_number.number = '01234567890'
+    >>> bank_account.save()
+
+Create Account Journal::
+
+    >>> Sequence = Model.get('ir.sequence')
+    >>> AccountJournal = Model.get('account.journal')
+
+    >>> sequence = Sequence(name="Satement",
+    ...     code='account.journal',
+    ...     company=company,
+    ...     )
+    >>> sequence.save()
+    >>> account_journal = AccountJournal(name="Statement",
+    ...     type='statement',
+    ...     credit_account=cash,
+    ...     debit_account=cash,
+    ...     sequence=sequence,
+    ... )
+    >>> account_journal.save()
+
+Create Statement Journal::
+
+    >>> StatementJournal = Model.get('account.statement.journal')
+
+    >>> journal = StatementJournal(name="Bank",
+    ...     journal=account_journal,
+    ...     bank_account=bank_account,
+    ...     validation='amount',
+    ...     )
+    >>> journal.save()
+
+Import OFX file::
+
+    >>> statement_import = Wizard('account.statement.import')
+    >>> ofx = file_open('account_statement_ofx/tests/OFX.txt', mode='rb').read()
+    >>> statement_import.form.file_ = ofx
+    >>> statement_import.form.file_format = 'ofx'
+    >>> statement_import.execute('import_')
+
+Check Statement::
+
+    >>> Statement = Model.get('account.statement')
+    >>> statement, = Statement.find([])
+    >>> statement.name
+    u'01234567890'
+    >>> statement.date
+    datetime.date(2018, 2, 22)
+    >>> statement.total_amount
+    Decimal('100.00')
+    >>> statement.number_of_lines
+    1
+    >>> len(statement.origins)
+    1
+    >>> origin, = statement.origins
+    >>> origin.number
+    u'0001'
+    >>> origin.date
+    datetime.date(2018, 2, 21)
+    >>> origin.amount
+    Decimal('100.00')
+    >>> origin.party == michael_scott_paper
+    True
+    >>> origin.description
+    u'COMMUNICATION'
+    >>> origin.informations['ofx_type']
+    u'credit'
