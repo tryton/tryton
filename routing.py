6710e9c71d079261887fba87212afec25b96c0f3
Sergi Almacellas Abellana <sergi@koolpi.com>
2018-03-26 14:12:22 +0200

    Use deactivable mixin
    issue4341
    review38401002

--- routing.py
+++ routing.py
@@ -1,33 +1,24 @@
 # This file is part of Tryton.  The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
-from trytond.model import ModelSQL, ModelView, fields, sequence_ordered
+from trytond.model import (
+    ModelSQL, ModelView, DeactivableMixin, fields, sequence_ordered)
 
 __all__ = ['Routing', 'Operation', 'RoutingStep', 'Routing_BOM']
 
 
-class Routing(ModelSQL, ModelView):
+class Routing(DeactivableMixin, ModelSQL, ModelView):
     'Routing'
     __name__ = 'production.routing'
     name = fields.Char('Name', required=True, translate=True)
-    active = fields.Boolean('Active', select=True)
     steps = fields.One2Many('production.routing.step', 'routing', 'Steps')
     boms = fields.Many2Many(
         'production.routing-production.bom', 'routing', 'bom', 'BOMs')
 
-    @classmethod
-    def default_active(cls):
-        return True
 
-
-class Operation(ModelSQL, ModelView):
+class Operation(DeactivableMixin, ModelSQL, ModelView):
     'Operation'
     __name__ = 'production.routing.operation'
     name = fields.Char('Operation', required=True, translate=True)
-    active = fields.Boolean('Active', select=True)
-
-    @classmethod
-    def default_active(cls):
-        return True
 
 
 class RoutingStep(sequence_ordered(), ModelSQL, ModelView):
