4fec4f3d6fe4e41915cfdab3f782cf29e794cca3
Kunkel Pierre <pierre.kunkel@coopengo.com>
2018-04-14 10:22:46 +0200

    Manage readonly state on boolean in Dict widget
    issue7341
    review42081002
    (grafted from c14d6c5c0e3ecdb1fcc787513c63a1e34cf8dcc8)
    
    --HG--
    branch : 4.2

--- src/view/form.js
+++ src/view/form.js
@@ -3890,6 +3890,10 @@ function eval_pyson(value){
         get_value: function() {
             return this.input.prop('checked');
         },
+        set_readonly: function(readonly) {
+            this._readonly = readonly;
+            this.input.prop('disabled', readonly);
+        },
         set_value: function(value) {
             this.input.prop('checked', value);
         }
