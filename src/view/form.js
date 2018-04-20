364ae231fb28103155309922c16f46181226c09c
Cédric Krier <ced@b2ck.com>
2018-04-20 12:48:36 +0200

    Disable dp.change event when setting date value
    DateTimePicker trigger the event when the date value is set which trigger the
    focus_out. When this happens during the display of a Dict widget, the keys not
    yet displayed are lost.
    
    issue7361
    review47331003
    (grafted from 6ff834bd15aa161e25db63a01fd7112881c707a2)
    
    --HG--
    branch : 4.6

--- src/view/form.js
+++ src/view/form.js
@@ -1379,7 +1379,12 @@ function eval_pyson(value){
             } else {
                 value = null;
             }
-            this.date.data('DateTimePicker').date(value);
+            this.date.off('dp.change');
+            try {
+                this.date.data('DateTimePicker').date(value);
+            } finally {
+                this.date.on('dp.change', this.focus_out.bind(this));
+            }
         },
         focus: function() {
             this.input.focus();
@@ -4053,7 +4058,13 @@ function eval_pyson(value){
             return value;
         },
         set_value: function(value) {
-            this.input.data('DateTimePicker').date(value);
+            this.input.off('dp.change');
+            try {
+                this.input.data('DateTimePicker').date(value);
+            } finally {
+                this.input.on('dp.change',
+                    this.parent_widget.focus_out.bind(this.parent_widget));
+            }
         }
     });
 
