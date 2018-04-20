51e0754d1ae38a87990fae38a1409c137abe7a20
Cédric Krier <ced@b2ck.com>
2018-04-20 18:42:40 +0200

    Do not convert 0 value into null in Float/Numeric fields
    issue7349
    review38171003
    (grafted from d59c78fcea95ce74c8029566d8e51d6aa6ed362d)
    
    --HG--
    branch : 4.0

--- src/model.js
+++ src/model.js
@@ -1616,7 +1616,7 @@
             return true;
         },
         convert: function(value) {
-            if (!value) {
+            if (!value && (value !== 0)) {
                 return null;
             }
             value = Number(value);
@@ -1659,7 +1659,7 @@
 
     Sao.field.Numeric = Sao.class_(Sao.field.Float, {
         convert: function(value) {
-            if (!value) {
+            if (!value && (value !== 0)) {
                 return null;
             }
             value = new Sao.Decimal(value);
