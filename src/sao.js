dafeb2462fb9e71cad1281379dd489c4275d3768
Sergi Almacellas Abellana <sergi@koolpi.com>
2018-06-06 16:18:39 +0200

    Convert value to datetime when setting calendar view default value
    issue7495
    review47471002
    (grafted from 36ab183366296a006a232f1bc345d3fdf5f65432)
    
    --HG--
    branch : 4.2

--- src/sao.js
+++ src/sao.js
@@ -104,7 +104,14 @@ var Sao = {};
     Sao.Decimal = Number;
 
     Sao.Date = function(year, month, day) {
-        var date = moment();
+        var date;
+        if (month === undefined) {
+            date = moment(year);
+            year = undefined;
+        }
+        else {
+            date = moment();
+        }
         date.year(year);
         date.month(month);
         date.date(day);
