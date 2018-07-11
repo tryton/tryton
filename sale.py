13b24da4f07165c1b84405222b679548d14c7514
Sergio Morillo <sergio.morillo@gmail.com>
2018-07-11 17:17:50 +0200

    Show number and reference in sale rec_name
    issue7553
    review47571002

--- sale.py
+++ sale.py
@@ -672,11 +672,14 @@ class Sale(Workflow, ModelSQL, ModelView, TaxableMixin):
             self.raise_user_error('invalid_method', (self.rec_name,))
 
     def get_rec_name(self, name):
+        items = []
         if self.number:
-            return self.number
-        elif self.reference:
-            return '[%s]' % self.reference
-        return '(%s)' % self.id
+            items.append(self.number)
+        if self.reference:
+            items.append('[%s]' % self.reference)
+        if not items:
+            items.append('(%s)' % self.id)
+        return ' '.join(items)
 
     @classmethod
     def search_rec_name(cls, name, clause):
