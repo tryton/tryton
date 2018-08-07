0c1e528c033bb2ed3199032b58639c8ba5a0d30d
Cédric Krier <ced@b2ck.com>
2018-08-07 14:01:27 +0200

    Move add/remove input next to the add and remove buttons
    The changeset c2b74d2ff7ed reordered the toolbar but left the input at the
    beginning while the add and remove buttons were moved in the middle next to
    the create button.
    
    issue7604
    review47641002
    (grafted from 861bae448ba1d1ccbcea523cecb4f170558e4e18)
    
    --HG--
    branch : 4.8

--- src/view/form.js
+++ src/view/form.js
@@ -2720,12 +2720,6 @@ function eval_pyson(value){
                 'class': 'input-group input-group-sm'
             }).appendTo(toolbar);
 
-            this.wid_text = jQuery('<input/>', {
-                type: 'text',
-                'class': 'form-control input-sm'
-            }).appendTo(group);
-            this.wid_text.hide();
-
             var buttons = jQuery('<div/>', {
                 'class': 'input-group-btn'
             }).appendTo(group);
@@ -2770,9 +2764,17 @@ function eval_pyson(value){
             this.but_next.click(this.next.bind(this));
 
             if (attributes.add_remove) {
-                this.wid_text.show();
+                this.wid_text = jQuery('<input/>', {
+                    type: 'text',
+                    'class': 'form-control input-sm'
+                }).appendTo(group);
                 // TODO add completion
                 //
+                //
+                buttons =  jQuery('<div/>', {
+                    'class': 'input-group-btn',
+                }).appendTo(group);
+
                 this.but_add = jQuery('<button/>', {
                     'class': 'btn btn-default btn-sm',
                     'type': 'button',
@@ -2968,7 +2970,7 @@ function eval_pyson(value){
             }.bind(this));
         },
         focus: function() {
-            if (this.wid_text.is(':visible')) {
+            if (this.attributes.add_remove) {
                 this.wid_text.focus();
             }
         },
