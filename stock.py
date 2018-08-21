67b22e8a3484ebbaae60312ab93359daf327d012
Cédric Krier <ced@b2ck.com>
2018-08-21 19:01:20 +0200

    Use party name of the address as name
    and name of party as attention name
    
    issue7556
    review58321002

--- stock.py
+++ stock.py
@@ -240,8 +240,8 @@ class CreateShippingUPS(Wizard):
 
     def get_shipping_party(self, party, address):
         shipping_party = {
-            'Name': party.name[:35],
-            'AttentionName': address.party.name[:35],
+            'Name': address.party_full_name[:35],
+            'AttentionName': party.full_name[:35],
             'Address': {
                 'AddressLine': [l[:35]
                     for l in (address.street or '').splitlines()[:3]],
