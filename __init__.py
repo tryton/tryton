23e63dc29445c759e3d393314314955484550b08
Cédric Krier <ced@b2ck.com>
2018-09-01 00:17:27 +0200

    Initial commit
    issue7518
    review54261002

---
+++ __init__.py
@@ -0,0 +1,27 @@
+# This file is part of Tryton.  The COPYRIGHT file at the top level of
+# this repository contains the full copyright notices and license terms.
+
+from trytond.pool import Pool
+from . import account
+from . import party
+from . import edocument
+
+__all__ = ['register']
+
+
+def register():
+    Pool.register(
+        account.Configuration,
+        account.CredentialChorus,
+        account.Invoice,
+        account.InvoiceChorus,
+        party.Party,
+        module='account_fr_chorus', type_='model')
+    Pool.register(
+        edocument.UNCEFACTInvoice,
+        module='account_fr_chorus', type_='model',
+        depends=['edocument_uncefact'])
+    Pool.register(
+        module='account_fr_chorus', type_='wizard')
+    Pool.register(
+        module='account_fr_chorus', type_='report')
---
+++ tests/test_account_fr_chorus.py
@@ -0,0 +1,20 @@
+# This file is part of Tryton.  The COPYRIGHT file at the top level of
+# this repository contains the full copyright notices and license terms.
+
+import unittest
+
+
+from trytond.tests.test_tryton import ModuleTestCase
+from trytond.tests.test_tryton import suite as test_suite
+
+
+class AccountFrChorusTestCase(ModuleTestCase):
+    'Test Account Fr Chorus module'
+    module = 'account_fr_chorus'
+
+
+def suite():
+    suite = test_suite()
+    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
+            AccountFrChorusTestCase))
+    return suite
