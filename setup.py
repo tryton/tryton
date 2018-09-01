47a915e9eb6ced5d7074e9f20a443324adfe6c53
Sergi Almacellas Abellana <sergi@koolpi.com>
2018-09-01 15:16:48 +0200

    Add proteus as test dependency

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ for dep in info.get('depends', []):
         requires.append(get_require_version('trytond_%s' % dep))
 requires.append(get_require_version('trytond'))
 
-tests_require = []
+tests_require = [get_require_version('proteus')]
 dependency_links = []
 if minor_version % 2:
     dependency_links.append('https://trydevpi.tryton.org/')
