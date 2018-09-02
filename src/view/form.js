d20385d9fccc2202f9dec5a45d10cf58d95fa270
Cédric Krier <ced@b2ck.com>
2018-09-02 10:56:43 +0200

    Add default focus method on Form Widget
    The editable tree is calling this method to set the focus on the edited widget
    so it must be part of the API of Widget.
    
    issue7661
    review54321002

--- src/view/form.js
+++ src/view/form.js
@@ -1115,7 +1115,10 @@ function eval_pyson(value){
             } else {
                 this.el.show();
             }
-        }
+        },
+        focus: function() {
+            this.el.focus();
+        },
     });
 
     Sao.View.Form.TranslateDialog = Sao.class_(Object,  {
