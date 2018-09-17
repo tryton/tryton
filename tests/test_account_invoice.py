40d4113031047e32981b7b14058127dee97d314e
Cédric Krier <ced@b2ck.com>
2018-09-17 17:51:43 +0200

    Add scenario about invoice in alternate currency with lower rate
    issue7192
    review52471002

--- tests/test_account_invoice.py
+++ tests/test_account_invoice.py
@@ -226,6 +226,11 @@ def suite():
             checker=doctest_checker,
             optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
     suite.addTests(doctest.DocFileSuite(
+            'scenario_invoice_alternate_currency_lower_rate.rst',
+            tearDown=doctest_teardown, encoding='utf-8',
+            checker=doctest_checker,
+            optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
+    suite.addTests(doctest.DocFileSuite(
             'scenario_renew_fiscalyear.rst',
             tearDown=doctest_teardown, encoding='utf-8',
             checker=doctest_checker,
