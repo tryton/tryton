b6fe590e64d7cfeb347044d5d3e0ca4960551a93
Cédric Krier <ced@b2ck.com>
2018-09-19 00:13:59 +0200

    Add quick return when evaluate simple expression
    issue7696
    review49571002
    (grafted from 94e38eeae5cd86985ba10cbc4304fe67c0c48502)
    
    --HG--
    branch : 4.6

--- src/model.js
+++ src/model.js
@@ -1106,6 +1106,13 @@
         },
         expr_eval: function(expr) {
             if (typeof(expr) != 'string') return expr;
+            if (!expr) {
+                return;
+            } else if (expr == '[]') {
+                return [];
+            } else if (expr == '{}') {
+                return {};
+            }
             var ctx = jQuery.extend({}, this.model.session.context);
             ctx.context = jQuery.extend({}, ctx);
             jQuery.extend(ctx.context, this.get_context());
