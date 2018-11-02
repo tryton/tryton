252361c37c46475ae02977c1c5d74c986d403662
Cédric Krier <ced@b2ck.com>
2018-11-02 20:02:53 +0100

    Require python-sql >= 0.9
    Since changeset c8697ddc5c32, it uses the feature of SELECT query as column
    from python-sql.
    
    issue7832
    review52521003
    (grafted from 29c51ade0606c45f90b2194ab0a42e63b39ffc4f)
    
    --HG--
    branch : 5.0

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ if minor_version % 2:
         'hg+http://hg.tryton.org/modules/%s#egg=%s-%s' % (
             name[8:], name, version))
 
-requires = []
+requires = ['python-sql >= 0.9']
 for dep in info.get('depends', []):
     if not re.match(r'(ir|res)(\W|$)', dep):
         requires.append(get_require_version('trytond_%s' % dep))
