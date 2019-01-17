e5319fc82d75994094c3c14dc1db12a0aa69ede9
Cédric Krier <ced@b2ck.com>
2019-01-17 17:22:22 +0100

    Add extras modules to ModuleTestCase
    issue7935
    review50661002

--- tests/test_carrier_percentage.py
+++ tests/test_carrier_percentage.py
@@ -15,6 +15,7 @@ from trytond.modules.currency.tests import create_currency
 class CarrierPercentageTestCase(ModuleTestCase):
     'Test CarrierPercentage module'
     module = 'carrier_percentage'
+    extras = ['purchase_shipment_cost', 'sale_shipment_cost']
 
     @with_transaction()
     def test_compute_percentage(self):
