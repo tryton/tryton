20b739beb239645b1559b80f9b6375839ef87335
Nicolas Évrard <nicoe@b2ck.com>
2019-01-23 14:37:31 +0100

    Do not prevent to reset X2Many widget when no record is linked to the form
    It can occur when for example, you delete a record. The current code skipped
    the reset of the widget which made it stays filled.
    (grafted from 2062dcd560dbf4b32fe58fa15c9a7b4ecdc7b762)
    
    --HG--
    branch : 4.8

--- src/view/form.js
+++ src/view/form.js
@@ -2933,9 +2933,6 @@ function eval_pyson(value){
             this._set_button_sensitive();
 
             this.prm.done(function() {
-                if (!record) {
-                    return;
-                }
                 if (field === undefined) {
                     this.screen.new_group();
                     this.screen.set_current_record(null);
@@ -3339,9 +3336,6 @@ function eval_pyson(value){
             Sao.View.Form.Many2Many._super.display.call(this, record, field);
 
             this.prm.done(function() {
-                if (!record) {
-                    return;
-                }
                 if (field === undefined) {
                     this.screen.new_group();
                     this.screen.set_current_record(null);
