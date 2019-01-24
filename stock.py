eb74bbf1c302c49fdf0e78b01a4b3d0d9048e2bc
Cédric Krier <ced@b2ck.com>
2019-01-24 21:38:47 +0100

    Clear origin when copying move
    If the copy is not a split, we should remove the invoice line origin as it is
    new move that should be invoiced.
    
    issue7657
    review64531002
    (grafted from 6720c66ab49cbae633f574e20338752aa04231f3)
    
    --HG--
    branch : 4.8

--- stock.py
+++ stock.py
@@ -244,6 +244,21 @@ class Move:
     def cancel(cls, moves):
         super(Move, cls).cancel(moves)
 
+    @classmethod
+    def copy(cls, moves, default=None):
+        pool = Pool()
+        InvoiceLine = pool.get('account.invoice.line')
+        moves = super(Move, cls).copy(moves, default=default)
+        if not Transaction().context.get('_stock_move_split'):
+            to_save = []
+            for move in moves:
+                if isinstance(move.origin, InvoiceLine):
+                    move.origin = None
+                    to_save.append(move)
+            if to_save:
+                cls.save(to_save)
+        return moves
+
 
 class ShipmentInternal:
     __metaclass__ = PoolMeta
