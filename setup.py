abb5f7e4ad3fc9d1584023f06f33ca608ced40c6
Cédric Krier <ced@b2ck.com>
2019-01-25 23:26:58 +0100

    Replace --process-dependency-link by --find-links
    issue8044
    review51611003

--- setup.py
+++ setup.py
@@ -53,8 +53,6 @@ requires.append(get_require_version('trytond'))
 
 tests_require = [get_require_version('trytond_edocument_uncefact')]
 dependency_links = []
-if minor_version % 2:
-    dependency_links.append('https://trydevpi.tryton.org/')
 
 setup(name=name,
     version=version,
