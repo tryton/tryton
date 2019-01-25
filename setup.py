eccd305755d1de3544b7d96a64437cfee8b245db
Cédric Krier <ced@b2ck.com>
2019-01-25 23:34:31 +0100

    Replace --process-dependency-link by --find-links
    issue8044
    
    --HG--
    branch : 4.8

--- setup.py
+++ setup.py
@@ -59,8 +59,6 @@ tests_require = [
     get_require_version('trytond_purchase_requisition'),
     ]
 dependency_links = []
-if minor_version % 2:
-    dependency_links.append('https://trydevpi.tryton.org/')
 
 setup(name=name,
     version=version,
