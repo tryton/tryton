18b540df0ec621f62735304509584452f9bec4cc
Cédric Krier <ced@b2ck.com>
2019-01-26 01:12:50 +0100

    Re-add trydevpi to dependency_links
    It is needed to install Tryton's dependencies of dependencies.
    
    issue8044
    
    --HG--
    branch : 4.8

--- setup.py
+++ setup.py
@@ -57,6 +57,8 @@ tests_require = [get_require_version('proteus'),
     get_require_version('trytond_account_statement'),
     'python-dateutil']
 dependency_links = []
+if minor_version % 2:
+    dependency_links.append('https://trydevpi.tryton.org/')
 
 setup(name=name,
     version=version,
