e6dd4bb84ca4f892ece928b708ceb5f6e0113472
Cédric Krier <ced@b2ck.com>
2019-02-06 22:51:18 +0100

    Lock records when processing
    issue8012
    review60511002
    (grafted from d404a49a74d2d906ce2b2d3bf14978b5d5c51427)
    
    --HG--
    branch : 4.4

--- payment.py
+++ payment.py
@@ -192,6 +192,7 @@ class Payment:
                     ('stripe_charge_id', '=', None),
                     ])
         for payment in payments:
+            cls.__lock([payment])
             try:
                 charge = stripe.Charge.create(**payment._charge_parameters())
             except (stripe.error.RateLimitError,
@@ -234,6 +235,25 @@ class Payment:
             'source': source,
             'idempotency_key': self.stripe_checkout_id,
             }
+
+    @classmethod
+    def __lock(cls, records):
+        from trytond.tools import grouped_slice, reduce_ids
+        from sql import Literal, For
+        transaction = Transaction()
+        database = transaction.database
+        connection = transaction.connection
+        table = cls.__table__()
+
+        if database.has_select_for():
+            for sub_records in grouped_slice(records):
+                where = reduce_ids(table.id, sub_records)
+                query = table.select(
+                    Literal(1), where=where, for_=For('UPDATE', nowait=True))
+                with connection.cursor() as cursor:
+                    cursor.execute(*query)
+        else:
+            database.lock(connection, cls._table)
 
 
 class Account(ModelSQL, ModelView):
