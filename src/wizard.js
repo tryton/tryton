e567ee2bb109854cc3dad13351a41ca8d3a6b52b
Cédric Krier <ced@b2ck.com>
2019-03-15 11:18:14 +0100

    Connect only once wizard dialog to keydown
    update is called on each state so it binds multiple times to keydown and call
    multiple times response with end state. This results in trying to delete
    multiple times the same wizard session which fails.
    
    issue8179
    review60591002
    (grafted from e937c0f4b24bbc2dd816b53bf0b986df580907db)
    
    --HG--
    branch : 4.8

--- src/wizard.js
+++ src/wizard.js
@@ -219,6 +219,14 @@
             this.dialog = dialog.modal;
             this.content = dialog.content;
             this.footer = dialog.footer;
+            this.dialog.on('keydown', function(e) {
+                if (e.which == Sao.common.ESC_KEYCODE) {
+                    e.preventDefault();
+                    if (this.end_state in this.states) {
+                        this.response(this.end_state);
+                    }
+                }
+            }.bind(this));
             dialog.body.append(this.widget).append(this.info_bar.el);
         },
         clean: function() {
@@ -248,14 +256,6 @@
             Sao.Wizard.Dialog._super.update.call(this, view, defaults,
                     buttons);
             this.dialog.modal('show');
-            this.dialog.on('keydown', function(e) {
-                if (e.which == Sao.common.ESC_KEYCODE) {
-                    e.preventDefault();
-                    if (this.end_state in this.states) {
-                        this.response(this.end_state);
-                    }
-                }
-            }.bind(this));
         },
         destroy: function(action) {
             Sao.Wizard.Dialog._super.destroy.call(this);
