1c4c1b80ec417a2b2aade0640e2a636322acd596
Cédric Krier <ced@b2ck.com>
2019-03-22 15:33:43 +0100

    Use find_packages to discover packages
    issue7963
    review253111002

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import io
 import os
 import re
 from configparser import ConfigParser
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 def read(fname):
@@ -68,10 +68,10 @@ setup(name=name,
     download_url=download_url,
     keywords='tryton account payment',
     package_dir={'trytond.modules.account_payment': '.'},
-    packages=[
-        'trytond.modules.account_payment',
-        'trytond.modules.account_payment.tests',
-        ],
+    packages=(
+        ['trytond.modules.account_payment'] +
+        ['trytond.modules.account_payment.%s' % p for p in find_packages()]
+        ),
     package_data={
         'trytond.modules.account_payment': (info.get('xml', [])
             + ['tryton.cfg', 'view/*.xml', 'locale/*.po', '*.fodt',
