e6e0aca862068174d84e729c27632e11b5f02bdc
Cédric Krier <ced@b2ck.com>
2019-03-22 15:33:43 +0100

    Use find_packages to discover packages
    issue7963
    review253111002

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import io
 import os
 import re
 from configparser import ConfigParser
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 def read(fname):
@@ -66,10 +66,10 @@ setup(name=name,
     download_url=download_url,
     keywords='tryton commission',
     package_dir={'trytond.modules.commission_waiting': '.'},
-    packages=[
-        'trytond.modules.commission_waiting',
-        'trytond.modules.commission_waiting.tests',
-        ],
+    packages=(
+        ['trytond.modules.commission_waiting'] +
+        ['trytond.modules.commission_waiting.%s' % p for p in find_packages()]
+        ),
     package_data={
         'trytond.modules.commission_waiting': (info.get('xml', [])
             + ['tryton.cfg', 'view/*.xml', 'locale/*.po', 'tests/*.rst']),
