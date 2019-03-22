b7d608d1c10fe588791cf29e714b65c43ebe2988
Cédric Krier <ced@b2ck.com>
2019-03-22 11:18:05 +0100

    Add test scenario with non default UoM
    issue7918
    review251121002

--- tests/test_product_cost_fifo.py
+++ tests/test_product_cost_fifo.py
@@ -22,4 +22,9 @@ def suite():
             tearDown=doctest_teardown, encoding='utf-8',
             checker=doctest_checker,
             optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
+    suite.addTests(doctest.DocFileSuite(
+            'scenario_product_cost_fifo_uom.rst',
+            tearDown=doctest_teardown, encoding='utf-8',
+            checker=doctest_checker,
+            optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
     return suite
