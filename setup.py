f834e59b9c3f5b95e692827839e67aa7c9d230fb
Cédric Krier <ced@b2ck.com>
2019-03-22 15:33:43 +0100

    Use find_packages to discover packages
    issue7963
    review253111002

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import io
 import os
 import re
 from configparser import ConfigParser
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 def read(fname):
@@ -66,10 +66,11 @@ setup(name=name,
     download_url=download_url,
     keywords='tryton production work timesheet',
     package_dir={'trytond.modules.production_work_timesheet': '.'},
-    packages=[
-        'trytond.modules.production_work_timesheet',
-        'trytond.modules.production_work_timesheet.tests',
-        ],
+    packages=(
+        ['trytond.modules.production_work_timesheet'] +
+        ['trytond.modules.production_work_timesheet.%s' % p
+            for p in find_packages()]
+        ),
     package_data={
         'trytond.modules.production_work_timesheet': (info.get('xml', [])
             + ['tryton.cfg', 'view/*.xml', 'locale/*.po', '*.fodt',
