e2a502b147a9ad1276c8dc712f0cbfdf8145101b
Cédric Krier <ced@b2ck.com>
2019-03-22 15:33:43 +0100

    Use find_packages to discover packages
    issue7963
    review253111002

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import io
 import os
 import re
 from configparser import ConfigParser
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 def read(fname):
@@ -65,10 +65,10 @@ setup(name=name,
     download_url=download_url,
     keywords='tryton carrier',
     package_dir={'trytond.modules.carrier': '.'},
-    packages=[
-        'trytond.modules.carrier',
-        'trytond.modules.carrier.tests',
-        ],
+    packages=(
+        ['trytond.modules.carrier'] +
+        ['trytond.modules.carrier.%s' % p for p in find_packages()]
+        ),
     package_data={
         'trytond.modules.carrier': (info.get('xml', [])
             + ['tryton.cfg', 'view/*.xml', 'locale/*.po', 'icons/*.svg']),
