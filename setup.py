d7aec848f448f7dbc39fd6c017cc14ebae2424de
Cédric Krier <ced@b2ck.com>
2019-03-22 15:33:44 +0100

    Use find_packages to discover packages
    issue7963
    review253111002
    (grafted from 411b7929bcda9791e22ab4cb3b5ab26f166fb919)
    
    --HG--
    branch : 5.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import io
 import os
 import re
 from configparser import ConfigParser
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 def read(fname):
@@ -66,9 +66,11 @@ setup(name=name,
     download_url=download_url,
     keywords='tryton sale group invoice',
     package_dir={'trytond.modules.sale_invoice_grouping': '.'},
-    packages=[
-        'trytond.modules.sale_invoice_grouping',
-        ],
+    packages=(
+        ['trytond.modules.sale_invoice_grouping'] +
+        ['trytond.modules.sale_invoice_grouping.%s' % p
+            for p in find_packages()]
+        ),
     package_data={
         'trytond.modules.sale_invoice_grouping': (info.get('xml', [])
             + ['tryton.cfg', 'view/*.xml', 'locale/*.po', 'tests/*.rst']),
