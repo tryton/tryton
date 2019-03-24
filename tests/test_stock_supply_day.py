f51edcec16fab1e57d783cd47a87d1006e988ec9
Cédric Krier <ced@b2ck.com>
2019-03-24 18:00:01 +0100

    Merge account kind and account type
    issue4990
    review68401002

--- tests/test_stock_supply_day.py
+++ tests/test_stock_supply_day.py
@@ -126,11 +126,11 @@ class StockSupplyDayTestCase(ModuleTestCase):
         with set_company(company):
             create_chart(company)
             receivable, = Account.search([
-                ('kind', '=', 'receivable'),
+                ('type.receivable', '=', True),
                 ('company', '=', company.id),
                 ])
             payable, = Account.search([
-                ('kind', '=', 'payable'),
+                ('type.payable', '=', True),
                 ('company', '=', company.id),
                 ])
             supplier, = Party.create([{
