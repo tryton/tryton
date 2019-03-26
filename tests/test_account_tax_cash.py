b44617ec24f64c7715d62bf2ff327859269c14fb
Cédric Krier <ced@b2ck.com>
2019-03-27 00:36:39 +0100

    Add test for cash basis on reconciliation
    issue8207
    review249121002

--- tests/test_account_tax_cash.py
+++ tests/test_account_tax_cash.py
@@ -26,6 +26,11 @@ def suite():
             checker=doctest_checker,
             optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
     suite.addTests(doctest.DocFileSuite(
+            'scenario_account_tax_cash_reconciliation.rst',
+            tearDown=doctest_teardown, encoding='utf-8',
+            checker=doctest_checker,
+            optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
+    suite.addTests(doctest.DocFileSuite(
             'scenario_account_tax_cash_supplier.rst',
             tearDown=doctest_teardown, encoding='utf-8',
             checker=doctest_checker,
