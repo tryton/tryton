4c4c433a731fafd627a09d8cd7f367165d172615
Cédric Krier <ced@b2ck.com>
2019-03-27 23:18:12 +0100

    Ceil quantity of production with the default UoM
    The computation of shortage as it adds float number (even rounded) can
    introduce float precision error. So we must ceil it to pass the digits
    validation.
    
    issue8058
    review287151002
    (grafted from a084e8f44d6caf39981bf60f5b871e91de27fbae)
    
    --HG--
    branch : 4.4

--- production.py
+++ production.py
@@ -133,6 +133,8 @@ class Production:
             date = today
         else:
             date -= datetime.timedelta(1)
+        uom = product.default_uom
+        quantity = uom.ceil(quantity)
         return cls(
             planned_date=date,
             company=company,
@@ -140,7 +142,7 @@ class Production:
             location=warehouse.production_location,
             product=product,
             bom=product.boms[0].bom if product.boms else None,
-            uom=product.default_uom,
+            uom=uom,
             quantity=quantity,
             state='request',
             )
