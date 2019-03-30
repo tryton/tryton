6d580e6b6487b0a5e6dad8a54c136c224a5d41ac
Cédric Krier <ced@b2ck.com>
2019-03-30 17:35:04 +0100

    Initial commit
    issue7920
    review45751002

---
+++ routes.py
@@ -0,0 +1,24 @@
+# This file is part of Tryton.  The COPYRIGHT file at the top level of
+# this repository contains the full copyright notices and license terms.
+
+from http import HTTPStatus
+
+from werkzeug.utils import redirect
+from werkzeug.exceptions import abort
+
+from trytond.protocols.wrappers import with_pool, with_transaction
+from trytond.wsgi import app
+
+
+@app.route('/s/<base64:database_name>$<shortened>')
+@with_pool
+@with_transaction(readonly=False)
+def shortened(request, pool, shortened):
+    ShortenedURL = pool.get('web.shortened_url')
+
+    try:
+        shortened_url = ShortenedURL.get(shortened)
+    except IndexError:
+        abort(HTTPStatus.NOT_FOUND)
+
+    return redirect(shortened_url.access(), code=301)
