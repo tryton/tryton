63d33e45c94b2151a671d4ca6c463d071e0d1e3b
Cédric Krier <ced@b2ck.com>
2019-04-24 23:42:44 +0200

    Do not make visible close tab button on small screen
    The navbar where there is already a close button is collapsed only on extra
    small screen.
    
    issue8291
    review267271002
    (grafted from 51dd5b57b381c782e45bf090e2ecbd730c050b06)
    
    --HG--
    branch : 4.6

--- src/tab.js
+++ src/tab.js
@@ -177,7 +177,7 @@
                 'role': 'menu'
             })).append(jQuery('<button/>', {
                 'type': 'button',
-                'class': 'close visible-xs visible-sm',
+                'class': 'close visible-xs',
                 'aria-label': Sao.i18n.gettext('Close')
             }).append(jQuery('<span/>', {
                 'aria-hidden': true
