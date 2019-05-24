a4dfd726e2134d2a5d07b3dcb73c3fa620bae17d
Sergi Almacellas Abellana <sergi@koolpi.com>
2019-05-24 09:23:21 +0200

    Allow services to be deactivated
    issue8351
    review251361002

--- service.py
+++ service.py
@@ -1,12 +1,12 @@
 # This file is part of Tryton.  The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
-from trytond.model import ModelSQL, ModelView, fields
+from trytond.model import ModelSQL, ModelView, DeactivableMixin, fields
 from trytond.pyson import Eval
 
 __all__ = ['Service']
 
 
-class Service(ModelSQL, ModelView):
+class Service(DeactivableMixin, ModelSQL, ModelView):
     "Subscription Service"
     __name__ = 'sale.subscription.service'
 
