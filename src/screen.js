36e8a2dad657c71ed4b11f88b2e57562e9f3ee7b
Cédric Krier <ced@b2ck.com>
2019-07-17 22:50:25 +0200

    Wait reload is done before enable the button
    We must return the promise of the reload as the button promise to avoid enable
    back the button before the screen is fully reloaded.
    
    issue8488
    review249661002

--- src/screen.js
+++ src/screen.js
@@ -1777,7 +1777,7 @@
         button: function(attributes) {
             var ids;
             var process_action = function(action) {
-                this.reload(ids, true).then(function() {
+                return this.reload(ids, true).then(function() {
                     if (typeof action == 'string') {
                         this.client_action(action);
                     }
