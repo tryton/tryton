59abb3cbf92f309b04c50a73023998ae46a18ba6
Cédric Krier <ced@b2ck.com>
2019-07-20 21:53:38 +0200

    Write edited values synchronously to avoid concurrency error
    issue8501
    review283741002
    (grafted from 6730553a835c51bfa6a88095b15a25a9b61f4911)
    
    --HG--
    branch : 5.0

--- src/view/form.js
+++ src/view/form.js
@@ -1231,7 +1231,6 @@ function eval_pyson(value){
             }.bind(this));
         },
         write: function(widget, dialog) {
-            var promises = [];
             this.languages.forEach(function(lang) {
                 var input = jQuery('[data-lang-id=' + lang.id + ']');
                 if (!input.attr('readonly')) {
@@ -1251,15 +1250,12 @@ function eval_pyson(value){
                         'method': 'model.' + widget.model.name  + '.write',
                         'params': params
                     };
-                    var prm = Sao.rpc(args, widget.model.session);
-                    promises.push(prm);
+                    Sao.rpc(args, widget.model.session, false);
                 }
             }.bind(this));
+            widget.record().cancel();
+            widget.view.display();
             this.close(dialog);
-            jQuery.when.apply(jQuery, promises).then(function() {
-                widget.record().cancel();
-                widget.view.display();
-            });
         }
     });
 
