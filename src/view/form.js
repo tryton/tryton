1603da8d08607342bc17f1c76d69f715e84f32da
Cédric Krier <ced@b2ck.com>
2019-07-25 10:29:09 +0200

    Remove input val when clearing Binary widget
    On chromium the input does not trigger a change event if the value is the same
    (the same file is selected twice).
    So we force the null value when the widget is cleared.
    
    issue8513
    review265681003

--- src/view/form.js
+++ src/view/form.js
@@ -3616,6 +3616,7 @@ function eval_pyson(value){
             }.bind(this));
         },
         clear: function() {
+            this.input_select.val(null);
             var filename_field = this.filename_field;
             if (filename_field) {
                 filename_field.set_client(this.record, null);
