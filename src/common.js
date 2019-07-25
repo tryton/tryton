9695e3d3782ec812f13e02b9063378418cd91c10
Cédric Krier <ced@b2ck.com>
2019-07-25 10:18:51 +0200

    Remove max-height on treeview to display dropdown
    As the optional scrollbar of the treeview will be removed, the dropdown input
    may no more be visible. So we must scroll until it is visible.
    
    issue8509
    review285581002

--- src/common.js
+++ src/common.js
@@ -3267,14 +3267,20 @@
             // (see http://www.w3.org/TR/css-overflow-3/#overflow-properties)
             this.dropdown.on('hide.bs.dropdown', function() {
                 this.input.focus();
-                this.input.closest('.treeview').css('overflow', '');
+                this.input.closest('.treeview')
+                    .css('overflow', '')
+                    .css('max-height', '');
                 this.input.closest('.modal-body').css('overflow', '');
                 this.input.closest('.navbar-collapse.in').css('overflow-y', '');
+                this.input[0].scrollIntoView();
             }.bind(this));
             this.dropdown.on('show.bs.dropdown', function() {
-                this.input.closest('.treeview').css('overflow', 'visible');
+                this.input.closest('.treeview')
+                    .css('overflow', 'visible')
+                    .css('max-height', 'none');
                 this.input.closest('.modal-body').css('overflow', 'visible');
                 this.input.closest('.navbar-collapse.in').css('overflow-y', 'visible');
+                this.input[0].scrollIntoView();
             }.bind(this));
         },
         set_actions: function(actions, action_activated) {
