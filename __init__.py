2a409da35abc721215ac0dc1b9029a95aa949ec1
Cédric Krier <ced@b2ck.com>
2019-07-28 14:34:20 +0200

    Add secondary unit
    issue8239
    review271131003

---
+++ __init__.py
@@ -0,0 +1,13 @@
+# This file is part of Tryton.  The COPYRIGHT file at the top level of
+# this repository contains the full copyright notices and license terms.
+
+from trytond.pool import Pool
+from . import account
+
+__all__ = ['register']
+
+
+def register():
+    Pool.register(
+        account.InvoiceLine,
+        module='account_invoice_secondary_unit', type_='model')
