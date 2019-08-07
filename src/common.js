912b96dbf53d19b1e56a7fb19118ee7418c0fd0b
Cédric Krier <ced@b2ck.com>
2019-08-07 19:17:15 +0200

    Prevent navigation key on completion input to leave the row
    We do not propagate down key event on input completion and skip keydown event
    from dropdown menu inside editable tree.
    
    issue8507
    review263901002
    (grafted from 0b7674791aa50ef32906c1805e36306b87e7bee9)
    
    --HG--
    branch : 5.0

--- src/common.js
+++ src/common.js
@@ -3227,6 +3227,7 @@
                 } else if (evt.which == Sao.common.DOWN_KEYCODE) {
                     if (this.dropdown.hasClass('open')) {
                         evt.preventDefault();
+                        evt.stopPropagation();
                         this.menu.find('li > a').first().focus();
                     }
                 }
