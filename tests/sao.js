5f91e10c0668a4254b21b8d2cf8551517dfca9bd
Cédric Krier <ced@b2ck.com>
2019-08-07 19:22:59 +0200

    Ensure DomainParse.group returns a list of clause
    issue8520
    review255681002

--- tests/sao.js
+++ tests/sao.js
@@ -1133,6 +1133,7 @@
             c(['Doe'])
             ]],
         ['Name: "John Doe"', [c(['Name', null, 'John Doe'])]],
+        ['Doe Name: John', [c(['Doe']), c(['Name', null, 'John'])]],
         ['Name: =Doe', [['Name', '=', 'Doe']]],
         ['Name: =Doe Name: >John', [
             c(['Name', '=', 'Doe']),
