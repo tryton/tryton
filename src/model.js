67285ecbc3cbd62d7f5caf4c2edd38c501fb9a65
Cédric Krier <ced@b2ck.com>
2019-08-18 18:55:19 +0200

    Correctly test empty fieldnames in on_change_with
    issue8537
    review273661002

--- src/model.js
+++ src/model.js
@@ -1044,7 +1044,7 @@
             }
             var result;
             fieldnames = Object.keys(fieldnames);
-            if (fieldnames) {
+            if (fieldnames.length) {
                 try {
                     if (fieldnames.length == 1) {
                         fieldname = fieldnames[0];
