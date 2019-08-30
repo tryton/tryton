f0ff3dbfb78234627d7a46792202ec7cb30d877e
Nicolas Évrard <nicoe@b2ck.com>
2019-08-31 01:40:47 +0200

    Do not send empty x2Many fields when saving
    issue8588
    review264091002
    (grafted from 25198402e9f72c848b450c7ca6398fbbc442dcf2)
    
    --HG--
    branch : 5.0

--- proteus/__init__.py
+++ proteus/__init__.py
@@ -885,8 +885,20 @@ class Model(object):
         'Return dictionary values'
         if fields is None:
             fields = self._values.keys()
-        return dict((x, getattr(self, '__%s_value' % x)) for x in fields
-                if x not in ('id', '_timestamp'))
+        values = {}
+        for name in fields:
+            if name in ['id', '_timestamp']:
+                continue
+            definition = self._fields[name]
+            if definition.get('readonly') and definition['type'] != 'one2many':
+                continue
+            values[name] = getattr(self, '__%s_value' % name)
+            # Sending an empty X2Many fields breaks ModelFieldAccess.check
+            if (definition['type'] in {'one2many', 'many2many'}
+                    and not values[name]):
+                breakpoint()
+                del values[name]
+        return values
 
     @property
     def _timestamp(self):
