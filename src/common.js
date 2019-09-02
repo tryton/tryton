58ea79382a53c30c2575579ec4201bb8660a79e7
Cédric Krier <ced@b2ck.com>
2019-09-02 18:28:23 +0200

    Do not consider readonly/non editable widget as focusable
    issue8475
    review277751002

--- src/common.js
+++ src/common.js
@@ -3461,7 +3461,8 @@
             return null;
         }
         if (~['input', 'select', 'textarea'].indexOf(
-                    element[0].tagName.toLowerCase())) {
+            element[0].tagName.toLowerCase()) &&
+            !element.prop('readonly')) {
             return element;
         }
 
