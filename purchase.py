6d79a53dc1733c2eb844547d6612698c3985cdd8
Thierry Bruyere <thierry.bruyere@saluc.com>
2019-09-07 23:13:33 +0200

    Add a global method to set default warehouse
    issue8396
    review277371003

--- purchase.py
+++ purchase.py
@@ -178,9 +178,7 @@ class Quotation(Workflow, ModelSQL, ModelView):
     @classmethod
     def default_warehouse(cls):
         Location = Pool().get('stock.location')
-        locations = Location.search(cls.warehouse.domain)
-        if len(locations) == 1:
-            return locations[0].id
+        return Location.get_default_warehouse()
 
     @classmethod
     def set_number(cls, quotations):
