9b43dd418545ca40d8e4035d2653b4ef90b7c00e
Cédric Krier <ced@b2ck.com>
2019-09-08 22:18:15 +0200

    Correct syntax from backport 9f4f66968fdc
    --HG--
    branch : 4.6

--- carrier.py
+++ carrier.py
@@ -104,7 +104,6 @@ class WeightPriceList(ModelSQL, ModelView):
     carrier = fields.Many2One('carrier', 'Carrier', required=True, select=True)
     weight = fields.Float('Weight',
         digits=(16, Eval('_parent_carrier', {}).get('weight_uom_digits', 2)))
-        digits=(16, Eval('_parent_carrier.weight_uom_digits', 2)))
     price = fields.Numeric('Price',
         digits=(16, Eval('_parent_carrier', {}).get(
                 'weight_currency_digits', 2)))
