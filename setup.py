33ca3d1132f50af0faf208da07bef9e1229f4cbb
Cédric Krier <ced@b2ck.com>
2019-09-11 23:52:27 +0200

    Require at least python-stdnum >= 1.8
    It is the minimal version to use get_cc_module from stdnum module.
    
    issue8646
    review264191002

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ if minor_version % 2:
         'hg+http://hg.tryton.org/modules/%s#egg=%s-%s' % (
             name[8:], name, version))
 
-requires = ['python-sql >= 0.4', 'python-stdnum']
+requires = ['python-sql >= 0.4', 'python-stdnum >= 1.8']
 for dep in info.get('depends', []):
     if not re.match(r'(ir|res)(\W|$)', dep):
         requires.append(get_require_version('trytond_%s' % dep))
