c09475b9ebf206871461c504bcbfc8d77b603c4a
Cédric Krier <ced@b2ck.com>
2019-09-29 18:29:40 +0200

    Initial commit
    issue8494
    review267641002

---
+++ tests/__init__.py
@@ -0,0 +1,9 @@
+# This file is part of Tryton.  The COPYRIGHT file at the top level of
+# this repository contains the full copyright notices and license terms.
+
+try:
+    from trytond.modules.sale_amendment.tests.test_sale_amendment import suite
+except ImportError:
+    from .test_sale_amendment import suite
+
+__all__ = ['suite']
