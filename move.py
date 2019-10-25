bfd595b8f4e9ef434953aea166a2696efe86fa37
Cédric Krier <ced@b2ck.com>
2019-10-25 23:47:52 +0200

    Filter by company when searching stock period for cache
    issue8760
    review282301002
    (grafted from 3d301f66a3713b2398e2a3baec5804f6dd2eecd9)
    
    --HG--
    branch : 5.0

--- move.py
+++ move.py
@@ -1230,6 +1230,7 @@ class Move(Workflow, ModelSQL, ModelView):
             periods = Period.search([
                     ('date', '<=', context['stock_date_end']),
                     ('state', '=', 'closed'),
+                    ('company', '=', company.id if company else -1),
                     ], order=[('date', 'DESC')], limit=1)
             if periods:
                 period, = periods
