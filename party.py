c533e1df2e397f376b0a37288d27e05495ccd37a
Nicolas Évrard <nicoe@b2ck.com>
2019-12-06 17:33:47 +0100

    Use direct access to backend classes
    issue8836
    review258421002

--- party.py
+++ party.py
@@ -48,8 +48,7 @@ class PartySalePriceList(ModelSQL, CompanyValueMixin):
 
     @classmethod
     def __register__(cls, module_name):
-        TableHandler = backend.get('TableHandler')
-        exist = TableHandler.table_exist(cls._table)
+        exist = backend.TableHandler.table_exist(cls._table)
 
         super(PartySalePriceList, cls).__register__(module_name)
 
