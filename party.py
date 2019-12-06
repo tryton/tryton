e0c1d7328376a27dbd7f8532cc5022ce4b39ef36
Nicolas Évrard <nicoe@b2ck.com>
2019-12-06 17:33:46 +0100

    Use direct access to backend classes
    issue8836
    review258421002

--- party.py
+++ party.py
@@ -145,8 +145,7 @@ class PartyCreditLimitAmount(ModelSQL, CompanyValueMixin):
 
     @classmethod
     def __register__(cls, module_name):
-        TableHandler = backend.get('TableHandler')
-        exist = TableHandler.table_exist(cls._table)
+        exist = backend.TableHandler.table_exist(cls._table)
 
         super(PartyCreditLimitAmount, cls).__register__(module_name)
 
