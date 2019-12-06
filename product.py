a881ecf4cacc205668f9577da57b383be05b258f
Nicolas Évrard <nicoe@b2ck.com>
2019-12-06 17:33:46 +0100

    Use direct access to backend classes
    issue8836
    review258421002

--- product.py
+++ product.py
@@ -244,8 +244,7 @@ class CategoryAccount(ModelSQL, CompanyValueMixin):
 
     @classmethod
     def __register__(cls, module_name):
-        TableHandler = backend.get('TableHandler')
-        exist = TableHandler.table_exist(cls._table)
+        exist = backend.TableHandler.table_exist(cls._table)
 
         super(CategoryAccount, cls).__register__(module_name)
 
