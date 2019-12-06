61d6247d342574d03b3aa0e8bbc90c69033e5c4b
Nicolas Évrard <nicoe@b2ck.com>
2019-12-06 17:33:46 +0100

    Use direct access to backend classes
    issue8836
    review258421002

--- configuration.py
+++ configuration.py
@@ -82,8 +82,7 @@ class ConfigurationSequence(ModelSQL, CompanyValueMixin):
 
     @classmethod
     def __register__(cls, module_name):
-        TableHandler = backend.get('TableHandler')
-        exist = TableHandler.table_exist(cls._table)
+        exist = backend.TableHandler.table_exist(cls._table)
 
         super(ConfigurationSequence, cls).__register__(module_name)
 
@@ -119,8 +118,7 @@ class ConfigurationSaleMethod(ModelSQL, ValueMixin):
 
     @classmethod
     def __register__(cls, module_name):
-        TableHandler = backend.get('TableHandler')
-        exist = TableHandler.table_exist(cls._table)
+        exist = backend.TableHandler.table_exist(cls._table)
 
         super(ConfigurationSaleMethod, cls).__register__(module_name)
 
