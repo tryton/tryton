402b9de34ad213e1ddbbd4bdbb090160182df607
Cédric Krier <ced@b2ck.com>
2019-12-11 18:42:41 +0100

    Do not copy UUID
    issue8899
    review258531002

--- line.py
+++ line.py
@@ -137,6 +137,15 @@ class Line(ModelSQL, ModelView):
                 gettext('timesheet.msg_line_duration_positive',
                     line=self.rec_name))
 
+    @classmethod
+    def copy(cls, lines, default=None):
+        if default is not None:
+            default = default.copy()
+        else:
+            default = {}
+        default.setdefault('uuid')
+        return super().copy(lines, default=default)
+
     @property
     def hours(self):
         return self.duration.total_seconds() / 60 / 60
