8f62fba792c712c3ca7e5c7343b1f960fa7bdcaf
Cédric Krier <ced@b2ck.com>
2019-12-28 17:52:30 +0100

    Consider event as all day only for date instances
    issue8922
    review282561002
    (grafted from 552b3d0940f6ebfd4989ab61402e817f4450e750)
    
    --HG--
    branch : 5.2

--- src/view/calendar.js
+++ src/view/calendar.js
@@ -116,7 +116,6 @@
             if (this.attributes.dtend) {
                 date_end = record.field_get_client(this.attributes.dtend);
             }
-            var allDay = true;
             var description = [];
             for (var i = 1; i < this.fields.length; i++) {
                 description.push(
@@ -125,9 +124,9 @@
             }
             description = description.join('\n');
             if (date_start) {
-                if (date_end && date_end.isDateTime) {
-                    allDay = false;
-                } else if (date_end && !date_end.isSame(date_start)  &&
+                var allDay = date_start.isDate &&
+                    (!date_end || date_end.isDate);
+                if (date_end && !date_end.isSame(date_start)  &&
                         this.screen.current_view.view_type == "calendar") {
                     // Add one day to allday event that last more than one day.
                     // http://github.com/fullcalendar/fullcalendar/issues/2909
