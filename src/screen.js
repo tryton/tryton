dbbf55e04ef6b58e86f1d7c82eec348a3233111e
Cédric Krier <ced@b2ck.com>
2020-02-10 23:40:51 +0100

    Wait for display to complete in Screen.remove
    The returned promise of remove must resolve only when the display of the screen
    is complete. Otherwise there is a miss-matching between screen and the view.
    
    issue9022
    review278491003
    (grafted from 57e788408a199ddcbfd13db27066de12ce12a98a)
    
    --HG--
    branch : 5.0

--- src/screen.js
+++ src/screen.js
@@ -1464,7 +1464,7 @@
                 }
 
                 return jQuery.when.apply(jQuery, prms).then(function() {
-                    this.display().done(function() {
+                    return this.display().done(function() {
                         this.set_cursor();
                     }.bind(this));
                 }.bind(this));
