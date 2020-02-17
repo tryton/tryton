0005fae2c7e6e64a4c93bbc35cf42b444725fef2
Cédric Krier <ced@b2ck.com>
2020-02-17 23:15:55 +0100

    Add polyfill for padStart and padEnd
    issue9067
    review266821004
    (grafted from 7e3abe856f6709ea5c5c435d0c2bcfe9e0b691e6)
    
    --HG--
    branch : 5.2

--- src/sao.js
+++ src/sao.js
@@ -35,6 +35,40 @@ var Sao = {};
             }
         });
     }
+    if (!String.prototype.padEnd) {
+        String.prototype.padEnd = function padEnd(targetLength, padString) {
+            targetLength = targetLength >> 0;
+            padString = String(
+                typeof padString !== 'undefined' ? padString : ' ');
+            if (this.length > targetLength) {
+                return String(this);
+            } else {
+                targetLength = targetLength - this.length;
+                if (targetLength > padString.length) {
+                    padString += padString.repeat(
+                        targetLength / padString.length);
+                }
+                return String(this) + padString.slice(0, targetLength);
+            }
+        };
+    }
+    if (!String.prototype.padStart) {
+        String.prototype.padStart = function padStart(targetLength, padString) {
+            targetLength = targetLength >> 0;
+            padString = String(
+                typeof padString !== 'undefined' ? padString : ' ');
+            if (this.length > targetLength) {
+                return String(this);
+            } else {
+                targetLength = targetLength - this.length;
+                if (targetLength > padString.length) {
+                    padString += padString.repeat(
+                        targetLength / padString.length);
+                }
+                return padString.slice(0, targetLength) + String(this);
+            }
+        };
+    }
     if (!Array.prototype.some) {
         Array.prototype.some = function(fun /*, thisp */) {
             if (this === null) {
