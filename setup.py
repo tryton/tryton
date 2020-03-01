aa2ccf76fcc5bddca83af1c7f8cb3e4f8fd5a26a
Cédric Krier <ced@b2ck.com>
2020-03-01 12:33:51 +0100

    Fix flake8 errors and warnings
    We add the flake8 configuration used so we ensure everyone uses the same.
    We remove the usage of __all__ for non public API.
    When possible, we rationalize the class name according to its __name__ and module.
    
    issue9082
    review297061002

--- setup.py
+++ setup.py
@@ -80,8 +80,9 @@ setup(name=name,
     keywords='tryton invoice secondary unit',
     package_dir={'trytond.modules.account_invoice_secondary_unit': '.'},
     packages=(
-        ['trytond.modules.account_invoice_secondary_unit'] +
-        ['trytond.modules.account_invoice_secondary_unit.%s' % p for p in find_packages()]
+        ['trytond.modules.account_invoice_secondary_unit']
+        + ['trytond.modules.account_invoice_secondary_unit.%s' % p
+            for p in find_packages()]
         ),
     package_data={
         'trytond.modules.account_invoice_secondary_unit': (info.get('xml', [])
@@ -95,7 +96,8 @@ setup(name=name,
         'Intended Audience :: Developers',
         'Intended Audience :: Financial and Insurance Industry',
         'Intended Audience :: Legal Industry',
-        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'License :: OSI Approved :: '
+        'GNU General Public License v3 or later (GPLv3+)',
         'Natural Language :: Bulgarian',
         'Natural Language :: Catalan',
         'Natural Language :: Chinese (Simplified)',
@@ -131,7 +133,7 @@ setup(name=name,
     entry_points="""
     [trytond.modules]
     account_invoice_secondary_unit = trytond.modules.account_invoice_secondary_unit
-    """,
+    """,  # noqa: E501
     test_suite='tests',
     test_loader='trytond.test_loader:Loader',
     tests_require=tests_require,
