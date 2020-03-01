d9e51b75744ae6ee530df9698e419a61c58485c4
Cédric Krier <ced@b2ck.com>
2020-03-01 12:33:51 +0100

    Fix flake8 errors and warnings
    We add the flake8 configuration used so we ensure everyone uses the same.
    We remove the usage of __all__ for non public API.
    When possible, we rationalize the class name according to its __name__ and module.
    
    issue9082
    review297061002

--- setup.py
+++ setup.py
@@ -79,8 +79,8 @@ setup(name=name,
     keywords='tryton statement import rule',
     package_dir={'trytond.modules.account_statement_rule': '.'},
     packages=(
-        ['trytond.modules.account_statement_rule'] +
-        ['trytond.modules.account_statement_rule.%s' % p
+        ['trytond.modules.account_statement_rule']
+        + ['trytond.modules.account_statement_rule.%s' % p
             for p in find_packages()]
         ),
     package_data={
@@ -95,7 +95,8 @@ setup(name=name,
         'Intended Audience :: Developers',
         'Intended Audience :: Financial and Insurance Industry',
         'Intended Audience :: Legal Industry',
-        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'License :: OSI Approved :: '
+        'GNU General Public License v3 or later (GPLv3+)',
         'Natural Language :: Bulgarian',
         'Natural Language :: Catalan',
         'Natural Language :: Chinese (Simplified)',
