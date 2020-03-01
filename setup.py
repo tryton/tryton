7c58116902e7e06e23676aae644c2a3ea30f23a8
Cédric Krier <ced@b2ck.com>
2020-03-01 12:33:53 +0100

    Fix flake8 errors and warnings
    We add the flake8 configuration used so we ensure everyone uses the same.
    We remove the usage of __all__ for non public API.
    When possible, we rationalize the class name according to its __name__ and module.
    
    issue9082
    review297061002

--- setup.py
+++ setup.py
@@ -82,8 +82,9 @@ setup(name=name,
     keywords='tryton sale secondary unit',
     package_dir={'trytond.modules.sale_secondary_unit': '.'},
     packages=(
-        ['trytond.modules.sale_secondary_unit'] +
-        ['trytond.modules.sale_secondary_unit.%s' % p for p in find_packages()]
+        ['trytond.modules.sale_secondary_unit']
+        + ['trytond.modules.sale_secondary_unit.%s' % p
+            for p in find_packages()]
         ),
     package_data={
         'trytond.modules.sale_secondary_unit': (info.get('xml', [])
@@ -97,7 +98,8 @@ setup(name=name,
         'Intended Audience :: Developers',
         'Intended Audience :: Financial and Insurance Industry',
         'Intended Audience :: Legal Industry',
-        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'License :: OSI Approved :: '
+        'GNU General Public License v3 or later (GPLv3+)',
         'Natural Language :: Bulgarian',
         'Natural Language :: Catalan',
         'Natural Language :: Chinese (Simplified)',
