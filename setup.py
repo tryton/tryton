b405e069bc2b05ebcd1b33d2f9d3aa627bcc8246
Cédric Krier <ced@b2ck.com>
2020-03-04 23:07:03 +0100

    Fix lxml version as zeep dependency
    lxml 4.4.0 has incorrect python_requires which make pip install it on Python
    3.4 which is not supported.
    
    issue9014
    review273001002
    
    --HG--
    branch : 5.0

--- setup.py
+++ setup.py
@@ -46,6 +46,8 @@ if minor_version % 2:
             name[8:], name, version))
 
 requires = ['zeep >= 0.12', 'PyPDF2']
+# Fix lxml dependency of zeep against wrong definition of 4.4.0 version
+requires.append('lxml >=2.0, < 4.4; python_version == "3.4"')
 for dep in info.get('depends', []):
     if not re.match(r'(ir|res)(\W|$)', dep):
         requires.append(get_require_version('trytond_%s' % dep))
