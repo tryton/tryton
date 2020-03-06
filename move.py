7bca3042e3bc49d84f6892b2ffa12d083f7f2045
Nicolas Évrard <nicoe@b2ck.com>
2020-03-06 11:21:30 +0100

    Test that balance line exists when grouping payment lines
    issue8754
    review284281003
    (grafted from 53a52828acac0a36e4e279b81046f9fc3772c669)
    
    --HG--
    branch : 5.2

--- move.py
+++ move.py
@@ -1832,8 +1832,9 @@ class GroupLines(Wizard):
             if line.account.reconcile:
                 to_reconcile[line.account].append(line)
 
-        balance_line.move = move
-        balance_line.save()
+        if balance_line:
+            balance_line.move = move
+            balance_line.save()
 
         for lines in to_reconcile.values():
             Line.reconcile(lines, delegate_to=balance_line)
@@ -1946,7 +1947,7 @@ class GroupLines(Wizard):
                 balance_line.second_currency = grouping['second_currency']
                 balance_line.amount_second_currency = (
                     -amount_second_currency)
-        balance_line.maturity_date = maturity_dates[balance_line.account]
+            balance_line.maturity_date = maturity_dates[balance_line.account]
         return move, balance_line
 
     def _counterpart_line(self, line):
