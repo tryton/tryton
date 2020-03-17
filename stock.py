a3a06d0f966ea776708a5208f84ab8d993c124c4
Cédric Krier <ced@b2ck.com>
2020-03-17 20:08:12 +0100

    Set all fields readonly for inactive records
    issue3747
    review295061011

--- stock.py
+++ stock.py
@@ -1,16 +1,8 @@
 # This file is part of Tryton.  The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
 from trytond.model import sequence_ordered
-from trytond.pyson import Eval, Bool
 from trytond.pool import PoolMeta
 
 
 class Location(sequence_ordered(), metaclass=PoolMeta):
     __name__ = 'stock.location'
-
-    @classmethod
-    def __setup__(cls):
-        super(Location, cls).__setup__()
-        previous_readonly = cls.sequence.states.get('readonly', Bool(False))
-        cls.sequence.states['readonly'] = previous_readonly | ~Eval('active')
-        cls.sequence.depends = ['active']
