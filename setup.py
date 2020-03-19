80e96f441c3b42218fbd2e6c2c70af690d91632c
Sergi Almacellas Abellana <sergi@koolpi.com>
2020-03-19 19:17:44 +0100

    Add support for python3.8
    issue8866
    review274421004

--- setup.py
+++ setup.py
@@ -80,6 +80,7 @@ setup(name=name,
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Topic :: Software Development :: Libraries :: Python Modules'
         ],
     platforms='any',
