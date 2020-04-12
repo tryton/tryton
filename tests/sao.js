6a9225c6173d3b2eb1cd8b02dfe324b744ec7b1c
Cédric Krier <ced@b2ck.com>
2020-04-12 23:21:08 +0200

    Skip invalid branch of domain without the inverted field
    Those branches of the domain should not prevent to compute a proper inverted
    domain for the field as the other fields will be fixed later.
    
    issue9200
    review311191002
    (grafted from 505aa0836b25cfeaf8818862c2cff24817cfbfdc)
    
    --HG--
    branch : 5.4

--- tests/sao.js
+++ tests/sao.js
@@ -2241,7 +2241,8 @@
             'domain_inversion(' + JSON.stringify(domain) + ', \'z\', ' +
                 JSON.stringify(context) + ')');
         context = {x: 5, y: 5};
-        QUnit.strictEqual(domain_inversion(domain, 'z', context), false,
+        QUnit.ok(compare(domain_inversion(domain, 'z', context),
+                [['z', '=', 3]]),
             'domain_inversion(' + JSON.stringify(domain) + ', \'z\', ' +
                 JSON.stringify(context) + ')');
         context = {x: 5, y: 7};
@@ -2266,7 +2267,8 @@
             'domain_inversion(' + JSON.stringify(domain) + ', \'z\', ' +
                 JSON.stringify(context) + ')');
         context = {y: 5};
-        QUnit.strictEqual(domain_inversion(domain, 'z', context), false,
+        QUnit.ok(compare(domain_inversion(domain, 'z', context),
+                [['z', '=', 3]]),
             'domain_inversion(' + JSON.stringify(domain) + ', \'z\', ' +
                 JSON.stringify(context) + ')');
         context = {x: 4, y: 7};
