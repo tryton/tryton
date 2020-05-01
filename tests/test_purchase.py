f1bb47f13b129b3ea0e68f7e360c6c9a24f0e939
Sergi Almacellas Abellana <sergi@koolpi.com>
2020-05-01 20:32:01 +0200

    Add test for product suppliers copy
    issue9017
    review315221002

--- tests/test_purchase.py
+++ tests/test_purchase.py
@@ -127,4 +127,9 @@ def suite():
             tearDown=doctest_teardown, encoding='utf-8',
             optionflags=doctest.REPORT_ONLY_FIRST_FAILURE,
             checker=doctest_checker))
+    suite.addTests(doctest.DocFileSuite(
+            'scenario_purchase_copy_product_suppliers.rst',
+            tearDown=doctest_teardown, encoding='utf-8',
+            optionflags=doctest.REPORT_ONLY_FIRST_FAILURE,
+            checker=doctest_checker))
     return suite
