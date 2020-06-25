3178d5041d9c03073dbf6f80d51ddaf14b4a0fbd
Cédric Krier <ced@b2ck.com>
2020-06-25 07:57:34 +0200

    Add tests scenario to package data

--- setup.py
+++ setup.py
@@ -85,7 +85,7 @@ setup(name=name,
         ),
     package_data={
         'trytond.modules.account_invoice_stock': (info.get('xml', [])
-            + ['tryton.cfg', 'view/*.xml', 'locale/*.po']),
+            + ['tryton.cfg', 'view/*.xml', 'locale/*.po', 'tests/*.rst']),
         },
     classifiers=[
         'Development Status :: 5 - Production/Stable',
