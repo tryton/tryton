84f66640b7104f04a9a1ff0e94b763a94a551613
Cédric Krier <ced@b2ck.com>
2020-06-29 17:29:45 +0200

    Escape external strings
    issue9394
    review293931002
    (grafted from d1858845ab3aebd0788b18c667c58617ee54ad4f)
    
    --HG--
    branch : 5.6

--- src/view/tree.js
+++ src/view/tree.js
@@ -1336,7 +1336,7 @@
                             if (item.length) {
                                 prefix.render(this.record, item);
                             } else {
-                                prefix_el.html(prefix.render(this.record));
+                                prefix_el.empty().append(prefix.render(this.record));
                             }
                         }
                     }
@@ -1345,7 +1345,7 @@
                     if (item.length) {
                         column.render(this.record, item);
                     } else {
-                        widget.html(column.render(this.record));
+                        widget.empty().append(column.render(this.record));
                     }
                     if (column.suffixes) {
                         for (var k = 0; k < column.suffixes.length; k++) {
@@ -1355,7 +1355,7 @@
                             if (item.length) {
                                 suffix.render(this.record, item);
                             } else {
-                                suffix_el.html(suffix.render(this.record));
+                                suffix_el.empty().append(suffix.render(this.record));
                             }
                         }
                     }
@@ -1692,7 +1692,7 @@
             this.tree.columns.forEach(function(col, idx) {
                 var td = this._get_column_td(idx);
                 var static_el = this.get_static_el(td);
-                static_el.html(col.render(this.record)).show();
+                static_el.empty().append(col.render(this.record)).show();
                 this.get_editable_el(td)
                     .empty()
                     .data('widget', null)
