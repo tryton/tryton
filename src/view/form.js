1be9e2bf81a00398c779c0c71587b14f1007dd56
Nicolas Évrard <nicolas.evrard@b2ck.com>
2020-06-29 17:33:06 +0200

    Sanitize RichtText fields content
    issue9405
    review327451002
    (grafted from 4e0e93b11cad63c6b25f5230055653edb21a334c)
    
    --HG--
    branch : 5.6

--- src/view/form.js
+++ src/view/form.js
@@ -2261,7 +2261,7 @@ function eval_pyson(value){
                     this.input.attr('spellcheck', 'true');
                 }
             }
-            this.input.html(value);
+            this.input.html(Sao.HtmlSanitizer.sanitize(value || ''));
         },
         focus: function() {
             this.input.focus();
@@ -2279,7 +2279,8 @@ function eval_pyson(value){
             this.field.set_client(this.record, value);
         },
         _normalize_markup: function(content) {
-            var el = jQuery('<div/>').html(content || '');
+            var el = jQuery('<div/>').html(
+                Sao.HtmlSanitizer.sanitize(content || ''));
             this._normalize(el);
             return el.html();
         },
