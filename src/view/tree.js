686f4785f7985e9f8c9012ec9feb4a9bdb023dd5
Maxime Richez <maxime.richez@saluc.com>
2020-09-22 10:18:39 +0200

    Set correct direction arrow from order with NULLS extra parameter
    Arrows direction icons in treeview are defined by ASC or DESC keywords. When
    using an order with composed sentence eg. "DESC NULLS FIRST", we need to
    extract first word to get arrow direction keyword.
    
    issue9587
    review296211002
    (grafted from f3d0a8d2b7f71355e67906d096b04d707970a7bc)
    
    --HG--
    branch : 5.4

--- src/view/tree.js
+++ src/view/tree.js
@@ -274,6 +274,7 @@
             if (order && (order.length == 1)) {
                 name = order[0][0];
                 direction = order[0][1];
+                direction = direction.trim().split(' ', 1)[0];
                 icon = {
                     'ASC': 'tryton-arrow-down',
                     'DESC': 'tryton-arrow-up',
