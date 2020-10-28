05c2fb9503909adf20690afac4e9c168e6a4ee8f
Cédric Krier <ced@b2ck.com>
2020-10-28 23:54:20 +0100

    Add Account Tax Code Scenario
    issue9743
    review328531002

--- tests/test_account.py
+++ tests/test_account.py
@@ -1679,4 +1679,9 @@ def suite():
             tearDown=doctest_teardown, encoding='utf-8',
             checker=doctest_checker,
             optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
+    suite.addTests(doctest.DocFileSuite(
+            'scenario_tax_code.rst',
+            tearDown=doctest_teardown, encoding='utf-8',
+            checker=doctest_checker,
+            optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
     return suite
