a7d265bd79daaf0ba43602f419bd5f46fa971c4c
Maxime Richez <maxime.richez@saluc.com>
2020-10-29 00:01:48 +0100

    Set invisible state when rendering Affix
    Without initial state, cell is displaying prefix or suffix even when empty.
    
    issue9763
    review306761002
    (grafted from 7a3a46b224741918d045c92e29bb690b5c48a334)
    
    --HG--
    branch : 5.4

--- src/view/tree.js
+++ src/view/tree.js
@@ -1842,6 +1842,7 @@
             var render = function() {
                 var value;
                 var field = record.model.fields[this.attributes.name];
+                field.set_state(record, ['invisible']);
                 var invisible = field.get_state_attrs(record).invisible;
                 if (invisible) {
                     cell.hide();
