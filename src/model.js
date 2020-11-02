25627d852c46ecb56703a0885c6ff8277c490e0b
Cédric Krier <ced@b2ck.com>
2020-11-02 12:12:29 +0100

    Test if val is not null before calling hasOwnProperty
    issue9779
    review326441002

--- src/model.js
+++ src/model.js
@@ -1440,7 +1440,7 @@
             var vals = Object.values(this._values);
             for (var i=0; i < vals.length; i++) {
                 var val = vals[i];
-                if (val.hasOwnProperty('destroy')) {
+                if (val && val.hasOwnProperty('destroy')) {
                     val.destroy();
                 }
             }
