395cb53546e3e1b3858beda978f2514f189eec3d
Sergi Almacellas Abellana <sergi@koolpi.com>
2020-11-23 18:04:57 +0100

    Allow multiple templates to be added/removed on a category
    issue7961
    review308201003

--- category.py
+++ category.py
@@ -1,6 +1,7 @@
 # This file is part of Tryton.  The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
 from trytond.model import ModelView, ModelSQL, fields, tree
+from trytond.pyson import PYSONEncoder
 
 
 class Category(tree(separator=' / '), ModelSQL, ModelView):
@@ -13,8 +14,25 @@ class Category(tree(separator=' / '), ModelSQL, ModelView):
     childs = fields.One2Many(
         'product.category', 'parent', string="Children",
         help="Used to add structure below the category.")
+    templates = fields.Many2Many(
+        'product.template-product.category', 'category', 'template',
+        "Products")
 
     @classmethod
     def __setup__(cls):
         super(Category, cls).__setup__()
         cls._order.insert(0, ('name', 'ASC'))
+        cls._buttons.update({
+                'add_products': {
+                    'icon': 'tryton-add',
+                    },
+                })
+
+    @classmethod
+    @ModelView.button_action('product.act_category_product')
+    def add_products(cls, categories):
+        return {
+            'res_id': [categories[0].id],
+            'pyson_domain': PYSONEncoder().encode(
+                [('id', '=', categories[0].id)]),
+             }
