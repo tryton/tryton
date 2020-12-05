97b0edcd041f263ad383bf8a6cd60406b406413e
Cédric Krier <ced@b2ck.com>
2020-12-05 11:53:48 +0100

    Add generic test for check erase party
    issue9895
    review324571002

--- tests/test_account_deposit.py
+++ tests/test_account_deposit.py
@@ -7,8 +7,10 @@ from trytond.tests.test_tryton import ModuleTestCase
 from trytond.tests.test_tryton import doctest_teardown
 from trytond.tests.test_tryton import doctest_checker
 
+from trytond.modules.company.tests import PartyCompanyCheckEraseMixin
 
-class AccountDepositTestCase(ModuleTestCase):
+
+class AccountDepositTestCase(PartyCompanyCheckEraseMixin, ModuleTestCase):
     'Test Account Deposit module'
     module = 'account_deposit'
 
