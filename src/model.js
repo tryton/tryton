779fcf081cc02c12368409d55fa914c8935d6fe7
Sergi Almacellas Abellana <sergi@koolpi.com>
2020-12-06 18:26:31 +0100

    Always convert float value when applying factor
    Otherwise Numeric fields are stored as float when they have no digits
    
    issue9912
    review326771002

--- src/model.js
+++ src/model.js
@@ -1907,8 +1907,9 @@
                 if (digits) {
                     // Round to avoid float precision error
                     // after the division by factor
-                    value = this.convert(value.toFixed(digits[1]));
+                    value = value.toFixed(digits[1]);
                 }
+                value = this.convert(value);
             }
             return value;
         },
