c9b517ffb9d35eaf98f8f71cca816f65e6c36dab
Sergi Almacellas Abellana <sergi@koolpi.com>
2020-12-06 18:19:06 +0100

    Do not remove first digits on Spanish identifiers in AEAT347 report
    issue9834
    review335111002
    (grafted from 33304864cd9e5dd53bc08fe8e088d05449e74ef1)
    
    --HG--
    branch : 5.6

--- party.py
+++ party.py
@@ -19,3 +19,18 @@ class Party(metaclass=PoolMeta):
                 if country and zip_ and country.code == 'ES':
                     self.es_province_code = zip_[:2]
                     break
+
+
+class Identifier(metaclass=PoolMeta):
+    __name__ = 'party.identifier'
+
+    def es_country(self):
+        if self.type == 'eu_vat':
+            return self.code[:2]
+        if self.type in {'es_cif', 'es_dni', 'es_nie', 'es_nif'}:
+            return 'ES'
+
+    def es_code(self):
+        if self.type == 'eu_vat':
+            return self.code[2:]
+        return self.code
