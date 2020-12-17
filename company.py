e8f71e48d3e927857ac6027445c6a52470652309
Cédric Krier <ced@b2ck.com>
2020-12-18 00:21:15 +0100

    Make hours per work optional
    issue5092
    review316481002

--- company.py
+++ company.py
@@ -7,10 +7,20 @@ from trytond.pool import PoolMeta, Pool
 class Company(metaclass=PoolMeta):
     __name__ = 'company.company'
 
-    hours_per_work_day = fields.Float("Hours per Work Day", required=True)
-    hours_per_work_week = fields.Float("Hours per Work Week", required=True)
-    hours_per_work_month = fields.Float("Hours per Work Month", required=True)
-    hours_per_work_year = fields.Float("Hours per Work Year", required=True)
+    hours_per_work_day = fields.Float("Hours per Work Day")
+    hours_per_work_week = fields.Float("Hours per Work Week")
+    hours_per_work_month = fields.Float("Hours per Work Month")
+    hours_per_work_year = fields.Float("Hours per Work Year")
+
+    @classmethod
+    def __register__(cls, module):
+        super().__register__(module)
+        table_h = cls.__table_handler__(module)
+
+        # Migration from 5.8:
+        for column in ['day', 'week', 'month', 'year']:
+            table_h.not_null_action(
+                'hours_per_work_%s' % column, action='remove')
 
     @staticmethod
     def default_hours_per_work_day():
