73843c64fed71714c0a6712eae6a952a48c7b7b8
Cédric Krier <ced@b2ck.com>
2020-12-27 00:19:30 +0100

    Use cache to fill Reference selections
    issue9235
    review335301003

--- complaint.py
+++ complaint.py
@@ -507,11 +507,9 @@ class Action(ModelSQL, ModelView):
     def get_result(cls):
         pool = Pool()
         Model = pool.get('ir.model')
+        get_name = Model.get_name
         models = cls._get_result()
-        models = Model.search([
-                ('model', 'in', models),
-                ])
-        return [(None, '')] + [(m.model, m.name) for m in models]
+        return [(None, '')] + [(m, get_name(m)) for m in models]
 
     def do(self):
         return getattr(self, 'do_%s' % self.action)()
