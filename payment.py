da91718ba1af243ec2da697309ac2d413f7f50da
Cédric Krier <ced@b2ck.com>
2020-12-27 00:19:30 +0100

    Use cache to fill Reference selections
    issue9235
    review335301003

--- payment.py
+++ payment.py
@@ -815,11 +815,9 @@ class Message(Workflow, ModelSQL, ModelView):
     @classmethod
     def get_origin(cls):
         IrModel = Pool().get('ir.model')
+        get_name = IrModel.get_name
         models = cls._get_origin()
-        models = IrModel.search([
-                ('model', 'in', models),
-                ])
-        return [(None, '')] + [(m.model, m.name) for m in models]
+        return [(None, '')] + [(m, get_name(m)) for m in models]
 
     @classmethod
     @ModelView.button
