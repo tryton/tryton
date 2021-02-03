a5d07f7298959c7dc9a89f03011f4066c7db41de
Cédric Krier <ced@b2ck.com>
2021-02-03 21:21:03 +0100

    Update unit price after invoice posted and move done
    issue9962
    review292991002

--- move.py
+++ move.py
@@ -308,7 +308,7 @@ class Move(Workflow, ModelSQL, ModelView):
             'from_location', 'to_location', 'company', 'currency'])
         cls._deny_modify_done_cancel = (cls._deny_modify_assigned
             | set(['planned_date', 'effective_date', 'state']))
-        cls._allow_modify_closed_period = {'cost_price'}
+        cls._allow_modify_closed_period = {'cost_price', 'unit_price'}
 
         t = cls.__table__()
         cls._sql_constraints += [
