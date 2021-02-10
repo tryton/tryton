ac3f07baee3ae38e74aa826de765b440c36aa5a3
Cédric Krier <ced@b2ck.com>
2021-02-10 23:52:21 +0100

    Depend on werkzeug before 2.0.0
    issue10077
    review324981002
    (grafted from 72ca50c290bba055a6029c083c960625589519bb)
    
    --HG--
    branch : 5.6

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ else:
 if local_version:
     version += '+' + '.'.join(local_version)
 
-requires = ['stripe >= 2.32.0', 'werkzeug']
+requires = ['stripe >= 2.32.0', 'werkzeug < 2']
 for dep in info.get('depends', []):
     if not re.match(r'(ir|res)(\W|$)', dep):
         requires.append(get_require_version('trytond_%s' % dep))
