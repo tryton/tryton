13439a34fd2c649f58ca9bc2f8580f59fb25f8d4
Cédric Krier <ced@b2ck.com>
2021-02-10 23:52:21 +0100

    Depend on werkzeug before 2.0.0
    issue10077
    review324981002

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ else:
 if local_version:
     version += '+' + '.'.join(local_version)
 
-requires = ['python-sql', 'werkzeug']
+requires = ['python-sql', 'werkzeug < 2']
 for dep in info.get('depends', []):
     if not re.match(r'(ir|res)(\W|$)', dep):
         requires.append(get_require_version('trytond_%s' % dep))
