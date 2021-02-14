11e9c902e66148a9678d6a544ef120fabddf1442
Dimitrios Josef Moustos <dotbit@gmail.com>
2021-02-14 17:39:13 +0100

    Add by, cn, cr, id, il, kr tax & personal identifiers
    issue9517
    review294751003

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ else:
 if local_version:
     version += '+' + '.'.join(local_version)
 
-requires = ['python-sql >= 0.4', 'python-stdnum >= 1.13']
+requires = ['python-sql >= 0.4', 'python-stdnum >= 1.15']
 for dep in info.get('depends', []):
     if not re.match(r'(ir|res)(\W|$)', dep):
         requires.append(get_require_version('trytond_%s' % dep))
