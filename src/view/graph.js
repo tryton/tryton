ce1d8b8091b784399dff4aa0cc8a9769be02f760
Cédric Krier <ced@b2ck.com>
2021-02-16 22:38:16 +0100

    Support sparse data in y's graph with domain
    The value when the domain skip the record should be undefined so the graph has
    no point at this ordinate.
    
    issue10072
    review337561002
    (grafted from df69747029ff4b59f13ff566c8313281fc9e54a1)
    
    --HG--
    branch : 5.0

--- src/view/graph.js
+++ src/view/graph.js
@@ -125,6 +125,9 @@
                         yfield = this.yfields[j];
                         key = yfield.key || yfield.name;
                         column = data.columns[key2columns[key]];
+                        if (column[pos] === undefined) {
+                            column[pos] = null;
+                        }
                         if (yfield.domain) {
                             var ctx = jQuery.extend({},
                                     Sao.Session.current_session.context);
@@ -138,6 +141,9 @@
                                 continue;
                             }
                         }
+                        if (!column[pos]) {
+                            column[pos] = 0;
+                        }
                         if (yfield.name == '#') {
                             column[pos] += 1;
                         } else {
@@ -145,7 +151,7 @@
                             if (value && value.isTimeDelta) {
                                 value = value.asSeconds();
                             }
-                            column[pos] = (column[pos] || 0) + (value || 0);
+                            column[pos] += value || 0;
                         }
                     }
                 }.bind(this);
@@ -263,11 +269,20 @@
             var config = Sao.View.Graph.HorizontalBar._super._c3_config
                 .call(this, data);
             config.axis.rotated = true;
+            return config;
         }
     });
 
     Sao.View.Graph.Line = Sao.class_(Sao.View.Graph.Chart, {
-        _chart_type: 'line'
+        _chart_type: 'line',
+        _c3_config: function(data) {
+            var config =  Sao.View.Graph.Line._super._c3_config
+                .call(this, data);
+            config.line = {
+                connectNull: true,
+            };
+            return config;
+        }
     });
 
     Sao.View.Graph.Pie = Sao.class_(Sao.View.Graph.Chart, {
