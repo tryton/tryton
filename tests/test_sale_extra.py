b96ad486828241cf363c857f2d2506d6437d6142
Cédric Krier <ced@b2ck.com>
2021-03-05 15:49:19 +0100

    Add test on context of company multivalue target fields
    issue10067
    review333851026

--- tests/test_sale_extra.py
+++ tests/test_sale_extra.py
@@ -7,8 +7,10 @@ from trytond.tests.test_tryton import ModuleTestCase
 from trytond.tests.test_tryton import doctest_teardown
 from trytond.tests.test_tryton import doctest_checker
 
+from trytond.modules.company.tests import CompanyMultiValueTestMixin
 
-class SaleExtraTestCase(ModuleTestCase):
+
+class SaleExtraTestCase(CompanyMultiValueTestMixin, ModuleTestCase):
     'Test Sale Extra module'
     module = 'sale_extra'
 
