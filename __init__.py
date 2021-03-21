8c3d7b5b05e180b4eec0a5d8ba8cee09d2eaa4f7
Cédric Krier <ced@b2ck.com>
2021-03-21 12:52:58 +0100

    Add stock_quantity_issue module
    issue9944
    review339291003

---
+++ __init__.py
@@ -0,0 +1,24 @@
+# This file is part of Tryton.  The COPYRIGHT file at the top level of
+# this repository contains the full copyright notices and license terms.
+
+from trytond.pool import Pool
+
+__all__ = ['register']
+
+from . import ir
+from . import stock
+
+
+def register():
+    Pool.register(
+        ir.Cron,
+        stock.QuantityIssue,
+        stock.QuantityIssueProduct,
+        stock.QuantityIssueGenerateStart,
+        module='stock_quantity_issue', type_='model')
+    Pool.register(
+        stock.QuantityIssueGenerate,
+        module='stock_quantity_issue', type_='wizard')
+    Pool.register(
+        stock.QuantityIssueProduction,
+        module='stock_quantity_issue', type_='model', depends=['production'])
---
+++ doc/index.rst
@@ -0,0 +1,12 @@
+###########################
+Stock Quantity Issue Module
+###########################
+
+The *Stock Quantity Issue Module* helps to solve stock quantity issues.
+
+
+.. toctree::
+   :maxdepth: 2
+
+   usage
+   design
