79dd12f6bf7fe3d87e805fcf599926a022d32965
Cédric Krier <ced@b2ck.com>
2021-03-28 15:53:33 +0200

    Add incoterm module
    issue9961
    review326951002

---
+++ doc/design.rst
@@ -0,0 +1,22 @@
+******
+Design
+******
+
+The *Incoterm Module* introduces the following concepts:
+
+.. _model-incoterm.incoterm:
+
+Incoterm
+========
+
+Each *Incoterm* has a sets of properties that defines under which conditions
+they can be used.
+
+.. seealso::
+
+   Incoterms can be found by opening the main menu item:
+
+      |Company --> Incoterms|__
+
+      .. |Company --> Incoterms| replace:: :menuselection:`Company --> Incoterms`
+      __ https://demo.tryton.org/model/incoterm.incoterm
---
+++ tests/test_incoterm.py
@@ -0,0 +1,32 @@
+# This file is part of Tryton.  The COPYRIGHT file at the top level of
+# this repository contains the full copyright notices and license terms.
+
+import unittest
+
+import doctest
+
+from trytond.tests.test_tryton import ModuleTestCase
+from trytond.tests.test_tryton import suite as test_suite
+from trytond.tests.test_tryton import doctest_teardown
+from trytond.tests.test_tryton import doctest_checker
+
+
+class IncotermTestCase(ModuleTestCase):
+    'Test Incoterm module'
+    module = 'incoterm'
+    extras = [
+        'carrier', 'purchase', 'purchase_request_quotation', 'sale',
+        'sale_shipment_cost', 'sale_opportunity', 'sale_invoice_grouping',
+        'stock', 'account_invoice', 'account_invoice_stock']
+
+
+def suite():
+    suite = test_suite()
+    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
+            IncotermTestCase))
+    suite.addTests(doctest.DocFileSuite(
+            'scenario_incoterm.rst',
+            tearDown=doctest_teardown, encoding='utf-8',
+            checker=doctest_checker,
+            optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
+    return suite
