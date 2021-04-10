19f7226b9c95ab17ef57fa1ece3b1179ae8cb9a0
Cédric Krier <ced@b2ck.com>
2021-04-10 23:46:24 +0200

    Check rule only if _check_access is set and enforce companies rule
    issue4080
    review343891009

--- tests/test_web_shop.py
+++ tests/test_web_shop.py
@@ -7,10 +7,10 @@ import unittest
 from trytond.tests.test_tryton import ModuleTestCase
 from trytond.tests.test_tryton import suite as test_suite
 
-from trytond.modules.company.tests import CompanyMultiValueTestMixin
+from trytond.modules.company.tests import CompanyTestMixin
 
 
-class WebShopTestCase(CompanyMultiValueTestMixin, ModuleTestCase):
+class WebShopTestCase(CompanyTestMixin, ModuleTestCase):
     'Test Web Shop module'
     module = 'web_shop'
     extras = ['product_attribute']
