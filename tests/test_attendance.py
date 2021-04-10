b8577eaec049d6bef8995ac00845171ae4d81c65
Cédric Krier <ced@b2ck.com>
2021-04-10 23:46:23 +0200

    Check rule only if _check_access is set and enforce companies rule
    issue4080
    review343891009

--- tests/test_attendance.py
+++ tests/test_attendance.py
@@ -8,10 +8,10 @@ from trytond.tests.test_tryton import suite as test_suite
 from trytond.tests.test_tryton import doctest_teardown
 from trytond.tests.test_tryton import doctest_checker
 
-from trytond.modules.company.tests import CompanyMultiValueTestMixin
+from trytond.modules.company.tests import CompanyTestMixin
 
 
-class CompanyAttendanceTestCase(CompanyMultiValueTestMixin, ModuleTestCase):
+class CompanyAttendanceTestCase(CompanyTestMixin, ModuleTestCase):
     'Test Company Attendance module'
     module = 'attendance'
     extras = ['timesheet']
