e1d2f74786f4dafbec4edf4896a77a8ea6715911
Cédric Krier <ced@b2ck.com>
2021-04-10 23:46:24 +0200

    Check rule only if _check_access is set and enforce companies rule
    issue4080
    review343891009

--- tests/test_sale_opportunity.py
+++ tests/test_sale_opportunity.py
@@ -7,10 +7,10 @@ from trytond.tests.test_tryton import ModuleTestCase
 from trytond.tests.test_tryton import doctest_teardown
 from trytond.tests.test_tryton import doctest_checker
 
-from trytond.modules.company.tests import CompanyMultiValueTestMixin
+from trytond.modules.company.tests import CompanyTestMixin
 
 
-class SaleOpportunityTestCase(CompanyMultiValueTestMixin, ModuleTestCase):
+class SaleOpportunityTestCase(CompanyTestMixin, ModuleTestCase):
     'Test SaleOpportunity module'
     module = 'sale_opportunity'
 
