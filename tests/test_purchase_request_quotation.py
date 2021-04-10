d4446984d08050d184aaefb6a5832f6ffdf6eb22
Cédric Krier <ced@b2ck.com>
2021-04-10 23:46:24 +0200

    Check rule only if _check_access is set and enforce companies rule
    issue4080
    review343891009

--- tests/test_purchase_request_quotation.py
+++ tests/test_purchase_request_quotation.py
@@ -10,11 +10,10 @@ from trytond.tests.test_tryton import suite as test_suite
 from trytond.tests.test_tryton import doctest_teardown
 from trytond.tests.test_tryton import doctest_checker
 
-from trytond.modules.company.tests import CompanyMultiValueTestMixin
+from trytond.modules.company.tests import CompanyTestMixin
 
 
-class PurchaseRequestForQuotationTestCase(
-        CompanyMultiValueTestMixin, ModuleTestCase):
+class PurchaseRequestForQuotationTestCase(CompanyTestMixin, ModuleTestCase):
     'Test Purchase Request For Quotation module'
     module = 'purchase_request_quotation'
 
