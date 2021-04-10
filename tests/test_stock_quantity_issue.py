131d76bf831c1683669dddc118ac9da8d147bc0b
Cédric Krier <ced@b2ck.com>
2021-04-10 23:46:24 +0200

    Check rule only if _check_access is set and enforce companies rule
    issue4080
    review343891009

--- tests/test_stock_quantity_issue.py
+++ tests/test_stock_quantity_issue.py
@@ -10,8 +10,10 @@ from trytond.tests.test_tryton import suite as test_suite
 from trytond.tests.test_tryton import doctest_teardown
 from trytond.tests.test_tryton import doctest_checker
 
+from trytond.modules.company.tests import CompanyTestMixin
 
-class StockQuantityIssueTestCase(ModuleTestCase):
+
+class StockQuantityIssueTestCase(CompanyTestMixin, ModuleTestCase):
     'Test Stock Quantity Issue module'
     module = 'stock_quantity_issue'
     extras = ['production']
