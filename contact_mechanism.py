364ebf6c9c5d99cdfbd4b69b2686ad228b0e8471
Cédric Krier <ced@b2ck.com>
2021-04-22 23:50:08 +0200

    Use AND-ed clause when searching contact mechanism with negate operator
    issue10309
    review334201002
    (grafted from 04d89a6a8e573612c0d3c3355365175f3adc22ee)
    
    --HG--
    branch : 5.0

--- contact_mechanism.py
+++ contact_mechanism.py
@@ -197,7 +197,11 @@ class ContactMechanism(
 
     @classmethod
     def search_rec_name(cls, name, clause):
-        return ['OR',
+        if clause[1].startswith('!') or clause[1].startswith('not '):
+            bool_op = 'AND'
+        else:
+            bool_op = 'OR'
+        return [bool_op,
             ('value',) + tuple(clause[1:]),
             ('value_compact',) + tuple(clause[1:]),
             ]
