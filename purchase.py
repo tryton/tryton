cc07524bfbf42ffe1a03b69d24fdd6c47e335f59
Sergi Almacellas Abellana <sergi@koolpi.com>
2021-05-10 12:39:02 +0200

    Do not show edited delivery date once the purchase is processed
    We must show the computed delivery date once the edition is not more possible
    
    issue10394
    review348341002

--- purchase.py
+++ purchase.py
@@ -1165,9 +1165,11 @@ class Line(sequence_ordered(), ModelSQL, ModelView):
     delivery_date = fields.Function(fields.Date('Delivery Date',
             states={
                 'invisible': ((Eval('type') != 'line')
-                    | Eval('delivery_date_edit', False)),
+                    | (Eval('delivery_date_edit', False)
+                        & ~Eval('purchase_state').in_(
+                            ['processing', 'done', 'cancelled']))),
                 },
-            depends=['type', 'delivery_date_edit']),
+            depends=['type', 'delivery_date_edit', 'purchase_state']),
         'on_change_with_delivery_date')
     delivery_date_edit = fields.Boolean(
         "Edit Delivery Date",
@@ -1182,7 +1184,9 @@ class Line(sequence_ordered(), ModelSQL, ModelView):
         "Delivery Date",
         states={
             'invisible': ((Eval('type') != 'line')
-                | ~Eval('delivery_date_edit', False)),
+                | ~Eval('delivery_date_edit', False)
+                | Eval('purchase_state').in_(
+                    ['processing', 'done', 'cancelled'])),
             'readonly': Eval('purchase_state').in_([
                     'processing', 'done', 'cancelled']),
             },
