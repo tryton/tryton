f94800130e11f271618f1fe1fe6dd50b443030d5
Sergi Almacellas Abellana <sergi@koolpi.com>
2021-05-28 09:09:45 +0200

    Add support for werkzeug 2.0
    issue10086
    review337571002

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ else:
 if local_version:
     version += '+' + '.'.join(local_version)
 
-requires = ['werkzeug < 2', 'Genshi']
+requires = ['werkzeug', 'Genshi']
 for dep in info.get('depends', []):
     if not re.match(r'(ir|res)(\W|$)', dep):
         requires.append(get_require_version('trytond_%s' % dep))
