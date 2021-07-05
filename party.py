dd611f804f5def43b48df6a679ad1a7e1d70740b
David Blanco Bautista <Davidoff.D777@gmail.com>
2021-07-05 23:51:52 +0200

    Do not use CompanyReport for LetterReport
    issue10514
    review352161002

--- party.py
+++ party.py
@@ -6,7 +6,7 @@ from trytond.pyson import Eval
 from trytond.tools.multivalue import migrate_property
 from trytond.transaction import Transaction
 
-from .company import CompanyReport
+from trytond.report import Report
 from .model import CompanyMultiValueMixin, CompanyValueMixin
 
 
@@ -127,7 +127,7 @@ class ContactMechanismLanguage(CompanyValueMixin, metaclass=PoolMeta):
             cls.contact_mechanism.depends.append('company')
 
 
-class LetterReport(CompanyReport):
+class LetterReport(Report):
     __name__ = 'party.letter'
 
     @classmethod
