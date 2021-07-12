df815651941f763c7d95a96e4a09aec4b12947da
Cédric Krier <ced@b2ck.com>
2021-07-12 22:56:26 +0200

    Always use proteus config argument in testing tools
    issue10548
    review338421002

--- tests/tools.py
+++ tests/tools.py
@@ -10,7 +10,7 @@ def add_cogs_accounts(accounts, company=None, config=None):
     Account = Model.get('account.account', config=config)
 
     if not company:
-        company = get_company()
+        company = get_company(config=config)
 
     accounts['cogs'], = Account.find([
             ('type.expense', '=', True),
