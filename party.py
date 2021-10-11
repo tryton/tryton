6ae4a666f3fb0e107d962efceceef9d10133af45
Maxime Richez <maxime.richez@saluc.com>
2021-10-11 15:35:46 +0200

    Add field access on referential document
    issue10269
    review332861003

--- party.py
+++ party.py
@@ -5,6 +5,7 @@ from trytond.model import ModelSQL, fields
 from trytond.pyson import Eval
 from trytond.pool import PoolMeta, Pool
 from trytond.tools.multivalue import migrate_property
+from trytond.transaction import Transaction
 from trytond.modules.company.model import (
     CompanyMultiValueMixin, CompanyValueMixin)
 
@@ -30,6 +31,17 @@ class Party(CompanyMultiValueMixin, metaclass=PoolMeta):
         config = Configuration(1)
         price_list = config.get_multivalue('sale_price_list', **pattern)
         return price_list.id if price_list else None
+
+    @classmethod
+    def copy(cls, parties, default=None):
+        context = Transaction().context
+        default = default.copy() if default else {}
+        if context.get('_check_access'):
+            fields = ['sale_price_lists', 'sale_price_list']
+            default_values = cls.default_get(fields, with_rec_name=False)
+            for fname in fields:
+                default.setdefault(fname, default_values.get(fname))
+        return super().copy(parties, default=default)
 
 
 class PartySalePriceList(ModelSQL, CompanyValueMixin):
