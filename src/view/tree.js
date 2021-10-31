f9d64eed06a04aa02034fdc5546ca6232a8b2d9f
Cédric Krier <ced@b2ck.com>
2021-10-31 10:24:32 +0100

    Do not remove disabled property after button execution
    We must let the display of the screen reset the state of the button.
    
    issue10922
    review391061002
    (grafted from 0b692f8ac30864b7ec75bf542909fc4b5ea81dcf)
    
    --HG--
    branch : 5.8

--- src/view/tree.js
+++ src/view/tree.js
@@ -2553,10 +2553,8 @@
             if (states.invisible || states.readonly) {
                 return;
             }
-            button.el.prop('disabled', true);
-            this.view.screen.button(this.attributes).always(function() {
-                button.el.prop('disabled', false);
-            });
+            button.el.prop('disabled', true);  // state will be reset at display
+            this.view.screen.button(this.attributes);
         }
     });
 
