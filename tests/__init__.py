4cbf1bd2d485f281f167a144a913451597836bd1
Cédric Krier <ced@b2ck.com>
2021-12-11 17:58:41 +0100

    Follow isort formatting
    issue10928

--- tests/__init__.py
+++ tests/__init__.py
@@ -2,7 +2,8 @@
 # this repository contains the full copyright notices and license terms.
 
 try:
-    from trytond.modules.account_de_skr03.tests.test_account_de_skr03 import suite  # noqa: E501
+    from trytond.modules.account_de_skr03.tests.test_account_de_skr03 import \
+        suite  # noqa: E501
 except ImportError:
     from .test_account_de_skr03 import suite
 
