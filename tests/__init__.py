72b613cd88fbc96b2b9719a2c418fb1e46a2c292
Cédric Krier <ced@b2ck.com>
2021-12-11 17:58:45 +0100

    Follow isort formatting
    issue10928

--- tests/__init__.py
+++ tests/__init__.py
@@ -2,7 +2,8 @@
 # this repository contains the full copyright notices and license terms.
 
 try:
-    from trytond.modules.stock_supply_forecast.tests.test_stock_supply_forecast import suite  # noqa: E501
+    from trytond.modules.stock_supply_forecast.tests.test_stock_supply_forecast import \
+        suite  # noqa: E501
 except ImportError:
     from .test_stock_supply_forecast import suite
 
