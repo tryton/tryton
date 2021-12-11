be9f7e10a4b2501d3dcfdcadc5c76daa14dbd426
Cédric Krier <ced@b2ck.com>
2021-12-11 17:58:43 +0100

    Follow isort formatting
    issue10928

--- tests/__init__.py
+++ tests/__init__.py
@@ -2,7 +2,8 @@
 # this repository contains the full copyright notices and license terms.
 
 try:
-    from trytond.modules.purchase_amendment.tests.test_purchase_amendment import suite  # noqa: E501
+    from trytond.modules.purchase_amendment.tests.test_purchase_amendment import \
+        suite  # noqa: E501
 except ImportError:
     from .test_purchase_amendment import suite
 
