033106bb2afe382a36d8f457afbc7fd989f94cc5
Cédric Krier <ced@b2ck.com>
2021-12-11 17:58:41 +0100

    Follow isort formatting
    issue10928

--- account.py
+++ account.py
@@ -5,9 +5,8 @@ from io import BytesIO, TextIOWrapper
 from coda import CODA
 
 from trytond.i18n import gettext
-from trytond.pool import PoolMeta, Pool
-
 from trytond.modules.account_statement.exceptions import ImportStatementError
+from trytond.pool import Pool, PoolMeta
 
 
 class StatementImportStart(metaclass=PoolMeta):
