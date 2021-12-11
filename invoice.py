d59a3c732f200577108217f244537afe90be5bb3
Cédric Krier <ced@b2ck.com>
2021-12-11 17:58:41 +0100

    Follow isort formatting
    issue10928

--- invoice.py
+++ invoice.py
@@ -1,7 +1,7 @@
 # This file is part of Tryton.  The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
-from trytond.transaction import Transaction
 from trytond.pool import PoolMeta
+from trytond.transaction import Transaction
 
 
 class Invoice(metaclass=PoolMeta):
