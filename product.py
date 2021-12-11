12f2b2c6c254a2009b2f13d93edba900ac2b35b7
Cédric Krier <ced@b2ck.com>
2021-12-11 17:58:43 +0100

    Follow isort formatting
    issue10928

--- product.py
+++ product.py
@@ -1,10 +1,9 @@
 # This file is part of Tryton.  The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
-from trytond.model import ModelSQL, ModelView, DeactivableMixin, fields
-from trytond.pool import PoolMeta
-
+from trytond.model import DeactivableMixin, ModelSQL, ModelView, fields
 from trytond.modules.product_classification.product import (
-    classification_tree, ClassificationMixin)
+    ClassificationMixin, classification_tree)
+from trytond.pool import PoolMeta
 
 
 class Template(metaclass=PoolMeta):
