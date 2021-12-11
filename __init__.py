33db57f887635a79047c962a642b6dd714e616ad
Cédric Krier <ced@b2ck.com>
2021-12-11 17:58:42 +0100

    Follow isort formatting
    issue10928

--- __init__.py
+++ __init__.py
@@ -2,8 +2,8 @@
 # this repository contains the full copyright notices and license terms.
 
 from trytond.pool import Pool
-from . import carrier
-from . import sale
+
+from . import carrier, sale
 
 __all__ = ['register']
 
--- tests/__init__.py
+++ tests/__init__.py
@@ -2,7 +2,8 @@
 # this repository contains the full copyright notices and license terms.
 
 try:
-    from trytond.modules.carrier_subdivision.tests.test_carrier_subdivision import suite  # noqa: E501
+    from trytond.modules.carrier_subdivision.tests.test_carrier_subdivision import \
+        suite  # noqa: E501
 except ImportError:
     from .test_carrier_subdivision import suite
 
