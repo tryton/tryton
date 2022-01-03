994a021a0900ebbd59f183287ca1f9cb12074a84
Cédric Krier <ced@b2ck.com>
2022-01-03 22:50:57 +0100

    Remove double TaxableMixin inheritage
    Sale line inherits already of TaxableMixin in sale module.
    
    issue11092
    review372951002

--- sale.py
+++ sale.py
@@ -4,7 +4,6 @@
 from decimal import Decimal
 
 from trytond.i18n import gettext
-from trytond.modules.account.tax import TaxableMixin
 from trytond.pool import Pool, PoolMeta
 
 from .exceptions import BadRequest
@@ -171,17 +170,8 @@ class SaleShipmentCost(metaclass=PoolMeta):
             self.carrier = carrier.id if carrier else None
 
 
-class Line(TaxableMixin, metaclass=PoolMeta):
+class Line(metaclass=PoolMeta):
     __name__ = 'sale.line'
-
-    @property
-    def taxable_lines(self):
-        return [(
-                getattr(self, 'taxes', None) or [],
-                getattr(self, 'unit_price', None) or Decimal(0),
-                getattr(self, 'quantity', None) or 0,
-                None,
-                )]
 
     @property
     def vsf_tax_amount(self):
