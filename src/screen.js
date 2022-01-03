564a5c59d409f793acae5b6f9bb6dce38dedc855
Cédric Krier <ced@b2ck.com>
2022-01-03 22:41:15 +0100

    Duplicate field properties before modifying them
    issue11087
    review381571004
    (grafted from 358798fea87451a53328f2bafd4c36847032f8e2)
    
    --HG--
    branch : 5.0

--- src/screen.js
+++ src/screen.js
@@ -1519,7 +1519,9 @@
                 if (props.selection instanceof Array) {
                     continue;
                 }
+                props = jQuery.extend({}, props);
                 props.selection = this.get_selection(props);
+                fields[name] = props;
             }
 
             if ('arch' in view_tree) {
