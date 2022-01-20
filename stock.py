43516af5545ffb76f06c6fded483fb773c9d3452
Cédric Krier <ced@b2ck.com>
2022-01-20 18:57:31 +0100

    Ensure customer moves are added in draft state
    We can not rely on the asynchronous sale processing to set to draft the
    customer moves because it can happen after the drop shipment is done.
    
    issue11121
    review376431002
    (grafted from b1e3bfa60f63e8f60b8b722c80efc28db573806c)
    
    --HG--
    branch : 5.0

--- stock.py
+++ stock.py
@@ -541,6 +541,7 @@ class ShipmentDrop(Workflow, ModelSQL, ModelView):
                                 move.shipment = shipment
                                 to_save.append(move)
         Move.save(to_save)
+        Move.draft(to_save)
         cls._synchronize_moves(shipments)
 
     @classmethod
