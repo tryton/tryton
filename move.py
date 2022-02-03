276d939431c0700c8b2d3f47b8b2c15b9cd99bb9
Cédric Krier <ced@b2ck.com>
2022-02-03 22:29:08 +0100

    Use select to True on create_date
    If the index is created only in __register__ then it is dropped before being
    created again.
    
    issue11197
    review389881002
    (grafted from 84e07157a0fc32fca47ff6fb52c8d7253cb6384d)
    
    --HG--
    branch : 6.2

--- move.py
+++ move.py
@@ -89,6 +89,7 @@ class Move(ModelSQL, ModelView):
     @classmethod
     def __setup__(cls):
         super(Move, cls).__setup__()
+        cls.create_date.select = True
         cls._check_modify_exclude = ['lines']
         cls._order.insert(0, ('date', 'DESC'))
         cls._order.insert(1, ('number', 'DESC'))
@@ -129,9 +130,6 @@ class Move(ModelSQL, ModelView):
 
         table = cls.__table_handler__(module_name)
         table.index_action(['journal', 'period'], 'add')
-
-        # Add index on create_date
-        table.index_action('create_date', action='add')
 
     @classmethod
     def order_post_number(cls, tables):
