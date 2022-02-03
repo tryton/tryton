536d803fa266eb7f8f3d61f05650c490988b9e03
Nicolas Évrard <nicoe@b2ck.com>
2022-02-03 16:03:18 +0100

    Propagate changed status to parent on instance button activation
    issue11145
    review389711002
    
    --HG--
    branch : 6.2

--- src/screen.js
+++ src/screen.js
@@ -1981,6 +1981,7 @@
                         return record.model.execute(attributes.name, [values],
                             this.context).then(function(changes) {
                             record.set_on_change(changes);
+                            record.group.changed();
                             record.group.root_group.screens.forEach(
                                 function(screen) {
                                     screen.display();
