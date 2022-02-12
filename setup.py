3793f0e5ec143a9c59b97133c05699d4b6c5a4fd
Cédric Krier <ced@b2ck.com>
2022-02-12 02:37:27 +0100

    Explicitly depends on lxml
    issue11243
    review370851002

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ else:
 if local_version:
     version += '+' + '.'.join(local_version)
 
-requires = ['zeep >= 0.12', 'PyPDF2']
+requires = ['zeep >= 0.12', 'PyPDF2', 'lxml']
 for dep in info.get('depends', []):
     if not re.match(r'(ir|res)(\W|$)', dep):
         requires.append(get_require_version('trytond_%s' % dep))
