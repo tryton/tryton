332ab8488de27d08f40a2459a336e00a6ea17a5e
Cédric Krier <ced@b2ck.com>
2022-03-06 13:36:25 +0100

    Return 0 factor for BOM without the product
    issue11241
    review390101002
    (grafted from 237cad37234f68b8ee0ff1d97f5b5714b149a7df)
    
    --HG--
    branch : 5.0

--- bom.py
+++ bom.py
@@ -33,6 +33,8 @@ class BOM(DeactivableMixin, ModelSQL, ModelView):
                 quantity = Uom.compute_qty(uom, quantity,
                     output.uom, round=False)
                 return quantity / output.quantity
+        else:
+            return 0.0
 
     @classmethod
     def copy(cls, records, default=None):
