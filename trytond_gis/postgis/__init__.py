cea18fb08ff65d021e4d03c91a9f6ec82dea60e4
Cédric Krier <ced@b2ck.com>
2022-04-04 00:35:38 +0200

    Manage DatabaseDataError in ModelSQL
    issue11314
    review399111003

--- trytond_gis/postgis/__init__.py
+++ trytond_gis/postgis/__init__.py
@@ -1,10 +1,11 @@
 # This file is part of trytond_gis.  The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
 from trytond.backend.postgresql.database import (
-    DatabaseIntegrityError, DatabaseOperationalError)
+    DatabaseDataError, DatabaseIntegrityError, DatabaseOperationalError)
 
 from .database import Database
 from .table import TableHandler
 
 __all__ = [
-    Database, DatabaseIntegrityError, DatabaseOperationalError, TableHandler]
+    Database, TableHandler,
+    DatabaseIntegrityError, DatabaseDataError, DatabaseOperationalError]
