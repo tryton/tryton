aa71cd4359a945f7839394433524bcd13b107edd
Nicolas Évrard <nicoe@b2ck.com>
2022-04-08 19:07:13 +0200

    Send only the required fields when fetching a readonly view definition
    issue11134
    review380081002

--- product.py
+++ product.py
@@ -50,14 +50,13 @@ class Product(metaclass=PoolMeta):
             'readonly': (~Eval('attribute_set')
                 & ~Eval('_parent_template', {}).get('attribute_set')),
             },
-        depends=['attribute_set', 'template'],
+        depends={'template'},
         help="Add attributes to the variant.")
     attributes_name = fields.Function(fields.Char(
             "Attributes Name",
             states={
                 'invisible': ~Eval('attribute_set'),
-                },
-            depends=['attribute_set']),
+                }),
         'on_change_with_attributes_name')
 
     @fields.depends('attribute_set', 'attributes')
