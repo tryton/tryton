ceb0dc11d969a7f06ae6fa2148a50c670188151c
Cédric Krier <ced@b2ck.com>
2022-04-16 18:30:20 +0200

    Replace test setuptools command by unittest discover
    issue9215
    review389851002

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,9 +1,2 @@
 # This file is part of Tryton.  The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
-
-try:
-    from trytond.modules.stock_shipment_cost_weight.tests.test_stock_shipment_cost_weight import suite  # noqa: E501, isort: skip
-except ImportError:
-    from .test_stock_shipment_cost_weight import suite
-
-__all__ = ['suite']
