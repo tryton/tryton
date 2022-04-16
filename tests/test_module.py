5f856103dd50eaa16076248eb5f7a8fbbf27fc31
Cédric Krier <ced@b2ck.com>
2022-04-16 18:30:17 +0200

    Replace test setuptools command by unittest discover
    issue9215
    review389851002

--- tests/test_module.py
+++ tests/test_module.py
@@ -1,8 +1,6 @@
 # This file is part of Tryton.  The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
-import unittest
 
-import trytond.tests.test_tryton
 from trytond.modules.account.tests import create_chart
 from trytond.modules.company.tests import (
     CompanyTestMixin, create_company, set_company)
@@ -169,8 +167,4 @@ class AccountTaxRuleCountryTestCase(CompanyTestMixin, ModuleTestCase):
             self.assertListEqual(tax_rule.apply(tax, pattern), [tax.id])
 
 
-def suite():
-    suite = trytond.tests.test_tryton.suite()
-    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
-        AccountTaxRuleCountryTestCase))
-    return suite
+del ModuleTestCase
