4282cf508b35af0b7c31de3e52645990ca39f4de
Cédric Krier <ced@b2ck.com>
2022-04-16 18:30:17 +0200

    Replace test setuptools command by unittest discover
    issue9215
    review389851002

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,9 +1,2 @@
 # This file is part of Tryton.  The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
-
-try:
-    from trytond.modules.marketing.tests.test_marketing import suite
-except ImportError:
-    from .test_marketing import suite
-
-__all__ = ['suite']
--- tests/test_module.py
+++ tests/test_module.py
@@ -1,10 +1,7 @@
 # This file is part of Tryton.  The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
 
-import unittest
-
 from trytond.tests.test_tryton import ModuleTestCase
-from trytond.tests.test_tryton import suite as test_suite
 
 
 class MarketingTestCase(ModuleTestCase):
@@ -12,8 +9,4 @@ class MarketingTestCase(ModuleTestCase):
     module = 'marketing'
 
 
-def suite():
-    suite = test_suite()
-    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
-            MarketingTestCase))
-    return suite
+del ModuleTestCase
