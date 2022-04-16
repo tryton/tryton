962b98fac0d3a9af8eafa7e9b2b561150fee9be3
Cédric Krier <ced@b2ck.com>
2022-04-16 18:30:17 +0200

    Replace test setuptools command by unittest discover
    issue9215
    review389851002

--- tests/test_module.py
+++ tests/test_module.py
@@ -1,17 +1,14 @@
 # This file is part of Tryton.  The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
-import doctest
-import unittest
+
 from decimal import Decimal
 
-import trytond.tests.test_tryton
 from trytond.modules.account.tests import create_chart, get_fiscalyear
 from trytond.modules.company.tests import (
     CompanyTestMixin, create_company, set_company)
 from trytond.modules.currency.tests import create_currency
 from trytond.pool import Pool
-from trytond.tests.test_tryton import (
-    ModuleTestCase, doctest_checker, doctest_teardown, with_transaction)
+from trytond.tests.test_tryton import ModuleTestCase, with_transaction
 from trytond.transaction import Transaction
 
 
@@ -280,12 +277,4 @@ class AnalyticAccountTestCase(CompanyTestMixin, ModuleTestCase):
                 (account3, Decimal('8.17'))])
 
 
-def suite():
-    suite = trytond.tests.test_tryton.suite()
-    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
-        AnalyticAccountTestCase))
-    suite.addTests(doctest.DocFileSuite('scenario_analytic_account.rst',
-            tearDown=doctest_teardown, encoding='utf-8',
-            checker=doctest_checker,
-            optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
-    return suite
+del ModuleTestCase
