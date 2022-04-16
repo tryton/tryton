be12192dd3f587c7ee2ac464eafb999f295eb3fc
Cédric Krier <ced@b2ck.com>
2022-04-16 18:30:18 +0200

    Replace test setuptools command by unittest discover
    issue9215
    review389851002

--- tests/test_module.py
+++ tests/test_module.py
@@ -1,15 +1,11 @@
 # This file is part of Tryton.  The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
 import datetime
-import doctest
-import unittest
 
-import trytond.tests.test_tryton
 from trytond.modules.company.tests import (
     CompanyTestMixin, PartyCompanyCheckEraseMixin, create_company, set_company)
 from trytond.pool import Pool
-from trytond.tests.test_tryton import (
-    ModuleTestCase, doctest_checker, doctest_teardown, with_transaction)
+from trytond.tests.test_tryton import ModuleTestCase, with_transaction
 from trytond.transaction import Transaction
 
 
@@ -186,13 +182,4 @@ class ProjectTestCase(
                 self.assertFalse(TimesheetLine.default_work())
 
 
-def suite():
-    suite = trytond.tests.test_tryton.suite()
-    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
-            ProjectTestCase))
-    suite.addTests(doctest.DocFileSuite(
-            'scenario_project_status.rst',
-            tearDown=doctest_teardown, encoding='utf-8',
-            checker=doctest_checker,
-            optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
-    return suite
+del ModuleTestCase
