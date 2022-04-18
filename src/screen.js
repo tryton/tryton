c8b554aa714cf16c9f43d22256ada3da5681f97a
Hodei Navarro <hodeinavarro13@gmail.com>
2022-04-18 19:42:08 +0200

    Use Set instead of Map to iter on common fields
    issue11416
    review378751003

--- src/screen.js
+++ src/screen.js
@@ -1772,7 +1772,7 @@
             }
 
             // Add common fields
-            const common_fields = new Map([
+            const common_fields = new Set([
                 ['id', Sao.i18n.gettext('ID'), 'integer'],
                 ['create_uid', Sao.i18n.gettext('Created by'), 'many2one'],
                 ['create_date', Sao.i18n.gettext('Created at'), 'datetime'],
