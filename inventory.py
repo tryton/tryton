13be05e6df66a1af00d7c699a924ed4bc538639c
EdbO <EdbO.design@gmail.com>
2022-05-07 18:48:45 +0200

    Rename inventory count button action
    This is to avoid collision with the ModelStorage.count method added by
    issue10623.
    
    issue11466
    review447071003

--- inventory.py
+++ inventory.py
@@ -86,7 +86,7 @@ class Inventory(Workflow, ModelSQL, ModelView):
                     'readonly': Eval('state') != 'draft',
                     'depends': ['state'],
                     },
-                'count': {
+                'do_count': {
                     'readonly': Eval('state') != 'draft',
                     'depends': ['state'],
                     },
@@ -311,7 +311,7 @@ class Inventory(Workflow, ModelSQL, ModelView):
 
     @classmethod
     @ModelView.button_action('stock.wizard_inventory_count')
-    def count(cls, inventories):
+    def do_count(cls, inventories):
         cls.complete_lines(inventories)
 
 
