bf7237ce421f1490f3895c21f4a687e027c3c062
Cédric Krier <ced@b2ck.com>
2022-05-19 21:58:31 +0200

    Correctly complete after opening OR-ed parenthesis
    The nested call to replace_ending_clause must be pushed in the result instead
    of concatenated because the clause can be OR-ed.
    And pslice must remove depth from the length of the sting.
    
    issue11477
    review421151003
    (grafted from b4f0e27a3198630d60b73ee59f0d175f707b3fec)
    
    --HG--
    branch : 6.2

--- tests/sao.js
+++ tests/sao.js
@@ -3003,6 +3003,7 @@
         ['', ["Name: "]],
         [' ', ["", "Name: "]],
         ["Name: foo or", ["Name: foo"]],
+        ['Name: foo (Name: foo or N', ["Name: foo (Name: foo or Name: "]],
         ].forEach(function(test) {
             var value = test[0];
             var expected = test[1];
