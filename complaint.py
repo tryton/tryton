7ff6ace1d91d0a8789f61b187eadc69aca5b53b8
Cédric Krier <ced@b2ck.com>
2022-06-02 11:21:31 +0200

    Do not fail when compute unit for line without unit filled
    issue11522
    review421241003
    (grafted from 04e82b5357320c84387d86221881fdb0cf09920d)
    
    --HG--
    branch : 6.0

--- complaint.py
+++ complaint.py
@@ -748,7 +748,7 @@ class Action_SaleLine(_Action_Line, ModelView, ModelSQL):
 
     @fields.depends('line')
     def on_change_with_unit(self, name=None):
-        if self.line:
+        if self.line and self.line.unit:
             return self.line.unit.id
 
     @fields.depends('line')
@@ -786,7 +786,7 @@ class Action_InvoiceLine(_Action_Line, ModelView, ModelSQL):
 
     @fields.depends('line')
     def on_change_with_unit(self, name=None):
-        if self.line:
+        if self.line and self.line.unit:
             return self.line.unit.id
 
     @fields.depends('line')
