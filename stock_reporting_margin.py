5ab28409442e54cf061dbd0d48ab29f427bd6bdb
Cédric Krier <ced@b2ck.com>
2022-06-15 22:03:38 +0200

    Join with a unique currency rate
    issue11255
    review435291004
    (grafted from c2f056739e4856f2530b491653dfe8e5a8cb0461)
    
    --HG--
    branch : 6.0

--- stock_reporting_margin.py
+++ stock_reporting_margin.py
@@ -93,7 +93,7 @@ class Abstract(ModelSQL, ModelView):
                 condition=(move.currency == currency_rate.currency)
                 & (currency_rate.start_date <= move.effective_date)
                 & ((currency_rate.end_date == Null)
-                    | (currency_rate.end_date >= move.effective_date))
+                    | (currency_rate.end_date > move.effective_date))
                 )
             .join(company,
                 condition=move.company == company.id)
@@ -103,7 +103,7 @@ class Abstract(ModelSQL, ModelView):
                 condition=(company.currency == currency_rate_company.currency)
                 & (currency_rate_company.start_date <= move.effective_date)
                 & ((currency_rate_company.end_date == Null)
-                    | (currency_rate_company.end_date >= move.effective_date))
+                    | (currency_rate_company.end_date > move.effective_date))
                 )
             .join(from_location,
                 condition=(move.from_location == from_location.id))
