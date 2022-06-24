04e13cd748c9c40f5782b239486e0f91225e4567
Adrià Tarroja Caubet <atarroja@kopen.es>
2022-06-24 09:19:13 +0200

    Do not compare debit or credit to None when searching
    issue11568
    review417441003
    (grafted from 6d06c13574dddb21e36970afd27ea674ddaf4de6)
    
    --HG--
    branch : 6.4

--- account.py
+++ account.py
@@ -1952,7 +1952,7 @@ class _GeneralLedgerAccount(ActivePeriodMixin, ModelSQL, ModelView):
                 break
 
         ids = [a.id for a in accounts
-            if operator_(getattr(a, fname), operand)]
+            if operand is not None and operator_(getattr(a, fname), operand)]
         return [('id', 'in', ids)]
 
     @classmethod
@@ -2000,7 +2000,7 @@ class _GeneralLedgerAccount(ActivePeriodMixin, ModelSQL, ModelView):
             }.get(operator_, lambda v, l: False)
 
         ids = [a.id for a in accounts
-            if operator_(getattr(a, name), operand)]
+            if operand is not None and operator_(getattr(a, name), operand)]
         return [('id', 'in', ids)]
 
     def get_currency(self, name):
