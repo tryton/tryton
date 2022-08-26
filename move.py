a9b6c5b0d1112362b1db480731864c2fb90f7e2d
Cédric Krier <ced@b2ck.com>
2022-08-26 09:52:51 +0200

    Call super in cancel move
    issue11662
    review419691003
    (grafted from 65f6463fefd52ced857713cefbacdf245ac4c6e6)
    
    --HG--
    branch : 6.0

--- move.py
+++ move.py
@@ -132,7 +132,7 @@ class Move(metaclass=PoolMeta):
     @ModelView.button
     @Workflow.transition('cancelled')
     def cancel(cls, moves):
-        pass
+        super().cancel(moves)
 
     @classmethod
     def delete(cls, moves):
