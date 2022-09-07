038c4f29c5ffb0b5a50ce9085caf5b5d1dd752a3
Sergi Almacellas Abellana <sergi@koolpi.com>
2022-09-07 17:02:23 +0200

    Set action attribute when running wizard from url
    issue11660
    review431461003
    (grafted from 09a85a5a8ed617e56f74dce3ed3dff96d4c6f124)
    
    --HG--
    branch : 6.4

--- src/sao.js
+++ src/sao.js
@@ -535,8 +535,8 @@ var Sao = {};
         }
         function open_wizard(path) {
             var attributes = {};
-            attributes.name = path[0];
-            if (!attributes.name) {
+            attributes.action = path[0];
+            if (!attributes.action) {
                 return;
             }
             try {
