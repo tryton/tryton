9fa64029a2b30a2706ff7b572d192082b7908cd9
Cédric Krier <ced@b2ck.com>
2022-09-11 15:33:39 +0200

    Add proteus as tests requirement
    issue10346

--- setup.py
+++ setup.py
@@ -67,7 +67,10 @@ for dep in info.get('depends', []):
         requires.append(get_require_version('trytond_%s' % dep))
 requires.append(get_require_version('trytond'))
 
-tests_require = [get_require_version('trytond_account_invoice')]
+tests_require = [
+    get_require_version('proteus'),
+    get_require_version('trytond_account_invoice'),
+    ]
 dependency_links = []
 if minor_version % 2:
     dependency_links.append(
