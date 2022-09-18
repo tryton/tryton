ae0da3fc528a73bb35b131e8891d05505e402132
Cédric Krier <ced@b2ck.com>
2022-09-18 12:10:23 +0200

    Include version in JavaScript code
    issue11697
    review417971003

--- src/sao.js
+++ src/sao.js
@@ -1,6 +1,8 @@
 /* This file is part of Tryton.  The COPYRIGHT file at the top level of
    this repository contains the full copyright notices and license terms. */
-var Sao = {};
+var Sao = {
+    __version__: '6.5.0',
+};
 
 (function() {
     'use strict';
