28100032d58bd5d62e0606ccf6bde5826921dc7e
Cédric Krier <ced@b2ck.com>
2022-09-25 20:26:21 +0200

    Expand sources from customer when needed
    issue11717
    review421741003

--- payment.py
+++ payment.py
@@ -1539,13 +1539,14 @@ class Customer(CheckoutMixin, DeactivableMixin, ModelSQL, ModelView):
             customer.save()
             Transaction().commit()
 
-    def retrieve(self):
+    def retrieve(self, **params):
         if not self.stripe_customer_id:
             return
         try:
             return stripe.Customer.retrieve(
                 api_key=self.stripe_account.secret_key,
-                id=self.stripe_customer_id)
+                id=self.stripe_customer_id,
+                **params)
         except (stripe.error.RateLimitError,
                 stripe.error.APIConnectionError) as e:
             logger.warning(str(e))
@@ -1555,7 +1556,7 @@ class Customer(CheckoutMixin, DeactivableMixin, ModelSQL, ModelView):
         if sources is not None:
             return sources
         sources = []
-        customer = self.retrieve()
+        customer = self.retrieve(expand=['sources'])
         if customer:
             for source in customer.sources:
                 name = source.id
@@ -1723,7 +1724,7 @@ class Customer(CheckoutMixin, DeactivableMixin, ModelSQL, ModelView):
             Transaction().commit()
 
     def fetch_fingeprints(self):
-        customer = self.retrieve()
+        customer = self.retrieve(expand=['sources'])
         if customer:
             for source in customer.sources:
                 if hasattr(source, 'fingerprint'):
