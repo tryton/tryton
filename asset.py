73857bde09bbc5e0b224825b5cafbe970109f490
Adrià Tarroja Caubet <atarroja@kopen.es>
2022-10-04 18:14:05 +0200

    Use existing XML id for messages
    issue11725
    review415731003

--- asset.py
+++ asset.py
@@ -649,7 +649,7 @@ class Asset(Workflow, ModelSQL, ModelView):
         for asset in assets:
             if asset.lines:
                 raise AccessError(
-                    gettext('account_asset.msg_draft_lines',
+                    gettext('account_asset.msg_asset_draft_lines',
                         asset=asset.rec_name))
 
     @classmethod
@@ -709,7 +709,7 @@ class Asset(Workflow, ModelSQL, ModelView):
         for asset in assets:
             if asset.state != 'draft':
                 raise AccessError(
-                    gettext('account_asset.msg_delete_draft',
+                    gettext('account_asset.msg_asset_delete_draft',
                         asset=asset.rec_name))
         return super(Asset, cls).delete(assets)
 
