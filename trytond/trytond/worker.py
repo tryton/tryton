3f8fff303231072be416b4eecfec94c42306b23d
Cédric Krier <ced@b2ck.com>
2022-12-08 13:34:01 +0100

    Reset timeout loop of worker on each iteration
    If there was a next task planned very soon, this should not become the default
    timeout for future loop.
    
    issue11913
    review415971003
    (grafted from 10426538d63f736318c9eb5028cf34d423701422)
    
    --HG--
    branch : 5.0

--- trytond/trytond/worker.py
+++ trytond/trytond/worker.py
@@ -59,15 +59,15 @@ def work(options):
     queues = [Queue(pool, mpool) for pool in initializer(options, False)]
 
     tasks = TaskList()
-    timeout = options.timeout
     try:
         while True:
+            timeout = options.timeout
             while len(tasks.filter()) >= processes:
                 time.sleep(0.1)
             for queue in queues:
                 task_id, next_ = queue.pull(options.name)
-                timeout = min(
-                    next_ or options.timeout, timeout, options.timeout)
+                if next_ is not None:
+                    timeout = min(next_, timeout)
                 if task_id:
                     tasks.append(queue.run(task_id))
                     break
