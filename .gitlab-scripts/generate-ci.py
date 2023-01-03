366d722b3b802e32d10d7aad9381b1f5e435cde9
Cédric Krier <ced@b2ck.com>
2023-01-03 22:02:01 +0100

    Pre-generate ci for modules (grafted from 0a02b8c74818d6d635dce5c4b61cec44f17d6db7)
    --HG--
    branch : 5.0

--- .gitlab-scripts/generate-ci.py
+++ .gitlab-scripts/generate-ci.py
@@ -6,39 +6,17 @@ from itertools import chain
 from string import Template
 
 BASE_DIR = os.path.dirname(__file__)
-TEMPLATE_DOC = Template("""
+TEMPLATE = Template("""
 check-doc-${name}:
   extends: .check-doc
   variables:
-     PACKAGE: ${package}
+    PACKAGE: ${package}
 
 trigger-doc-build-${name}:
   extends: .trigger-doc-build
   variables:
-     PACKAGE: ${package}
-
-""")
-
-TEMPLATE = Template("""
-test-${name}:
-  extends: .test-tox
-  variables:
     PACKAGE: ${package}
-    TAG_PATTERN: '/^${tag_name}-.*/'
 
-""")
-
-TEMPLATE_IMAGE = Template("""
-test-${name}:
-  extends: .test-tox
-  image: $${CI_DEPENDENCY_PROXY_GROUP_IMAGE_PREFIX}/tryton/${package}-test:$${PYTHON_VERSION}
-  variables:
-    PACKAGE: ${package}
-    TAG_PATTERN: '/^${tag_name}-.*/'
-
-""")  # noqa: E501
-
-TEMPLATE_DB = Template("""
 test-${name}-sqlite:
   extends: .test-sqlite
   variables:
@@ -50,32 +28,7 @@ test-${name}-postgresql:
   variables:
     PACKAGE: ${package}
     TAG_PATTERN: '/^${tag_name}-.*/'
-
 """)
-
-TEMPLATE_GIS = Template("""
-test-${name}-postgis:
-  extends: .test-postgis
-  variables:
-    PACKAGE: ${package}
-    TAG_PATTERN: '/^${tag_name}-.*/'
-
-""")
-
-TEMPLATE_NPM_IMAGE = Template("""
-test-${name}:
-  extends: .test-npm
-  image: $${CI_DEPENDENCY_PROXY_GROUP_IMAGE_PREFIX}/tryton/${package}-test
-  variables:
-    PACKAGE: ${package}
-    TAG_PATTERN: '/^${tag_name}-.*/'
-
-""")
-
-NO_DB = {'proteus'}
-CUSTOM_IMAGE = {'tryton'}
-CUSTOM_NPM_IMAGE = {'sao'}
-GIS = {'trytond-gis'}
 
 
 def main(filename):
@@ -87,8 +40,7 @@ def main(filename):
 
 
 def render(file):
-    file.write(open(os.path.join(BASE_DIR, 'gitlab-ci.yml'), 'r').read())
-    packages = list(all_packages())
+    packages = sorted(all_packages())
     for package in packages:
         name = tag_name = package.replace('/', '-')
         if tag_name.startswith('modules-'):
@@ -98,29 +50,13 @@ def render(file):
             'package': package,
             'tag_name': tag_name,
             }
-        if os.path.exists(os.path.join(package, 'doc')):
-            file.write(TEMPLATE_DOC.substitute(mapping))
-        if (not os.path.exists(os.path.join(package, 'tox.ini'))
-                and not os.path.exists(os.path.join(package, 'package.json'))):
-            continue
-        if package in NO_DB:
-            file.write(TEMPLATE.substitute(mapping))
-        elif package in CUSTOM_IMAGE:
-            file.write(TEMPLATE_IMAGE.substitute(mapping))
-        elif package in CUSTOM_NPM_IMAGE:
-            file.write(TEMPLATE_NPM_IMAGE.substitute(mapping))
-        elif package in GIS:
-            file.write(TEMPLATE_GIS.substitute(mapping))
-        else:
-            file.write(TEMPLATE_DB.substitute(mapping))
+        file.write(TEMPLATE.substitute(mapping))
 
 
 def all_packages():
     root_dir = os.path.dirname(BASE_DIR)
     for filename in chain(
-            glob.glob('*/setup.py', root_dir=root_dir),
-            glob.glob('modules/*/setup.py', root_dir=root_dir),
-            glob.glob('*/package.json', root_dir=root_dir)):
+            glob.glob('modules/*/setup.py', root_dir=root_dir)):
         yield os.path.dirname(filename)
 
 
