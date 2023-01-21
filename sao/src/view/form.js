da5bec4ff7cc299ebab78dc5ce196af518c27cff
Cédric Krier <ced@b2ck.com>
2023-01-21 19:37:19 +0100

    Instantiate always Screen for xxx2Many fields with the same parameters
    The screen must be instantiated with the context of the parent screen so
    eventual field_view_get would give the same result.
    The limit must always be set to None/null to ensure to load all the records.
    The breadcrumb must be set to show navigation to the user.
    
    Closes #3140
    (grafted from ced0956c41cd1192b98eb4a9e9e23f85c86b6240)
    
    --HG--
    branch : 6.4

--- sao/src/view/form.js
+++ sao/src/view/form.js
@@ -3213,6 +3213,7 @@ function eval_pyson(value){
                 row_activate: this.activate.bind(this),
                 exclude_field: attributes.relation_field || null,
                 limit: null,
+                context: this.view.screen.context,
                 pre_validate: attributes.pre_validate,
                 breadcrumb: breadcrumb,
             });
@@ -3786,6 +3787,7 @@ function eval_pyson(value){
                 row_activate: this.activate.bind(this),
                 readonly: true,
                 limit: null,
+                context: this.view.screen.context,
                 breadcrumb: breadcrumb,
             });
             this.screen.windows.push(this);
@@ -3950,12 +3952,16 @@ function eval_pyson(value){
                 // Remove the first tree view as mode is form only
                 view_ids.shift();
             }
-            return new Sao.Screen(this.attributes.relation, {
+            var model = this.attributes.relation;
+            var breadcrumb = jQuery.extend([], this.view.screen.breadcrumb);
+            breadcrumb.push(this.attributes.string || Sao.common.MODELNAME.get(model));
+            return new Sao.Screen(model, {
                 'domain': domain,
                 'view_ids': view_ids,
                 'mode': ['form'],
                 'views_preload': this.attributes.views,
-                'context': context
+                'context': context,
+                'breadcrumb': breadcrumb,
             });
         },
         edit: function() {
