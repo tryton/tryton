4436731771f09a9918cadddd6c3d8b55a96118af
Cédric Krier <ced@b2ck.com>
2023-01-25 00:38:36 +0100

    Convert empty value as null instead of 0 in Dict numeric fields
    Closes #12038
    (grafted from 19675c62ab6ce40063c941c4aa26db297504e009)
    
    --HG--
    branch : 6.0

--- sao/src/view/form.js
+++ sao/src/view/form.js
@@ -4987,7 +4987,11 @@ function eval_pyson(value){
             }
         },
         get_value: function() {
-            var value = Number(this.input.val());
+            var value = this.input.val();
+            if (!value && (value !== 0)) {
+                return null;
+            }
+            value = Number(value);
             if (isNaN(value)) {
                 return null;
             }
@@ -5010,7 +5014,11 @@ function eval_pyson(value){
     Sao.View.Form.Dict.Numeric = Sao.class_(Sao.View.Form.Dict.Float, {
         class_: 'dict-numeric',
         get_value: function() {
-            var value = new Sao.Decimal(this.input.val());
+            var value = this.input.val();
+            if (!value && (value !== 0)) {
+                return null;
+            }
+            value = new Sao.Decimal(value);
             if (isNaN(value.valueOf())) {
                 return null;
             }
