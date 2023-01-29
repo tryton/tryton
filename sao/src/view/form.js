0c4734dca8e6c75f5a9ac85033d4bd550c210fe6
Cédric Krier <ced@b2ck.com>
2023-01-30 00:16:07 +0100

    Update One2Many validate to use promise based API of pre_validate
    Closes #12039
    (grafted from ec2a2ebc47a61443a3c08b1c5af658c061789c0e)
    
    --HG--
    branch : 6.0

--- sao/src/view/form.js
+++ sao/src/view/form.js
@@ -3530,13 +3530,8 @@ function eval_pyson(value){
                         return;
                     }
                     if (this.screen.pre_validate) {
-                        return record.pre_validate().then(function(validate) {
-                            if (!validate) {
-                                prm.reject();
-                                return;
-                            }
-                            prm.resolve();
-                        });
+                        return record.pre_validate().then(
+                            prm.resolve, prm.reject);
                     }
                     prm.resolve();
                 }.bind(this));
