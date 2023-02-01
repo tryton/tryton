cfa32f43ca75d3c587d45d9be527caa9237d06dd
Nicolas Évrard <nicoe@b2ck.com>
2023-02-01 17:54:26 +0100

    Call set_modified on record in set_client
    403bc7a95b0a introduced set_modified as a record method but not a field one.
    It was unfortunately called on the field in set_client in the same commit.
    
    Closes #12061
    (grafted from c806e4e039cb874404457ffc0a5a67c4bfe80c76)
    
    --HG--
    branch : 6.4

--- sao/src/model.js
+++ sao/src/model.js
@@ -1527,7 +1527,7 @@
                 this.changed(record);
                 record.validate(null, true, false, true);
             } else if (force_change) {
-                this.set_modified();
+                record.set_modified();
                 this.changed(record);
                 record.validate(null, true, false, true);
             }
