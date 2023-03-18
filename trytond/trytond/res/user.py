d28df75a7fc61f8d31c6e520dbe99891c180b0a2
Cédric Krier <ced@b2ck.com>
2023-03-18 13:12:30 +0100

    Remove password hash also from the transaction cache
    Closes #12120
    (grafted from 7b5157206d328bfe020fabece59fb7747dd2dd9d)
    
    --HG--
    branch : 5.0

--- trytond/trytond/res/user.py
+++ trytond/trytond/res/user.py
@@ -354,10 +354,27 @@ class User(DeactivableMixin, ModelSQL, ModelView):
     @classmethod
     def read(cls, ids, fields_names=None):
         result = super(User, cls).read(ids, fields_names=fields_names)
-        if not fields_names or 'password_hash' in fields_names:
-            for values in result:
+        cache = Transaction().get_cache().get(cls.__name__)
+        for values in result:
+            if 'password_hash' in values:
                 values['password_hash'] = None
+            if cache and values['id'] in cache:
+                cache[values['id']]['password_hash'] = None
         return result
+
+    @classmethod
+    def search(
+            cls, domain, offset=0, limit=None, order=None, count=False,
+            query=False):
+        users = super().search(
+            domain, offset=offset, limit=limit, order=order, count=count,
+            query=query)
+        cache = Transaction().get_cache().get(cls.__name__)
+        if not count and not query:
+            for user in users:
+                if cache and user.id in cache:
+                    cache[user.id]['password_hash'] = None
+        return users
 
     @classmethod
     def create(cls, vlist):
