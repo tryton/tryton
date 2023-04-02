2dc0c29b70d11d09981810b7f18ecd6f911369a0
Cédric Krier <ced@b2ck.com>
2023-04-02 18:30:21 +0200

    Generate packages in parallel

--- .gitlab-scripts/generate-packages.sh
+++ .gitlab-scripts/generate-packages.sh
@@ -4,6 +4,6 @@ set -eu
 PACKAGES=`realpath "${1}"`
 mkdir -p "${PACKAGES}"
 
-find . -name 'cookiecutter*' -prune -o -name setup.py -print | while read path; do
-    (cd "`dirname ${path}`" && python setup.py sdist && cp dist/* "${PACKAGES}")
-done
+(find . -name 'cookiecutter*' -prune -o -name setup.py -print | while read path; do
+    echo "`dirname ${path}`"
+done) | xargs --max-procs=$(( `nproc` * 2)) -I '{}' python "{}/setup.py" --quiet sdist --dist-dir "${PACKAGES}"
