3d8c2809318de4bbba78154c3001cc33e2630641
Cédric Krier <ced@b2ck.com>
2023-04-07 14:33:34 +0200

    Set company from the dunning line when generating dunning
    Closes #12192
    (grafted from 0a0a644d5a20e6f838c30a7ef7f3d4c550422142)
    
    --HG--
    branch : 5.0

--- modules/account_dunning/dunning.py
+++ modules/account_dunning/dunning.py
@@ -273,6 +273,7 @@ class Dunning(ModelSQL, ModelView):
         else:
             return
         return cls(
+            company=line.account.company,
             line=line,
             procedure=procedure,
             level=level,
