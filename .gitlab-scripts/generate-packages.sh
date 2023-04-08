cc34413c0ec3310d9a9a993432c37fc9385deea4
Cédric Krier <ced@b2ck.com>
2023-04-08 14:47:08 +0200

    Change to current directory to build package
    setuptools expect to be run from the root package directory.

--- .gitlab-scripts/generate-packages.sh
+++ .gitlab-scripts/generate-packages.sh
@@ -6,4 +6,4 @@ mkdir -p "${PACKAGES}"
 
 (find . -name 'cookiecutter*' -prune -o -name setup.py -print | while read path; do
     echo "`dirname ${path}`"
-done) | xargs --max-procs=$(( `nproc` * 2)) -I '{}' python "{}/setup.py" --quiet sdist --dist-dir "${PACKAGES}"
+done) | xargs --max-procs=$(( `nproc` * 2)) -I '{}' sh -c "cd \"{}\" && python setup.py --quiet sdist --dist-dir \"${PACKAGES}\""
