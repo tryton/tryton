4d950fc8e985c167b534a87661fa6a03294b08a5
Cédric Krier <ced@b2ck.com>
2023-04-08 11:12:20 +0200

    Do not quote sequence name coming from pg_get_serial_sequence
    The result of the function is already quoted to be suitably formatted for
    passing to the sequence functions.

--- trytond/trytond/backend/postgresql/database.py
+++ trytond/trytond/backend/postgresql/database.py
@@ -430,8 +430,7 @@ class Database(DatabaseInterface):
         cursor = connection.cursor()
         cursor.execute("SELECT pg_get_serial_sequence(%s, %s)", (table, 'id'))
         sequence_name, = cursor.fetchone()
-        cursor.execute(SQL("SELECT last_value FROM {}").format(
-                Identifier(*sequence_name.split('.'))))
+        cursor.execute(f"SELECT last_value FROM {sequence_name}")
         return cursor.fetchone()[0]
 
     def lock(self, connection, table):
