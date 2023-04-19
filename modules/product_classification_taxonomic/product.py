ac4c087d98d71f715ae61a7eca4412ea7fa3e221
Cédric Krier <ced@b2ck.com>
2023-04-19 16:06:51 +0200

    Add empty option for taxon rank
    Closes #12228
    (grafted from efa359afaf8b3f2169f4ba4ffb7866e1e6337b56)
    
    --HG--
    branch : 6.0

--- modules/product_classification_taxonomic/product.py
+++ modules/product_classification_taxonomic/product.py
@@ -20,6 +20,7 @@ class Taxon(classification_tree('product.taxon'), ModelSQL, ModelView):
     'Taxon'
     __name__ = 'product.taxon'
     rank = fields.Selection([
+            (None, ""),
             ('domain', 'Domain'),
             ('kingdom', 'Kingdom'),
             ('phylum', 'Phylum'),
