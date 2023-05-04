6a5449f315052ade1091259a51818ab64a30bd79
Cédric Krier <ced@b2ck.com>
2023-05-04 14:42:40 +0200

    Test account consolidation with company tests

--- modules/account_consolidation/tests/test_module.py
+++ modules/account_consolidation/tests/test_module.py
@@ -1,9 +1,21 @@
 # This file is part of Tryton.  The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
+from trytond.modules.company.tests import CompanyTestMixin
 from trytond.tests.test_tryton import ModuleTestCase
 
 
-class AccountConsolidationTestCase(ModuleTestCase):
+class CompanyAcountConsolidationTestMixin(CompanyTestMixin):
+
+    @property
+    def _skip_company_rule(self):
+        return super()._skip_company_rule | {
+            ('account.move', 'consolidation_company'),
+            ('account.invoice', 'consolidation_company'),
+            }
+
+
+class AccountConsolidationTestCase(
+        CompanyAcountConsolidationTestMixin, ModuleTestCase):
     "Test Account Consolidation module"
     module = 'account_consolidation'
     extras = ['account_invoice']
