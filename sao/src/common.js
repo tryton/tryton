c25c95dd59ec4f2a0cd80c8ab5792004e7166c35
Cédric Krier <ced@b2ck.com>
2023-05-17 12:00:23 +0200

    Declare blob as variable in image_url (grafted from a1f60a5423395af9f974237424b9776f2b9913b3)
    --HG--
    branch : 6.4

--- sao/src/common.js
+++ sao/src/common.js
@@ -4251,7 +4251,7 @@
             }
         } catch (e) {
         }
-        blob = new Blob([data], {type: type});
+        var blob = new Blob([data], {type: type});
         return window.URL.createObjectURL(blob);
     };
 
