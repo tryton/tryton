0bde64589a8497c5a40386b8e49ae2c1cc8ea949
Cédric Krier <ced@b2ck.com>
2023-06-09 08:17:20 +0200

    Do not try to login if Authorization has no username
    User application request has an Authorization header so an Authorization
    instance is returned but it can not be used for login.
    
    Closes #12312
    (grafted from 0a4e13728c0a9bb24536cb1033d95ca6749a2b9d)
    
    --HG--
    branch : 6.8

--- trytond/trytond/protocols/wrappers.py
+++ trytond/trytond/protocols/wrappers.py
@@ -119,7 +119,7 @@ class Request(_Request):
             user_id = security.check(
                 database_name, auth.get('userid'), auth.get('session'),
                 context=context)
-        else:
+        elif auth.username:
             parameters = getattr(auth, 'parameters', auth)
             try:
                 user_id = security.login(
@@ -127,6 +127,8 @@ class Request(_Request):
                     context=context)
             except RateLimitException:
                 abort(HTTPStatus.TOO_MANY_REQUESTS)
+        else:
+            user_id = None
         return user_id
 
     @cached_property
