6db20607e0faefa30865f5d8056d192fb23defb7
Adrià Tarroja Caubet <atarroja@kopen.es>
2023-07-05 16:32:56 +0200

    Reuse cumulate per party from context as default value for general ledger line
    Closes #12367
    (grafted from 5a95256398af9136cab85c1ca4c4d84ae23b70ec)
    
    --HG--
    branch : 6.8

--- modules/account/account.py
+++ modules/account/account.py
@@ -2510,7 +2510,7 @@ class GeneralLedgerLineContext(GeneralLedgerAccountContext):
 
     @classmethod
     def default_party_cumulate(cls):
-        return False
+        return Transaction().context.get('party_cumulate', False)
 
     @classmethod
     def get_context(cls, fields_names=None):
