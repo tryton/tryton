4452f906b7b63591687e5143c32351914b1e51f1
Cédric Krier <ced@b2ck.com>
2023-07-22 16:44:22 +0200

    Update Chrorus connection to use PISTE API
    Closes #9009

--- modules/account_fr_chorus/account.py
+++ modules/account_fr_chorus/account.py
@@ -3,8 +3,11 @@
 import base64
 import datetime
 import logging
+import posixpath
+from collections import defaultdict
 
-import requests
+from oauthlib.oauth2 import BackendApplicationClient, TokenExpiredError
+from requests_oauthlib import OAuth2Session
 from sql.functions import CharLength
 
 from trytond.config import config
@@ -18,11 +21,14 @@ from trytond.transaction import Transaction
 
 from .exceptions import InvoiceChorusValidationError
 
-CERT = (
-    config.get('account_fr_chorus', 'certificate'),
-    config.get('account_fr_chorus', 'privatekey'))
-BASE_URL = config.get(
-    'account_fr_chorus', 'url', default='https://chorus-pro.gouv.fr:5443')
+OAUTH_TOKEN_URL = {
+    'service-qualif': 'https://sandbox-oauth.piste.gouv.fr/api/oauth/token',
+    'service': 'https://oauth.piste.gouv.fr/api/oauth/token',
+    }
+API_URL = {
+    'service-qualif': 'https://sandbox-api.piste.gouv.fr',
+    'service': 'https://api.piste.gouv.fr',
+    }
 EDOC2SYNTAX = {
     'edocument.uncefact.invoice': 'IN_DP_E1_CII_16B',
     }
@@ -56,23 +62,29 @@ class Configuration(_SyntaxMixin, metaclass=PoolMeta):
         'required': Bool(Eval('chorus_login')),
         }
 
-    chorus_login = fields.MultiValue(fields.Char("Chorus Login", strip=False))
+    chorus_piste_client_id = fields.MultiValue(
+        fields.Char("Piste Client ID", strip=False))
+    chorus_piste_client_secret = fields.MultiValue(
+        fields.Char("Piste Client Secret", strip=False, states=_states))
+    chorus_login = fields.MultiValue(fields.Char("Login", strip=False))
     chorus_password = fields.MultiValue(fields.Char(
-            "Chorus Password", strip=False, states=_states))
+            "Password", strip=False, states=_states))
     chorus_service = fields.MultiValue(fields.Selection([
                 (None, ""),
                 ('service-qualif', "Qualification"),
                 ('service', "Production"),
-                ], "Chorus Service", states=_states))
+                ], "Service", states=_states))
     chorus_syntax = fields.Selection(
-        'get_syntaxes', "Chorus Syntax", states=_states)
+        'get_syntaxes', "Syntax", states=_states)
 
     del _states
 
     @classmethod
     def multivalue_model(cls, field):
         pool = Pool()
-        if field in {'chorus_login', 'chorus_password', 'chorus_service'}:
+        if field in {
+                'chorus_piste_client_id', 'chorus_piste_client_secret',
+                'chorus_login', 'chorus_password', 'chorus_service'}:
             return pool.get('account.credential.chorus')
         return super(Configuration, cls).multivalue_model(field)
 
@@ -81,6 +93,9 @@ class CredentialChorus(ModelSQL, CompanyValueMixin):
     "Account Credential Chorus"
     __name__ = 'account.credential.chorus'
 
+    chorus_piste_client_id = fields.Char("Piste Client ID", strip=False)
+    chorus_piste_client_secret = fields.Char(
+        "Piste Client Secret", strip=False)
     chorus_login = fields.Char("Login", strip=False)
     chorus_password = fields.Char("Password", strip=False)
     chorus_service = fields.Selection([
@@ -90,15 +105,48 @@ class CredentialChorus(ModelSQL, CompanyValueMixin):
             ], "Service")
 
     @classmethod
-    def post(cls, path, payload):
+    def get_session(cls):
         pool = Pool()
         Configuration = pool.get('account.configuration')
         config = Configuration(1)
-        url = '/'.join((BASE_URL, config.chorus_service, path))
-        auth = (config.chorus_login, config.chorus_password)
-        resp = requests.post(
-            url, json=payload, cert=CERT, verify=True, auth=auth,
-            timeout=TIMEOUT)
+        client = BackendApplicationClient(
+            client_id=config.chorus_piste_client_id)
+        session = OAuth2Session(client=client)
+        cls._get_token(session)
+        return session
+
+    @classmethod
+    def _get_token(cls, session):
+        pool = Pool()
+        Configuration = pool.get('account.configuration')
+        config = Configuration(1)
+        return session.fetch_token(
+            OAUTH_TOKEN_URL[config.chorus_service],
+            client_id=config.chorus_piste_client_id,
+            client_secret=config.chorus_piste_client_secret)
+
+    @classmethod
+    def post(cls, path, payload, session=None):
+        pool = Pool()
+        Configuration = pool.get('account.configuration')
+        config = Configuration(1)
+        if not session:
+            session = cls.get_session()
+        base_url = API_URL[config.chorus_service]
+        url = posixpath.join(base_url, path)
+        account = f'{config.chorus_login}:{config.chorus_password}'
+        headers = {
+            'cpro-account': base64.b64encode(account.encode('utf-8')),
+            }
+        try:
+            resp = session.post(
+                url, headers=headers, json=payload,
+                verify=True, timeout=TIMEOUT)
+        except TokenExpiredError:
+            cls._get_token(session)
+            resp = session.post(
+                url, headers=headers, json=payload,
+                verify=True, timeout=TIMEOUT)
         resp.raise_for_status()
         return resp.json()
 
@@ -206,14 +254,18 @@ class InvoiceChorus(ModelSQL, ModelView, _SyntaxMixin, metaclass=PoolMeta):
                         transaction.context.get('company')),
                     ])
 
+        sessions = defaultdict(Credential.get_session)
         cls.lock(records)
         for record in records:
             # Use clear cache after a commit
             record = cls(record.id)
             record.lock()
-            with Transaction().set_context(**record._send_context()):
+            context = record._send_context()
+            with transaction.set_context(**context):
                 payload = record.get_payload()
-                resp = Credential.post('factures/deposer/flux', payload)
+                resp = Credential.post(
+                    'cpro/factures/v1/deposer/flux', payload,
+                    session=sessions[tuple(context.items())])
                 if resp['codeRetour']:
                     logger.error(
                         "Error when sending invoice %d to chorus: %s",
