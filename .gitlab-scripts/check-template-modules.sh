e3444ecac86b55d65779fa2c228eafb0354daa2f
Cédric Krier <ced@b2ck.com>
2023-08-12 13:52:54 +0200

    Check modules against template model
    Some files in modules should always be the same as the template.
    We add a CI task to check those files against a new templated version of the
    module.

---
+++ .gitlab-scripts/check-template-modules.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+set -eu
+
+OUTPUT=`mktemp -d`
+find modules -name setup.py -print | while read path
+do
+    path=`dirname "${path}"`
+    module=`basename "${path}"`
+    printf "Check $module ... "
+    cookiecutter --no-input cookiecutter-module -o "${OUTPUT}" module_name="${module}"
+    for file in tox.ini .readthedocs.yaml doc/conf.py
+    do
+        cmp "${path}/${file}" "${OUTPUT}/${module}/${file}"
+    done
+    printf "OK\n"
+done
