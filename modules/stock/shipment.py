b187ee52be75e56c3291dbf73284fb15b82d903d
Cédric Krier <ced@b2ck.com>
2023-08-18 14:47:39 +0200

    Store synchronized moves of customer shipment even when quantity is not updated
    ed41959390ff added a synchronization on assign to prevent infinite warning
    loop on pack if it creates new move without origin.
    But 38e2a76db719 refactoring lost this behavior.
    
    Closes #12503
    (grafted from 903204d5d1728e818ec44bf5aa0a629c2efd1a65)
    
    --HG--
    branch : 6.6

--- modules/stock/shipment.py
+++ modules/stock/shipment.py
@@ -1493,7 +1493,7 @@ class ShipmentOut(ShipmentAssignMixin, Workflow, ModelSQL, ModelView):
                         move.uom)
                     if quantity and move.quantity != qty:
                         move.quantity = qty
-                        moves.append(move)
+                    moves.append(move)
         Move.save(moves)
 
     @classmethod
