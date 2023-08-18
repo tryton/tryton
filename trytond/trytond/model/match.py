8ba9c5119bf36d5b6aa260af72959d68e5eea3db
Cédric Krier <ced@b2ck.com>
2023-08-18 17:06:26 +0200

    Convert pattern values into boolean when matching boolean field
    Closes #12504
    (grafted from b09b54e3915c92d465537bab02d66d559bce4295)
    
    --HG--
    branch : 6.6

--- trytond/trytond/model/match.py
+++ trytond/trytond/model/match.py
@@ -9,12 +9,16 @@ class MatchMixin(object):
         '''Match on pattern
         pattern is a dictionary with model field as key
         and matching value as value'''
-        for field, pattern_value in pattern.items():
-            value = getattr(self, field)
+        for field_name, pattern_value in pattern.items():
+            value = getattr(self, field_name)
             if not match_none and value is None:
                 continue
-            if self._fields[field]._type == 'many2one':
+            field = self._fields[field_name]
+            if field._type == 'many2one':
                 value = value.id if value else value
+            elif field._type == 'boolean':
+                value = bool(value)
+                pattern_value = bool(pattern_value)
             if value != pattern_value:
                 return False
         return True
