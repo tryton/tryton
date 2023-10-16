d58c3467abfeb24e3eff3f5ddb325f690b5a703c
Maxime Richez <mrichez@saluc.com>
2023-10-16 16:48:40 +0200

    Allow to send email without files to attach
    Closes #12645
    (grafted from 177d9695220f1ec64eab311e11ff7c97b1880cfb)
    
    --HG--
    branch : 6.6

--- trytond/trytond/ir/email_.py
+++ trytond/trytond/ir/email_.py
@@ -186,12 +186,13 @@ class Email(ResourceAccessMixin, ModelSQL, ModelView):
             body=body,
             resource=record)
         email.save()
-        with Transaction().set_context(_check_access=False):
-            attachments_ = []
-            for name, data in files:
-                attachments_.append(
-                    Attachment(resource=email, name=name, data=data))
-            Attachment.save(attachments_)
+        if files:
+            with Transaction().set_context(_check_access=False):
+                attachments_ = []
+                for name, data in files:
+                    attachments_.append(
+                        Attachment(resource=email, name=name, data=data))
+                Attachment.save(attachments_)
         return email
 
     @classmethod
