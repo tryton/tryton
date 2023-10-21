35458f62499afe53918b57dd606b56b0c772d68e
Cédric Krier <ced@b2ck.com>
2023-10-21 15:46:53 +0200

    Do not fail when reconciling an empty list
    Closes #12626
    (grafted from 2f04f787260ea6bd52ecf4b3ac414d343c78dc70)
    
    --HG--
    branch : 6.6

--- modules/account/move.py
+++ modules/account/move.py
@@ -1400,6 +1400,8 @@ class Line(MoveLineMixin, ModelSQL, ModelView):
 
         reconciliations = []
         for lines in lines_list:
+            if not lines:
+                continue
             for line in lines:
                 if line.reconciliation:
                     raise AccessError(
