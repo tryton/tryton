332bf2d30a0292bc29934c60ca35858637f23b39
Cédric Krier <ced@b2ck.com>
2023-11-04 19:58:21 +0100

    Support x86_64 and arm64 darwin build
    Closes #10046

--- tryton/make-darwin-installer.sh
+++ tryton/make-darwin-installer.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 set -e
 version=`./setup.py --version`
+machine=`python3 -c "import platform; print(platform.machine())"`
 rm -rf build dist
 ./setup.py compile_catalog
 ./setup-freeze.py bdist_mac
@@ -10,6 +11,7 @@ for f in CHANGELOG COPYRIGHT LICENSE; do
     cp ${f} dist/${f}.txt
 done
 cp -r doc dist/
-rm -f "tryton-${version}.dmg"
-hdiutil create "tryton-${version}.dmg" -volname "Tryton Client ${version}" \
+rm -f "tryton-${machine}-${version}.dmg"
+hdiutil create "tryton-${machine}-${version}.dmg" \
+    -volname "Tryton Client ${machine} ${version}" \
     -fs HFS+ -srcfolder dist
