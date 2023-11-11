fde4fa9a1e6d023a987b827992c573538cd46632
Cédric Krier <ced@b2ck.com>
2023-11-11 19:34:27 +0100

    Prevent switching row when double click on expander
    Closes #12732
    (grafted from f1ce4d6a8fe85a007eae863ce9ecea181f725de7)
    
    --HG--
    branch : 6.8

--- sao/src/view/tree.js
+++ sao/src/view/tree.js
@@ -1523,6 +1523,11 @@
                 this.expander.children().html('&nbsp;');
                 this.expander.on('click keypress',
                         Sao.common.click_press(this.toggle_row.bind(this)));
+                this.expander.dblclick((evt) => {
+                    // prevent calling switch_row on td
+                    evt.preventDefault();
+                    evt.stopImmediatePropagation();
+                });
             }
 
             for (var i = 0; i < this.tree.columns.length; i++) {
