14596be0b85602ec431d3363515619e33dde41c9
Cédric Krier <ced@b2ck.com>
2023-12-19 18:09:12 +0100

    Add CompanyTestMixin to sale advance payment tests
    Closes #12845

--- modules/sale_advance_payment/tests/test_module.py
+++ modules/sale_advance_payment/tests/test_module.py
@@ -1,10 +1,11 @@
 # This file is part of Tryton.  The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
 
+from trytond.modules.company.tests import CompanyTestMixin
 from trytond.tests.test_tryton import ModuleTestCase
 
 
-class SaleAdvancePaymentTestCase(ModuleTestCase):
+class SaleAdvancePaymentTestCase(CompanyTestMixin, ModuleTestCase):
     'Test Sale Advance Payment module'
     module = 'sale_advance_payment'
 
