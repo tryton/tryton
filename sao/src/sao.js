6d1830ee4eb7e0e48b9e0b56566f692b091ad762
Cédric Krier <ced@b2ck.com>
2024-02-03 12:00:11 +0100

    Increase version number
    --HG--
    branch : 7.0

--- sao/src/sao.js
+++ sao/src/sao.js
@@ -1,7 +1,7 @@
 /* This file is part of Tryton.  The COPYRIGHT file at the top level of
    this repository contains the full copyright notices and license terms. */
 var Sao = {
-    __version__: '7.0.6',
+    __version__: '7.0.7',
 };
 
 (function() {
