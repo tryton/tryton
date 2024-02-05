8910c3215b18d99a3934901d8b39d2b6a85bfe01
Cédric Krier <ced@b2ck.com>
2024-02-05 23:15:21 +0100

    Do not delete purchase request with a purchase line when supply stock
    The design does not ensure that draft purchase request can not be linked to a
    purchase line.
    
    Closes #12974
    (grafted from 88d4acee578219e8c193b56ceb5a4f3bf3d7b1aa)
    
    --HG--
    branch : 6.8

--- modules/stock_supply/purchase_request.py
+++ modules/stock_supply/purchase_request.py
@@ -142,6 +142,7 @@ class PurchaseRequest(metaclass=PoolMeta):
         products = set(products)
         reqs = cls.search([
                 ('state', '=', 'draft'),
+                ('purchase_line', '=', None),
                 ('company', '=', company.id),
                 ('origin', 'like', 'stock.order_point,%'),
                 ])
