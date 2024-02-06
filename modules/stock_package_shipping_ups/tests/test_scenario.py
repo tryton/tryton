9795da51797f4a986a0d078b2793caa2f31dfb55
Cédric Krier <ced@b2ck.com>
2024-02-06 16:11:50 +0100

    Check OAuth environment to run UPS scenario
    6723c2dced5b changed the environment variables needed to run the scenario.

--- modules/stock_package_shipping_ups/tests/test_scenario.py
+++ modules/stock_package_shipping_ups/tests/test_scenario.py
@@ -5,9 +5,8 @@ import os
 
 from trytond.tests.test_tryton import load_doc_tests
 
-if (os.getenv('UPS_USER_ID')
-        and os.getenv('UPS_PASSWORD')
-        and os.getenv('UPS_LICENSE')
+if (os.getenv('UPS_CLIENT_ID')
+        and os.getenv('UPS_CLIENT_SECRET')
         and os.getenv('UPS_ACCOUNT_NUMBER')):
     def load_tests(*args, **kwargs):
         return load_doc_tests(__name__, __file__, *args, **kwargs)
