1eb61dcb1acfd08e2dc66dad29e02c10982f14db
Cédric Krier <ced@b2ck.com>
2024-02-12 23:54:07 +0100

    Do not copy sale lines when copying purchase requests
    Closes #12918
    (grafted from 3a4693cd3e305eb2b2ae8d34c92ecef275ee9adc)
    
    --HG--
    branch : 7.0

--- modules/sale_supply/purchase.py
+++ modules/sale_supply/purchase.py
@@ -19,6 +19,12 @@ class Request(metaclass=PoolMeta):
         return super()._get_origin() | {'sale.sale'}
 
     @classmethod
+    def copy(cls, requests, default=None):
+        default = default.copy() if default is not None else {}
+        default.setdefault('sale_lines')
+        return super().copy(requests, default=default)
+
+    @classmethod
     def delete(cls, requests):
         pool = Pool()
         Sale = pool.get('sale.sale')
