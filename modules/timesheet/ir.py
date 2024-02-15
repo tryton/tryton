20c51464d52ff95907f6911af22cc1141428a38d
Sergi Almacellas Abellana <sergi@kopen.es>
2024-02-16 00:35:52 +0100

    Add employees to rule context for timesheet reporting models
    Closes #13038
    (grafted from adad3463a8f430575298cf05ec614f4fb4f43916)
    
    --HG--
    branch : 7.0

--- modules/timesheet/ir.py
+++ modules/timesheet/ir.py
@@ -3,6 +3,13 @@
 
 from trytond.pool import Pool, PoolMeta
 
+_EMPLOYEES_RULE_MODELS = {
+    'timesheet.line',
+    'timesheet.hours_employee',
+    'timesheet.hours_employee_weekly',
+    'timesheet.hours_employee_monthly',
+    }
+
 
 class Rule(metaclass=PoolMeta):
     __name__ = 'ir.rule'
@@ -12,7 +19,7 @@ class Rule(metaclass=PoolMeta):
         pool = Pool()
         User = pool.get('res.user')
         context = super()._get_context(model_name)
-        if model_name == 'timesheet.line':
+        if model_name in _EMPLOYEES_RULE_MODELS:
             context['employees'] = User.get_employees()
         return context
 
@@ -21,6 +28,6 @@ class Rule(metaclass=PoolMeta):
         pool = Pool()
         User = pool.get('res.user')
         key = super()._get_cache_key(model_name)
-        if model_name == 'timesheet.line':
+        if model_name in _EMPLOYEES_RULE_MODELS:
             key = (*key, User.get_employees())
         return key
