d1c62a07275f8483a79d9aa2b43a2197545a2b5c
Cédric Krier <ced@b2ck.com>
2024-03-08 18:23:26 +0100

    Add searcher for payment currency of account move line

--- modules/account_payment/account.py
+++ modules/account_payment/account.py
@@ -38,7 +38,7 @@ class MoveLine(metaclass=PoolMeta):
         'get_payment_amount', searcher='search_payment_amount')
     payment_currency = fields.Function(fields.Many2One(
             'currency.currency', "Payment Currency"),
-        'get_payment_currency')
+        'get_payment_currency', searcher='search_payment_currency')
     payments = fields.One2Many('account.payment', 'line', 'Payments',
         readonly=True,
         states={
@@ -146,6 +146,16 @@ class MoveLine(metaclass=PoolMeta):
             return self.second_currency.id
         elif self.currency:
             return self.currency.id
+
+    @classmethod
+    def search_payment_currency(cls, name, clause):
+        return ['OR',
+            [('second_currency', *clause[1:]),
+                ('second_currency', '!=', None),
+                ],
+            [('currency', *clause[1:]),
+                ('second_currency', '=', None)],
+            ]
 
     def get_payment_kind(self, name):
         if self.account.type.receivable or self.account.type.payable:
