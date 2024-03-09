ceb0ff37007002cd93f8639949de5f1b72fe17d7
Cédric Krier <ced@b2ck.com>
2024-03-09 10:52:41 +0100

    Use size from attributes when setting url_size parameters for Image
    Closes #13089

--- sao/src/view/form.js
+++ sao/src/view/form.js
@@ -1190,7 +1190,7 @@ function eval_pyson(value){
                         var url = new URL(name, window.location);
                         url.searchParams.set(
                             this.attributes.url_size,
-                            attributes.size || 48);
+                            this.attributes.size || 48);
                         name = url.href;
                     }
                     this.img.attr('src', name);
