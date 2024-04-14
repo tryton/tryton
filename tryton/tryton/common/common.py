84924321264b27c4a62099f0a5ecb9c4a9686016
Cédric Krier <ced@b2ck.com>
2024-04-14 11:07:33 +0200

    Always raise RPCException in RPCExecute
    So when calling RPCExecute without processing exception, all exceptions can be
    catch with RPCException.
    
    Closes #12874
    (grafted from 372dbf53977e872ae62dbc8d6ce5bdc3bcacec69)
    
    --HG--
    branch : 6.8

--- tryton/tryton/common/common.py
+++ tryton/tryton/common/common.py
@@ -1260,7 +1260,7 @@ class RPCProgress(object):
 
         def return_():
             if self.exception:
-                raise self.exception
+                raise RPCException(self.exception)
             else:
                 return self.res
 
