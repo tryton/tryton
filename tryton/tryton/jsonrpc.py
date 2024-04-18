38b29f3bb4ff914a281cf136275d931471b8944a
Cédric Krier <ced@b2ck.com>
2024-04-18 19:15:47 +0200

    Encode with gzip only if a session exists
    Since 1923117e935d the server does not accept compressed content for
    unauthenticated request.
    (grafted from 97cbe01195da6938e7f718d07e436c8a2856e3a2)
    
    --HG--
    branch : 6.0

--- tryton/tryton/jsonrpc.py
+++ tryton/tryton/jsonrpc.py
@@ -130,7 +130,6 @@ class JSONUnmarshaller(object):
 class Transport(xmlrpc.client.SafeTransport):
 
     accept_gzip_encoding = True
-    encode_threshold = 1400  # common MTU
 
     def __init__(
             self, fingerprints=None, ca_certs=None, session=None):
@@ -232,6 +231,11 @@ class Transport(xmlrpc.client.SafeTransport):
         self._connection[1].timeout = DEFAULT_TIMEOUT
         self._connection[1].sock.settimeout(DEFAULT_TIMEOUT)
         return self._connection[1]
+
+    @property
+    def encode_threshold(self):
+        if self.session:
+            return 1400  # common MTU
 
 
 class ServerProxy(xmlrpc.client.ServerProxy):
