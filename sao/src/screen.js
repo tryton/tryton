f30d8934cb2c79267cceeee5995c08605cf77958
Cédric Krier <ced@b2ck.com>
2024-04-21 15:24:26 +0200

    Wait for display to finish when reloading record (grafted from 9023fdf384db2961e4dea922f5194b09f879834f)
    --HG--
    branch : 7.0

--- sao/src/screen.js
+++ sao/src/screen.js
@@ -1919,7 +1919,7 @@
                 promises.push(this.group.parent.root_parent.reload());
             }
             return jQuery.when.apply(jQuery, promises).then(() => {
-                this.display();
+                return this.display();
             });
         },
         get_buttons: function() {
