74a69ef57dd19de10458910c5b86d5f70d53acf9
Cédric Krier <ced@b2ck.com>
2024-06-08 17:19:40 +0200

    Fix test_set_key of FieldDictTestCase
    Closes #13314
    (grafted from 00f2b092c74fd4b8974a464f7685f24e71a6e025)
    
    --HG--
    branch : 6.0

--- trytond/trytond/tests/test_field_dict.py
+++ trytond/trytond/tests/test_field_dict.py
@@ -886,7 +886,7 @@ class FieldDictTestCase(unittest.TestCase):
         self.assertDictEqual(
             dict_.dico_string_keys, {'a': 'A', 'type': "Type"})
 
-    @with_transaction
+    @with_transaction()
     def test_set_key(self):
         "Test setting a key of dict"
         Dict = Pool().get('test.dict')
@@ -897,7 +897,7 @@ class FieldDictTestCase(unittest.TestCase):
                     }])
 
         with self.assertRaises(TypeError):
-            dict.dico['a'] = 5
+            dict_.dico['a'] = 5
 
 
 @unittest.skipUnless(backend.name == 'postgresql',
