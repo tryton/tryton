d04d76fb3a7e393f26719f1a751125b3c2329470
Cédric Krier <ced@b2ck.com>
2024-06-10 20:04:39 +0200

    Set focus to the first focusable child of the cell
    Closes #13327
    (grafted from edd4d4ea6416280f2c03f3e6b87a9b62e6cc333c)
    
    --HG--
    branch : 6.0

--- sao/src/view/tree.js
+++ sao/src/view/tree.js
@@ -1145,7 +1145,7 @@
                         if (this.editable && new_) {
                             td.trigger('click');
                         }
-                        td.find(':input,[tabindex=0]').focus();
+                        Sao.common.find_focusable_child(td).focus();
                     }
                 }
             }.bind(this);
