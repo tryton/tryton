e95068c0bbb00493dd39d699877f3eb0faab4819
Cédric Krier <ced@b2ck.com>
2024-06-30 01:53:24 +0200

    Do not iterate over MultiSelection value of Dict key if it is null
    Closes #13375
    (grafted from d938889e4f42517b7433685091e6cfce5080063a)
    
    --HG--
    branch : 7.2

--- sao/src/view/form.js
+++ sao/src/view/form.js
@@ -5398,7 +5398,9 @@ function eval_pyson(value){
                 return value.map(function(e) { return JSON.parse(e); });
             },
             set_value: function(value) {
-                value = value.map(function(e) { return JSON.stringify(e); });
+                if (value) {
+                    value = value.map(function(e) { return JSON.stringify(e); });
+                }
                 this.input.val(value);
             }
         });
