8edd9f79f8e727a0e08a8fa734279d5c5a55bded
Cédric Krier <ced@b2ck.com>
2024-10-02 17:02:55 +0200

    Use equality operators when stringify in domain clause with singleton
    Closes #13557

--- tryton/tryton/tests/test_common_domain_parser.py
+++ tryton/tryton/tests/test_common_domain_parser.py
@@ -682,8 +682,12 @@ class DomainParserTestCase(TestCase):
         self.assertEqual(
             dom.string([('name', 'in', ['John', 'Jane'])]), 'Name: John;Jane')
         self.assertEqual(
+            dom.string([('name', 'in', ['John'])]), 'Name: =John')
+        self.assertEqual(
             dom.string([('name', 'not in', ['John', 'Jane'])]),
             'Name: !John;Jane')
+        self.assertEqual(
+            dom.string([('name', 'not in', ['John'])]), 'Name: !=John')
         self.assertEqual(
             dom.string([
                     ('name', 'ilike', '%Doe%'),
