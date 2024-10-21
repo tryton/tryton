a948cfe046e55e14ad9adbdb39ace2d1f2603a6a
Cédric Krier <ced@b2ck.com>
2024-10-22 01:18:57 +0200

    Save each valid record pasted
    Closes #3312
    (grafted from 26ae2d02e7399dfee30e31a984044329546c6416)
    
    --HG--
    branch : 7.2

--- tryton/tryton/gui/window/view_form/view/list.py
+++ tryton/tryton/gui/window/view_form/view/list.py
@@ -790,6 +790,7 @@ class ViewTree(View):
                         break
             if not record.validate():
                 break
+            record.save()
             idx += 1
         self.record = record
         self.screen.display(set_cursor=True)
