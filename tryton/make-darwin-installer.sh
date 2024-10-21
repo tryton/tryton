4588bf7235c8b05c52c746999752fb4c9097442e
Cédric Krier <ced@b2ck.com>
2024-10-21 18:01:53 +0200

    Update pixbuf loaders and immodules when building installers
    Closes #13611
    (grafted from e5fd55d3e783988236505502614e4ae708cce785)
    
    --HG--
    branch : 7.0

--- tryton/make-darwin-installer.sh
+++ tryton/make-darwin-installer.sh
@@ -2,6 +2,16 @@
 set -e
 version=`./setup.py --version`
 rm -rf build dist
+mkdir -p darwin/gtk-3.0
+GDK_PIXBUF_MODULEDIR="$(dirname $(which gdk-pixbuf-query-loaders))/../lib/gdk-pixbuf-2.0/2.10.0/loaders" \
+    gdk-pixbuf-query-loaders \
+    | sed -e '/^#/d' \
+    | sed  -e 's@/.*/lib/gdk-pixbuf-2.0@\@executable_path/lib/gdk-pixbuf-2.0@' \
+    > darwin/gtk-3.0/gdk-pixbuf.loaders
+gtk-query-immodules-3.0 \
+    | sed -e '/^#/d' \
+    | sed  -e 's@/.*/lib/gtk-3.0@\@executable_path/lib/gtk-3.0@' \
+    > darwin/gtk-3.0/gtk.immodules
 ./setup.py compile_catalog
 ./setup-freeze.py bdist_mac
 mkdir dist
