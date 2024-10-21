dfbb40b6bf6c70351cf9ffc993ccb8affaa5d592
Cédric Krier <ced@b2ck.com>
2024-10-21 18:01:53 +0200

    Update pixbuf loaders and immodules when building installers
    Closes #13611
    (grafted from e5fd55d3e783988236505502614e4ae708cce785)
    
    --HG--
    branch : 6.0

--- tryton/make-win32-installer.sh
+++ tryton/make-win32-installer.sh
@@ -4,6 +4,9 @@ version=`./setup.py --version`
 series=${version%.*}
 bits=`python -c "import platform; print(platform.architecture()[0])"`
 rm -rf build dist
+mkdir -p win32/gtk-3.0
+gdk-pixbuf-query-loaders | sed -e '/^#/d' > win32/gtk-3.0/gdk-pixbuf.loaders
+gtk-query-immodules-3.0 | sed -e '/^#/d' > win32/gtk-3.0/gtk.immodules
 ./setup.py compile_catalog
 ./setup-freeze.py install_exe -d dist
 cp `which gdbus.exe` dist/
