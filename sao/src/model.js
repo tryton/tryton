213cc9958c71ad2a35d9eb920b804f4ea62b9a94
Cédric Krier <ced@b2ck.com>
2024-11-13 11:22:21 +0100

    Test if pre_validate is an empty object
    Closes #13675
    (grafted from 1d88e5b4e36de11517ae059ea04d352d2bde3161)
    
    --HG--
    branch : 7.2

--- sao/src/model.js
+++ sao/src/model.js
@@ -1804,7 +1804,7 @@
                             setdefault = false;
                         }
                     }
-                    if (setdefault && !pre_validate) {
+                    if (setdefault && jQuery.isEmptyObject(pre_validate)) {
                         this.set_client(record, value);
                         state_attrs.domain_readonly = domain_readonly;
                     }
@@ -2658,7 +2658,7 @@
             }
             for (const record2 of (record._values[this.name] || [])) {
                 if (!record2.get_loaded() && (record2.id >= 0) &&
-                        !pre_validate) {
+                        jQuery.isEmptyObject(pre_validate)) {
                     continue;
                 }
                 if (!record2.validate(null, softvalidation, ldomain, true)) {
