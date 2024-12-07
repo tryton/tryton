49dd545155bd6c1e72733a45dd153a031448ec96
Pedro Nuñez <pnunez@healthpoint.es>
2024-12-08 00:10:32 +0100

    Exclude report revisions from invoice copy
    Closes #13698
    (grafted from 904df25a1b5e3ce3f77940511b5c8142e104fc4c)
    
    --HG--
    branch : 7.0

--- modules/account_invoice/invoice.py
+++ modules/account_invoice/invoice.py
@@ -1435,6 +1435,7 @@ class Invoice(Workflow, ModelSQL, ModelView, TaxableMixin, InvoiceReportMixin):
         default.setdefault('tax_amount_cache', None)
         default.setdefault('validated_by')
         default.setdefault('posted_by')
+        default.setdefault('invoice_report_revisions', None)
         return super(Invoice, cls).copy(invoices, default=default)
 
     @classmethod
