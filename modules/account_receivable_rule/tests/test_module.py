b4b2b5674a67b532f8000a6722375a61118bdd06
Cédric Krier <ced@b2ck.com>
2024-12-09 14:42:37 +0100

    Add company in context of journal on account receivable rule
    Closes #13742

--- modules/account_receivable_rule/tests/test_module.py
+++ modules/account_receivable_rule/tests/test_module.py
@@ -1,10 +1,11 @@
 # This file is part of Tryton.  The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
 
+from trytond.modules.company.tests import CompanyTestMixin
 from trytond.tests.test_tryton import ModuleTestCase
 
 
-class AccountReceivableRuleTestCase(ModuleTestCase):
+class AccountReceivableRuleTestCase(CompanyTestMixin, ModuleTestCase):
     'Test Account Receivable Rule module'
     module = 'account_receivable_rule'
 
