e8f0704d0b1e429a7d9de068c59cffee60f86b41
Cédric Krier <ced@b2ck.com>
2024-12-11 11:02:07 +0100

    Do not check for integrity error the required fields with set method
    Closes #13745

--- trytond/trytond/tests/field_binary.py
+++ trytond/trytond/tests/field_binary.py
@@ -1,7 +1,7 @@
 # This file is part of Tryton.  The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
 
-from trytond.model import ModelSQL, fields
+from trytond.model import Check, ModelSQL, fields
 from trytond.pool import Pool
 
 
@@ -24,6 +24,20 @@ class BinaryRequired(ModelSQL):
     binary = fields.Binary('Binary Required', required=True)
 
 
+class BinaryRequiredSQLConstraint(ModelSQL):
+    __name__ = 'test.binary_required_sql_constraint'
+    binary = fields.Binary('Binary Required', required=True)
+    constraint = fields.Boolean("Constraint")
+
+    @classmethod
+    def __setup__(cls):
+        super().__setup__()
+        t = cls.__table__()
+        cls._sql_constraints.append(
+            ('constraint', Check(t, t.constraint),
+                'tests.msg_binary_required_sql_constraint'))
+
+
 class BinaryFileStorage(ModelSQL):
     __name__ = 'test.binary_filestorage'
     binary = fields.Binary('Binary', file_id='binary_id')
@@ -35,5 +49,6 @@ def register(module):
         Binary,
         BinaryDefault,
         BinaryRequired,
+        BinaryRequiredSQLConstraint,
         BinaryFileStorage,
         module=module, type_='model')
