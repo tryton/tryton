c5b039fade669873838188f46a8102daf84a41af
Cédric Krier <ced@b2ck.com>
2024-12-12 23:39:43 +0100

    Return the attachment promise in attach callback
    This way the button stays disabled until the drop down menu is filled.
    
    Closes #13755

--- sao/src/tab.js
+++ sao/src/tab.js
@@ -1142,7 +1142,7 @@
             var record = this.screen.current_record;
             var menu = dropdown.find('.dropdown-menu');
             menu.empty();
-            Sao.Window.Attachment.get_attachments(record)
+            return Sao.Window.Attachment.get_attachments(record)
                 .then(function(attachments) {
                     attachments.forEach(function(value) {
                         var name = value[0],
