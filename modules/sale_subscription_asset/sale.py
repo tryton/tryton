2a06af2fe1f3de1b8c6e9bc6fecba8492cb5fb4d
Cédric Krier <ced@b2ck.com>
2025-01-01 17:44:16 +0100

    Do not copy asset lot when copying sale subscription line
    Closes #13785
    (grafted from 4926ea6614fabc74223b4debe0c7ed92c5e70c1f)
    
    --HG--
    branch : 7.4

--- modules/sale_subscription_asset/sale.py
+++ modules/sale_subscription_asset/sale.py
@@ -118,5 +118,5 @@ class SubscriptionLine(metaclass=PoolMeta):
             default = {}
         else:
             default = default.copy()
-        default.setdefault('lot')
+        default.setdefault('asset_lot')
         return super(SubscriptionLine, cls).copy(lines, default)
