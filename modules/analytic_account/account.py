269c2023873494bad27eda5bbfe41421658d847b
Julio Alberto Bravo Montanos <julioalbertobra.ba1@gmail.com>
2025-02-01 23:00:19 +0100

    Limit account types used for analytic distribution
    Closes #13106
    (grafted from 6ff379a908735c09092e0c43a422d04c40bb9d59)
    
    --HG--
    branch : 7.4

--- modules/analytic_account/account.py
+++ modules/analytic_account/account.py
@@ -354,6 +354,7 @@ class AccountDistribution(ModelView, ModelSQL):
         'analytic_account.account', "Account", required=True,
         domain=[
             ('root', '=', Eval('root', -1)),
+            ('type', 'in', ['normal', 'distribution']),
             ])
     ratio = fields.Numeric("Ratio", required=True,
         domain=[
