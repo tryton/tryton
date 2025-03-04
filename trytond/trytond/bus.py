44db85ae84b5583e506bf5b192cc04593b72bfdb
Cédric Krier <ced@b2ck.com>
2025-03-04 23:00:16 +0100

    Query the database after each timeout of the listeners
    This permit to keep the connection alive when the database is killing idle
    connections.
    
    Closes #13902
    (grafted from 12e80f41c37e1f77d1ae539e84989956a70da0d9)
    
    --HG--
    branch : 7.2

--- trytond/trytond/bus.py
+++ trytond/trytond/bus.py
@@ -171,6 +171,8 @@ class LongPollingBus:
                     for event in events:
                         event.set()
                 now = time.time()
+                # Keep connected
+                cursor.execute('SELECT 1')
         except Exception:
             logger.error('bus listener on "%s" crashed', database,
                 exc_info=True)
