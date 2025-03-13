ee2ff2adf80043d326e66798fd544d7e132bd503
Cédric Krier <ced@b2ck.com>
2025-03-13 12:56:38 +0100

    Set party of statement line readonly when state is not draft
    We must keep existing states when adding required states to party.
    
    Closes #13916
    (grafted from 1d22a245f92e3c24fa7dcc794b32b81b84305c82)
    
    --HG--
    branch : 7.4

--- modules/account_statement/statement.py
+++ modules/account_statement/statement.py
@@ -796,10 +796,10 @@ class Line(origin_mixin(_states), sequence_ordered(), ModelSQL, ModelView):
                 | Bool(Eval('origin', 0))),
             }
         cls.account.required = True
-        cls.party.states = {
-            'required': (Eval('party_required', False)
-                & (Eval('statement_state') == 'draft')),
-            }
+        cls.party.states['required'] = (
+            cls.party.states.get('required', False)
+            | (Eval('party_required', False)
+                & (Eval('statement_state') == 'draft')))
         cls._sql_constraints += [
             ('second_currency_sign',
                 Check(
