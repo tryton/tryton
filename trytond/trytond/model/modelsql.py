5bc3c53f6b46c89c55055764a89b02e39fc8b29e
Cédric Krier <ced@b2ck.com>
2025-03-15 13:31:57 +0100

    Compare integrity error message with the constraint name prefixed by the table name
    The TableHandler adds the table name as prefix to the constraint name so when
    comparing with the integrity error message, it should also be added.
    Especially if the constraint name is hashed by the TableHandler conversion
    because the result is completely different.
    
    Closes #13922

--- trytond/trytond/model/modelsql.py
+++ trytond/trytond/model/modelsql.py
@@ -617,6 +617,7 @@ class ModelSQL(ModelStorage):
                         gettext('ir.msg_required_validation',
                             **cls.__names__(field_name))) from exception
         for name, constraint, error in cls._sql_constraints:
+            name = cls._table + '_' + name
             if backend.TableHandler.convert_name(name) in str(exception):
                 raise SQLConstraintError(gettext(error)) from exception
         for _, constraint, error in cls._sql_constraints:
