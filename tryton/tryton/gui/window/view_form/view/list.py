b9f09c4cb05df58a91e20913e2b0ec88b08f2027
Cédric Krier <ced@b2ck.com>
2025-04-26 18:19:18 +0200

    Vendor GenericTreeModel for tryton list
    This is needed to support PyGObject >= 3.51 from the binary packages of the
    Windows and MacOS hosts which can not be pinned.
    
    Closes #13979
    (grafted from 7f3e8b2165e7869e41749bb52879eaac5bb4b111)
    
    --HG--
    branch : 7.0

--- tryton/tryton/gui/window/view_form/view/list.py
+++ tryton/tryton/gui/window/view_form/view/list.py
@@ -7,7 +7,6 @@ import sys
 from functools import wraps
 
 from gi.repository import Gdk, GLib, GObject, Gtk
-from pygtkcompat.generictreemodel import GenericTreeModel
 
 import tryton.common as common
 from tryton.common import (
@@ -21,6 +20,7 @@ from tryton.pyson import PYSONDecoder
 
 from . import View, XMLViewParser
 from .list_gtk.editabletree import EditableTreeView, TreeView
+from .list_gtk.generictreemodel import GenericTreeModel
 from .list_gtk.widget import (
     M2M, M2O, O2M, O2O, URL, Affix, Binary, Boolean, Button, Char, Date, Dict,
     Float, Image, Int, MultiSelection, ProgressBar, Reference, Selection, Text,
