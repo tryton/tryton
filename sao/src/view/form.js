0bfe0895aeb89e702eee8aa0f02c5f472c349214
Cédric Krier <ced@b2ck.com>
2025-09-02 14:28:57 +0200

    Update height of list-form when it is set on xxx2Many widget
    The current view element is the `list-form` so it is not found. Instead we must
    search from the content element of the xxx2Many widget because it replicate the
    CSS rule.
    
    Closes #14212

--- sao/src/view/form.js
+++ sao/src/view/form.js
@@ -3657,7 +3657,7 @@ function eval_pyson(value){
                 this.screen.size_limit = size_limit;
                 this.screen.display();
                 if (this.attributes.height !== undefined) {
-                    this.screen.current_view.el
+                    this.content
                         .find('.treeview,.list-form').first()
                         .css('min-height', this.attributes.height + 'px')
                         .css('max-height', this.attributes.height + 'px');
@@ -4219,7 +4219,7 @@ function eval_pyson(value){
                 }
                 this.screen.display();
                 if (this.attributes.height !== undefined) {
-                    this.screen.current_view.el
+                    this.content
                         .find('.treeview,.list-form').first()
                         .css('min-height', this.attributes.height + 'px')
                         .css('max-height', this.attributes.height + 'px');
