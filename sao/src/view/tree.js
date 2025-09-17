760fc573fd664560b220f749f123993ed762949d
Nicolas Évrard <nicoe@b2ck.com>
2025-09-17 11:41:30 +0200

    Use && as the boolean operator instead of &
    The typo was introduced in d5338ef23267
    (grafted from ec103dad45ac16a043b3d0a156d977d97879053c)
    
    --HG--
    branch : 7.6

--- sao/src/view/tree.js
+++ sao/src/view/tree.js
@@ -889,7 +889,7 @@
                     this.display_size = Sao.config.display_size;
                 }
             }
-            if (!this.display_size &
+            if (!this.display_size &&
                 (!jQuery.isEmptyObject(selected) ||
                     !jQuery.isEmptyObject(expanded))) {
                 this.display_size = Sao.config.display_size;
