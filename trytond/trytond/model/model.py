0414c1cc7f4c0a5493a96974fb9bb12c8c06d4e5
Cédric Krier <ced@b2ck.com>
2025-09-24 09:25:49 +0200

    Remove unnecessary __ne__ implementation on Model
    In standard Python already implement __ne__ as not __eq__.

--- trytond/trytond/model/model.py
+++ trytond/trytond/model/model.py
@@ -382,9 +382,6 @@ class Model(URLMixin, PoolBase, metaclass=ModelMeta):
             return NotImplemented
         return self.id < other.id
 
-    def __ne__(self, other):
-        return not self == other
-
     def __hash__(self):
         return hash((self.__name__, id(self) if self.id is None else self.id))
 
