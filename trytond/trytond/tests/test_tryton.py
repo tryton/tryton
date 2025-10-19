29e05688d60e274b0e44575a9f01c42db012a81e
Cédric Krier <ced@b2ck.com>
2025-10-19 10:43:23 +0200

    Use a MagicMock with a return value when patching ModelStorage copy for testing
    Closes #14300
    (grafted from 2f6936bc0970ec6108351380526af1d9015b79bb)
    
    --HG--
    branch : 7.6

--- trytond/trytond/tests/test_tryton.py
+++ trytond/trytond/tests/test_tryton.py
@@ -898,7 +898,7 @@ class ModuleTestCase(_DBTestCase):
     @with_transaction()
     def test_modelstorage_copy(self):
         "Test copied default values"
-        copy = unittest.mock.Mock()
+        copy = unittest.mock.MagicMock(return_value=[])
         with unittest.mock.patch.object(
                 ModelStorage, 'copy', new=classmethod(copy)):
             for mname, model in Pool().iterobject():
