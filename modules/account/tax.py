6c77a89891c177051f7daaeed2beb0bd44afd26b
Cédric Krier <ced@b2ck.com>
2025-11-04 10:28:04 +0100

    Add missing depends on currency to the TaxableMixin._round_taxes method
    Closes #14348
    (grafted from c3e0f5727931a8674b377930ff9df896f95cbdbb)
    
    --HG--
    branch : 7.4

--- modules/account/tax.py
+++ modules/account/tax.py
@@ -1254,6 +1254,7 @@ class TaxableMixin(object):
 
         return _TaxKey(**line)
 
+    @fields.depends('currency')
     def _round_taxes(self, taxes):
         if not self.currency:
             return
