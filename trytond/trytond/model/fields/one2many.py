0ed69e3a5bdabc6cc54558687d507effa0ad6658
Nicolas Évrard <nicoe@b2ck.com>
2025-11-14 18:11:20 +0100

    Search on the Target model in O2M when it has an active field
    When comparing a one2many to None we should search on the Target model in order
    to ensure the correct handling of the active field. Otherwise there might be
    records that wouldn't be found by the search yet their one2many fields come up
    empty.
    
    Closes #14370
    (grafted from 715161fe8b3351072d7a52e698c95e75269b06a9)
    
    --HG--
    branch : 6.0

--- trytond/trytond/model/fields/one2many.py
+++ trytond/trytond/model/fields/one2many.py
@@ -316,8 +316,10 @@ class One2Many(Field):
                     where &= history_where
                 if origin_where:
                     where &= origin_where
-                if self.filter:
-                    query = Target.search(self.filter, order=[], query=True)
+                if self.filter or hasattr(Target, 'active'):
+                    # Use an empty filter to apply the active test
+                    filter_ = self.filter if self.filter else []
+                    query = Target.search(filter_, order=[], query=True)
                     where &= target.id.in_(query)
                 query = target.select(origin, where=where)
                 expression = ~table.id.in_(query)
