de436a27ca778196b6367c8d1c1b0e2463a47545
Cédric Krier <ced@b2ck.com>
2025-11-21 01:26:31 +0100

    Increase version number
    --HG--
    branch : 7.4

--- trytond/trytond/__init__.py
+++ trytond/trytond/__init__.py
@@ -13,7 +13,7 @@ try:
 except ImportError:
     requests_utils = None
 
-__version__ = "7.4.21"
+__version__ = "7.4.22"
 
 if not os.environ.get('TRYTOND_APPNAME'):
     os.environ['TRYTOND_APPNAME'] = os.path.basename(
