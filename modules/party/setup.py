bf11acb591c94a1fde9a4dfbec9284874fd0f79b
Cédric Krier <ced@b2ck.com>
2026-01-11 11:44:09 +0100

    Update required version of python-stdnum to 2.22 for party module

--- modules/party/setup.py
+++ modules/party/setup.py
@@ -44,7 +44,7 @@ else:
     download_url = 'http://downloads.tryton.org/%s.%s/' % (
         major_version, minor_version)
 
-requires = ['python-sql >= 0.4', 'python-stdnum >= 1.19']
+requires = ['python-sql >= 0.4', 'python-stdnum >= 2.2']
 for dep in info.get('depends', []):
     if not re.match(r'(ir|res)(\W|$)', dep):
         requires.append(get_require_version('trytond_%s' % dep))
