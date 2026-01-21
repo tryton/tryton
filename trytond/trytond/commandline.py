140ee27021cb6c21438df510bb19dc5116384c93
Cédric Krier <ced@b2ck.com>
2026-01-21 19:12:34 +0100

    Add environment variable to enable development mode
    Closes #14530
    (grafted from b251595fa8aa5524ec888bc384743da3bb3b7a5d)
    
    --HG--
    branch : 7.6

--- trytond/trytond/commandline.py
+++ trytond/trytond/commandline.py
@@ -75,6 +75,7 @@ def get_parser():
     parser.add_argument("-v", "--verbose", action='count',
         dest="verbose", default=0, help="increase verbosity")
     parser.add_argument('--dev', dest='dev', action='store_true',
+        default=bool(int(os.getenv('TRYTOND_DEV', 0))),
         help='enable development mode')
 
     logging_config = os.environ.get('TRYTOND_LOGGING_CONFIG')
