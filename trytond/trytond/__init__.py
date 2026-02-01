9d8b9efc960ddc53080b15d2774df298c3144f12
Cédric Krier <ced@b2ck.com>
2026-02-01 10:57:40 +0100

    Increase version number
    --HG--
    branch : 7.0

--- trytond/trytond/__init__.py
+++ trytond/trytond/__init__.py
@@ -10,7 +10,7 @@ from email import charset
 import __main__
 from lxml import etree, objectify
 
-__version__ = "7.0.45"
+__version__ = "7.0.46"
 
 if not os.environ.get('TRYTOND_APPNAME'):
     os.environ['TRYTOND_APPNAME'] = os.path.basename(
