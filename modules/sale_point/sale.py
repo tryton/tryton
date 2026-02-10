f54149ce309cf9900226525521042618f2279cc6
Cédric Krier <ced@b2ck.com>
2026-02-10 12:43:51 +0100

    Decorate post method of sale point with the button decorator
    Closes #14594
    (grafted from 3c30bf919e36e7c8f6b68666ae9508f7a58b0f74)
    
    --HG--
    branch : 7.6

--- modules/sale_point/sale.py
+++ modules/sale_point/sale.py
@@ -336,6 +336,7 @@ class POSSale(Workflow, ModelSQL, ModelView, TaxableMixin):
         cls.save(sales)
 
     @classmethod
+    @ModelView.button
     @Workflow.transition('posted')
     def post(cls, sales):
         pool = Pool()
