70e49b8e5efca02a83458764c29eca021254f0cb
Nicolas Évrard <nicoe@b2ck.com>
2026-03-17 17:45:22 +0100

    Reset the session login when cancelling the password challenge
    Closes #14687
    (grafted from 457504232b31685ffd7d6380f6b22568c35629b3)
    
    --HG--
    branch : 7.6

--- sao/src/session.js
+++ sao/src/session.js
@@ -341,6 +341,7 @@
                 if (service_window.closed) {
                     window.clearInterval(timer);
                     session.database = database;
+                    session.login = null;
                     session.restore();
                     if (session.session) {
                         dfd.resolve(session);
@@ -559,6 +560,9 @@
                         });
                     }).prependTo(dialog.footer);
                     dialog.modal.find('.modal-dialog').removeClass('modal-sm');
+                    prm.fail(() => {
+                        session.login = null;
+                    });
                     return dialog;
                 },
             });
