542430cf35ebecf996d3359429a32651c8a57d94
Cédric Krier <ced@b2ck.com>
2026-03-21 09:21:43 +0100

    Convert to boolean the readonly state of the "Process" button on the inbound email
    Closes #14700
    (grafted from ffe40330e3f6567fbd03e3139d7ff7580d342b4c)
    
    --HG--
    branch : 7.8

--- modules/inbound_email/inbound_email.py
+++ modules/inbound_email/inbound_email.py
@@ -15,7 +15,7 @@ import trytond.config as config
 from trytond.model import (
     ModelSQL, ModelStorage, ModelView, fields, sequence_ordered)
 from trytond.pool import Pool
-from trytond.pyson import Eval
+from trytond.pyson import Bool, Eval
 from trytond.transaction import Transaction
 from trytond.url import http_host
 
@@ -130,7 +130,7 @@ class Email(ModelSQL, ModelView):
         cls._order = [('id', 'DESC')]
         cls._buttons.update(
             process={
-                'readonly': Eval('rule'),
+                'readonly': Bool(Eval('rule')),
                 'depends': ['rule'],
                 },
             )
