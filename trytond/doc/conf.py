145ce8f2f850655e3fdfb6df36406c3717a1baaa
Cédric Krier <ced@b2ck.com>
2026-03-22 09:07:23 +0100

    Make retrieval of metadata via build really quiet

--- trytond/doc/conf.py
+++ trytond/doc/conf.py
@@ -22,7 +22,7 @@ def get_info():
 
     info = dict()
 
-    metadata_cmd = 'python -m build --quiet --metadata'
+    metadata_cmd = 'python -m build -qq --metadata'
     if os.environ.get('DOC_NO_ISOLATION'):
         metadata_cmd += ' --no-isolation'
     metadata = subprocess.check_output(
