3a359ecb92b54a72a590b570a9d062d4d99ceac5
Cédric Krier <ced@b2ck.com>
2026-03-24 16:57:31 +0100

    Permit only valid lines to be selected for reconciliation
    Closes #14703
    (grafted from fa337e74fc4710e7aa11ba0b928619db21b228ae)
    
    --HG--
    branch : 7.6

--- modules/account/move.py
+++ modules/account/move.py
@@ -2514,6 +2514,7 @@ class ReconcileShow(ModelView):
                     ],
                 ('second_currency', '=', Eval('currency', -1)),
                 ],
+            ('state', '=', 'valid'),
             ])
 
     _write_off_states = {
