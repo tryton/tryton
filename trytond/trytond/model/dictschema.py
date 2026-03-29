ebbd1d00600e75e76644c65d2f6f5692acd768eb
José Antonio Díaz Miralles <tiyujopite1@gmail.com>
2026-03-29 22:31:44 +0200

    Include inactive keys from the dict schema when searching
    Closes #14694
    (grafted from fb1c66a12b6d1794b40983c4012ca4f3674381cb)
    
    --HG--
    branch : 7.0

--- trytond/trytond/model/dictschema.py
+++ trytond/trytond/model/dictschema.py
@@ -12,7 +12,7 @@ from trytond.pool import Pool
 from trytond.pyson import Eval, PYSONDecoder
 from trytond.rpc import RPC
 from trytond.tools import slugify
-from trytond.transaction import Transaction
+from trytond.transaction import Transaction, inactive_records
 
 
 class DomainError(ValidationError):
@@ -186,7 +186,8 @@ class DictSchemaMixin(object):
 
     @classmethod
     def search_get_keys(cls, domain, limit=None):
-        schemas = cls.search(domain, limit=limit)
+        with inactive_records():
+            schemas = cls.search(domain, limit=limit)
         return cls.get_keys(schemas)
 
     @classmethod
