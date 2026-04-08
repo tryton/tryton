a1a08ef8d985d2eb0a142f5e4dae1af3e3c8dc23
Cédric Krier <ced@b2ck.com>
2026-04-08 18:49:25 +0200

    Remove invalid SEPA mandate when payment is reset to draft
    Closes #14756

--- modules/account_payment_sepa/payment.py
+++ modules/account_payment_sepa/payment.py
@@ -489,6 +489,19 @@ class Payment(metaclass=PoolMeta):
             ('sepa_info_id', operator, code_value, *extra),
             ]
 
+    @classmethod
+    @ModelView.button
+    @Workflow.transition('draft')
+    def draft(cls, payments):
+        to_remove_mandate = []
+        for payment in payments:
+            if (payment.sepa_mandate
+                    and payment.sepa_mandate.state != 'validated'):
+                to_remove_mandate.append(payment)
+        if to_remove_mandate:
+            cls.write(to_remove_mandate, {'sepa_mandate': None})
+        super().draft(payments)
+
 
 class Payment_Invoice(metaclass=PoolMeta):
     __name__ = 'account.payment'
