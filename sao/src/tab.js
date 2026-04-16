3c1ffb95d17616d810fdc4f7b7d805c192197b28
Cédric Krier <ced@b2ck.com>
2026-04-16 23:00:54 +0200

    Execute only one menu item and button actions at a time per tab
    Closes #14769
    (grafted from 4a6e16fdbcc2c35018a3b6ec76ccbbebf442c936)
    
    --HG--
    branch : 7.0

--- sao/src/tab.js
+++ sao/src/tab.js
@@ -13,6 +13,7 @@
             this.name = '';
             this.name_el = jQuery('<span/>');
             this.view_prm = jQuery.when();
+            this._action_running = false;
         },
         menu_def: function() {
             return [
@@ -153,8 +154,13 @@
                     this.menu_buttons[item.id] = menuitem;
                     link.click(evt => {
                         evt.preventDefault();
-                        if (!menuitem.hasClass('disabled')) {
-                            this[item.id]();
+                        if (!menuitem.hasClass('disabled')
+                            && !this._action_running) {
+                            this._action_running = true;
+                            (this[item.id]() || jQuery.when())
+                                .always(() => {
+                                    this._action_running = false;
+                                });
                         }
                     });
                 } else if (!item && previous) {
@@ -254,17 +260,14 @@
                 }
                 this.buttons[item.id].click(item, event => {
                     var item = event.data;
-                    var button = this.buttons[item.id];
-                    // Use data instead of disabled prop because the action may
-                    // actually disable the button.
-                    if (button.data('disabled')) {
+                    if (this._action_running) {
                         event.preventDefault();
                         return;
                     }
-                    button.data('disabled', true);
+                    this._action_running = true;
                     (this[item.id](this) || jQuery.when())
-                        .always(function() {
-                            button.data('disabled', false);
+                        .always(() => {
+                            this._action_running = false;
                         });
                 });
             };
