420217e3e9ad8ac28ec165c0d5a4feb66566ead9
Cédric Krier <ced@b2ck.com>
2026-04-21 11:41:23 +0200

    Use python3 to make tryton installer

--- tryton/make-win32-installer.sh
+++ tryton/make-win32-installer.sh
@@ -2,12 +2,12 @@
 set -e
 version=`./setup-freeze.py --version`
 series=${version%.*}
-bits=`python -c "import platform; print(platform.architecture()[0])"`
+bits=`python3 -c "import platform; print(platform.architecture()[0])"`
 rm -rf build dist
 mkdir -p win32/gtk-3.0
 gdk-pixbuf-query-loaders | sed -e '/^#/d' > win32/gtk-3.0/gdk-pixbuf.loaders
 gtk-query-immodules-3.0 | sed -e '/^#/d' > win32/gtk-3.0/gtk.immodules
-python ./hatch_build.py
+python3 ./hatch_build.py
 ./setup-freeze.py install_exe -d dist
 cp `which gdbus.exe` dist/
 makensis -DVERSION=${version} -DSERIES=${series} -DBITS=${bits} setup.nsi
