ee37dab303554a7b0b7611e0481fe1c5f68b924c
Cédric Krier <ced@b2ck.com>
2026-06-08 09:09:23 +0200

    Convert toolbar into mutable objects before modification
    Since 619fd425e3bb the toolbar may be immutable when it comes from the cache.
    
    Closes #14881
    (grafted from 9e8d1bca2c7fd2fc4ce70284e069d4b06f5ec810)
    
    --HG--
    branch : 7.0

--- modules/account/move.py
+++ modules/account/move.py
@@ -1518,8 +1518,9 @@ class Line(DescriptionOriginMixin, MoveLineMixin, ModelSQL, ModelView):
                     ('company', '=', company),
                     ('journal', '=', journal),
                     ])
+            toolbar = dict(toolbar)
+            action = list(toolbar['action'])
             for template in templates:
-                action = toolbar['action']
                 # Use template id for action id to auto-select the template
                 action.append({
                         'name': template.name,
@@ -1527,6 +1528,7 @@ class Line(DescriptionOriginMixin, MoveLineMixin, ModelSQL, ModelView):
                         'wiz_name': 'account.move.template.create',
                         'id': template.id,
                         })
+            toolbar['action'] = action
         return toolbar
 
     @classmethod
