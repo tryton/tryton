8feb50c71c72513e240caa06bf14ab6b119c8578
Cédric Krier <ced@b2ck.com>
2026-06-11 14:29:17 +0200

    Support query without where clause when searching on Many2One with where operator
    When not using subquery but EXISTS and having a target domain converted into
    UNION, the query result does not have a 'where' clause.
    
    Closes #14889

--- trytond/trytond/tests/test_field_many2one.py
+++ trytond/trytond/tests/test_field_many2one.py
@@ -332,6 +332,27 @@ class SearchTestCaseMixin:
                 ], query=True)
         self.assertIn(self._strategy, str(query))
 
+    @with_transaction()
+    def test_search_where_target_union(self):
+        "Test search on Many2One using where and UNION-ed query on target"
+        pool = Pool()
+        Target = pool.get('test.many2one_target')
+        Many2One = pool.get('test.many2one')
+        target1, target2 = Target.create([
+                {'value': 1},
+                {'value': 2},
+                ])
+        many2one1, many2one2 = Many2One.create([
+                {'many2one': target1},
+                {'many2one': target2},
+                ])
+
+        many2ones = Many2One.search([
+                ('many2one', 'where',
+                    ['OR', ('value', '=', 1), ('write_uid.id', '=', -1)]),
+                ])
+        self.assertListEqual(many2ones, [many2one1])
+
 
 class FieldMany2OneTestCase(
         TestCase, CommonTestCaseMixin, SearchTestCaseMixin):
