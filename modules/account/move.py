66b701d151134915628b9b392ea2be3b7038df69
Cédric Krier <ced@b2ck.com>
2026-06-18 14:37:36 +0200

    Call the same super method for validate_fields of account move line
    Closes #14901
    (grafted from e8394b1a0c0f3b7b3266a9bbb65d84b90a5f0e7e)
    
    --HG--
    branch : 7.8

--- modules/account/move.py
+++ modules/account/move.py
@@ -1401,7 +1401,7 @@ class Line(DescriptionOriginMixin, MoveLineMixin, ModelSQL, ModelView):
 
     @classmethod
     def validate_fields(cls, lines, field_names):
-        super().validate(lines)
+        super().validate_fields(lines, field_names)
         cls.check_account(lines, field_names)
 
     @classmethod
