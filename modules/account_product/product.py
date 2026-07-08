f943718de417c3558557fda08274c8600db01bd6
Cédric Krier <ced@b2ck.com>
2026-07-08 08:41:19 +0200

    Backout f190e8f99aae
    In the branch 7.0 the product category does not have accounting_templates.
    
    --HG--
    branch : 7.0

--- modules/account_product/product.py
+++ modules/account_product/product.py
@@ -252,12 +252,6 @@ class Category(CompanyMultiValueMixin, metaclass=PoolMeta):
         else:
             return self.supplier_taxes_deductible_rate
 
-    @classmethod
-    def copy(cls, categories, default=None):
-        default = default.copy() if default is not None else {}
-        default.setdefault('accounting_templates')
-        return super().copy(categories, default=default)
-
 
 class CategoryAccount(ModelSQL, CompanyValueMixin):
     "Category Account"
