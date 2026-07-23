9a51438e80d04d2b90b2c2285769666f284d93b5
Cédric Krier <ced@b2ck.com>
2026-07-23 18:27:41 +0200

    Prevent switching row when double click on Many2One link
    Closes #14967

--- sao/src/view/tree.js
+++ sao/src/view/tree.js
@@ -2837,6 +2837,11 @@
                 params.context = this.field.get_context(record);
                 Sao.Tab.create(params);
             });
+            cell.dblclick((evt) => {
+                // prevent calling switch_row on td
+                evt.preventDefault();
+                evt.stopImmediatePropagation();
+            });
         }
     });
 
