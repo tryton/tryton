02359a7ec0727a96168a3e34935a5de80b021ae3
Cédric Krier <ced@b2ck.com>
2026-08-15 08:42:55 +0200

    Increase version number
    --HG--
    branch : 7.0

--- trytond/trytond/__init__.py
+++ trytond/trytond/__init__.py
@@ -12,7 +12,7 @@ from lxml import etree, objectify
 
 from ._safe_genshi import genshi_patch
 
-__version__ = "7.0.56"
+__version__ = "7.0.57"
 
 if not os.environ.get('TRYTOND_APPNAME'):
     os.environ['TRYTOND_APPNAME'] = os.path.basename(
