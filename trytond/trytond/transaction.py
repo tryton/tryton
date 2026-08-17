00e8703e5e563d3b769aabe662085ff0382c9256
Cédric Krier <ced@b2ck.com>
2026-08-17 17:32:55 +0200

    Set all attributes of transaction before connecting to the database
    The connection to the database may fail and thus stop and rollback may be
    called. All the attributes must be set to avoid to raise another AttributeError
    exception.
    
    Closes #15022
    (grafted from 654923a8085d4804dd217e487c80085c53eb470b)
    
    --HG--
    branch : 7.8

--- trytond/trytond/transaction.py
+++ trytond/trytond/transaction.py
@@ -188,13 +188,7 @@ class Transaction(object):
             # it is strictly before all transactions started after
             # but it may be also before transactions started before
             self.started_at = self.monotonic_time()
-            if not database_name:
-                database = backend.Database().connect()
-            else:
-                database = backend.Database(database_name).connect()
-            Flavor.set(backend.Database.flavor)
             self.user = user
-            self.database = database
             self.readonly = readonly
             self.close = close
             self.context = ImmutableDict(context or {})
@@ -207,6 +201,12 @@ class Transaction(object):
             self.counter = 0
             self._datamanagers = []
 
+            if not database_name:
+                database = backend.Database().connect()
+            else:
+                database = backend.Database(database_name).connect()
+            Flavor.set(backend.Database.flavor)
+            self.database = database
             self.connection = database.get_connection(readonly=readonly,
                 autocommit=autocommit, statement_timeout=timeout)
             count = 0
