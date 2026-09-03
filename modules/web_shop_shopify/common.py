46bb056d81ed2ef9d4b4281517fc2f14b0ee74c9
Cédric Krier <ced@b2ck.com>
2026-09-03 15:49:20 +0200

    Use a sentinel as default value for setting attribute only if changed
    For new instances, we must set also the attribute with value `None` as they may
    be tested later.
    
    Closes #15060
    (grafted from bfe3756af384f4fce94244761d5f5dd3f62d0ff6)
    
    --HG--
    branch : 7.8

--- modules/web_shop_shopify/common.py
+++ modules/web_shop_shopify/common.py
@@ -187,6 +187,6 @@ class IdentifiersMixin(IdentifiersUpdateMixin):
 
 
 def setattr_changed(record, name, value):
-    if getattr(record, name, None) != value:
+    if getattr(record, name, object()) != value:
         setattr(record, name, value)
         return True
