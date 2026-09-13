fef30e24b1bce21aa091f3076dfac0db9b36a528
Cédric Krier <ced@b2ck.com>
2026-09-13 13:57:29 +0200

    Use explicit order for shipments and invoices on sale and purchase
    Closes #15090

--- modules/sale/sale.py
+++ modules/sale/sale.py
@@ -261,7 +261,8 @@ class Sale(
             ], 'Invoice State', readonly=True, required=True, sort=False)
     to_invoice = fields.Boolean("To Invoice", readonly=True)
     invoices = fields.Function(fields.Many2Many(
-            'account.invoice', None, None, "Invoices"),
+            'account.invoice', None, None, "Invoices",
+            order=[('id', 'ASC')]),
         'get_invoices', searcher='search_invoices')
     invoices_ignored = fields.Many2Many(
         'sale.sale-ignored-account.invoice', 'sale', 'invoice',
@@ -270,12 +271,14 @@ class Sale(
             ('id', 'in', Eval('invoices', [])),
             ('state', '=', 'cancelled'),
             ],
+        order=[('id', 'ASC')],
         states={
             'invisible': ~Eval('invoices_ignored', []),
             })
     invoices_recreated = fields.Many2Many(
         'sale.sale-recreated-account.invoice', 'sale', 'invoice',
-        'Recreated Invoices', readonly=True)
+        'Recreated Invoices', readonly=True,
+        order=[('id', 'ASC')])
     shipment_method = fields.Selection([
             ('manual', 'Manual'),
             ('order', 'On Order Processed'),
@@ -294,10 +297,12 @@ class Sale(
             ], "Shipment State", readonly=True, required=True, sort=False)
     to_ship = fields.Boolean("To Ship", readonly=True)
     shipments = fields.Function(fields.Many2Many(
-            'stock.shipment.out', None, None, "Shipments"),
+            'stock.shipment.out', None, None, "Shipments",
+            order=[('id', 'ASC')]),
         'get_shipments', searcher='search_shipments')
     shipment_returns = fields.Function(fields.Many2Many(
-            'stock.shipment.out.return', None, None, "Shipment Returns"),
+            'stock.shipment.out.return', None, None, "Shipment Returns",
+            order=[('id', 'ASC')]),
         'get_shipment_returns', searcher='search_shipment_returns')
     moves = fields.Function(
         fields.Many2Many('stock.move', None, None, "Stock Moves"),
